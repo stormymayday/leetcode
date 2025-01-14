27ab8df4955cfb54c9432aeb492575123acc2cbe
Aidar <stormymayday7@gmail.com>
2025-01-14 13:04:31 -0800

    Time: 69 ms (21.65%) | Memory: 51 MB (84.98%) - LeetSync

---
+++ 30-days-of-javascript/2762-cache-with-time-limit/cache-with-time-limit.ts
@@ -0,0 +1,46 @@
+class TimeLimitedCache {
+
+    cache;
+    
+    constructor() {
+        this.cache = new Map();
+    }
+    
+    set(key: number, value: number, duration: number): boolean {
+
+        const alreadyExists = this.cache.get(key);
+
+        if(alreadyExists) {
+            clearTimeout(alreadyExists.timeoutId);
+        }
+
+        const timeoutId = setTimeout(() => {
+            this.cache.delete(key);
+        }, duration);
+
+        this.cache.set(key, {value, timeoutId});
+
+        return Boolean(alreadyExists);
+        
+    }
+    
+    get(key: number): number {
+        if(this.cache.get(key)) {
+            return this.cache.get(key).value;
+        } else {
+            return -1;
+        }
+        
+    }
+    
+    count(): number {
+        return this.cache.size;
+    }
+}
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
