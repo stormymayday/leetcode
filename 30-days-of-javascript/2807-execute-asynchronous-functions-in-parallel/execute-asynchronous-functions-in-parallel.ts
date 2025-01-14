5178e219da9c33c451e7259f80b224de140c2164
Aidar <stormymayday7@gmail.com>
2025-01-14 15:20:05 -0800

    Time: 87 ms (16.50%) | Memory: 52.2 MB (6.36%) - LeetSync

---
+++ 30-days-of-javascript/2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.ts
@@ -0,0 +1,34 @@
+type Fn<T> = () => Promise<T>
+
+function promiseAll<T>(functions: Fn<T>[]): Promise<T[]> {
+
+    return new Promise((resolve, reject) => {
+
+        let unresolved = functions.length;
+        const results = [];
+
+        if(unresolved === 0) {
+            resolve(results);
+            // return;
+        }
+
+        functions.forEach((fn, index) => {
+
+            fn().then((result) => {
+                results[index] = result;
+                unresolved--;
+                if(unresolved === 0) {
+                    resolve(results);
+                }
+            }).catch(reject);
+
+        });
+
+    });
+    
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
