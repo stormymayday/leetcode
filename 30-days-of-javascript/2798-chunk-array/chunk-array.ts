c5fd2065787008b81b91ed8012a781fd9bcd4609
Aidar <stormymayday7@gmail.com>
2025-01-14 15:31:14 -0800

    Time: 61 ms (55.68%) | Memory: 53.3 MB (32.16%) - LeetSync

---
+++ 30-days-of-javascript/2798-chunk-array/chunk-array.ts
@@ -0,0 +1,23 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | Array<JSONValue>;
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+
+    const result = [];
+    let chunk = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        chunk.push(arr[i]);
+        if(chunk.length === size) {
+            result.push(chunk);
+            chunk = [];
+        }
+    }
+
+    if(chunk.length) {
+        result.push(chunk);
+    }
+
+    return result;
+    
+};
