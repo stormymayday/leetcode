d4cc8c743426fc9acc1149fdf18b76593b51f0a7
Aidar <stormymayday7@gmail.com>
2025-01-14 15:26:51 -0800

    Time: 61 ms (37.50%) | Memory: 51.8 MB (56.18%) - LeetSync

---
+++ 30-days-of-javascript/2864-is-object-empty/is-object-empty.ts
@@ -0,0 +1,25 @@
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+type Obj = Record<string, JSONValue> | JSONValue[]
+
+function isEmpty(obj: Obj): boolean {
+    
+    if(obj == null) {
+        return true;
+    }
+
+    if(Array.isArray(obj) || typeof obj === 'string') {
+        return obj.length === 0;
+    }
+
+    if(obj instanceof Map || obj instanceof Set) {
+        return obj.size === 0;
+    }
+
+    const prototype = Object.getPrototypeOf(obj);
+    if(prototype === null || prototype === Object.prototype) {
+        return Object.keys(obj).length === 0;
+    }
+
+    return true;
+
+};
