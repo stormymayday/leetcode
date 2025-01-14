0dfa02ca7d06128fadd3b8d78987ada4de8ca40f
Aidar <stormymayday7@gmail.com>
2025-01-14 15:08:39 -0800

    Time: 62 ms (67.58%) | Memory: 51.7 MB (44.14%) - LeetSync

---
+++ 30-days-of-javascript/2743-debounce/debounce.ts
@@ -0,0 +1,23 @@
+type F = (...args: number[]) => void
+
+function debounce(fn: F, t: number): F {
+
+    let timeoutId = null;
+    
+    return function(...args) {
+
+        clearTimeout(timeoutId);
+
+        timeoutId = setTimeout(() => {
+            fn(...args);
+        }, t);
+        
+    }
+};
+
+/**
+ * const log = debounce(console.log, 100);
+ * log('Hello'); // cancelled
+ * log('Hello'); // cancelled
+ * log('Hello'); // Logged at t=100ms
+ */
