021793090cff4f19b9468ab10634652a6cf4422e
Aidar <stormymayday7@gmail.com>
2025-01-14 12:28:04 -0800

    Time: 70 ms (47.42%) | Memory: 51.9 MB (38.86%) - LeetSync

--- 30-days-of-javascript/2859-add-two-promises/add-two-promises.ts
+++ 30-days-of-javascript/2859-add-two-promises/add-two-promises.ts
@@ -1,34 +1,44 @@
 type P = Promise<number>
 
+function mergeResults(res1: unknown, res2: unknown):number {
+    try {
+        if(typeof res1 === 'number' && typeof res2 === 'number') {
+            return res1 + res2;
+        }
+        throw new Error('Unsupported data types');
+    } catch {
+        throw new Error('Unsupported data types');
+    }
+}
+
 async function addTwoPromises(promise1: P, promise2: P): P {
+
+    let unresolved = 2;
+    let res1: number;
+    let res2: number;
 
     return new Promise((resolve, reject) => {
 
-        let unresolved = 2;
-        let res1: number;
-        let res2: number;
-
-        if(unresolved === 0) {
-            resolve(res1 + res2);
+        function helper() {
+            if(unresolved === 0) {
+                resolve(mergeResults(res1, res2));
+            }
         }
 
         promise1.then((result) => {
             res1 = result;
             unresolved--;
-            if(unresolved === 0) {
-                resolve(res1 + res2);
-            }
+            helper();
         }).catch(reject);
 
         promise2.then((result) => {
             res2 = result;
             unresolved--;
-            if(unresolved === 0) {
-                resolve(res1 + res2);
-            }
+            helper();
         }).catch(reject);
 
     });
+    
 };
 
 /**
