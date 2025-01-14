16f7581367536df59b6edf2a13c549d4105f162f
Aidar <stormymayday7@gmail.com>
2025-01-14 11:53:34 -0800

    Time: 69 ms (8.53%) | Memory: 52 MB (20.73%) - LeetSync

---
+++ 2746-filter-elements-from-array/filter-elements-from-array.ts
@@ -0,0 +1,15 @@
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] {
+
+    const newArr: number[] = [];
+
+    for(let i = 0; i < arr.length; i++) {
+        if(fn(arr[i], i)) {
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
+    
+};
