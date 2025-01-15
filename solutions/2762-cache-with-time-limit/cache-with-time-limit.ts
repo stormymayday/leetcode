d6c8c1e232ae579b114b3cd95f3bd1dee0a5eda5
Aidar <stormymayday7@gmail.com>
2025-01-15 15:55:35 -0800

    Time: 66 ms (35.78%) | Memory: 51.1 MB (85.31%) - LeetSync

---
+++ solutions/2762-cache-with-time-limit/cache-with-time-limit.ts
@@ -0,0 +1,40 @@
+class TimeLimitedCache {
+
+    cache: Map<number, { value: number, timeoutId: NodeJS.Timeout }>
+    
+    constructor() {
+        this.cache = new Map();
+    }
+    
+    set(key: number, value: number, duration: number): boolean {
+        const alreadyExists = this.cache.get(key);
+        if(alreadyExists) {
+            clearTimeout(alreadyExists.timeoutId);
+        }
+        const timeoutId = setTimeout(() => {
+            this.cache.delete(key);
+        }, duration);
+        this.cache.set(key, { value, timeoutId});
+        return Boolean(alreadyExists);
+        
+    }
+    
+    get(key: number): number {
+        if(this.cache.has(key)) {
+            return this.cache.get(key).value;
+        } else {
+            return -1;
+        }
+    }
+    
+    count(): number {
+        return this.cache.size;
+    }
+}
+
+/**
+ * const timeLimitedCache = new TimeLimitedCache()
+ * timeLimitedCache.set(1, 42, 1000); // false
+ * timeLimitedCache.get(1) // 42
+ * timeLimitedCache.count() // 1
+ */
