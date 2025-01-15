5cf37eacd28f77b2e54bc21faf5c1f44ccf436ab
Aidar <stormymayday7@gmail.com>
2025-01-14 18:32:38 -0800

    Time: 53 ms (87.12%) | Memory: 51.7 MB (59.34%) - LeetSync

--- 30-days-of-javascript/2797-event-emitter/event-emitter.ts
+++ 30-days-of-javascript/2797-event-emitter/event-emitter.ts
@@ -5,38 +5,35 @@ type Subscription = {
 
 class EventEmitter {
 
-    events: { [key:string] : Callback[] };
+    events: Map<string, Set<Callback>>;
 
     constructor() {
-        this.events = {};
+        this.events = new Map();
     }
     
     subscribe(eventName: string, callback: Callback): Subscription {
-
-        if(!this.events[eventName]) {
-            this.events[eventName] = [];
+        
+        if(!this.events.has(eventName)) {
+            this.events.set(eventName, new Set());
         }
 
-        this.events[eventName].push(callback);
-        
+        this.events.get(eventName).add(callback);
+
         return {
             unsubscribe: () => {
-                this.events[eventName] = this.events[eventName].filter((cb) => {
-                    return cb !== callback;
-                })
+                this.events.get(eventName).delete(callback);
             }
         };
     }
     
     emit(eventName: string, args: any[] = []): any[] {
-        if(!this.events[eventName]) {
+        if(!this.events.get(eventName)) {
             return [];
         }
-
         const result = [];
-        this.events[eventName].forEach((cb) => {
+        this.events.get(eventName).forEach((cb) => {
             result.push(cb(...args));
-        })
+        });
         return result;
     }
 }
