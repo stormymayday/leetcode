fe4243a5a7551077fc986f61a7986b4a4050e42e
Aidar <stormymayday7@gmail.com>
2025-01-15 14:21:23 -0800

    Time: 171 ms (5.18%) | Memory: 74.9 MB (14.64%) - LeetSync

---
+++ solutions/2742-group-by/group-by.ts
@@ -0,0 +1,28 @@
+interface Array<T> {
+    groupBy(fn: (item: T) => string): Record<string, T[]>
+}
+
+
+Array.prototype.groupBy = function(fn) {
+
+    const result = {};
+
+    for(let i = 0; i < this.length; i++) {
+
+        const key = fn(this[i]);
+
+        if(!result[key]) {
+            result[key] = [];
+        }
+
+        result[key].push(this[i]);
+
+    }
+
+    return result;
+    
+}
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
