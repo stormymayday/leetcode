0ad647576a4d31970ded3edb11cb6436d08ef980
Aidar <stormymayday7@gmail.com>
2025-01-17 20:16:53 -0800

    Time: 0 ms (100.00%) | Memory: 52.1 MB (25.76%) - LeetSync

--- solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,20 +1,20 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
 
-    // const duplicates: number[] = [];
-    const hashMap: Set<number> = new Set();
-    const intersection: Set<number> = new Set();
+    const hashSet: Set<number> = new Set();
+    const intersection = [];
 
     for(let i = 0; i < nums1.length; i++) {
-        hashMap.add(nums1[i]);
+        hashSet.add(nums1[i]);
     }
 
     for(let i = 0; i < nums2.length; i++) {
-        if(hashMap.has(nums2[i])) {
-            intersection.add(nums2[i]);
+        if(hashSet.has(nums2[i])) {
+            intersection.push(nums2[i]);
+            hashSet.delete(nums2[i])
         }
     }
 
 
-    return Array.from(intersection);
+    return intersection;
     
 };
