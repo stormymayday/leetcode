6d5c70bc31b77fb96e4734c6e409c2ad8a0d230d
Aidar <stormymayday7@gmail.com>
2025-02-07 10:12:42 -0800

    Time: 86 ms (57.12%) | Memory: 58.6 MB (91.39%) - LeetSync

--- solutions/2804-compact-object/compact-object.js
+++ solutions/2804-compact-object/compact-object.js
@@ -4,31 +4,36 @@
  */
 var compactObject = function(obj) {
 
-    // Base Case - Primitives
+    // The base case (handling primitives)
     if(typeof obj !== 'object' || obj == null) {
         return obj;
     }
-
+    
     // Arrays
     if(Array.isArray(obj)) {
         const compactArray = [];
-        for(let i = 0; i < obj.length; i++) {
-            const compactElement = compactObject(obj[i]);
+        for(const element of obj) {
+            // Recursion
+            const compactElement = compactObject(element);
+            // THIS IS WHERE THE CHECK HAPPENS
             if(compactElement) {
                 compactArray.push(compactElement);
             }
         }
+        // Recursion ENDS
         return compactArray;
     }
 
     // Objects
     const compactObj = {};
     for(const key in obj) {
+        // Recursion
         const compactEntry = compactObject(obj[key]);
+        // THIS IS WHERE THE CHECK HAPPENS
         if(compactEntry) {
             compactObj[key] = compactEntry;
         }
     }
+    // Recursion ENDS
     return compactObj;
-    
 };
