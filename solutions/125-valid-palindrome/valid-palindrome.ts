2388c81acb4f0014e1b1f92731c1d51323bee4d7
Aidar <stormymayday7@gmail.com>
2025-02-06 18:31:32 -0800

    Time: 3 ms (88.22%) | Memory: 53 MB (67.07%) - LeetSync

---
+++ solutions/125-valid-palindrome/valid-palindrome.ts
@@ -0,0 +1,20 @@
+function isPalindrome(s: string): boolean {
+    
+    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    if(s.length <= 1) return true;
+
+    let left = 0
+    let right = s.length - 1;
+
+    while(right > left) {
+        if(s[left] !== s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+
+    return true;
+
+};
