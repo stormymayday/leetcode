d4308f68bc4faed0e87ca695a26f5a3a4e51609f
Aidar <stormymayday7@gmail.com>
2025-02-08 23:31:30 -0800

    Time: 22 ms (32.53%) | Memory: 55.7 MB (36.04%) - LeetSync

---
+++ solutions/242-valid-anagram/valid-anagram.ts
@@ -0,0 +1,32 @@
+function isAnagram(s: string, t: string): boolean {
+
+    const freqMap = {};
+
+    for(const char of s) {
+        if(freqMap[char]) {
+            freqMap[char]++;
+        } else {
+            freqMap[char] = 1;
+        }
+    }
+
+    for(const char of t) {
+        if(!freqMap[char]) {
+            return false;
+        }
+
+        if(freqMap[char] > 0) {
+            freqMap[char]--;
+            if(freqMap[char] === 0) {
+                delete freqMap[char];
+            }
+        }
+    }
+
+    if(Object.keys(freqMap).length === 0) {
+        return true;
+    } else {
+        return false;
+    }
+    
+};
