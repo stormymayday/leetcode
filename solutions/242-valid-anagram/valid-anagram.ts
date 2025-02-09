851a642949f480fb45eebabaf8cb4b56cf7064c8
Aidar <stormymayday7@gmail.com>
2025-02-08 23:46:41 -0800

    Time: 28 ms (24.93%) | Memory: 56.2 MB (16.50%) - LeetSync

--- solutions/242-valid-anagram/valid-anagram.ts
+++ solutions/242-valid-anagram/valid-anagram.ts
@@ -1,36 +1,5 @@
 function isAnagram(s: string, t: string): boolean {
 
-    if(s.length !== t.length) {
-        return false;
-    }
-
-    const freqMap = {};
-
-    for(const char of s) {
-        if(freqMap[char]) {
-            freqMap[char]++;
-        } else {
-            freqMap[char] = 1;
-        }
-    }
-
-    for(const char of t) {
-        if(!freqMap[char]) {
-            return false;
-        }
-
-        if(freqMap[char] > 0) {
-            freqMap[char]--;
-            if(freqMap[char] === 0) {
-                delete freqMap[char];
-            }
-        }
-    }
-
-    if(Object.keys(freqMap).length === 0) {
-        return true;
-    } else {
-        return false;
-    }
+    return s.split('').sort().join('') === t.split('').sort().join('');
     
 };
