bc00b5f3852c6e6f62dfee0a770bf7e3c3f95f2a
Aidar <stormymayday7@gmail.com>
2025-02-09 12:42:45 -0800

    Time: 122 ms (7.32%) | Memory: 52.2 MB (80.81%) - LeetSync

---
+++ solutions/1-two-sum/two-sum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = 0; j < nums.length; j++) {
+                if (i !== j) {
+                    if(nums[i] + nums[j] === target) {
+                        return [i, j];
+                    }
+                }
+            }
+        }
+    }
