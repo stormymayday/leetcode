78b6947a190a0e56025f010803ebb6d3049c78f1
Aidar <stormymayday7@gmail.com>
2025-02-11 20:26:26 -0800

    Time: 12 ms (52.87%) | Memory: 56 MB (60.77%) - LeetSync

---
+++ solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -0,0 +1,22 @@
+function lengthOfLongestSubstring(s: string): number {
+        let result = 0;
+
+        if (!s.length) {
+            return result;
+        }
+
+        const hashSet = new Set();
+        let left = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            while (hashSet.has(s[right])) {
+                hashSet.delete(s[left]);
+                left++;
+            }
+
+            hashSet.add(s[right]);
+            result = Math.max(result, hashSet.size);
+        }
+
+        return result;
+    }
