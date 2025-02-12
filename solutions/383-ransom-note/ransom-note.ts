05890a3b19a741a31b0bf5710624f83b80fd8282
Aidar <stormymayday7@gmail.com>
2025-02-12 07:54:44 -0800

    Time: 24 ms (21.93%) | Memory: 55.9 MB (16.81%) - LeetSync

--- solutions/383-ransom-note/ransom-note.ts
+++ solutions/383-ransom-note/ransom-note.ts
@@ -1,27 +1,27 @@
 function canConstruct(ransomNote: string, magazine: string): boolean {
 
-    if(magazine.length < ransomNote.length) {
+    if(ransomNote.length > magazine.length) {
         return false;
     }
 
     const freqMap = {};
 
     for(const char of magazine) {
-        if(freqMap[char]) {
-            freqMap[char]++;
-        } else {
+        if(!freqMap[char]) {
             freqMap[char] = 1;
+        } else {
+            freqMap[char]++;
         }
     }
 
     for(const char of ransomNote) {
-
-        if(!freqMap[char]) {
+        if(!freqMap[char]){
             return false;
         }
         freqMap[char]--;
     }
 
-    return true;
 
+    return true;
+    
 };
