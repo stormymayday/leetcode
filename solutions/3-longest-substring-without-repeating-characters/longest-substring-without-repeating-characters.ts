e800e72a572e65e4e5af8568256ec12c6615d554
Aidar <stormymayday7@gmail.com>
2025-02-12 22:49:35 -0800

    Time: 9 ms (71.18%) | Memory: 56.1 MB (60.74%) - LeetSync

--- solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,22 +1,27 @@
 function lengthOfLongestSubstring(s: string): number {
-        let result = 0;
 
-        if (!s.length) {
-            return result;
-        }
+    let result = 0;
 
-        const hashSet = new Set();
-        let left = 0;
-
-        for (let right = 0; right < s.length; right++) {
-            while (hashSet.has(s[right])) {
-                hashSet.delete(s[left]);
-                left++;
-            }
-
-            hashSet.add(s[right]);
-            result = Math.max(result, hashSet.size);
-        }
-
+    if(!s.length) {
         return result;
     }
+
+    const hashSet = new Set();
+
+    let left = 0;
+
+    for(let right = 0; right < s.length; right++) {
+
+        while(hashSet.has(s[right])) {
+            hashSet.delete(s[left]);
+            left++;
+        }
+
+        hashSet.add(s[right]);
+        result = Math.max(result, hashSet.size);
+
+    }
+
+    return result;
+    
+};
