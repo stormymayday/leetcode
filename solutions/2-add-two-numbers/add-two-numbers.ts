42fecabe0ba0b18609f8e5cfc2fd839b3a599a3f
Aidar <stormymayday7@gmail.com>
2025-02-16 07:47:39 -0800

    Time: 6 ms (39.54%) | Memory: 56.8 MB (97.94%) - LeetSync

--- solutions/2-add-two-numbers/add-two-numbers.ts
+++ solutions/2-add-two-numbers/add-two-numbers.ts
@@ -12,23 +12,33 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 
+    // creating dummy node
     let result = new ListNode();
+    // current pointer
     let current = result;
 
+    // carray intially 0
     let carry = 0;
+
+    // while current listNodes exist OR carry still has value left
     while(l1 || l2 || carry) {
 
-        let v1 = l1 ? l1.val : 0;
-        let v2 = l2 ? l2.val : 0;
+        // extracting values from listNodes if nodes exist
+        let val1 = l1 ? l1.val : 0;
+        let val2 = l2 ? l2.val : 0;
 
-        let sum = v1 + v2 + carry;
+        // calculating sum and carry
+        let sum = val1 + val2 + carry;
         carry = Math.floor(sum / 10);
         sum = sum % 10;
 
+        // creating new listNode 
         current.next = new ListNode(sum);
-        current = current.next
+        // moving current pointer forward
+        current = current.next;
 
-       l1 = l1 ? l1.next : null;
+        // moving listNode pointers forward if nodes exist
+        l1 = l1 ? l1.next : null;
         l2 = l2 ? l2.next : null;
 
     }
