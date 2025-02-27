2a251b56101b5bdc1288c0d6c3648a68aecf30bb
Aidar <stormymayday7@gmail.com>
2025-02-27 14:47:47 -0800

    Time: 0 ms (100.00%) | Memory: 58.3 MB (11.88%) - LeetSync

--- solutions/61-rotate-list/rotate-list.ts
+++ solutions/61-rotate-list/rotate-list.ts
@@ -19,31 +19,26 @@ function rotateRight(head: ListNode | null, k: number): ListNode | null {
 
     // Finding length and the tail
     let length = 1; // starting from 1 initally
-    let tail = head; // starting from head
+    let tail = head;
     while(tail.next) {
         tail = tail.next;
         length++;
     }
 
-    // If length is equak to k we can return 
+    // Edge Case 2: If length is equal to k we can return 
     if(length === k) {
         return head;
     }
 
-    // Fidning actual (if k > length) or rotations
+    // Fidning the actual (if k > length) number or rotations
     const numberOfRotations = k % length;
 
-    // Move to the pivot point and rotate
+    // Finding the pivot / break point
     let current = head;
-    // Correct:
     for(let i = 0; i < length - numberOfRotations - 1; i++) {
-    // Error:
-    // for(let i = 0; i < numberOfRotations; i++) {
-    // Error:
-    // for(let i = 0; i < numberOfRotations + 1; i++) {
         current = current.next;
     }
-    // now current is at the pivot point (new head)
+    // now current is at the pivot point (new tail)
 
     // connecting tail to head
     tail.next = head;
@@ -51,7 +46,7 @@ function rotateRight(head: ListNode | null, k: number): ListNode | null {
     // shifting head to it's new position (next after pivot)
     head = current.next;
 
-    // unlinling at pivot point
+    // unlinking list at the pivot point
     current.next = null;
     
     return head;
