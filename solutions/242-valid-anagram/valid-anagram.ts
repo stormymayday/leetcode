5c7936d1ffc18edfff7d36dc0d25d5fabe4c0abb
Aidar <stormymayday7@gmail.com>
2025-02-28 12:00:33 -0800

    Time: 10 ms (82.29%) | Memory: 57.3 MB (17.63%) - LeetSync

--- solutions/242-valid-anagram/valid-anagram.ts
+++ solutions/242-valid-anagram/valid-anagram.ts
@@ -1,20 +1,37 @@
 function isAnagram(s: string, t: string): boolean {
-
-    if(s.length !== t.length) {
+    // An anagram is a rearrangement of the exact same characters
+    // no more, no less.
+    if (s.length !== t.length) {
         return false;
     }
 
-    const freqMap = {};
+    const hashMap = {};
 
-    for(const char of s) {
-        freqMap[char] = (freqMap[char] || 0) + 1;
+    // filling the hashmap
+    for(let i = 0; i < s.length; i++) {
+        // If the character is not in the map
+        if(hashMap[s[i]] === undefined) {
+            // add the character as a key and set value to 1
+            hashMap[s[i]] = 1;
+        } else {
+            // otherwise, increment the value by 1
+            hashMap[s[i]]++;
+        }
     }
 
-    for(const char of t) {
-        if(!freqMap[char]) {
+    for(let i = 0; i < t.length; i++) {
+        // if the character is in the map
+        if(hashMap[t[i]] !== undefined) {
+            // decrement count
+            hashMap[t[i]]--;
+            // delete the character when count reaches 0
+            if(hashMap[t[i]] === 0) {
+                delete hashMap[t[i]];
+            }
+        } else {
+            // otherwise, (character is not in the map) return 'false'
             return false;
         }
-        freqMap[char]--;
     }
 
     return true;
