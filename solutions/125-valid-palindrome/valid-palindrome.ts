ab0f896f7c712b66fe80ee97d494363d4dd9dec3
Aidar <stormymayday7@gmail.com>
2025-02-28 19:22:19 -0800

    Time: 1 ms (98.47%) | Memory: 58.9 MB (15.53%) - LeetSync

--- solutions/125-valid-palindrome/valid-palindrome.ts
+++ solutions/125-valid-palindrome/valid-palindrome.ts
@@ -1,14 +1,23 @@
+function isAlphanumeric(char: string): boolean {
+    return (char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9');
+}
+
 function isPalindrome(s: string): boolean {
-    
-    s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
 
     if(s.length <= 1) return true;
 
     let left = 0
     let right = s.length - 1;
 
-    while(right > left) {
-        if(s[left] !== s[right]) {
+    while(left < right) {
+        while(left < right && !isAlphanumeric(s[left])) {
+            left++;
+        }
+        while(left < right && !isAlphanumeric(s[right])) {
+            right--;
+        }
+        // Compare characters in a case-insensitive manner
+        if(s[left].toLowerCase() !== s[right].toLowerCase()) {
             return false;
         }
         left++;
