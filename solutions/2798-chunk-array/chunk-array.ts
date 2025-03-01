4eb3469cf3013e834f3d007b78719d1e4634430a
Aidar <stormymayday7@gmail.com>
2025-02-28 18:43:11 -0800

    Time: 53 ms (80.86%) | Memory: 59 MB (5.61%) - LeetSync

--- solutions/2798-chunk-array/chunk-array.ts
+++ solutions/2798-chunk-array/chunk-array.ts
@@ -5,6 +5,7 @@ function chunk(arr: Obj[], size: number): Obj[][] {
 
     const result = [];
     let chunk = [];
+
     for(let i = 0; i < arr.length; i++) {
         chunk.push(arr[i]);
         if(chunk.length === size) {
@@ -13,7 +14,7 @@ function chunk(arr: Obj[], size: number): Obj[][] {
         }
     }
 
-    if(chunk.length) {
+    if(chunk.length > 0) {
         result.push(chunk);
     }
 
