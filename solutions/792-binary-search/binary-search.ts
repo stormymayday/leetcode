ec3bb685649156ff3f9fa79e4745fba30e5f63c0
Aidar <stormymayday7@gmail.com>
2025-03-03 11:42:16 -0800

    Time: 0 ms (100.00%) | Memory: 58 MB (22.24%) - LeetSync

--- solutions/792-binary-search/binary-search.ts
+++ solutions/792-binary-search/binary-search.ts
@@ -4,17 +4,15 @@ function search(nums: number[], target: number): number {
     let right = nums.length - 1;
 
     while(left <= right) {
+        let middle = Math.floor(left + (right - left) / 2);
 
-        const middle = Math.floor(left + (right - left) / 2);
-
-        if(nums[middle] === target) {
+        if(nums[middle]=== target) {
             return middle;
         } else if(nums[middle] > target) {
             right = middle - 1;
         } else {
             left = middle + 1;
         }
-
     }
 
     return -1;
