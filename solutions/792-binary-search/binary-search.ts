537e2d70095ed5d67a1080d7e8f6eaf0ac76e503
Aidar <stormymayday7@gmail.com>
2025-03-03 11:56:09 -0800

    Time: 0 ms (100.00%) | Memory: 59.9 MB (9.75%) - LeetSync

--- solutions/792-binary-search/binary-search.ts
+++ solutions/792-binary-search/binary-search.ts
@@ -1,20 +1,33 @@
 function search(nums: number[], target: number): number {
 
-    let left = 0;
-    let right = nums.length - 1;
+    function binarySearch(nums, target, left, right) {
 
-    while(left <= right) {
-        let middle = Math.floor(left + (right - left) / 2);
-
-        if(nums[middle]=== target) {
-            return middle;
-        } else if(nums[middle] > target) {
-            right = middle - 1;
-        } else {
-            left = middle + 1;
+        // Base Case 1: If pointers cross, the target is was not found
+        if(left > right) {
+            return -1;
         }
+
+        const middle = Math.floor(left + (right - left) / 2);
+
+        // Base Case 2: target is found, return index
+        if(nums[middle] === target) {
+            return middle;
+        } 
+
+        // Recursive Calls:
+        if(nums[middle] > target) {
+            // If middle is greater than taret, searching left
+            return binarySearch(nums, target, left, middle - 1);
+        } else {
+             // If middle is less than taret, searching rigjt
+            return binarySearch(nums, target, middle + 1, right);
+        }
+
     }
 
-    return -1;
-    
+    return binarySearch(nums, target, 0, nums.length - 1);
+
 };
+
+// The time complexity is O(log n) which is optimal for searching in a sorted array
+// Space complexity is O(log n) due to the recursive call stack.
