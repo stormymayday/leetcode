f54459ed1199798dda2ff839cd3b5812ead094f8
Aidar <stormymayday7@gmail.com>
2025-03-04 12:57:37 -0800

    Time: 43 ms (94.55%) | Memory: 55.8 MB (7.08%) - LeetSync

--- solutions/1013-fibonacci-number/fibonacci-number.ts
+++ solutions/1013-fibonacci-number/fibonacci-number.ts
@@ -7,7 +7,12 @@ function fib(n: number): number {
     let b = 1;
 
     for(let i = 2; i <= n; i++) {
-        [a, b] = [b, a + b];
+        // Store the current value of b in a temporary variable
+        const temp = b;
+        // Update b with the sum of a and b
+        b = a + b;
+        // Set a to the previous value of b (stored in temp)
+        a = temp;
     }
 
     return b;
