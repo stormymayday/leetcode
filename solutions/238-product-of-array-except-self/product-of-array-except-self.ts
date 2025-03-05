4dd845ee92fea700ca3b25b649a764228c65ac0d
Aidar <stormymayday7@gmail.com>
2025-03-05 12:18:26 -0800

    Time: 6 ms (71.25%) | Memory: 69.9 MB (27.95%) - LeetSync

--- solutions/238-product-of-array-except-self/product-of-array-except-self.ts
+++ solutions/238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,26 +1,21 @@
 function productExceptSelf(nums: number[]): number[] {
-    const prefixArray = [];
-    const postfixArray = [];
-    const result = [];
-    
-    // Calculate prefix products (product of all elements to the LEFT)
+
+    const length = nums.length;
+
+    const result = new Array(length).fill(1);
+
     let prefix = 1;
-    for (let i = 0; i < nums.length; i++) {
-        prefixArray.push(prefix);
+    for(let i = 0; i < length; i++) {
+        result[i] = prefix;
         prefix *= nums[i];
     }
-    
-    // Calculate postfix products (product of all elements to the RIGHT)
+
     let postfix = 1;
-    for (let i = nums.length - 1; i >= 0; i--) {
-        postfixArray[i] = postfix;
+    for(let i = length - 1; i >= 0; i--) {
+        result[i] *= postfix;
         postfix *= nums[i];
     }
-    
-    // Multiply prefix and postfix products
-    for (let i = 0; i < nums.length; i++) {
-        result.push(prefixArray[i] * postfixArray[i]);
-    }
-    
+
     return result;
-}
+    
+};
