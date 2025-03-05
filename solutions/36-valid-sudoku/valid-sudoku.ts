bedff435a75201db9f110a74f06d490b67398512
Aidar <stormymayday7@gmail.com>
2025-03-05 14:37:21 -0800

    Time: 6 ms (53.48%) | Memory: 59.8 MB (12.98%) - LeetSync

--- solutions/36-valid-sudoku/valid-sudoku.ts
+++ solutions/36-valid-sudoku/valid-sudoku.ts
@@ -1,57 +1,53 @@
 function isValidSudoku(board: string[][]): boolean {
 
-    // checking columns
+    // key is a row number, value is a set of row values
+    const rowMap = new Map();
+
+    // key is a col number, value is a set of col values
+    const colMap = new Map();
+
+    // key is [(Math.floor(row/3)), (Math.floor(col/3)], value os a set of sub square values
+    const subSquareMap = new Map();
+
     for(let row = 0; row < 9; row++) {
-        const rowSet = new Set();
         for(let col = 0; col < 9; col++) {
-            const cell = board[row][col];
-            if(cell === '.') {
+
+            // extracting value from the current board cell
+            const value = board[row][col];
+
+            // if value is empty (dot)
+            if(value === '.') {
+                // skip to the next iteration
                 continue;
             }
-            if(rowSet.has(cell)) {
+
+            // getting key for current sub square
+            const subSquareKey = `${Math.floor(row/3)},${Math.floor(col/3)}`;
+
+            // Initialize sets if they don't exist
+            if(!rowMap.has(row)) {
+                rowMap.set(row, new Set());
+            }
+            if(!colMap.has(col)) {
+                colMap.set(col, new Set());
+            }
+            if(!subSquareMap.has(subSquareKey)) {
+                subSquareMap.set(subSquareKey, new Set());
+            }
+
+            // checking if he have found duplicate
+            if(rowMap.get(row).has(value) || colMap.get(col).has(value) || subSquareMap.get(subSquareKey).has(value)) {
                 return false;
-            } else {
-                rowSet.add(cell);
             }
+
+            // adding value to the sets
+            rowMap.get(row).add(value);
+            colMap.get(col).add(value) 
+            subSquareMap.get(subSquareKey).add(value)
+
         }
     }
 
-    // checking columns
-    for(let col = 0; col < 9; col++) {
-        const colSet = new Set();
-        for(let row = 0; row < 9; row++) {
-            const cell = board[row][col];
-            if(cell === '.') {
-                continue;
-            }
-            if(colSet.has(cell)) {
-                return false;
-            } else {
-                colSet.add(cell);
-            }
-        }
-    }
-
-    // checking sub-boards
-    for(let row = 0; row < 9; row+=3) {
-        for(let col = 0; col < 9; col+=3) {
-            const subBoardSet = new Set();
-            for(let subRow = 0; subRow < 3; subRow++) {
-                for(let subCol = 0; subCol <3; subCol++) {
-                    const cell = board[row + subRow][col + subCol];
-                    if(cell === '.') {
-                        continue;
-                    }
-                    if(subBoardSet.has(cell)) {
-                        return false;
-                    } else {
-                        subBoardSet.add(cell);
-                    }
-                }
-            }
-        }
-    }
-
+    // No duplicates were found, valid board
     return true;
-    
 };
