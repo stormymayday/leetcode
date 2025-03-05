a80b34d0b93a1aa91c95d73fc12e02b7c197b90d
Aidar <stormymayday7@gmail.com>
2025-03-04 16:14:08 -0800

    Time: 46 ms (23.67%) | Memory: 71.5 MB (5.50%) - LeetSync

--- solutions/49-group-anagrams/group-anagrams.ts
+++ solutions/49-group-anagrams/group-anagrams.ts
@@ -1,19 +1,30 @@
 function groupAnagrams(strs: string[]): string[][] {
-    // Object to store anagram groups
+    // Object to store grouped anagrams
     const hashMap = {};
     // Iterate through each string in the input array
     for(const string of strs) {
-        // Sort the characters of the string
-        // This creates a key for anagrams
-        const sorted = string.split('').sort().join('');
-        // If no group exists for this sorted string, create an empty array
-        if(hashMap[sorted] === undefined) {
-            hashMap[sorted] = [];
+        // Create a count array to track character frequencies
+        // Array of 26 zeros representing a-z
+        const charFrequencyCount = new Array(26).fill(0);
+        // Count the frequency of each character in the string
+        for(const char of string) {
+            // Get the character code by subracting charater code of 'a'
+            const charCode = char.charCodeAt(0) - 'a'.charCodeAt(0);
+            // Increment the count for that character code
+            charFrequencyCount[charCode] += 1;
         }
-        // Add the original string to its corresponding anagram group
-        hashMap[sorted].push(string);
-    }
-    // Return an array of all anagram groups
-    return Object.values(hashMap);
+        // Create a key by joining the charFrequencyCount array
+        // Using '#' as a separator in the key prevents accidental merging of different frequency arrays (e.g., 10,1 vs. 1,0,1).
+        const key = charFrequencyCount.join('#');
+        // If the key doesn't exist in the result object, create an empty array
+        if(hashMap[key] === undefined) {
+            hashMap[key] = [];
+        }
+        // Add the current string to its anagram group
+        hashMap[key].push(string);
 
+    }
+    // Return the values (grouped anagram arrays) from the result object
+    return Object.values(hashMap);
+    
 };
