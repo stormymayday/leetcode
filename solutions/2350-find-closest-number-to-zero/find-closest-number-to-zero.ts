69102ba2a9d562a0aa577ca265d11b1d33f819be
Aidar <stormymayday7@gmail.com>
2025-03-05 17:38:12 -0800

    Time: 1 ms (77.14%) | Memory: 59 MB (7.35%) - LeetSync

--- solutions/2350-find-closest-number-to-zero/find-closest-number-to-zero.ts
+++ solutions/2350-find-closest-number-to-zero/find-closest-number-to-zero.ts
@@ -1,22 +1,19 @@
-function findClosestNumber(nums: number[]): number { 
+function findClosestNumber(nums: number[]): number {
     // Initializes 'closest' to the first element in the array. Array is guaranteed to have atleast 1 element
     let closest = nums[0]; 
     
     // Loop through each number in the 'nums' array.
     for (const num of nums) { 
+        
         // Check if the absolute value of the current number ('num') is smaller than the absolute value of 'closest'.
-        if (Math.abs(num) < Math.abs(closest)) { 
-             // If true, update 'closest' to the current number ('num') because it's closer to zero.
+        // OR if both numbers have the same absolute value, the positive one (the one with a larger actual value) is preferred.
+        if (Math.abs(num) < Math.abs(closest) || 
+            (Math.abs(num) === Math.abs(closest) && num > closest)) { 
+            // If any of the above conditions is true, update 'closest' to the current number ('num').
             closest = num; 
         }
     }
-
-    // After the loop, if 'closest' is negative, check if the positive counterpart of 'closest' exists in the array.
-    if (closest < 0 && nums.includes(Math.abs(closest))) { 
-        // If a positive counterpart exists, return the absolute value of 'closest' (i.e., the positive number).
-        return Math.abs(closest); 
-    } else { 
-        // If 'closest' is already positive, or no counterpart was found, return 'closest' as is.
-        return closest; 
-    }
+    // After the loop, return 'closest', which now holds the number closest to zero. 
+    return closest; 
+    
 }
