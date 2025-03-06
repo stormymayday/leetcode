8df29d49152989dbffb894e564a1bfdfad4b5a1e
Aidar <stormymayday7@gmail.com>
2025-03-05 19:45:27 -0800

    Time: 56 ms (62.42%) | Memory: 55.6 MB (18.27%) - LeetSync

--- solutions/1894-merge-strings-alternately/merge-strings-alternately.ts
+++ solutions/1894-merge-strings-alternately/merge-strings-alternately.ts
@@ -1,39 +1,26 @@
-function mergeAlternately(word1: string, word2: string): string { 
-    // Initialize an empty string 'result' to store the merged string.
-    let result = ""; 
-    
-    // Initialize two pointers, 'p1' for word1 and 'p2' for word2.
-    let p1 = 0; 
-    let p2 = 0; 
-    
-    // Continue looping as long as both pointers are within the bounds of the strings.
-    while (p1 < word1.length && p2 < word2.length) { 
-        
-        // Add the character from word1 at position 'p1' to the result.
-        result += word1[p1]; 
-        // Increment the pointer for word1.
-        p1++; 
-        
-        // Add the character from word2 at position 'p2' to the result.
-        result += word2[p2]; 
-        // Increment the pointer for word2.
-        p2++; 
-        
+function mergeAlternately(word1: string, word2: string): string {
+
+    const result = [];
+
+    let i = 0;
+    let j = 0;
+
+    while(i < word1.length && j < word2.length) {
+        result.push(word1[i]);
+        result.push(word2[j]);
+        i++;
+        j++;
     }
 
-    // If there are any remaining characters in word1, append them to the result.
-    if (p1 < word1.length) { 
-        
-        result += word1.slice(p1); 
+    while(i < word1.length) {
+        result.push(word1[i]);
+        i++;
     }
 
-    // If there are any remaining characters in word2, append them to the result.
-    if (p2 < word2.length) { 
-        
-        result += word2.slice(p2); 
+     while(j < word2.length) {
+        result.push(word2[j]);
+        j++;
     }
 
-    // Return the merged string.
-    return result; 
-    
-}
+    return result.join("");
+};
