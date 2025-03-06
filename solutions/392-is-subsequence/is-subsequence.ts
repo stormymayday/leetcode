7abc53441a72ca5af6c7bc9386e79836c7ec2260
Aidar <stormymayday7@gmail.com>
2025-03-06 12:57:49 -0800

    Time: 3 ms (29.52%) | Memory: 56.6 MB (10.00%) - LeetSync

--- solutions/392-is-subsequence/is-subsequence.ts
+++ solutions/392-is-subsequence/is-subsequence.ts
@@ -1,21 +1,30 @@
 function isSubsequence(s: string, t: string): boolean {
-    // Edge case: empty string is always a subsequence
-    if (s.length === 0) return true;
+    // Edge case 1: empty string is always a subsequence
+    if (s.length === 0) {
+        return true;
+    }
 
-     // Pointer for string s
+    // Edge case 2: s is longer than t
+    if (s.length > t.length) {
+        return false;
+    }
+
+    // Pointer for string s
     let sIndex = 0;
     
     for(let tIndex = 0; tIndex < t.length; tIndex++) {
-        // If current characters match, move s pointer forward
+        // If current characters match
         if(s[sIndex] === t[tIndex]) {
+            // move s pointer forward
             sIndex++;
         } 
         
-        // Check if we've matched all characters in s
+        // Once sIndex is out of bounds (goes past last index in s)
         if(sIndex === s.length) {
+            // We are done! It is a subsequence
             return true;
         }
     }
-
+    // Subsequence was not found / reached
     return false;
 };
