9cf156da8fd97561fa9799474b19d0a69ee4b182
Aidar <stormymayday7@gmail.com>
2025-03-09 14:25:44 -0700

    Time: 0 ms (100.00%) | Memory: 56.6 MB (20.53%) - LeetSync

--- solutions/48-rotate-image/rotate-image.ts
+++ solutions/48-rotate-image/rotate-image.ts
@@ -1,35 +1,41 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
 function rotate(matrix: number[][]): void {
 
-     const n: number = matrix.length;
-    
+    // storing matrix length
+    const n = matrix.length;
+
     // Process layer by layer from outside to inside
-    for (let layer = 0; layer < Math.floor(n / 2); layer++) {
+    for(let layer = 0; layer < Math.floor(n / 2); layer++) {
 
-        // Define boundaries of current layer
-        const top: number = layer;        
-        const left: number = layer;
-        const right: number = n - 1 - layer;
-        const bottom: number = n - 1 - layer
-        
+        // boundaries for current layer
+        let topRow = layer
+        let rightCol = n - 1 - layer;
+        let bottomRow = n - 1 - layer;
+        let leftCol = layer;
+
         // For each element in the current layer
-        for (let i = 0; i < right - left; i++) {
+        for(let i = 0; i < rightCol - leftCol; i++) {
+            // TEMP VARS
+            // Storing all four elements that will be rotated
+            let currentTopRowElement = matrix[topRow][leftCol + i];
+            let currentRightColElement = matrix[topRow + i][rightCol];
+            let currentBottomRowElement = matrix[bottomRow][rightCol - i];
+            let currentLeftColElement = matrix[bottomRow - i][leftCol];
 
-            // Store all four elements that will be rotated
-            const topLeft: number = matrix[top][left + i];
-            const topRight: number = matrix[top + i][right];
-            const bottomRight: number = matrix[bottom][right - i];
-            const bottomLeft: number = matrix[bottom - i][left];
-            
-            // PERFORM CLOCKWISE ROTATION USING SAVED VALUES
+            // SWAPPING
+            // top row goes into right col
+            matrix[topRow + i][rightCol] = currentTopRowElement;
+            // right col goes into bottom row
+            matrix[bottomRow][rightCol - i] = currentRightColElement;
+            // bottom row goes into left col
+            matrix[bottomRow - i][leftCol] = currentBottomRowElement;
+            // left col goes into tio row
+            matrix[topRow][leftCol + i] = currentLeftColElement
 
-            // Right column gets value from top row
-            matrix[top + i][right] = topLeft;
-            // Bottom row gets value from right column
-            matrix[bottom][right - i] = topRight;
-             // Left column gets value from bottom row
-            matrix[bottom - i][left] = bottomRight;
-            // Top row gets value from left column
-            matrix[top][left + i] = bottomLeft;
         }
+
     }
-}
+    
+};
