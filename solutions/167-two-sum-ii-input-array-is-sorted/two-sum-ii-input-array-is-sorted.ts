c85c5a1c596c56e9a09765ebdddebaecf2185880
Aidar <stormymayday7@gmail.com>
2025-03-09 15:46:11 -0700

    Time: 0 ms (100.00%) | Memory: 55.7 MB (32.27%) - LeetSync

--- solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,24 +1,23 @@
 function twoSum(numbers: number[], target: number): number[] {
-
-    if(!numbers.length) {
-        return [];
-    }
-
-    let i = 0;
-    let j = numbers.length - 1;
-
-    while(i < j) {
-
-        if(numbers[i] + numbers[j] === target) {
-            return [i + 1,  j + 1];
-        } else if(numbers[i] + numbers[j] > target) {
-            j--;
+    // Initialize two pointers - one at the start and one at the end of the array
+    let left = 0;
+    let right = numbers.length - 1;
+    // Iterate until pointers meet
+    while (left < right) {
+        // Sums to the target!
+        if (numbers[left] + numbers[right] === target) {
+            // Return both indices (adding 1 to convert to 1-indexed)
+            return [left + 1, right + 1];
+        } else if (numbers[left] + numbers[right] > target) {
+            // Sum is too large
+            // Need a smaller value
+            // Since array is sorted, moving right pointer left will decrease the sum
+            right--;
         } else {
-            i++;
+            // Otherwise, sum is too small
+            // Need a larger value
+            // Since array is sorted, moving left pointer right will increase the sum
+            left++;
         }
- 
     }
-
-    return [];
-    
-};
+}
