50ef20c44a4d57fa67bca80bded797d135c5f1f4
Aidar <stormymayday7@gmail.com>
2025-03-08 17:36:34 -0800

    Time: 0 ms (100.00%) | Memory: 56.8 MB (14.45%) - LeetSync

--- solutions/48-rotate-image/rotate-image.ts
+++ solutions/48-rotate-image/rotate-image.ts
@@ -1,41 +1,35 @@
 function rotate(matrix: number[][]): void {
 
-    const n: number = matrix.length;
+     const n: number = matrix.length;
     
     // Process layer by layer from outside to inside
     for (let layer = 0; layer < Math.floor(n / 2); layer++) {
-        // Define boundaries of current layer
-        // Starting index of this layer
-        const first: number = layer;
-        // Ending index of this layer
-        const last: number = n - 1 - layer;
-        
-        // For each element in the current layer's top row
-        for (let i = first; i < last; i++) {
-            // Calculate offset from beginning of layer
-            const offset: number = i - first;
-            
-            // Store all four elements that will be rotated
-            // Top element
-            const top: number = matrix[first][i];
-            // Right element             
-            const right: number = matrix[i][last];
-            // Bottom element           
-            const bottom: number = matrix[last][last - offset];
-            // Left element
-            const left: number = matrix[last - offset][first];
-            
-            // Perform clockwise rotation using saved values
 
-            // Right gets value from top
-            matrix[i][last] = top;
-            // Bottom gets value from right
-            matrix[last][last - offset] = right;
-            // Left gets value from bottom
-            matrix[last - offset][first] = bottom;
-            // Top gets value from left
-            matrix[first][i] = left;
+        // Define boundaries of current layer
+        const top: number = layer;        
+        const left: number = layer;
+        const right: number = n - 1 - layer;
+        const bottom: number = n - 1 - layer
+        
+        // For each element in the current layer
+        for (let i = 0; i < right - left; i++) {
+
+            // Store all four elements that will be rotated
+            const topLeft: number = matrix[top][left + i];
+            const topRight: number = matrix[top + i][right];
+            const bottomRight: number = matrix[bottom][right - i];
+            const bottomLeft: number = matrix[bottom - i][left];
             
+            // PERFORM CLOCKWISE ROTATION USING SAVED VALUES
+
+            // Right column gets value from top row
+            matrix[top + i][right] = topLeft;
+            // Bottom row gets value from right column
+            matrix[bottom][right - i] = topRight;
+             // Left column gets value from bottom row
+            matrix[bottom - i][left] = bottomRight;
+            // Top row gets value from left column
+            matrix[top][left + i] = bottomLeft;
         }
     }
 }
