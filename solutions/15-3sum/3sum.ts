54c927288f0fc8f4f81d7e336dd32a933dfd002c
Aidar <stormymayday7@gmail.com>
2025-03-09 17:51:48 -0700

    Time: 577 ms (13.86%) | Memory: 71.5 MB (32.74%) - LeetSync

--- solutions/15-3sum/3sum.ts
+++ solutions/15-3sum/3sum.ts
@@ -1,30 +1,42 @@
 function threeSum(nums: number[]): number[][] {
 
-    nums.sort((a, b) => a - b);
-        const count = new Map();
-        for (let num of nums) {
-            count.set(num, (count.get(num) || 0) + 1);
+    nums.sort((a,b) => a - b);
+
+    const frequencyMap = new Map();
+    for(const num of nums) {
+        if(frequencyMap.get(num) === undefined) {
+            frequencyMap.set(num, 1);
+        } else {
+            frequencyMap.set(num, frequencyMap.get(num) + 1);
+        }
+    }
+
+    const result = [];
+    for(let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        frequencyMap.set(num, frequencyMap.get(num) - 1);
+        if(i > 0 && nums[i] === nums[i - 1]) {
+            continue;
         }
 
-        const res = [];
-        for (let i = 0; i < nums.length; i++) {
-            count.set(nums[i], count.get(nums[i]) - 1);
-            if (i > 0 && nums[i] === nums[i - 1]) continue;
-
-            for (let j = i + 1; j < nums.length; j++) {
-                count.set(nums[j], count.get(nums[j]) - 1);
-                if (j > i + 1 && nums[j] === nums[j - 1]) continue;
-
-                const target = -(nums[i] + nums[j]);
-                if (count.get(target) > 0) {
-                    res.push([nums[i], nums[j], target]);
-                }
+        for(let j = i + 1; j < nums.length; j++) {
+            const num = nums[j];
+            frequencyMap.set(num, frequencyMap.get(num) - 1);
+            if(j > i + 1 && nums[j] === nums[j - 1]) {
+                continue;
             }
-
-            for (let j = i + 1; j < nums.length; j++) {
-                count.set(nums[j], count.get(nums[j]) + 1);
+            const target = -(nums[i] + nums[j]);
+            if(frequencyMap.get(target) > 0) {
+                result.push([nums[i], nums[j], target]);
             }
         }
-        return res;
+
+        for (let j = i + 1; j < nums.length; j++) {
+            frequencyMap.set(nums[j], frequencyMap.get(nums[j]) + 1);
+        }
+
+    }
+    
+    return result;
     
 };
