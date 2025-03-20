9158020cee6c7c73e33286ddab183c86e7f69026
Aidar <stormymayday7@gmail.com>
2025-03-19 18:15:42 -0700

    Time: 6 ms (67.92%) | Memory: 63.3 MB (26.70%) - LeetSync

--- solutions/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
+++ solutions/1019-squares-of-a-sorted-array/squares-of-a-sorted-array.ts
@@ -3,7 +3,7 @@ function sortedSquares(nums: number[]): number[] {
     const result = [];
 
     for(let i = 0; i < nums.length; i++) {
-        result.push(Math.abs(nums[i])*Math.abs(nums[i]));
+        result.push(nums[i]*nums[i]);
     }
 
     return result.sort((a,b) => a - b);
