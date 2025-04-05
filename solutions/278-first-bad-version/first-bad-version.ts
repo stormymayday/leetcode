47e89d7024bdd2415135b8a49e83e30f3d24b9b4
Aidar <stormymayday7@gmail.com>
2025-04-05 08:49:17 -0700

    Time: 40 ms (89.15%) | Memory: 55.9 MB (12.33%) - LeetSync

--- solutions/278-first-bad-version/first-bad-version.ts
+++ solutions/278-first-bad-version/first-bad-version.ts
@@ -8,16 +8,30 @@
 var solution = function(isBadVersion: any) {
 
     return function(n: number): number {
+
         let left = 1;
         let right = n;
+
+        // if left === right stop
         while(left < right) {
-            const middle = Math.floor((left + right) / 2);
-            if(isBadVersion(middle)) {
-                right = middle;
+
+            const mid = Math.floor((left + right) / 2);
+
+            if(isBadVersion(mid)) {
+                // found bad version!
+                // discard everything to right (those are all bad)
+                // But keep account this one because it might be the first one
+                right = mid;
             } else {
-                left = middle + 1;
+                // good version
+                // discard everything to the left AND this one
+                left = mid + 1;
             }
+
         }
+
+        // can return either pointer at the end
         return left;
+        
     };
 };
