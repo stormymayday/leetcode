acd23793c3c74f77f8b6a1bb0ed03666c5cefa1e
Aidar <stormymayday7@gmail.com>
2025-04-05 12:36:17 -0700

    Time: 0 ms (100.00%) | Memory: 54.5 MB (69.96%) - LeetSync

--- solutions/74-search-a-2d-matrix/search-a-2d-matrix.ts
+++ solutions/74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -1,26 +1,55 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-    
-    for(let i = 0; i < matrix.length; i++) {
 
-        let left = 0;
-        let right = matrix[i].length - 1;
+    // Dimensions
+    const ROWS = matrix.length;
+    const COLS = matrix[0].length;
 
-        while(left <= right) {
+    // First Binary Search: find the target row
+    let topRow = 0;
+    let bottomRow = ROWS - 1;
+    while(topRow <= bottomRow) {
 
-            const mid = Math.floor((left+right)/2);
+        // calculate the middle row
+        const midRow = Math.floor((topRow + bottomRow) / 2);
 
-            if(matrix[i][mid] > target) {
-                right = mid - 1;
-            } else if(matrix[i][mid] < target) {
-                left= mid + 1;
-            } else {
-                return true;
-            }
-
+        // if target is greater than the largest element in this (middle) row
+        if(target > matrix[midRow][COLS - 1]) {
+            // Move the topRow down past the current middle
+            topRow = midRow + 1;
         }
-
+        // if target is smaller than the smallest element in this (middle) row
+        else if(target < matrix[midRow][0]) {
+           // Move the bottom row up past the current middle
+           bottomRow = midRow - 1;
+        }
+        // target can be in this row
+        else {
+            break;
+        }
     }
 
-    return false;
+    // Check if target row exists (the row pointers didn't cross)
+    if(topRow > bottomRow) {
+        // Pointers have crossed
+        return false;
+    }
 
+    // Second Binary Search: search the targetRow
+    const targetRow = Math.floor((topRow + bottomRow) / 2);
+    let left = 0;
+    let right = COLS - 1;
+    while(left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        if(matrix[targetRow][mid] > target) {
+            right = mid - 1;
+        } else if(matrix[targetRow][mid] < target) {
+            left = mid + 1;
+        } else {
+            return true;
+        }
+    }
+
+    // Target was not found
+    return false;
+    
 };
