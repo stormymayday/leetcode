7b31bb2a3efb9c8eee9d38fc456033f82282b99a
Aidar <stormymayday7@gmail.com>
2025-04-05 17:59:14 -0700

    Time: 0 ms (100.00%) | Memory: 56.2 MB (26.75%) - LeetSync

--- solutions/74-search-a-2d-matrix/search-a-2d-matrix.ts
+++ solutions/74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -1,55 +1,26 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
 
-    // Dimensions
     const ROWS = matrix.length;
     const COLS = matrix[0].length;
 
-    // First Binary Search: find the target row
-    let topRow = 0;
-    let bottomRow = ROWS - 1;
-    while(topRow <= bottomRow) {
-
-        // calculate the middle row
-        const midRow = Math.floor((topRow + bottomRow) / 2);
-
-        // if target is greater than the largest element in this (middle) row
-        if(target > matrix[midRow][COLS - 1]) {
-            // Move the topRow down past the current middle
-            topRow = midRow + 1;
-        }
-        // if target is smaller than the smallest element in this (middle) row
-        else if(target < matrix[midRow][0]) {
-           // Move the bottom row up past the current middle
-           bottomRow = midRow - 1;
-        }
-        // target can be in this row
-        else {
-            break;
-        }
-    }
-
-    // Check if target row exists (the row pointers didn't cross)
-    if(topRow > bottomRow) {
-        // Pointers have crossed
-        return false;
-    }
-
-    // Second Binary Search: search the targetRow
-    const targetRow = Math.floor((topRow + bottomRow) / 2);
     let left = 0;
-    let right = COLS - 1;
+    let right = ROWS * COLS - 1;
+
     while(left <= right) {
+
         const mid = Math.floor((left + right) / 2);
-        if(matrix[targetRow][mid] > target) {
+        const midValue = matrix[Math.floor(mid/COLS)][mid%COLS];
+
+        if(midValue > target) {
             right = mid - 1;
-        } else if(matrix[targetRow][mid] < target) {
+        } else if(midValue < target) {
             left = mid + 1;
         } else {
             return true;
         }
+
     }
 
-    // Target was not found
     return false;
     
 };
