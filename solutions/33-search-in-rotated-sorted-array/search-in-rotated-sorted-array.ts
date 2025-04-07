ce90cd60c96b53fd330c125f7fdd098e0579d07f
Aidar <stormymayday7@gmail.com>
2025-04-06 20:28:26 -0700

    Time: 0 ms (100.00%) | Memory: 56 MB (40.38%) - LeetSync

--- solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -1,42 +1,39 @@
 function search(nums: number[], target: number): number {
-    
+
     let left = 0;
     let right = nums.length - 1;
-    while(left < right) {
-        const mid = Math.floor((left+right)/2);
-        if(nums[mid] > nums[right]) {
-            left = mid + 1;
-        } else {
-            right = mid;
-        }
-    }
-
-    const minIndex = left;
-
-    if(minIndex === 0) {
-        left = 0;
-        right = nums.length - 1;
-    } else if(target >= nums[0] && target <= nums[minIndex - 1]) {
-        left = 0;
-        right = minIndex - 1;
-    } else if(target >= nums[minIndex] && target <= nums[nums.length - 1]) {
-        left = minIndex;
-        right = nums.length - 1;
-    } else {
-        return -1;
-    }
 
     while(left <= right) {
+
         const mid = Math.floor((left+right)/2);
-        if(nums[mid] > target) {
-            right = mid - 1;
-        } else if(nums[mid] < target) {
-            left = mid + 1;
-        } else {
+
+        if(target === nums[mid]) {
             return mid;
         }
+
+        // Left Sorted Portion
+        if(nums[mid] >= nums[left]) {
+            if(target > nums[mid]) {
+                left = mid + 1;
+            } else if(target < nums[mid] && target < nums[left]) {
+                left = mid + 1;
+            } else {
+                // target is less that mid but greater than left
+                right = mid - 1;
+            }
+        }
+        // Right Sorted Portion
+        else {
+            if(target < nums[mid]) {
+                right = mid - 1;
+            } else if(target > nums[mid] && target > nums[right]) {
+                right = mid - 1;
+            } else {
+                // target is greater than mid but less than right
+                left = mid + 1;
+            }
+        }
     }
-
+    
     return -1;
-
 };
