40d78a668c16dc37194e0651257ccda529ffa9aa
Aidar <stormymayday7@gmail.com>
2025-04-10 08:41:19 -0700

    Time: 17 ms (79.94%) | Memory: 65.8 MB (60.83%) - LeetSync

--- solutions/838-design-linked-list/design-linked-list.ts
+++ solutions/838-design-linked-list/design-linked-list.ts
@@ -6,39 +6,56 @@ class Node {
         this.next = null;
     }
 }
+
 class MyLinkedList {
+
     head: Node | null;
     tail: Node | null;
     length: number;
+
     constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
-    getNode(index: number): Node | undefined {
-        if (index < 0 || index >= this.length) {
-            return undefined;
-        }
-        let current = this.head;
-        for (let i = 0; i < index; i++) {
-            current = current!.next;
-        }
-        return current as Node;
-    }
-
-    get(index: number): number {
-        const target = this.getNode(index);
-        if (target) {
-            return target.value;
+    // push
+    addAtTail(val: number): void {
+        const newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
         } else {
-            return -1;
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
+    }
+
+    pop(): void {
+        if(!this.head) {
+            return;
+        } else {
+            let temp = this.head;
+            let before = this.head;
+            while(temp.next) {
+                before = temp;
+                temp = temp.next;
+            }
+            this.tail = before;
+            this.tail.next = null;
+            this.length--;
+            if(this.length === 0) {
+                this.head = null;
+                this.tail = null;
+            }
         }
     }
 
+    // unshift
     addAtHead(val: number): void {
         const newNode = new Node(val);
-        if (!this.head) {
+        if(!this.head) {
             this.head = newNode;
             this.tail = newNode;
         } else {
@@ -48,92 +65,114 @@ class MyLinkedList {
         this.length++;
     }
 
-    addAtTail(val: number): void {
-        const newNode = new Node(val);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
+    shift():void {
+        if(!this.head) {
+            return;
         } else {
-            this.tail!.next = newNode;
-            this.tail = newNode;
+            const temp = this.head;
+            this.head = this.head.next;
+            temp.next = null;
+            this.length--;
+            if(this.length === 0) {
+                this.tail = null;
+            }
         }
-        this.length++;
     }
 
-    removeAtHead(): Node | undefined {
-        if (!this.head) {
+    getNode(index: number): Node | undefined {
+        if(index < 0 || index >= this.length) {
             return undefined;
+        } else {
+            let temp = this.head;
+            for(let i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp;
         }
-        const temp = this.head;
-        this.head = this.head.next;
-        this.length--;
-        if (this.length === 0) {
-            this.tail = null;
-        }
-        temp.next = null;
-        return temp;
     }
 
-    removeAtTail(): Node | undefined {
-        if (!this.head) {
-            return undefined;
+    get(index: number): number {
+        const node = this.getNode(index);
+        if(node) {
+            return node.value;
+        } else {
+            return - 1;
         }
-        let current = this.head;
-        let beforeTail = this.head;
-        while (current.next) {
-            beforeTail = current;
-            current = current.next;
-        }
-        this.tail = beforeTail;
-        this.tail.next = null;
-        this.length--;
-        if (this.length === 0) {
-            this.head = null;
-            this.tail = null;
-        }
-        return current;
     }
 
+    // insert
     addAtIndex(index: number, val: number): void {
-        if (index < 0 || index > this.length) {
+        if(index < 0 || index > this.length) {
             return;
         }
-        if (index === 0) {
-            this.addAtHead(val);
-            return;
+
+        if(index === 0) {
+            return this.addAtHead(val);
         }
-        if (index === this.length) {
-            this.addAtTail(val);
-            return;
+
+        if(index === this.length) {
+            return this.addAtTail(val);
         }
+
         const before = this.getNode(index - 1);
-        if (before) {
+        if(before && before.next) {
             const newNode = new Node(val);
             newNode.next = before.next;
             before.next = newNode;
             this.length++;
-        } else {
-            return;
         }
     }
 
+    // remove
     deleteAtIndex(index: number): void {
-        if (index < 0 || index >= this.length) {
+        if(index < 0 || index >= this.length) {
             return;
         }
-        if (index === 0) {
-            this.removeAtHead();
-            return;
+
+        if(index === 0) {
+            return this.shift();
         }
-        if (index === this.length - 1) {
-            this.removeAtTail();
-            return;
+
+        if(index === this.length - 1) {
+            return this.pop();
         }
+
         const before = this.getNode(index - 1);
-        const target = before!.next;
-        before!.next = target!.next;
-        target!.next = null;
+        const target = before.next;
+        before.next = target.next;
+        target.next = null;
         this.length--;
-        return;
+    }
+
+    reverse():void {
+
+        if(!this.head) {
+            return;
+        }
+
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+
+        let before = null;
+        let after = null;
+
+        while(temp) {
+            after = temp.next;
+            temp.next = before;
+            before = temp;
+            temp = after;
+        }
+
     }
 }
+
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * var obj = new MyLinkedList()
+ * var param_1 = obj.get(index)
+ * obj.addAtHead(val)
+ * obj.addAtTail(val)
+ * obj.addAtIndex(index,val)
+ * obj.deleteAtIndex(index)
+ */
