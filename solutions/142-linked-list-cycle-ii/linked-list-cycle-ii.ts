6914b0004325e11b239c49b34b67291942ff1580
Aidar <stormymayday7@gmail.com>
2025-04-14 18:28:45 -0700

    Time: 56 ms (39.84%) | Memory: 58.4 MB (60.56%) - LeetSync

--- solutions/142-linked-list-cycle-ii/linked-list-cycle-ii.ts
+++ solutions/142-linked-list-cycle-ii/linked-list-cycle-ii.ts
@@ -11,19 +11,26 @@
  */
 
 function detectCycle(head: ListNode | null): ListNode | null {
+
     if(!head) {
         return null;
     }
 
     const set = new Set();
     let current = head;
-    while(current) {
-        if(set.has(current)) {
-            return current;
-        } else {
-            set.add(current);
+
+    while(!set.has(current)) {
+
+        if(current.next === null) {
+            return null;
         }
+
+        set.add(current);
+
         current = current.next;
+
     }
-    return null;
+
+    return current;
+    
 };
