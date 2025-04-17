6e705caec9545cd3a03b53eaeea9eac4f3a8006f
Aidar <stormymayday7@gmail.com>
2025-04-16 18:16:47 -0700

    Time: 1 ms (94.89%) | Memory: 66.1 MB (74.43%) - LeetSync

--- solutions/143-reorder-list/reorder-list.ts
+++ solutions/143-reorder-list/reorder-list.ts
@@ -15,90 +15,49 @@
  */
 function reorderList(head: ListNode | null): void {
 
-    const dummyNode = new ListNode(-1, head);
-    let current = dummyNode;
-
-    // Finding the length
-    let length = 0;
-    while(current) {
-
-        current = current.next;
-        if(current) {
-            length++;
-        }
+    if(!head) {
+        return;
     }
 
-    // List is longer than 1 node
-    if(length > 1) {
+    // 1. Finding the middle:
+    let slow = head;
+    let fast = head.next;
+    while(fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+    // now 'slow' is at the middle
+    // therefore, 'slow.next' is at the 'head' of the second list
+    let second = slow.next;
+    // split the list in half;
+    slow.next = null;
 
-        // Finding the middle 'index'
-        let mid;
-        if(length % 2 === 0) {
-            // If the length is even:
-            mid = length/2;
-        } else {
-            // if the length is odd:
-            mid = Math.ceil(length/2);
-        }
+    // 2. Reversing the second half:
+    let prev = null;
+    while(second) {
+        let next = second.next;
+        second.next = prev;
+        prev = second;
+        second = next;
+    }
+    // now 'prev' is the new 'head' of the second list
 
-        // Finding the middle node
-        current = head; // will end up at the end of the first list
-        let i = 1;
-        while(i !== mid) {
-            current = current.next;
-            i++;
-        }
+    // 3. Merging
+    let first = head;
+    second = prev;
+    // Keep going util one of the pointers (first or second) is null
+    // Since we know that second list can be shorter, we go with 'second'
+    while(second) {
 
-        // now 'current' is at the 'tail' of the first list
-        // therefore, current.next is at the 'head' of the second list
+        // Saving references to next nodes for each list
+        const firstNext = first.next;
+        const secondNext = second.next;
 
-        // saving pointer reference
-        let headTwo = current.next;
+        first.next = second;
+        first = firstNext;
 
-        // servering list in half
-        current.next = null;
-
-        // reversing the second half
-        let prev = null;
-        while(headTwo) {
-            let next = headTwo.next;
-            headTwo.next = prev;
-            prev = headTwo;
-            headTwo = next;
-        }
-
-        // now 'prev' is 'head' of the second list;
-
-        // Merging two lists
-        current = dummyNode;
-        while(prev) {
-
-            // starting from the first half:
-
-            // attaching 'left'
-            current.next = head;
-            // moving head forward
-            head = head.next;
-            // moving current forward
-            current = current.next;
-
-            // Second List:
-
-            // attaching 'right'
-            current.next = prev;
-            // moving prev 'backwards/forward'
-            prev = prev.next;
-            // moving current forward
-            current = current.next;
-
-        }
-
-        // Edge Case: odd number of nodes
-        // left will have a leftover node
-        if(head) {
-            current.next = head;
-        }
-        
+        second.next = first;
+        second = secondNext;
 
     }
     
