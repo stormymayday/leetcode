63aaa156b9bcf08442044f35f99a9f932de723d3
Aidar <stormymayday7@gmail.com>
2025-04-16 17:00:15 -0700

    Time: 2 ms (70.17%) | Memory: 65.5 MB (91.76%) - LeetSync

--- solutions/143-reorder-list/reorder-list.ts
+++ solutions/143-reorder-list/reorder-list.ts
@@ -15,50 +15,31 @@
  */
 function reorderList(head: ListNode | null): void {
     
-    // Lets create a dummy Node and attach it to the head
-    const dummyNode = new ListNode(-1, head);
-    // The next node will be the starting point of the reordered list
+    const arr = [];
+    let current = head;
 
-    let current = dummyNode;
-
-    while(head) {
-        
-        // attach head
-        current.next = head;
-
-        // move head forward
-        head = head.next; // can be null
-
-        // move current forward
+    while(current) {
+        arr.push(current);
         current = current.next;
-        
-        // Search for 'tail'
-        // start at 'new' head (was moved one step forward)
-        let prev = head; // can be null
-        if(prev) {
-            // if not null
-            let tail = prev.next; // can be null
+    }
 
-            if(tail) {
+    let left = 0;
+    let right = arr.length - 1;
 
-                while(tail.next) {
-                    prev = tail;
-                    tail = tail.next;
-                }
+    while(left < right) {
 
-                // attach 'tail'
-                current.next = tail;
+        arr[left].next = arr[right];
+        left++;
 
-                // shit 'current' forward
-                current = current.next;
-
-                // sever of the tail from the original list
-                prev.next = null
-
-            }
-
+        if(left >= right) {
+            break;
         }
 
+        arr[right].next = arr[left];
+        right--;
+
     }
+
+    arr[left].next = null;
 
 };
