a74366d1957c70b5755c2f74daee354ddf827165
Aidar <stormymayday7@gmail.com>
2025-04-17 17:35:51 -0700

    Time: 2 ms (93.09%) | Memory: 62 MB (80.74%) - LeetSync

--- solutions/2-add-two-numbers/add-two-numbers.ts
+++ solutions/2-add-two-numbers/add-two-numbers.ts
@@ -12,37 +12,41 @@
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
 
-    // creating dummy node
-    let result = new ListNode();
-    // current pointer
-    let current = result;
+    const dummyNode = new ListNode(-1, null);
+    let current = dummyNode;
 
-    // carray intially 0
     let carry = 0;
-
-    // while current listNodes exist OR carry still has value left
+    // Edge Case: check if carry remains
     while(l1 || l2 || carry) {
 
-        // extracting values from listNodes if nodes exist
-        let val1 = l1 ? l1.val : 0;
-        let val2 = l2 ? l2.val : 0;
+        // extracting the value if nodes exist
+        const num1 = l1 ? l1.val : 0;
+        const num2 = l2 ? l2.val : 0;
 
-        // calculating sum and carry
-        let sum = val1 + val2 + carry;
-        carry = Math.floor(sum / 10);
-        sum = sum % 10;
+        // cacluating the result
+        let result = num1 + num2 + carry;
 
-        // creating new listNode 
-        current.next = new ListNode(sum);
-        // moving current pointer forward
+        // calculating carry
+        carry = Math.floor(result/10); // if result is less than 10 carry will be zero
+
+        // if result is less than 10, modding it by 10 will not change it
+        // otherwise, it will get the 'ones' place
+        result = result % 10;
+
+        const newNode = new ListNode(result);
+        current.next = newNode;
+
+        // Updating pointers
         current = current.next;
-
-        // moving listNode pointers forward if nodes exist
-        l1 = l1 ? l1.next : null;
-        l2 = l2 ? l2.next : null;
+        if(l1) {
+            l1 = l1.next;
+        }
+        if(l2) {
+            l2 = l2.next;
+        }
 
     }
 
-    return result.next;
+    return dummyNode.next;
     
 };
