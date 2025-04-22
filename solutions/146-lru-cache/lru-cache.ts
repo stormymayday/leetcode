ca502f465a3bc93d50393b67ed4d3ba5e1ebc6f7
Aidar <stormymayday7@gmail.com>
2025-04-21 17:16:36 -0700

    Time: 125 ms (17.47%) | Memory: 111.2 MB (34.26%) - LeetSync

--- solutions/146-lru-cache/lru-cache.ts
+++ solutions/146-lru-cache/lru-cache.ts
@@ -1,8 +1,10 @@
 class Node {
+    key: number;
     value: number;
     next: Node | null;
     prev: Node | null;
-    constructor(value) {
+    constructor(key, value) {
+        this.key = key;
         this.value = value;
         this.next = null;
         this.prev = null;
@@ -10,112 +12,68 @@ class Node {
 }
 
 class LRUCache {
-
-    head: Node | null;
-    tail: Node | null;
-    length: number;
     capacity: number;
-    lookup: Map<number, Node>;
-    reverseLookup: Map<Node, number>;
-
+    cache: Map<number, Node>;
+    left: Node;
+    right: Node;
     constructor(capacity: number) {
-        this.head = this.tail = null;
-        this.length = 0;
         this.capacity = capacity;
-        this.lookup = new Map<number, Node>();
-        this.reverseLookup = new Map<Node, number>();
+        this.cache = new Map();
+        this.left = new Node(0,0);
+        this.right = new Node(0,0);
+        this.left.next = this.right;
+        this.right.prev = this.left;
     }
 
     get(key: number): number {
-        // 1. check the cache for existence
-        const node = this.lookup.get(key);
-        if (!node) {
-            // doesn't exist
+        const node = this.cache.get(key);
+
+        if(!node) {
             return -1;
         }
-        // 2. Move the node to the front of the list
-        this.detach(node);
-        this.prepend(node);
 
-        // 3. return the value found
+        this.detach(node);
+        this.append(node);
+
         return node.value;
     }
 
     put(key: number, value: number): void {
-        // Does it exist?
-        let node = this.lookup.get(key);
-        // If it doesn't
-        if (!node) {
-            // we need to insert
-            node = new Node(value);
-            this.length++;
-            this.prepend(node);
-
-            // check capacity and evict if over
-            this.trimCache();
-
-            this.lookup.set(key, node);
-            this.reverseLookup.set(node, key);
-        }
-        // If it does
-        else {
-            // move it to the front of the list
+        if(this.cache.has(key)) {
+            const node = this.cache.get(key);
             this.detach(node);
-            this.prepend(node);
-            // update to the value
-            node.value = value;
+        }
+        const newNode = new Node(key, value);
+        this.cache.set(key, newNode);
+        this.append(newNode);
+
+        if(this.cache.size > this.capacity) {
+            const lru = this.left.next;
+            this.detach(lru);
+            this.cache.delete(lru.key);
         }
     }
 
-    detach(node: Node): void {
-        if (node.prev) {
-            node.prev.next = node.next;
+    detach(node: Node) {
+        const prev = node.prev;
+        const next = node.next;
+        if(prev) {
+            prev.next = next;
         }
-        if (node.next) {
-            node.next.prev = node.prev;
+        if(next) {
+            next.prev = prev;
         }
-        // Edge Cases: Head & Tail
-        if (this.head === node) {
-            this.head = this.head.next;
-        }
-        if (this.tail === node) {
-            this.tail = this.tail.prev;
-        }
-
-        node.next = null;
-        node.prev = null;
     }
 
-    prepend(node: Node): void {
-        // If the list is empty
-        if (!this.head) {
-            this.head = this.tail = node;
-            return;
-        }
-        // Connect node to current head
-        node.next = this.head;
-        this.head.prev = node;
-        // Update head pointer
-        this.head = node;
-    }
+    append(node: Node) {
+        const prev = this.right.prev;
+        const next = this.right;
 
-    trimCache(): void {
-        // If we're under capacity, do nothing
-        if (this.length <= this.capacity) {
-            return;
-        }
+        prev.next = node;
+        next.prev = node;
 
-        const tail = this.tail as Node;
-
-        this.detach(this.tail as Node);
-
-        const key = this.reverseLookup.get(tail);
-
-        this.lookup.delete(key);
-
-        this.reverseLookup.delete(tail);
-
-        this.length--;
+        node.prev = prev;
+        node.next = next;
     }
 }
 
