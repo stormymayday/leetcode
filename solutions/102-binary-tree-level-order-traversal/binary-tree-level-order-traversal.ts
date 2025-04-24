d2b8e62555c0a383078d365c982de9e1c07167c4
Aidar <stormymayday7@gmail.com>
2025-04-24 11:37:58 -0700

    Time: 5 ms (12.93%) | Memory: 60.9 MB (39.67%) - LeetSync

---
+++ solutions/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.ts
@@ -0,0 +1,119 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+class Node<T> {
+    value: T;
+    next: Node<T> | null;
+    constructor(value: T) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class CustomQueue<T> {
+    first: Node<T> | null;
+    last: Node<T> | null;
+    length: number;
+
+    constructor() {
+        this.first = null;
+        this.last = null;
+        this.length = 0;
+    }
+
+    enqueue(value: T): CustomQueue<T> {
+        const newNode = new Node(value);
+
+        // check if queue is empty
+        if (this.length === 0) {
+            // queue is empty
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            // queue is not empty
+            this.last!.next = newNode;
+            this.last = newNode;
+        }
+        this.length++;
+        return this;
+    }
+
+    dequeue(): T | undefined {
+        if (this.length === 0) {
+            return undefined;
+        }
+
+        const temp = this.first;
+        this.first = this.first!.next;
+        temp!.next = null;
+        this.length--;
+        if (this.length === 0) {
+            this.last = null;
+        }
+        return temp!.value;
+    }
+}
+
+function levelOrder(root: TreeNode | null): number[][] {
+
+    // Edge Case: Empty Tree
+    if(!root) {
+        return [];
+    }
+
+    // Initialize queue and results array
+    const queue = new CustomQueue();
+    const results = [];
+
+    // Set a pointer at the root
+    let current = root;
+
+    // Enqueue the root
+    queue.enqueue(current);
+
+    // Iterate while queue is not empty
+    while(queue.length !== 0) {
+
+        // to hold values at each level
+        let level = [];
+
+        // counter variable
+        let currentLevelLength = queue.length;
+
+        for(let i = currentLevelLength; i > 0; i--) {
+
+            // dequeue the node
+            current = queue.dequeue() as TreeNode;
+
+            // push the value int current LEVEL array
+            level.push(current.val);
+
+            // check left
+            if(current.left) {
+                queue.enqueue(current.left);
+            }
+
+            // check right
+            if(current.right) {
+                queue.enqueue(current.right);
+            }
+
+        }
+
+        results.push(level);
+
+    }
+
+    return results;
+};
