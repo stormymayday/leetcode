49bb2a9add59cf2c61523a5c117939a36cac6c26
Aidar <stormymayday7@gmail.com>
2025-04-28 09:16:36 -0700

    Time: 0 ms (100.00%) | Memory: 65.6 MB (70.04%) - LeetSync

---
+++ solutions/450-delete-node-in-a-bst/delete-node-in-a-bst.ts
@@ -0,0 +1,49 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function helper(val, currentNode) {
+
+    if(currentNode === null) {
+        return null;
+    }
+
+    if(val < currentNode.val) {
+        currentNode.left = helper(val, currentNode.left);
+    } else if(val > currentNode.val) {
+        currentNode.right = helper(val, currentNode.right);
+    } else {
+        if(currentNode.left === null && currentNode.right === null) {
+            return null;
+        } else if(currentNode.left === null) {
+            currentNode = currentNode.right;
+        } else if(currentNode.right === null) {
+            currentNode = currentNode.left;
+        } else {
+            const subTreeMin = minval(currentNode.right);
+            currentNode.val = subTreeMin;
+            currentNode.right = helper(subTreeMin, currentNode.right);
+        }
+    }
+
+    return currentNode;
+}
+function minval(currentNode) {
+    while(currentNode.left !== null) {
+        currentNode = currentNode.left;
+    }
+    return currentNode.val;
+}
+function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
+    root = helper(key, root);
+    return root;
+};
