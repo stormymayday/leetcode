25275318f042cde2a9f285bb86a2d70a37248392
Aidar <stormymayday7@gmail.com>
2025-05-01 14:55:15 -0700

    Time: 0 ms (100.00%) | Memory: 55.4 MB (74.05%) - LeetSync

--- solutions/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.ts
+++ solutions/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.ts
@@ -13,28 +13,15 @@
  */
 
 function preorderTraversal(root: TreeNode | null): number[] {
-
-    // Edge Case: Empty Root
+    
+    // Base Case
     if(!root) {
         return [];
     }
 
-    const result = [];
-    const stack = [root];
+    const leftValues = preorderTraversal(root.left);
+    const rightValues = preorderTraversal(root.right);
 
-    while(stack.length > 0) {
-        const currentNode = stack.pop();
-        result.push(currentNode.val);
-       
-        if(currentNode.right) {
-            stack.push(currentNode.right);
-        }
-         // We push 'left' last to 'pop' it first
-        if(currentNode.left) {
-            stack.push(currentNode.left);
-        }
-    }
+    return [root.val, ...leftValues, ...rightValues];
 
-    return result;
-    
 };
