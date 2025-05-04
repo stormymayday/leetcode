8cf6d7b39ffe9abe0542735a925018f02baedbc8
Aidar <stormymayday7@gmail.com>
2025-05-04 13:55:49 -0700

    Time: 1 ms (66.38%) | Memory: 60.3 MB (51.88%) - LeetSync

--- solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
+++ solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
@@ -14,37 +14,47 @@
 
 function isValidBST(root: TreeNode | null): boolean {
 
-    // Edge Case 1: empty tree is valid
-    if(!root) {
+    const result: number[] = [];
+
+    // Fill in the array using In Order Traversal
+    inOrderTraversal(root, result);
+
+    // Check if array is sorted or not
+    return isSorted(result);
+    
+};
+
+function inOrderTraversal(root: TreeNode | null, arr: number[]):void {
+    // Base Case
+    if(root === null) {
+        return;
+    }
+
+    // 1. Recurse Left
+    inOrderTraversal(root.left, arr);
+
+    // 2. Visit Node
+    arr.push(root.val);
+
+    // 3. Recurse Right
+    inOrderTraversal(root.right, arr);
+
+}
+
+function isSorted(arr: number[]):boolean {
+    // If array is empty or has only 1 element
+    if(arr.length < 2) {
+        // consider it sorted
         return true;
     }
 
-    // Get the tree values into an array using DFS InOrder traversal
-    const result = [];
-    function DFSInOrder(root) {
-        // 1. Traverse Left
-        if(root.left) {
-            DFSInOrder(root.left);
-        }
-        // 2. Visit Node
-        result.push(root.val);
-        // 3. Traverse Right
-        if(root.right) {
-            DFSInOrder(root.right);
-        }
-    }
-    DFSInOrder(root);
-
-    // Edge Case 2: only one node is valid
-    if(result.length < 2) {
-        return true;
-    }
-
-    for(let i = 1; i < result.length; i++) {
-        if(result[i] <= result[i - 1]) {
+    // Starting from index 1
+    for(let i = 1; i < arr.length; i++) {
+        if(arr[i] <= arr[i - 1]) {
+            // False if greater or equal
             return false;
         }
     }
+
     return true;
-    
-};
+}
