63d16204335d749998313186893867f48ce562fc
Aidar <stormymayday7@gmail.com>
2025-05-14 11:37:27 -0700

    Time: 3 ms (16.47%) | Memory: 60.2 MB (57.80%) - LeetSync

--- solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
+++ solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
@@ -14,47 +14,31 @@
 
 function isValidBST(root: TreeNode | null): boolean {
 
-    const result: number[] = [];
-
-    // Fill in the array using In Order Traversal
-    inOrderTraversal(root, result);
-
-    // Check if array is sorted or not
-    return isSorted(result);
-    
-};
-
-function inOrderTraversal(root: TreeNode | null, arr: number[]):void {
-    // Base Case
-    if(root === null) {
-        return;
-    }
-
-    // 1. Recurse Left
-    inOrderTraversal(root.left, arr);
-
-    // 2. Visit Node
-    arr.push(root.val);
-
-    // 3. Recurse Right
-    inOrderTraversal(root.right, arr);
-
-}
-
-function isSorted(arr: number[]):boolean {
-    // If array is empty or has only 1 element
-    if(arr.length < 2) {
-        // consider it sorted
+    if(!root) {
         return true;
     }
 
-    // Starting from index 1
-    for(let i = 1; i < arr.length; i++) {
-        if(arr[i] <= arr[i - 1]) {
-            // False if greater or equal
+    const numbers = [];
+    // traverse and fill in the numbers array
+    function traverseInOrder(root) {
+        if(root === null) {
+            return;
+        }
+        traverseInOrder(root.left);
+        numbers.push(root.val);
+        traverseInOrder(root.right);
+    }
+    traverseInOrder(root); // kick off the recursion
+
+    if(numbers.length < 2) {
+        return true;
+    }
+
+    for(let i = 1; i < numbers.length; i++) {
+        if(numbers[i] <= numbers[i - 1]) {
             return false;
         }
     }
-
     return true;
-}
+    
+};
