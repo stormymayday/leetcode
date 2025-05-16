406429447b2b2e252670adc4d00bc2ae84f73f5a
Aidar <stormymayday7@gmail.com>
2025-05-16 13:50:48 -0700

    Time: 1 ms (81.55%) | Memory: 56.5 MB (74.93%) - LeetSync

--- solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,27 +1,27 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
 
-    // Create hashMap out of the first array
-    const hashMap1 = {};
-    for(const num of nums1) {
-        if(!(num in hashMap1)) {
-            hashMap1[num] = true;
+    // Create a hash set out of one of the arrays
+    const hashSet = new Set(nums1);
+
+    const result = [];
+    // Iterate over the second array
+    for(let i = 0; i < nums2.length; i++) {
+
+        const currentNum = nums2[i];
+
+        // check if currentNum is in the set
+        if(hashSet.has(currentNum)) {
+
+            // If it is, add it to the result
+            result.push(currentNum);
+
+            // delete it from the set
+            hashSet.delete(currentNum);
+
         }
+
     }
 
-    // Create hashMap out of the second array
-    const hashMap2 = {};
-    for(const num of nums2) {
-        if(!(num in hashMap2)) {
-            hashMap2[num] = true;
-        }
-    }
-    
-    // Iterate over either map and check if key exists in the other map
-    const result = [];
-    for(const key in hashMap1) {
-        if(key in hashMap2) {
-            result.push(parseInt(key));
-        }
-    }
     return result;
+    
 };
