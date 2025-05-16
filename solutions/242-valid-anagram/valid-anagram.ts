91a6bf19003b10fd553d6dadb9d38014c676635c
Aidar <stormymayday7@gmail.com>
2025-05-15 21:29:02 -0700

    Time: 13 ms (61.92%) | Memory: 61.8 MB (16.04%) - LeetSync

--- solutions/242-valid-anagram/valid-anagram.ts
+++ solutions/242-valid-anagram/valid-anagram.ts
@@ -1,28 +1,33 @@
-function buildFrequencyMap(str: string):Map<string, number> {
-    const map = new Map();
-    for(let i = 0; i < str.length; i++) {
-        const char = str[i];
-        map.set(char, (map.get(char) || 0) + 1);
-    }
-    return map;
-}
 function isAnagram(s: string, t: string): boolean {
-    // Edge Case: different length
+
     if(s.length !== t.length) {
         return false;
     }
 
-    const freqMap1 = buildFrequencyMap(s);
-
-    for(const char of t) {
-        if(!freqMap1.has(char)) {
-            return false;
+    const freqMap1 = {};
+    for(const char of s) {
+        if(freqMap1[char] === undefined) {
+            freqMap1[char] = 1;
         } else {
-            freqMap1.set(char, freqMap1.get(char) - 1);
-            if(freqMap1.get(char) === 0) {
-                freqMap1.delete(char);
-            }
+            freqMap1[char]++;
+        }
+    }
+
+    const freqMap2 = {};
+    for(const char of t) {
+        if(freqMap2[char] === undefined) {
+            freqMap2[char] = 1;
+        } else {
+            freqMap2[char]++;
+        }
+    }
+
+    for(const key in freqMap1) {
+        const value = freqMap1[key];
+        if(freqMap2[key] !== value) {
+            return false;
         }
     }
     return true;
+    
 };
