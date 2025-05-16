1c685deddf5d0476b5311d453f5226a6213a0635
Aidar <stormymayday7@gmail.com>
2025-05-16 13:10:31 -0700

    Time: 0 ms (100.00%) | Memory: 57.1 MB (50.79%) - LeetSync

--- solutions/1-two-sum/two-sum.ts
+++ solutions/1-two-sum/two-sum.ts
@@ -1,29 +1,15 @@
 function twoSum(nums: number[], target: number): number[] {
-    // Edge Case: If the array has fewer than 2 elements
-    if (nums.length < 2) {
-        // A valid pair is not possible
-        return [];
-    }
-
-    // Initialize an empty hash map to store values and their indices
+    
     const hashMap = {};
 
-    // Iterate through the array
-    for (let i = 0; i < nums.length; i++) {
-        // Calculate the difference needed to reach the target
+    for(let i = 0; i < nums.length; i++) {
+
         const difference = target - nums[i];
 
-        // Check if the difference is not in the hash map
-        if (hashMap[difference] === undefined) {
-            // If not, store the current value's index in the hash map
-            // Key = current value, Value = current index
-            hashMap[nums[i]] = i;
-        } else {
-            // If the difference exists, return the indices of the current value and the matching pair
+        if(difference in hashMap) {
             return [i, hashMap[difference]];
+        } else {
+            hashMap[nums[i]] = i;
         }
     }
-
-    // Return empty array if no valid pair was found after iterating through the array
-    return [];
 };
