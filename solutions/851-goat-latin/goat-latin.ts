7e38d6e27aba4747de11e36fa35abc83fa416edd
Aidar <stormymayday7@gmail.com>
2025-05-16 23:18:17 -0700

    Time: 2 ms (1.43%) | Memory: 56.3 MB (22.86%) - LeetSync

---
+++ solutions/851-goat-latin/goat-latin.ts
@@ -0,0 +1,38 @@
+function toGoatLatin(sentence: string): string {
+
+    const set = new Set(['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']);
+    
+    const words = sentence.split(" ");
+
+    const result = [];
+
+    for(let i = 0; i < words.length; i++) {
+        const word = words[i].split("");
+
+        let processedWord = [];
+
+        // word starts with a vowel
+        if(set.has(word[0])) {
+            processedWord = [...word];
+            processedWord.push('m');
+            processedWord.push('a');
+        }
+        // word starts with a consonant
+        else {
+            processedWord = [...word.slice(1)];
+            processedWord.push(word[0]);
+            processedWord.push('m');
+            processedWord.push('a');
+        }
+
+        // appending 'a' based on current index
+        for(let j = 0; j <= i; j++) {
+            processedWord.push('a');
+        }
+
+        result.push(processedWord.join(""));
+    }
+
+    return result.join(" ");
+
+};
