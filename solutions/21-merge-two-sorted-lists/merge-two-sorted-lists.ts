c8e0aeb4b02b7155c753b1b459d003d52106030b
Aidar <stormymayday7@gmail.com>
2025-05-19 12:42:25 -0700

    Time: 2 ms (23.38%) | Memory: 58.8 MB (29.71%) - LeetSync

--- solutions/21-merge-two-sorted-lists/merge-two-sorted-lists.ts
+++ solutions/21-merge-two-sorted-lists/merge-two-sorted-lists.ts
@@ -11,32 +11,31 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
+
+    let dummyNode = new ListNode(0);
+    let tail = dummyNode;
+    let current1 = list1;
+    let current2 = list2;
     
-    const dummyNode = new ListNode();
-    let current = dummyNode;
-
-    while(list1 && list2) {
-
-        if(list1.val < list2.val) {
-            current.next = list1;
-            list1 = list1.next;
+    while(current1 && current2) {
+        if(current1.val < current2.val) {
+            tail.next = current1;
+            current1 = current1.next;
         } else {
-            current.next = list2;
-            list2 = list2.next;
+            tail.next = current2;
+            current2 = current2.next;
         }
-
-        current = current.next;
-
+        tail = tail.next;
     }
 
-    if(list1) {
-        current.next = list1;
+    if(current1) {
+        tail.next = current1;
     }
 
-    if(list2) {
-        current.next = list2;
+    if(current2) {
+        tail.next = current2;
     }
 
     return dummyNode.next;
-
+    
 };
