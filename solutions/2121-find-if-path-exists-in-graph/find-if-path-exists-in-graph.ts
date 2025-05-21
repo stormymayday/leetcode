7297c3e8faff0cc597990a9c3a89d9e6483142be
Aidar <stormymayday7@gmail.com>
2025-05-20 22:41:31 -0700

    Time: 483 ms (82.26%) | Memory: 172.3 MB (59.68%) - LeetSync

--- solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
+++ solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
@@ -1,42 +1,47 @@
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
     const graph = buildGraph(edges);
-    return hasPathDFS(graph, source, destination, new Set());
-};
+    return hasPathDFS(graph, source, destination, new Set<number>());
+}
 
-function buildGraph(edges: number[][]) {
-    const aList = {};
-    for(const edge of edges) {
-        const [a, b] = edge;
-        if(!(a in aList)) {
+function buildGraph(edges: number[][]): Record<number, number[]> {
+
+    const aList: Record<number, number[]> = {};
+
+    for (const [a, b] of edges) {
+        if (!(a in aList)) {
             aList[a] = [];
         }
-        if(!(b in aList)) {
+        if (!(b in aList)) {
             aList[b] = [];
         }
         aList[a].push(b);
         aList[b].push(a);
     }
+
     return aList;
 }
 
-function hasPathDFS(graph, src, dst, visited):boolean {
-    const stack = [src];
-    while(stack.length > 0) {
-        const current = stack.pop();
+function hasPathDFS(
+    graph: Record<number, number[]>,
+    src: number,
+    dst: number,
+    visited: Set<number>
+): boolean {
+    
+    const stack: number[] = [src];
 
-        if(current === dst) {
-            return true;
-        }
+    while (stack.length > 0) {
+        const current = stack.pop()!;
+        
+        if (current === dst) return true;
+        if (visited.has(current)) continue;
 
-        if(visited.has(current)) {
-            continue;
-        } else {
-            visited.add(current);
-        }
+        visited.add(current);
 
-        for(const neighbor of graph[current]) {
+        for (const neighbor of graph[current]) {
             stack.push(neighbor);
         }
     }
+
     return false;
 }
