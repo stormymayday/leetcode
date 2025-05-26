bddb480d638d454eb90111379fd487a794676ec8
Aidar <stormymayday7@gmail.com>
2025-05-25 17:21:00 -0700

    Time: 17 ms (21.65%) | Memory: 62.8 MB (41.46%) - LeetSync

--- solutions/695-max-area-of-island/max-area-of-island.ts
+++ solutions/695-max-area-of-island/max-area-of-island.ts
@@ -1,36 +1,35 @@
 function maxAreaOfIsland(grid: number[][]): number {
-    let maxIsland = 0;
+    let max = -Infinity;
     const visited = new Set();
     for(let r = 0; r < grid.length; r++) {
         for(let c = 0; c < grid[0].length; c++) {
-            maxIsland = Math.max(maxIsland, matrixDFS(grid, r, c, visited));
+            max = Math.max(max, matrixDFS(grid, r, c, visited));
         }
     }
-    return maxIsland;
+    return max;
 };
 
 function matrixDFS(grid, r, c, visited) {
-    // Base Case 1: out of bounds
+    // Base Case 1: Out of bounds
     const rowInBounds = 0 <= r && r < grid.length;
     const colInBounds = 0 <= c && c < grid[0].length;
     if(!rowInBounds || !colInBounds) {
         return 0;
     }
 
-    // Base Case 2: visited
-    const position = `$${r},${c}`;
+    // Base Case 2: Visited
+    const position = `${r},${c}`;
     if(visited.has(position)) {
         return 0;
     } else {
         visited.add(position);
     }
 
-    // Base Case 3: water
+    // Base Case 3: Water
     if(grid[r][c] === 0) {
         return 0;
     }
 
-    // Explore
     let size = 1;
     size += matrixDFS(grid, r - 1, c, visited);
     size += matrixDFS(grid, r + 1, c, visited);
