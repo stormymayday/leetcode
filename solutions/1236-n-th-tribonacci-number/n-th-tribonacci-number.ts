b0338417f198d51001dd84835b448228ad1a792b
Aidar <stormymayday7@gmail.com>
2025-05-26 23:00:58 -0700

    Time: 0 ms (100.00%) | Memory: 53.8 MB (89.55%) - LeetSync

---
+++ solutions/1236-n-th-tribonacci-number/n-th-tribonacci-number.ts
@@ -0,0 +1,15 @@
+function tribonacci(n: number, memo: Record<number, number> = {}): number {
+    if(n in memo) {
+        return memo[n];
+    }
+
+    if(n === 0) {
+        return 0;
+    }
+    if(n == 1 || n === 2) {
+        return 1;
+    }
+    
+    memo[n] = tribonacci(n - 1, memo) + tribonacci(n - 2, memo) + tribonacci(n - 3, memo);
+    return memo[n];
+};
