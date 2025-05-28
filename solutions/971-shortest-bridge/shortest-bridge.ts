af3c3fd37ffdb139dc4cec7aaafddccecaeb48ff
Aidar <stormymayday7@gmail.com>
2025-05-28 11:23:43 -0700

    Time: 91 ms (7.69%) | Memory: 64.4 MB (82.69%) - LeetSync

--- solutions/971-shortest-bridge/shortest-bridge.ts
+++ solutions/971-shortest-bridge/shortest-bridge.ts
@@ -1,9 +1,8 @@
 function shortestBridge(grid: number[][]): number {
-    // Step 1: find the first island and record it's coordinates
+    // Step 1: Find the first island
     let firstIsland = null;
     outer: for(let r = 0; r < grid.length; r++) {
         for(let c = 0; c < grid[0].length; c++) {
-            // matrixDFS
             if(grid[r][c] === 1) {
                 firstIsland = matrixDFS(grid, r, c, new Set());
                 break outer;
@@ -11,19 +10,19 @@ function shortestBridge(grid: number[][]): number {
         }
     }
 
-    // Step 2: BFS for the second island, count and return the distance
+    // Step 2: BFS towards the second island
     return matrixBFS(grid, firstIsland) - 1;
-    };
+};
 
-    function isInBounds(grid, r, c) {
+function isInBounds(grid, r, c) {
     const rowInBounds = 0 <= r && r < grid.length;
     const colInBounds = 0 <= c && c < grid[0].length;
     return rowInBounds && colInBounds;
-    }
+}
 
-    function matrixDFS(grid, r, c, visited) {
+function matrixDFS(grid, r, c, visited) {
     // Base Case 1: Out of bounds
-    if(!isInBounds(grid, r, c)) {
+    if(isInBounds(grid, r, c) === false) {
         return visited;
     }
 
@@ -38,55 +37,50 @@ function shortestBridge(grid: number[][]): number {
         return visited;
     }
 
-    // Record current position
+    // Recursive Step
     visited.add(position);
 
-    // Explore Up, Down, Left, Right
+    // Explore four directions
     matrixDFS(grid, r - 1, c, visited);
     matrixDFS(grid, r + 1, c, visited);
     matrixDFS(grid, r, c - 1, visited);
     matrixDFS(grid, r, c + 1, visited);
 
-    // Return 'visited' set
+    // return the set of island positions
     return visited;
-};
+}
 
 function matrixBFS(grid, firstIslandPositions) {
 
-  const queue = [];
-  const visited = new Set(firstIslandPositions);
+    const visited = new Set(firstIslandPositions);
+    const queue = [];
 
-  // Enqueueing First Island coordinates
-  for(const position of firstIslandPositions) {
-    const[r, c] = position.split(',').map(Number);
-    queue.push({row: r, col: c, distance: 0});
-  }
-
-  // BFS Logic
-  while(queue.length > 0) {
-
-    const {row, col, distance} = queue.shift();
-    
-    // Check if coordinates point to a 'Land' and not in the set
-    const position = `${row},${col}`;
-    if(grid[row][col] === 1 && !firstIslandPositions.has(position)) {
-      return distance;
+    for(const position of firstIslandPositions) {
+        const [row, col] = position.split(',').map(Number);
+        queue.push({row: row, col: col, distance: 0});
     }
 
-     // Explore 4 directions (Up, Down, Left, Right)
-    const deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    for(const delta of deltas) {
-      const [rowDelta, colDelta] = delta;
-      const neighborRow = row + rowDelta;
-      const neighborCol = col + colDelta;
-      const position = `${neighborRow},${neighborCol}`;
-      if(
-        isInBounds(grid, neighborRow, neighborCol) 
-        && !visited.has(position)
-      ) {
-        visited.add(position);
-        queue.push({row: neighborRow, col: neighborCol, distance: distance + 1});
-      } 
+    while(queue.length > 0) {
+
+        const {row, col, distance} = queue.shift();
+        const currentPosition = `${row},${col}`;
+        if(grid[row][col] === 1 && !firstIslandPositions.has(currentPosition)) {
+            return distance;
+        }
+
+        const deltas = [[-1, 0],[1, 0], [0, -1], [0, 1]];
+        for(const delta of deltas) {
+            const [rowDelta, colDelta] = delta;
+            const neighborRow = row + rowDelta;
+            const neighborCol = col + colDelta;
+            const neighborPosition = `${neighborRow},${neighborCol}`;
+            if(isInBounds(grid, neighborRow, neighborCol) && !visited.has(neighborPosition)) {
+                visited.add(neighborPosition);
+                queue.push({row: neighborRow, col: neighborCol, distance: distance + 1});
+            }
+        }
+
     }
-  }
+
+    // second island is guaranteed, thus, late return is required
 }
