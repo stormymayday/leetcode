3a80b99a10df41e2be3b9aabcd0b5c83fa9b902c
Aidar <stormymayday7@gmail.com>
2025-05-28 11:31:40 -0700

    Time: 10 ms (85.67%) | Memory: 63.2 MB (71.35%) - LeetSync

--- solutions/207-course-schedule/course-schedule.ts
+++ solutions/207-course-schedule/course-schedule.ts
@@ -2,7 +2,7 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const graph = buildGraph(numCourses, prerequisites);
     const visiting = new Set();
     const visited = new Set();
-    for(const node of graph.keys()) {
+    for(const node in graph) {
         if(hasCycle(graph, node, visiting, visited) === true) {
             return false;
         }
@@ -11,13 +11,13 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
 };
 
 function buildGraph(n, edges) {
-    const adjList = new Map();
+    const adjList = {};
     for(let i = 0; i < n; i++) {
-        adjList.set(i, []);
+        adjList[i] = [];
     }
     for(const edge of edges) {
-        const [a,b ] = edge;
-        adjList.get(a).push(b);
+        const [a, b] = edge;
+        adjList[a].push(b);
     }
     return adjList;
 }
@@ -29,13 +29,17 @@ function hasCycle(graph, node, visiting, visited) {
     if(visiting.has(node)) {
         return true;
     }
+    
     visiting.add(node);
-    for(const neighbor of graph.get(node)) {
+
+    for(const neighbor of graph[node]) {
         if(hasCycle(graph, neighbor, visiting, visited) === true) {
             return true;
         }
     }
+
     visiting.delete(node);
     visited.add(node);
+
     return false;
 }
