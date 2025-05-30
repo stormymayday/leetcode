5ab730512af8c208da1d421ce63de7a366594a98
Aidar <stormymayday7@gmail.com>
2025-05-29 21:26:08 -0700

    Time: 122 ms (24.13%) | Memory: 64 MB (24.25%) - LeetSync

---
+++ solutions/322-coin-change/coin-change.ts
@@ -0,0 +1,35 @@
+function coinChange(coins: number[], amount: number): number {
+    
+    const result = helper(amount, coins);
+
+    if(result === Infinity) {
+        return -1;
+    } else {
+        return result;
+    }
+
+};
+
+function helper(amount: number, coins: number[], memo: Record<number, number> = {}) {
+    if(amount in memo) {
+        return memo[amount];
+    }
+
+    if(amount === 0) {
+        return 0;
+    }
+
+    if(amount < 0) {
+        return Infinity;
+    }
+
+    let minCoins = Infinity;
+
+    for(const coin of coins) {
+        const numCoins = 1 + helper(amount - coin, coins, memo);
+        minCoins = Math.min(minCoins, numCoins);
+    }
+
+    memo[amount] = minCoins;
+    return minCoins;
+}
