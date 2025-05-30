b8e6f9fd810bbbdb098da65b9190eb1c8ba064b3
Aidar <stormymayday7@gmail.com>
2025-05-29 21:57:05 -0700

    Time: 7 ms (6.21%) | Memory: 56.9 MB (18.46%) - LeetSync

--- solutions/62-unique-paths/unique-paths.ts
+++ solutions/62-unique-paths/unique-paths.ts
@@ -1,25 +1,24 @@
-function uniquePaths(m: number, n: number, memo: Record<string, number> = {}): number {
 
+function uniquePaths(m: number, n: number, memo: Record<string, number> = {}): number {
     const key1 = `${m},${n}`;
     const key2 = `${n},${m}`;
-
+    
     if(key1 in memo) {
         return memo[key1];
     }
-
     if(key2 in memo) {
         return memo[key2];
     }
-
-    if(m === 1 && n === 1) {
+    
+    if(m === 1 || n === 1) {
         return 1;
     }
     if(m === 0 || n === 0) {
         return 0;
     }
-
-    const paths =  uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo);
+    
+    const paths = uniquePaths(m - 1, n, memo) + uniquePaths(m, n - 1, memo);
     memo[key1] = paths;
     memo[key2] = paths;
     return paths;
-};
+}
