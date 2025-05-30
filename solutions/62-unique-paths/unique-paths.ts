8077d4c983d42b5e45d086823ec5ae5444a96884
Aidar <stormymayday7@gmail.com>
2025-05-30 11:17:17 -0700

    Time: 3 ms (18.96%) | Memory: 57.6 MB (14.26%) - LeetSync

--- solutions/62-unique-paths/unique-paths.ts
+++ solutions/62-unique-paths/unique-paths.ts
@@ -11,7 +11,7 @@ function uniquePaths(m: number, n: number, memo:Record<string, number> = {}): nu
         return memo[key2];
     }
 
-    if(m === 1 && n === 1) {
+    if(m === 1 || n === 1) {
         return 1;
     }
     if(m === 0 || n === 0) {
