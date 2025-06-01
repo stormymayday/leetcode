3714b2e876342ccdfc16c0670f79b53cd26cd40a
Aidar <stormymayday7@gmail.com>
2025-06-01 00:07:48 -0700

    Time: 1817 ms (5.02%) | Memory: 64.5 MB (9.80%) - LeetSync

---
+++ solutions/55-jump-game/jump-game.ts
@@ -0,0 +1,25 @@
+function canJump(nums: number[], i: number = 0, memo: Record<number, boolean> = {}): boolean {
+    
+    // Base Case: memo fetching
+    if(i in memo) {
+        return memo[i];
+    }
+
+    // Base Case: Reaching or passing the last index
+    if(i >= nums.length - 1) {
+        return true;
+    }
+
+    const maxSteps = nums[i];
+    for(let step = 1; step <= maxSteps; step += 1) {
+        if(canJump(nums, i + step, memo) === true) {
+            memo[i] = true;
+            return true;
+        }
+    }
+
+    // Caching
+    memo[i] = false;
+    
+    return false;
+};
