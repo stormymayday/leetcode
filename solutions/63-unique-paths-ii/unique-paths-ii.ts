ac4da48f30dc5750909179b3fcc3a44cc4bc2518
Aidar <stormymayday7@gmail.com>
2025-06-02 16:08:21 -0700

    Time: 3 ms (22.19%) | Memory: 57.9 MB (13.75%) - LeetSync

--- solutions/63-unique-paths-ii/unique-paths-ii.ts
+++ solutions/63-unique-paths-ii/unique-paths-ii.ts
@@ -1,25 +1,23 @@
-function uniquePathsWithObstacles(grid: number[][], r: number = 0, c: number = 0, memo: Record<string, number> = {}): number {
-    const position = `${r},${c}`;
+function uniquePathsWithObstacles(grid: number[][], r:number = 0, c:number = 0, memo: Record<string, number> = {}): number {
+    const key = `${r},${c}`;
 
-    // Base Case 1: Memo fetching
-    if(position in memo) {
-        return memo[position];
+    if(key in memo) {
+        return memo[key];
     }
 
-    // Base Case 2: Out of bounds or walls
-    if(r === grid.length
-        || c === grid[0].length
-        || grid[r][c] === 1
+    if(
+        r === grid.length ||
+        c === grid[0].length ||
+        grid[r][c] === 1
     ) {
         return 0;
     }
 
-    // Base Case 3: Finish
-    if(r === grid.length -1 && c === grid[0].length - 1) {
+    if(r === grid.length - 1 && c === grid[0].length - 1) {
         return 1;
     }
 
-    // Recursive Step
-    memo[position] = uniquePathsWithObstacles(grid, r + 1, c, memo) + uniquePathsWithObstacles(grid, r, c + 1, memo);
-    return memo[position];
+    memo[key] = uniquePathsWithObstacles(grid, r + 1, c, memo) + uniquePathsWithObstacles(grid, r, c + 1, memo);
+    
+    return memo[key];
 };
