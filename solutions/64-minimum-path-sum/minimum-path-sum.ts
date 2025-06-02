85b361350209fa07ef84f1f74ba6788bb4534e71
Aidar <stormymayday7@gmail.com>
2025-06-02 16:19:13 -0700

    Time: 45 ms (10.91%) | Memory: 68.1 MB (13.27%) - LeetSync

--- solutions/64-minimum-path-sum/minimum-path-sum.ts
+++ solutions/64-minimum-path-sum/minimum-path-sum.ts
@@ -1,25 +1,20 @@
 function minPathSum(grid: number[][], r: number = 0, c: number = 0, memo: Record<string, number> = {}): number {
-    const position = `${r},${c}`;
+    const key = `${r},${c}`;
 
-    // Base Case: memo fetching
-    if(position in memo) {
-        return memo[position];
+    if(key in memo) {
+        return memo[key];
     }
 
-    // Base Case: out of bounds
-    if(r === grid.length || c === grid[0].length) {
+    if(r === grid.length || c === grid[0].length)  {
         return Infinity;
     }
 
-    // Base Case: finish
     if(r === grid.length - 1 && c === grid[0].length - 1) {
         return grid[r][c];
     }
 
-    // Recursive Step
     const down = minPathSum(grid, r + 1, c, memo);
     const right = minPathSum(grid, r, c + 1, memo);
-
-    memo[position] = grid[r][c] + Math.min(down, right);
-    return memo[position];
+    memo[key] = grid[r][c] + Math.min(down, right);
+    return memo[key];
 };
