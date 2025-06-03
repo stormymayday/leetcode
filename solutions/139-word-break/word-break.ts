2c54b3f3e7f926f1af5779926a17f429e4620aa9
Aidar <stormymayday7@gmail.com>
2025-06-03 00:18:04 -0700

    Time: 2 ms (90.22%) | Memory: 54.9 MB (99.37%) - LeetSync

--- solutions/139-word-break/word-break.ts
+++ solutions/139-word-break/word-break.ts
@@ -1,26 +1,21 @@
-function wordBreak(s: string, wordDict: string[], i:number = 0, memo: Record<string, boolean> = {}): boolean {
-    // Base Case: memo fetching
+function wordBreak(s: string, words: string[], i: number = 0, memo: Record<number, boolean> = {}): boolean {
     if(i in memo) {
         return memo[i];
     }
-    
-    // Base Case: Empty String
+
     if(i === s.length) {
         return true;
     }
 
-    // Recursive Step
-    for(const word of wordDict) {
-        // Check if the word (at the current index i) is a prefix of s
-        if(s.startsWith(word, i) === true) {
-            // Make a recursive call shifting the index past the word length
-            if(wordBreak(s, wordDict, i + word.length, memo) === true) {
+    for(const word of words) {
+        // check if word is a perfix at current index
+        if(s.startsWith(word, i)) {
+            if(wordBreak(s, words, i + word.length, memo) === true) {
                 memo[i] = true;
                 return true;
             }
         }
     }
-
     memo[i] = false;
     return false;
 };
