07d7842aa942d6cdeaf9855d246330d331841188
Aidar <stormymayday7@gmail.com>
2025-06-03 09:47:52 -0700

    Time: 45 ms (10.91%) | Memory: 67.9 MB (14.45%) - LeetSync

--- solutions/64-minimum-path-sum/minimum-path-sum.ts
+++ solutions/64-minimum-path-sum/minimum-path-sum.ts
@@ -1,11 +1,11 @@
-function minPathSum(grid: number[][], r: number = 0, c: number = 0, memo: Record<string, number> = {}): number {
+function minPathSum(grid: number[][], r:number = 0, c:number = 0, memo: Record<string, number> = {}): number {
     const key = `${r},${c}`;
 
     if(key in memo) {
         return memo[key];
     }
 
-    if(r === grid.length || c === grid[0].length)  {
+    if(r === grid.length || c === grid[0].length) {
         return Infinity;
     }
 
