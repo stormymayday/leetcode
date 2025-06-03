ffbb8a77347aac6e388220caef085f98f46817c8
Aidar <stormymayday7@gmail.com>
2025-06-03 12:41:17 -0700

    Time: 1928 ms (5.25%) | Memory: 207.9 MB (5.25%) - LeetSync

--- solutions/1250-longest-common-subsequence/longest-common-subsequence.ts
+++ solutions/1250-longest-common-subsequence/longest-common-subsequence.ts
@@ -1,33 +1,23 @@
-function longestCommonSubsequence(text1: string, text2: string, p1: number = 0, p2: number = 0, memo: Record<string, number> = {}): number {
+function longestCommonSubsequence(str1: string, str2: string, p1: number = 0, p2: number = 0, memo: Record<string, number> = {}): number {
     const key = `${p1},${p2}`;
-    
-    // Base Case: memo fetching
+
     if(key in memo) {
         return memo[key];
     }
 
-    // Base Case: one of the strings is empty (pointer reached the end)
-    if(p1 === text1.length || p2 === text2.length) {
+    if(p1 === str1.length || p2 === str2.length) {
         return 0;
     }
 
-    // Recursive Step
-    if(text1[p1] === text2[p2]) {
-        // Characters match!
-        // Make 1 recursive call advancing both pointers
-        // Add 1 to the result (to count the match)
-        memo[key] = 1 + longestCommonSubsequence(text1, text2, p1 + 1, p2 + 1, memo);
+    if(str1[p1] === str2[p2]) {
+        memo[key] = 1 + longestCommonSubsequence(str1, str2, p1 + 1, p2 + 1, memo);
     } else {
-        // Characters don't match!
-        // Make 2 recursive calls, one for each string and take the max
         memo[key] = Math.max(
-            // Left - advance p1 (remove first char of text1)
-            longestCommonSubsequence(text1, text2, p1 + 1, p2, memo),
-            // Right - advance p2 (remove first char of text2)
-            longestCommonSubsequence(text1, text2, p1, p2 + 1, memo)
-        )
+            longestCommonSubsequence(str1, str2, p1 + 1, p2, memo),
+            longestCommonSubsequence(str1, str2, p1, p2 + 1, memo),
+        );
     }
-    
-    return memo[key];
 
+    return memo[key];
+    
 };
