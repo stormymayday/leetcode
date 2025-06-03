85384119cfe751eff804a09f5ba3440df9cd48a5
Aidar <stormymayday7@gmail.com>
2025-06-02 17:40:19 -0700

    Time: 0 ms (100.00%) | Memory: 55.2 MB (60.93%) - LeetSync

--- solutions/198-house-robber/house-robber.ts
+++ solutions/198-house-robber/house-robber.ts
@@ -1,19 +1,15 @@
 function rob(nums: number[], i: number = 0, memo: Record<number, number> = {}): number {
-    // Base Case: memo fetching
     if(i in memo) {
         return memo[i];
     }
 
-    // Base Case: out of bounds
     if(i >= nums.length) {
         return 0;
     }
 
-    // Recursive Step
     const include = nums[i] + rob(nums, i + 2, memo);
     const exclude = rob(nums, i + 1, memo);
 
-    // Caching
     memo[i] = Math.max(include, exclude);
 
     return memo[i];
