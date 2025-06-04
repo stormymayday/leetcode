856d80f3fe944523e6a98dee7748adc899126559
Aidar <stormymayday7@gmail.com>
2025-06-04 12:56:38 -0700

    Time: 13 ms (41.10%) | Memory: 60.4 MB (62.19%) - LeetSync

--- solutions/695-max-area-of-island/max-area-of-island.ts
+++ solutions/695-max-area-of-island/max-area-of-island.ts
@@ -1,35 +1,38 @@
 function maxAreaOfIsland(grid: number[][]): number {
-    let max = 0;
+    let largest = 0;
     const visited = new Set();
-    for(let r = 0; r < grid.length; r++) {
-        for(let c = 0; c < grid[0].length; c++) {
+    for(let r = 0; r < grid.length; r += 1) {
+        for(let c = 0; c < grid[0].length; c += 1) {
             if(grid[r][c] === 1) {
-                max = Math.max(max, matrixDFS(grid, r, c, visited));
+                largest = Math.max(largest, matrixDFS(grid, r, c, visited));
             }
         }
     }
-    return max;
+    return largest;
 };
 
 function matrixDFS(grid, r, c, visited) {
     if(isInBounds(grid, r, c) === false) {
         return 0;
     }
+
     if(grid[r][c] === 0) {
         return 0;
     }
+
     const position = `${r},${c}`;
     if(visited.has(position)) {
         return 0;
-    } else {
-        visited.add(position);
     }
+
+    visited.add(position);
 
     let count = 1;
     count += matrixDFS(grid, r - 1, c, visited);
     count += matrixDFS(grid, r + 1, c, visited);
     count += matrixDFS(grid, r, c - 1, visited);
     count += matrixDFS(grid, r, c + 1, visited);
+
     return count;
 }
 
