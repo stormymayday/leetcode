df47f97564b7d3e1987629f875671e9599c8e282
Aidar <stormymayday7@gmail.com>
2025-06-04 18:28:51 -0700

    Time: 125 ms (6.25%) | Memory: 68.2 MB (6.25%) - LeetSync

--- solutions/971-shortest-bridge/shortest-bridge.ts
+++ solutions/971-shortest-bridge/shortest-bridge.ts
@@ -1,86 +1,82 @@
 function shortestBridge(grid: number[][]): number {
-    // Step 1: Find the first island
     let firstIsland = null;
-    outer: for(let r = 0; r < grid.length; r++) {
-        for(let c = 0; c < grid[0].length; c++) {
+    outer: for(let r = 0; r < grid.length; r += 1) {
+        for(let c = 0; c <= grid[0].length; c += 1) {
             if(grid[r][c] === 1) {
-                firstIsland = matrixDFS(grid, r, c, new Set());
+                firstIsland = dfs(grid, r, c, new Set());
                 break outer;
             }
         }
     }
 
-    // Step 2: BFS towards the second island
-    return matrixBFS(grid, firstIsland) - 1;
+    return bfs(grid, firstIsland);
 };
 
-function isInBounds(grid, r, c) {
-    const rowInBounds = 0 <= r && r < grid.length;
-    const colInBounds = 0 <= c && c < grid[0].length;
-    return rowInBounds && colInBounds;
-}
-
-function matrixDFS(grid, r, c, visited) {
-    // Base Case 1: Out of bounds
+function dfs(grid, r, c, visited) {
     if(isInBounds(grid, r, c) === false) {
         return visited;
     }
 
-    // Base Case 2: Water
     if(grid[r][c] === 0) {
         return visited;
     }
 
-    // Base Case 3: Visited
     const position = `${r},${c}`;
     if(visited.has(position)) {
         return visited;
     }
 
-    // Recursive Step
     visited.add(position);
+    
+    dfs(grid, r - 1, c, visited);
+    dfs(grid, r + 1, c, visited);
+    dfs(grid, r, c - 1, visited);
+    dfs(grid, r, c + 1, visited);
 
-    // Explore four directions
-    matrixDFS(grid, r - 1, c, visited);
-    matrixDFS(grid, r + 1, c, visited);
-    matrixDFS(grid, r, c - 1, visited);
-    matrixDFS(grid, r, c + 1, visited);
-
-    // return the set of island positions
     return visited;
 }
 
-function matrixBFS(grid, firstIslandPositions) {
-
-    const visited = new Set(firstIslandPositions);
+function bfs(grid, firstIsland) {
+    const visited = new Set();
+    visited.add(firstIsland);
     const queue = [];
-
-    for(const position of firstIslandPositions) {
-        const [row, col] = position.split(',').map(Number);
-        queue.push({row: row, col: col, distance: 0});
+    for(const position of firstIsland) {
+        const [r, c] = position.split(',').map(Number);
+        queue.push({row: r, col: c, distance: 0});
     }
-
     while(queue.length > 0) {
 
         const {row, col, distance} = queue.shift();
-        const currentPosition = `${row},${col}`;
-        if(grid[row][col] === 1 && !firstIslandPositions.has(currentPosition)) {
-            return distance;
+        
+        const position = `${row},${col}`;
+        if(grid[row][col] === 1 && !firstIsland.has(position)) {
+            return distance - 1;
         }
 
-        const deltas = [[-1, 0],[1, 0], [0, -1], [0, 1]];
+        const deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]];
         for(const delta of deltas) {
             const [rowDelta, colDelta] = delta;
             const neighborRow = row + rowDelta;
             const neighborCol = col + colDelta;
             const neighborPosition = `${neighborRow},${neighborCol}`;
-            if(isInBounds(grid, neighborRow, neighborCol) && !visited.has(neighborPosition)) {
+            if(
+                isInBounds(grid, neighborRow, neighborCol) === true
+                && !visited.has(neighborPosition)
+            ) {
                 visited.add(neighborPosition);
-                queue.push({row: neighborRow, col: neighborCol, distance: distance + 1});
+                queue.push({
+                    row: neighborRow,
+                    col: neighborCol,
+                    distance: distance + 1
+                });
             }
         }
 
     }
+}
 
-    // second island is guaranteed, thus, late return is required
+function isInBounds(grid, r, c) {
+    const rowInBounds = 0 <= r && r < grid.length;
+    const colInBounds = 0 <= c && c < grid[0].length;
+    return rowInBounds && colInBounds;
 }
