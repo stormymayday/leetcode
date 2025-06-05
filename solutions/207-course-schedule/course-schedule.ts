78ba9ce42d8c9198d279263ea77772336eb1bf27
Aidar <stormymayday7@gmail.com>
2025-06-04 20:56:01 -0700

    Time: 13 ms (72.68%) | Memory: 61.3 MB (94.79%) - LeetSync

--- solutions/207-course-schedule/course-schedule.ts
+++ solutions/207-course-schedule/course-schedule.ts
@@ -2,7 +2,7 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const graph = buildGraph(numCourses, prerequisites);
     const visiting = new Set();
     const visited = new Set();
-    for(const node in graph) {
+    for(const node of graph.keys()) {
         if(hasCycle(graph, node, visiting, visited) === true) {
             return false;
         }
@@ -10,36 +10,32 @@ function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     return true;
 };
 
-function buildGraph(n, edges) {
-    const adjList = {};
-    for(let i = 0; i < n; i++) {
-        adjList[i] = [];
-    }
-    for(const edge of edges) {
-        const [a, b] = edge;
-        adjList[a].push(b);
-    }
-    return adjList;
-}
-
 function hasCycle(graph, node, visiting, visited) {
-    if(visited.has(node)) {
-        return false;
-    }
     if(visiting.has(node)) {
         return true;
     }
-    
+    if(visited.has(node)) {
+        return false;
+    }
     visiting.add(node);
-
-    for(const neighbor of graph[node]) {
+    for(const neighbor of graph.get(node)) {
         if(hasCycle(graph, neighbor, visiting, visited) === true) {
             return true;
         }
     }
-
     visiting.delete(node);
     visited.add(node);
-
     return false;
+}
+
+function buildGraph(n, edges) {
+    const adjList = new Map();
+    for(let i = 0; i < n; i += 1) {
+        adjList.set(i, []);
+    }
+    for(const edge of edges) {
+        const [a, b] = edge;
+        adjList.get(a).push(b);
+    }
+    return adjList;
 }
