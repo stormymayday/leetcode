089b8e4a3af6b07a46b5bf720f809440b8c60b9a
Aidar <stormymayday7@gmail.com>
2025-06-05 18:47:22 -0700

    Time: 0 ms (100.00%) | Memory: 59 MB (44.13%) - LeetSync

--- solutions/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
+++ solutions/104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.ts
@@ -13,11 +13,9 @@
  */
 
 function maxDepth(root: TreeNode | null): number {
-
     if(root === null) {
         return 0;
     }
 
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
-    
 };
