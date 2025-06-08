d6529f6b105b6e77a6c656bac613caf640cb7a95
Aidar <stormymayday7@gmail.com>
2025-06-07 20:39:57 -0700

    Time: 1 ms (85.91%) | Memory: 57 MB (66.84%) - LeetSync

--- solutions/20-valid-parentheses/valid-parentheses.ts
+++ solutions/20-valid-parentheses/valid-parentheses.ts
@@ -1,56 +1,22 @@
 function isValid(s: string): boolean {
-
-    // If the string is empty
-    if (s.length === 0) {
-        // it's valid
-        return true;
-    }
-
-    // If the length is odd
-    if (s.length % 2 !== 0) {
-        // Cannot valid
-        return false;
-    }
-
-    // Mapping opening to closing
-    const parens = new Map([
-        ["(", ")"],
-        ["{", "}"],
-        ["[", "]"],
-    ]);
-
-    // Stack to track opening parentheses
+    const parens = {
+        "{": "}",
+        "(": ")",
+        "[": "]"
+    };
     const stack = [];
-
-    for (let i = 0; i < s.length; i++) {
-
+    for(let i = 0; i < s.length; i += 1) {
         const currentParen = s[i];
-
-        // If it's an opening parenthesis
-        if (parens.has(currentParen)) {
-
-            // push to stack
+        // Opening - push
+        if(currentParen in parens) {
             stack.push(currentParen);
-
         } else {
-
-            // If the stack is empty
-            if (stack.length === 0) {
-                // It's invalid
-                return false;
-            }
-
-            // Remove the last opening paren from the stack
+            // Closing - pop & check
             const poppedParen = stack.pop();
-
-            // Check if the current paren matches the last opening one
-            if (parens.get(poppedParen) !== currentParen) {
+            if(parens[poppedParen] !== currentParen) {
                 return false;
             }
-            
         }
     }
-
-    // stack should be empty at the end (no leftover opening parens)
     return stack.length === 0;
-}
+};
