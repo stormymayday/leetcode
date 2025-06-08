3c171ca83e52fdf33c1ddbd0f3dfe944af269a9a
Aidar <stormymayday7@gmail.com>
2025-06-08 09:14:05 -0700

    Time: 0 ms (100.00%) | Memory: 55.4 MB (41.99%) - LeetSync

--- solutions/394-decode-string/decode-string.ts
+++ solutions/394-decode-string/decode-string.ts
@@ -1,25 +1,32 @@
 function decodeString(s: string): string {
     const stack = [];
     for(let i = 0; i < s.length; i += 1) {
-        const currentChar = s[i];
 
-        if(currentChar !== ']') {
-            stack.push(currentChar);
+        const char = s[i];
+
+        if(char !== ']') {
+            stack.push(char);
         } else {
             let segment = '';
+            // pop until opening paren
             while(stack[stack.length - 1] !== '[') {
                 segment = stack.pop() + segment;
             }
+            // pope opening paren
             stack.pop();
             let digit = '';
+            // pop the digit (until char cannot be converted to a number)
             while(stack.length > 0 && !isNaN(stack[stack.length - 1])) {
                 digit = stack.pop() + digit;
             }
-            const count = parseInt(digit, 10);
-            for(let k = 0; k < count; k += 1) {
+            // convert to 'digit' str a number
+            const count = parseInt(digit);
+            // push 'segment' 'count' number of times
+            for(let n = 0; n < count; n += 1) {
                 stack.push(segment);
             }
         }
+
     }
     return stack.join('');
 };
