095106c7ee0a0c018b8597f6783f5d180a1b493e
Aidar <stormymayday7@gmail.com>
2025-06-09 07:05:46 -0700

    Time: 40 ms (97.01%) | Memory: 82.3 MB (66.42%) - LeetSync

--- solutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.ts
+++ solutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.ts
@@ -4,26 +4,29 @@ class NumMatrix {
         const ROWS = matrix.length;
         const COLS = matrix[0].length;
         this.sumMatrix = [];
-        // creating the sumMatrix
         for(let i = 0; i < ROWS + 1; i += 1) {
             this.sumMatrix.push(new Array(COLS + 1).fill(0));
         }
-        // filling the sumMatrix
+
         for(let r = 0; r < ROWS; r += 1) {
             let prefixSum = 0;
             for(let c = 0; c < COLS; c += 1) {
                 prefixSum += matrix[r][c];
-                const above = this.sumMatrix[r][c + 1];
+                const above = this.sumMatrix[r][c+1];
                 this.sumMatrix[r + 1][c + 1] = prefixSum + above;
             }
         }
     }
 
     sumRegion(row1: number, col1: number, row2: number, col2: number): number {
-        const bottomRight = this.sumMatrix[row2 + 1][col2 + 1];
-        const above = this.sumMatrix[row1][col2 + 1];
-        const left = this.sumMatrix[row2 + 1][col1];
-        const topLeft = this.sumMatrix[row1][col1];
+        row1 += 1;
+        row2 += 1;
+        col1 += 1;
+        col2 += 1;
+        const bottomRight = this.sumMatrix[row2][col2];
+        const above = this.sumMatrix[row1 -1][col2];
+        const left = this.sumMatrix[row2][col1 - 1];
+        const topLeft = this.sumMatrix[row1 - 1][col1 - 1];
         return bottomRight - above - left + topLeft;
     }
 }
