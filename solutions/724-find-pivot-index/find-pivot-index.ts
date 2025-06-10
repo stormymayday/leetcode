15ffc4759dad4dd75671b19999c3020b0d2fd268
Aidar <stormymayday7@gmail.com>
2025-06-10 12:49:12 -0700

    Time: 1 ms (90.12%) | Memory: 56.8 MB (97.06%) - LeetSync

--- solutions/724-find-pivot-index/find-pivot-index.ts
+++ solutions/724-find-pivot-index/find-pivot-index.ts
@@ -1,27 +1,18 @@
 function pivotIndex(nums: number[]): number {
     const n = nums.length;
-    const prefixSums = [];
-    const postfixSums = [];
-
     let total = 0;
     for(let i = 0; i < n; i += 1) {
         total += nums[i];
-        prefixSums.push(total);
     }
 
-    total = 0;
-    for(let i = n - 1; i >= 0; i -= 1) {
-        total += nums[i];
-        postfixSums[i] = total;
-    }
-
+    let leftSum = 0;
     for(let i = 0; i < n; i += 1) {
-        const prefixSum = i > 0 ? prefixSums[i - 1] : 0;
-        const postfixSum = i < n - 1 ? postfixSums[ i + 1] : 0;
-        if(prefixSum === postfixSum) {
+        const rightSum = total - leftSum - nums[i];
+        if(leftSum === rightSum) {
             return i;
         }
+        leftSum += nums[i];
     }
-
+    
     return -1;
 };
