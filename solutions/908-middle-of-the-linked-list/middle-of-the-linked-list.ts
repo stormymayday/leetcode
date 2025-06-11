e030004d19e07bf9006e59b41e6c21c276dbafb1
Aidar <stormymayday7@gmail.com>
2025-06-11 12:41:15 -0700

    Time: 0 ms (100.00%) | Memory: 55 MB (72.02%) - LeetSync

--- solutions/908-middle-of-the-linked-list/middle-of-the-linked-list.ts
+++ solutions/908-middle-of-the-linked-list/middle-of-the-linked-list.ts
@@ -11,19 +11,11 @@
  */
 
 function middleNode(head: ListNode | null): ListNode | null {
-
-    if(!head || !head.next) {
-        return head;
-    }
-
     let slow = head;
     let fast = head;
-
     while(fast && fast.next) {
         slow = slow.next;
         fast = fast.next.next;
     }
-
     return slow;
-    
 };
