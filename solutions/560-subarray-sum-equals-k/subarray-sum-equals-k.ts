20bd2b08d96a302dcda677fff3ee6aadf0d1326a
Aidar <stormymayday7@gmail.com>
2025-06-10 20:22:14 -0700

    Time: 31 ms (35.58%) | Memory: 68.5 MB (9.30%) - LeetSync

--- solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.ts
+++ solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.ts
@@ -1,21 +1,17 @@
 function subarraySum(nums: number[], k: number): number {
     let count = 0;
-    const prefixSums = [];
-    let sum = 0;
-    for(const num of nums) {
-        sum += num;
-        prefixSums.push(sum);
-    }
     const differences = { 0: 1 };
-    for(const sum of prefixSums) {
-        const difference = sum - k;
+    let prefixSum = 0;
+    for(const num of nums) {
+        prefixSum += num;
+        const difference = prefixSum - k;
         if(difference in differences) {
             count += differences[difference];
         }
-        if(sum in differences) {
-            differences[sum] += 1;
+        if(prefixSum in differences) {
+            differences[prefixSum] += 1;
         } else {
-            differences[sum] = 1;
+            differences[prefixSum] = 1;
         }
     }
     return count;
