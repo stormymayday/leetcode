932484b257b224403f6774c6b5cc5479d593c393
Aidar <stormymayday7@gmail.com>
2025-06-12 15:55:42 -0700

    Time: 47 ms (67.32%) | Memory: 57.9 MB (68.87%) - LeetSync

--- solutions/141-linked-list-cycle/linked-list-cycle.ts
+++ solutions/141-linked-list-cycle/linked-list-cycle.ts
@@ -11,21 +11,14 @@
  */
 
 function hasCycle(head: ListNode | null): boolean {
-
     let slow = head;
     let fast = head;
-
     while(fast && fast.next) {
-
         slow = slow.next;
         fast = fast.next.next;
-
         if(slow === fast) {
             return true;
         }
-
     }
-
     return false;
-
 };
