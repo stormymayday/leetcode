ec12e6a0316da56d192f90e1df4f2dc4254ce438
Aidar <stormymayday7@gmail.com>
2025-06-12 22:12:47 -0700

    Time: 49 ms (50.34%) | Memory: 55.6 MB (19.41%) - LeetSync

--- solutions/1013-fibonacci-number/fibonacci-number.ts
+++ solutions/1013-fibonacci-number/fibonacci-number.ts
@@ -1,15 +1,13 @@
 function fib(n: number, memo: Record<number, number> = {}): number {
-    // Base Case: memo fetching
+
     if(n in memo) {
         return memo[n];
     }
 
-    // Base Case: if number is less than 2
     if(n < 2) {
         return n;
     }
 
-    // Recursive Step
-    memo[n] = fib(n - 1, memo) + fib(n - 2, memo); // Caching
+    memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
     return memo[n];
 };
