cf8969a5db964d173bb3f51d98073ee84b0f759f
Aidar <stormymayday7@gmail.com>
2025-06-14 13:37:40 -0700

    Time: 158 ms (31.41%) | Memory: 62.9 MB (28.93%) - LeetSync

--- solutions/279-perfect-squares/perfect-squares.ts
+++ solutions/279-perfect-squares/perfect-squares.ts
@@ -1,4 +1,5 @@
 function numSquares(n: number, memo: Record<number, number> = {}): number {
+
     if(n in memo) {
         return memo[n];
     }
@@ -7,12 +8,11 @@ function numSquares(n: number, memo: Record<number, number> = {}): number {
         return 0;
     }
 
-    let minSquares = Infinity
+    let minSquares = Infinity;
     for(let i = 1; i <= Math.sqrt(n); i += 1) {
         const square = i * i;
         minSquares = Math.min(minSquares, 1 + numSquares(n - square, memo));
     }
-
     memo[n] = minSquares;
     return minSquares;
 };
