51438914575d1e9ac710991851ce99addfaeff28
Aidar <stormymayday7@gmail.com>
2025-06-14 18:46:12 -0700

    Time: 1758 ms (5.23%) | Memory: 208.1 MB (5.23%) - LeetSync

--- solutions/1250-longest-common-subsequence/longest-common-subsequence.ts
+++ solutions/1250-longest-common-subsequence/longest-common-subsequence.ts
@@ -1,23 +1,22 @@
-function longestCommonSubsequence(str1: string, str2: string, p1: number = 0, p2: number = 0, memo: Record<string, number> = {}): number {
-    const key = `${p1},${p2}`;
+function longestCommonSubsequence(text1: string, text2: string, p1: number = 0, p2: number = 0, memo: Record<string, number> = {}): number {
+    
+    const key = `${p1}.${p2}`;
 
     if(key in memo) {
         return memo[key];
     }
 
-    if(p1 === str1.length || p2 === str2.length) {
+    if(p1 === text1.length || p2 === text2.length) {
         return 0;
     }
 
-    if(str1[p1] === str2[p2]) {
-        memo[key] = 1 + longestCommonSubsequence(str1, str2, p1 + 1, p2 + 1, memo);
+    if(text1[p1] === text2[p2]) {
+        memo[key] = 1 + longestCommonSubsequence(text1, text2, p1 + 1, p2 + 1, memo);
     } else {
-        memo[key] = Math.max(
-            longestCommonSubsequence(str1, str2, p1 + 1, p2, memo),
-            longestCommonSubsequence(str1, str2, p1, p2 + 1, memo),
-        );
+        const moveP1 = longestCommonSubsequence(text1, text2, p1 + 1, p2, memo);
+        const moveP2 = longestCommonSubsequence(text1, text2, p1, p2 + 1, memo);
+        memo[key] = Math.max(moveP1, moveP2);
     }
 
     return memo[key];
-    
 };
