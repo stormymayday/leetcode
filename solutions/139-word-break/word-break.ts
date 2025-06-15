4fc96d030eff36d9a6e2674ac7825a39fec1b967
Aidar <stormymayday7@gmail.com>
2025-06-14 20:15:58 -0700

    Time: 3 ms (83.33%) | Memory: 54.9 MB (98.28%) - LeetSync

--- solutions/139-word-break/word-break.ts
+++ solutions/139-word-break/word-break.ts
@@ -1,23 +1,24 @@
-function wordBreak(s: string, words: string[], i: number = 0, memo: Record<number, boolean> = {}): boolean {
-    if(i in memo) {
-        return memo[i];
+function wordBreak(s: string, wordDict: string[], index: number = 0, memo: Record<number, boolean> = {}): boolean {
+    
+    if(index in memo) {
+        return memo[index];
     }
 
-    if(i === s.length) {
+    // Base Case: index reaches the end
+    if(index === s.length) {
         return true;
     }
 
-    for(const word of words) {
-        // check if prefix
-        if(s.startsWith(word, i)) {
-            // Recurse
-            if(wordBreak(s, words, i + word.length, memo) === true) {
-                memo[i] = true;
+    for(const word of wordDict) {
+        // check if word is a prefix
+        if(s.startsWith(word, index)) {
+            if(wordBreak(s, wordDict, index + word.length, memo) === true) {
+                memo[index] = true;
                 return true;
             }
         }
     }
 
-    memo[i] = false;
+    memo[index] = false;
     return false;
 };
