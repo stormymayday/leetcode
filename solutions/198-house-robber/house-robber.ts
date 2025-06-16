7e85d8888f20d21fb9512fcd210fab12d7fe3e8b
Aidar <stormymayday7@gmail.com>
2025-06-15 17:09:32 -0700

    Time: 0 ms (100.00%) | Memory: 53.4 MB (96.99%) - LeetSync

--- solutions/198-house-robber/house-robber.ts
+++ solutions/198-house-robber/house-robber.ts
@@ -1,4 +1,4 @@
-function rob(nums: number[], i:number = 0, memo: Record<number, number> = {}): number {
+function rob(nums: number[], i: number = 0, memo: Record<number, number> = {}): number {
     
     if(i in memo) {
         return memo[i];
@@ -8,7 +8,10 @@ function rob(nums: number[], i:number = 0, memo: Record<number, number> = {}): n
         return 0;
     }
 
-    memo[i] = Math.max((nums[i] + rob(nums, i + 2, memo)), rob(nums, i + 1, memo));
+    const include = nums[i] + rob(nums, i + 2, memo);
+    const exclude = rob(nums, i + 1, memo);
+
+    memo[i] = Math.max(include, exclude);
 
     return memo[i];
 };
