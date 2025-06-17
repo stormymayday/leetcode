d96ecb91795333b769dd7cb9e4d8fe6fc24015aa
Aidar <stormymayday7@gmail.com>
2025-06-17 15:33:57 -0700

    Time: 60 ms (57.46%) | Memory: 65.5 MB (55.92%) - LeetSync

--- solutions/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
+++ solutions/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
@@ -13,61 +13,20 @@
  */
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
-    // Edge Case
-    if(!root || !p || !q) {
+	if(root === null) {
         return null;
     }
 
-    function pathfinder(root: TreeNode, target: TreeNode): TreeNode[] | null {
-        // Base Case 1: target not found
-        if(root === null) {
-            // if target was not found, return 'null'
-            return null;
-        }
-
-        // Base Case 2: target found
-        if(root === target) {
-            // if target was found, return array containing node
-            return [root];
-        }
-
-        // Recurse Left
-        const leftPath = pathfinder(root.left, target);
-        // Recurse Right
-        const rightPath = pathfinder(root.right, target);
-
-        // target was found in the left subtree
-        if(leftPath !== null) {
-            leftPath.push(root);
-            return leftPath;
-        } 
-        // target was found in the right subtree
-        else if(rightPath !== null) {
-            rightPath.push(root);
-            return rightPath;
-        } 
-        // target was not found
-        else {
-            return null;
-        }
+    if(root === p || root === q) {
+        return root;
     }
 
-    const path1: TreeNode[] | null = pathfinder(root, p);
-    const path2: TreeNode[] | null = pathfinder(root, q);
+    const left = lowestCommonAncestor(root.left, p, q);
+    const right = lowestCommonAncestor(root.right, p, q);
 
-    // If target was not found
-    if(path1 === null || path2 === null) {
-        return null;
+    if(left && right) {
+        return root;
+    } else {
+        return left || right;
     }
-
-    // Creating a set from path1 for O(1) lookup
-    const set1 = new Set(path1);
-
-    // Iterating over path2
-    for(const node of path2) {
-        if(set1.has(node)) {
-            return node;
-        }
-    }
-	
 };
