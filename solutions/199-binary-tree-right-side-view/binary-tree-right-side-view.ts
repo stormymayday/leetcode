033fc29a8564a3a44443cce39c97cf185f0b2623
Aidar <stormymayday7@gmail.com>
2025-06-17 17:00:33 -0700

    Time: 0 ms (100.00%) | Memory: 57.5 MB (55.15%) - LeetSync

---
+++ solutions/199-binary-tree-right-side-view/binary-tree-right-side-view.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function rightSideView(root: TreeNode | null): number[] {
+    const result = [];
+    if(root === null) {
+        return result;
+    }
+    const queue = [root];
+    while(queue.length > 0) {
+        const queueLength = queue.length;
+        const level = [];
+        for(let i = 0; i < queueLength; i += 1) {
+            const current = queue.shift();
+            level.push(current.val);
+            if(current.left) {
+                queue.push(current.left);
+            }
+            if(current.right) {
+                queue.push(current.right);
+            }
+        }
+        result.push(level[level.length - 1]);
+    }
+    return result;
+};
