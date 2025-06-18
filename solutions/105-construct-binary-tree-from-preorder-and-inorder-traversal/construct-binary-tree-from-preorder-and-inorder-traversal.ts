c9eaeeaeb0fbd733b78c9b5abe03fbc88982ca44
Aidar <stormymayday7@gmail.com>
2025-06-18 16:57:20 -0700

    Time: 73 ms (22.24%) | Memory: 131.1 MB (31.37%) - LeetSync

--- solutions/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ solutions/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -13,54 +13,23 @@
  */
 
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-
-    // creating a map for faster index lookup
-    const inorderIndexMap = new Map();
-    for(let i = 0; i < inorder.length; i++) {
-        inorderIndexMap.set(inorder[i], i);
+    if(preorder.length === 0) {
+        return null;
     }
-    
-    function helper(
-        inorder,
-        preorder,
-        inorderIndexMap,
-        inorderStart = 0,
-        inorderEnd = inorder.length - 1,
-        preorderStart = 0,
-        preorderEnd = preorder.length - 1
-    ) {
-        // Base Base: start and end indexes have crossed
-        if(inorderStart > inorderEnd) {
-            return null;
-        }
 
-        const value = preorder[preorderStart]; // get the root value
-        const root = new TreeNode(value); // create new root with that value
-        const mid = inorderIndexMap.get(value); // fetch corresponding index from inorder array
-        const leftSize = mid - inorderStart; // number of elements passed to the left subtree
+    const rootVal = preorder[0];
+    const root = new TreeNode(rootVal);
 
-        root.left = helper(
-            inorder,
-            preorder,
-            inorderIndexMap,
-            inorderStart, // inorderStart stays the same
-            mid - 1, // inorderEnd stops 1 shy of mid
-            preorderStart + 1, // preorderStart increments by 1 to skip the root
-            preorderStart + leftSize // preorderEnd 
-        );
+    const mid = inorder.indexOf(rootVal);
 
-        root.right = helper(
-            inorder,
-            preorder,
-            inorderIndexMap,
-            mid + 1, // inorderStart is now 1 past the mid
-            inorderEnd, // inorderEnd stays the same
-            preorderStart + leftSize + 1, // 'right' preorderStart moves 1 spot after the 'left' side
-            preorderEnd // preorderEnd stays the same
-        );
+    const leftInOrder = inorder.slice(0, mid);
+    const rightInOrder = inorder.slice(mid + 1);
 
-        return root;
-    }
-    return helper(inorder, preorder, inorderIndexMap);
+    const leftPreOrder = preorder.slice(1, leftInOrder.length + 1);
+    const rightPreOrder = preorder.slice(leftInOrder.length + 1);
 
+    root.left = buildTree(leftPreOrder, leftInOrder);
+    root.right = buildTree(rightPreOrder, rightInOrder);
+
+    return root;
 };
