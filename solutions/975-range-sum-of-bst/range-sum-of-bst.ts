7c1ce0ec82ff26bee61d7d473eb4c2f6b6687df1
Aidar <stormymayday7@gmail.com>
2025-06-19 11:52:24 -0700

    Time: 1 ms (90.88%) | Memory: 90 MB (5.11%) - LeetSync

--- solutions/975-range-sum-of-bst/range-sum-of-bst.ts
+++ solutions/975-range-sum-of-bst/range-sum-of-bst.ts
@@ -1,35 +1,17 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
 function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
+    if (!root) return 0;
+    
     let sum = 0;
-    if(root === null) {
-        return sum;
+    if (root.val >= low && root.val <= high) {
+        sum += root.val;
     }
-    const queue = [root];
-    while(queue.length > 0) {
-        const current = queue.shift();
-        if(current.val >= low && current.val <= high) {
-            sum += current.val;
-        }
-        if(current.left !== null && current.val >= low) {
-            queue.push(current.left);
-        }
-        if(current.right !== null && current.val <= high) {
-            queue.push(current.right);
-        }
-
+    
+    if (root.val > low) {
+        sum += rangeSumBST(root.left, low, high);
     }
+    if (root.val < high) {
+        sum += rangeSumBST(root.right, low, high);
+    }
+    
     return sum;
-};
+}
