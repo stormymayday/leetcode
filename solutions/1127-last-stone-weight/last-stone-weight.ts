cbc463d6522513bb6e063a91139144392f5d90cb
Aidar <stormymayday7@gmail.com>
2025-06-21 20:14:37 -0700

    Time: 1 ms (90.00%) | Memory: 57.3 MB (44.00%) - LeetSync

---
+++ solutions/1127-last-stone-weight/last-stone-weight.ts
@@ -0,0 +1,19 @@
+function lastStoneWeight(stones: number[]): number {
+    stones.sort((a,b) => a - b);
+    while(true) {
+        if(stones.length > 1) {
+            const x = stones.pop();
+            const y = stones.pop();
+            if(x === y) {
+                continue;
+            } else {
+                stones.push(Math.abs(x - y));
+                stones.sort((a,b) => a - b);
+            }
+        } else if(stones.length === 1) {
+            return stones.pop();
+        } else {
+            return 0;
+        }
+    }
+};
