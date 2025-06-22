fb574b3bd999e9fd73676dfa78ff0618739bbc82
Aidar <stormymayday7@gmail.com>
2025-06-22 16:08:19 -0700

    Time: 30 ms (86.63%) | Memory: 68.6 MB (95.06%) - LeetSync

--- solutions/1014-k-closest-points-to-origin/k-closest-points-to-origin.ts
+++ solutions/1014-k-closest-points-to-origin/k-closest-points-to-origin.ts
@@ -1,6 +1,7 @@
 function kClosest(points: number[][], k: number): number[][] {
     points.sort((a, b) => {
-        return Math.sqrt((a[0]**2 + a[1]**2)) - Math.sqrt((b[0]**2 + b[1]**2));
+        // return Math.sqrt((a[0]**2 + a[1]**2)) - Math.sqrt((b[0]**2 + b[1]**2));
+        return (a[0]**2 + a[1]**2) - (b[0]**2 + b[1]**2);
     });
     return points.slice(0, k);
 };
