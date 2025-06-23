988bd969ac56b0f4a07a6a04c6bb7ffc6a24995a
Aidar <stormymayday7@gmail.com>
2025-06-22 22:19:02 -0700

    Time: 48 ms (46.63%) | Memory: 66.8 MB (34.36%) - LeetSync

---
+++ solutions/621-task-scheduler/task-scheduler.ts
@@ -0,0 +1,106 @@
+class MaxHeap {
+    values: number[];
+    constructor() {
+        this.values = [];
+    }
+    push(val:number):void {
+        this.values.push(val);
+        let current = this.values.length - 1;
+        // sift up
+        while(current > 0) {
+            const parentIndex = Math.floor((current - 1)/2);
+            if(this.values[current] > this.values[parentIndex]) {
+                this.swap(current, parentIndex);
+                current = parentIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    pop(): number | undefined {
+        if(this.values.length === 0) {
+            return undefined;
+        }
+        if(this.values.length === 1) {
+            return this.values.pop();
+        }
+        const max = this.values[0];
+        this.values[0] = this.values.pop();
+        this.siftDown(0);
+        return max;
+    }
+    heapify(nums: number[]):void {
+        this.values = nums;
+        let current = Math.floor((this.values.length - 2)/2);
+        while(current >= 0) {
+            this.siftDown(current);
+            current -= 1;
+        }
+    }
+    siftDown(index: number):void {
+        let current = index;
+        while(current < this.values.length - 1) {
+            const leftChildIndex = 2 * current + 1;
+            const rightChildIndex = 2 * current + 2;
+            const leftChildValue = this.values[leftChildIndex] === undefined ? -Infinity : this.values[leftChildIndex];
+            const rightChildValue = this.values[rightChildIndex] === undefined ? -Infinity : this.values[rightChildIndex];
+            const largerChildIndex = leftChildValue > rightChildValue ? leftChildIndex : rightChildIndex;
+            const largerChildValue = leftChildValue > rightChildValue ? leftChildValue : rightChildValue;
+            if(this.values[current] < largerChildValue) {
+                this.swap(current, largerChildIndex);
+                current = largerChildIndex;
+            } else {
+                break;
+            }
+        }
+    }
+    swap(index1:number, index2:number):void {
+        const temp = this.values[index1];
+        this.values[index1] = this.values[index2];
+        this.values[index2] = temp;
+    }
+    size():number {
+        return this.values.length;
+    }
+}
+function leastInterval(tasks: string[], n: number): number {
+    const freqMap = new Map();
+    for(const task of tasks) {
+        if(!freqMap.has(task)) {
+            freqMap.set(task, 1);
+        } else {
+            freqMap.set(task, freqMap.get(task) + 1);
+        }
+    }
+
+    const frequencies = [];
+    for(const [task, frequency] of freqMap.entries()) {
+        frequencies.push(frequency);
+    }
+
+    const maxHeap = new MaxHeap();
+    maxHeap.heapify(frequencies);
+
+    let time = 0;
+    const queue = []; // [frequency, time + n]
+
+    while(maxHeap.size() > 0 || queue.length > 0) {
+
+        time += 1;
+
+        if(maxHeap.size() > 0) {
+            const frequency = maxHeap.pop() - 1;
+            if(frequency > 0) {
+                queue.push([frequency, time + n]);
+            }
+        }
+
+        if(queue.length > 0 && queue[0][1] === time) {
+            const [frequency, time] = queue.shift();
+            maxHeap.push(frequency);
+        }
+
+    }
+
+    return time;
+};
