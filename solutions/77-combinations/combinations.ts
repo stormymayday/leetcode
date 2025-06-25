c6eb4bfe4bc91c723aaec35fde83cea32ae3fcd2
Aidar <stormymayday7@gmail.com>
2025-06-24 20:22:20 -0700

    Time: 422 ms (5.16%) | Memory: 145.1 MB (18.91%) - LeetSync

--- solutions/77-combinations/combinations.ts
+++ solutions/77-combinations/combinations.ts
@@ -1,28 +1,20 @@
-function combine(n: number, k: number): number[][] {
-    function helper(start: number, remaining: number, k: number) {
-        // Base Case 1: One Combination
-        if(k === 0) {
-            return [[]];
-        }
-
-        // Base Case 1: Zero Combinations
-        if(k > remaining) {
-            return [];
-        }
-
-        // Recursive Call 1: Include current number (start)
-        const partialCombos = helper(start + 1, remaining - 1, k - 1);
-        const combosWithFirst = [];
-        for(const partialCombo of partialCombos) {
-            combosWithFirst.push([start, ...partialCombo]);
-        }
-
-        // Recursive Call 2: Exclude current number (start)
-        const combosWithoutFirst = helper(start + 1, remaining - 1, k);
-
-        // Combine 
-        return [...combosWithFirst, ...combosWithoutFirst];
-
+function combine(n: number, k: number, start: number = 1): number[][] {
+    if(k === 0) {
+        return [[]];
     }
-    return helper(1, n, k);
+    if(k > n) {
+        return [];
+    }
+
+    const first = start;
+    const partialCombos = combine(n - 1, k - 1, start + 1);
+    const combosWithFirst = [];
+    for(const partialCombo of partialCombos) {
+        combosWithFirst.push([first, ...partialCombo]);
+    }
+
+    const combosWithoutFirst = combine(n - 1, k, start + 1);
+
+    return [...combosWithFirst, ...combosWithoutFirst];
+
 };
