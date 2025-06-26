89ade297bc848550375f9867753421b028883635
Aidar <stormymayday7@gmail.com>
2025-06-25 19:11:59 -0700

    Time: 3 ms (51.70%) | Memory: 58.8 MB (98.41%) - LeetSync

--- solutions/46-permutations/permutations.ts
+++ solutions/46-permutations/permutations.ts
@@ -1,28 +1,20 @@
 function permute(nums: number[]): number[][] {
-    // Base case: Input array is empty
     if(nums.length === 0) {
-        // an array containing one empty permutation.
         return [[]];
     }
 
-    // Recursive  Step
-    // Take the first element from the array
     const first = nums[0];
-
-    // Get all permutations of the remaining elements
-    const permsWithoutFirst = permute(nums.slice(1));
-
+    const partialPerms = permute(nums.slice(1));
     const fullPerms = [];
-    // Iterate over the permutations without first
-    for(const perm of permsWithoutFirst) {
-        // Iterate over every INDEX of the permutation
-        for(let i = 0; i <= perm.length; i += 1) {
+    for(const partialPerm of partialPerms) {
+        for(let i = 0; i <= partialPerm.length; i += 1) {
             fullPerms.push([
-                ...perm.slice(0, i), // everything before current index
-                first, // insert first AT the current index
-                ...perm.slice(i) // everything after current index
+                ...partialPerm.slice(0, i),
+                first,
+                ...partialPerm.slice(i)
             ]);
         }
     }
+
     return fullPerms;
 };
