b944fbee28e013580855bd3654bfdc1a12edf4e8
Aidar <stormymayday7@gmail.com>
2025-07-01 09:11:39 -0700

    Time: 19 ms (16.13%) | Memory: 63.5 MB (21.24%) - LeetSync

--- solutions/695-max-area-of-island/max-area-of-island.ts
+++ solutions/695-max-area-of-island/max-area-of-island.ts
@@ -4,12 +4,44 @@ function maxAreaOfIsland(grid: number[][]): number {
   for(let r = 0; r < grid.length; r += 1) {
     for(let c = 0; c < grid[0].length; c += 1) {
         if(grid[r][c] === 1 && !visited.has(`${r},${c}`)) {
-            largest = Math.max(largest, dfs(grid, r, c, visited));
+            // largest = Math.max(largest, dfs(grid, r, c, visited));
+            largest = Math.max(largest, bfs(grid, r, c, visited));
         }
     }
   }
   return largest;
 };
+
+function bfs(grid: number[][], r:number, c: number, visited: Set<string>):number {
+    const position = `${r},${c}`;
+    if(visited.has(position)) {
+        return 0;
+    }
+
+    visited.add(position);
+    const queue = [[r, c]];
+    let size = 1;
+    while(queue.length > 0) {
+        const [row, col] = queue.shift();
+        const deltas = [[-1, 0], [1, 0], [0, -1], [0,1]];
+        for(const delta of deltas) {
+            const [rowDelta, colDelta] = delta;
+            const neighborRow = row + rowDelta;
+            const neighborCol = col + colDelta;
+            const neighborPosition = `${neighborRow},${neighborCol}`;
+            if(
+                isInBounds(grid, neighborRow, neighborCol) === true
+                && !visited.has(neighborPosition)
+                && grid[neighborRow][neighborCol] === 1
+            ) {
+                visited.add(neighborPosition);
+                size += 1;
+                queue.push([neighborRow, neighborCol]);
+            }
+        }
+    }
+    return size;
+}
 
 function dfs(grid: number[][], r:number, c:number, visited: Set<string>):number {
     // Base Case: out of bounds
