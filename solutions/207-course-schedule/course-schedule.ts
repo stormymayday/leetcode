5c85623659bf75c63c9758800047cd38e6f37e08
Aidar <stormymayday7@gmail.com>
2025-07-02 19:09:45 -0700

    Time: 15 ms (55.29%) | Memory: 63.1 MB (67.41%) - LeetSync

--- solutions/207-course-schedule/course-schedule.ts
+++ solutions/207-course-schedule/course-schedule.ts
@@ -1,41 +1,54 @@
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
-    const graph = buildGraph(numCourses, prerequisites);
-    const visiting = new Set();
-    const visited = new Set();
-    for(const node of graph.keys()) {
-        if(hasCycle(graph, node, visiting, visited) === true) {
-            return false;
+    
+    const adjList = buildAdjList(numCourses, prerequisites);
+
+    const visiting = new Set<number>();
+    const visited = new Set<number>();
+
+    for(const course of adjList.keys()) {
+        if(cycleDetect(adjList, course, visiting, visited) === true) {
+            return false; // cycle detected -> can't finish
         }
     }
-    return true;
+
+    return true; // no cycle -> can finish
+
 };
 
-function hasCycle(graph, node, visiting, visited) {
-    if(visiting.has(node)) {
-        return true;
-    }
-    if(visited.has(node)) {
+function cycleDetect(adjList: Map<number, Set<number>>, src: number, visiting: Set<number>, visited: Set<number>):boolean {
+    if(visited.has(src)) {
         return false;
     }
-    visiting.add(node);
-    for(const neighbor of graph.get(node)) {
-        if(hasCycle(graph, neighbor, visiting, visited) === true) {
+
+    if(visiting.has(src)) {
+        return true;
+    }
+
+    visiting.add(src);
+
+    for(const neighbor of adjList.get(src)) {
+        if(cycleDetect(adjList, neighbor, visiting, visited) === true) {
             return true;
         }
     }
-    visiting.delete(node);
-    visited.add(node);
+
+    visiting.delete(src);
+    visited.add(src);
+
     return false;
 }
 
-function buildGraph(n, edges) {
-    const adjList = new Map();
+function buildAdjList(n:number, edges: number[][]):Map<number, Set<number>> {
+    const adjList = new Map<number, Set<number>>();
+
     for(let i = 0; i < n; i += 1) {
-        adjList.set(i, []);
+        adjList.set(i, new Set<number>());
     }
+
     for(const edge of edges) {
-        const [a, b] = edge;
-        adjList.get(a).push(b);
+        const [src, dst] = edge;
+        adjList.get(src).add(dst);
     }
+
     return adjList;
 }
