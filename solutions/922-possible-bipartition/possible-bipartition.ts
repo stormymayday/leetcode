07c426bbd66305bdcaee543051d205dc61a1ee9a
Aidar <stormymayday7@gmail.com>
2025-07-04 14:04:44 -0700

    Time: 46 ms (16.67%) | Memory: 65.2 MB (72.22%) - LeetSync

--- solutions/922-possible-bipartition/possible-bipartition.ts
+++ solutions/922-possible-bipartition/possible-bipartition.ts
@@ -3,13 +3,32 @@ function possibleBipartition(n: number, dislikes: number[][]): boolean {
     const state = new Map<number, boolean>();
     for(const node of adjList.keys()) {
         if(!state.has(node)) {
-            if(bfs(adjList, node, state) === false) {
+            // if(bfs(adjList, node, state) === false) {
+            //     return false;
+            // }
+            if(dfs(adjList, node, state, true) === false) {
                 return false;
             }
         }
     }
     return true;
 };
+
+function dfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, boolean>, stateVal: boolean):boolean {
+    if(state.has(src)) {
+        return state.get(src) === stateVal;
+    }
+
+    state.set(src, stateVal);
+
+    for(const neighbor of adjList.get(src)) {
+        if(dfs(adjList, neighbor, state, !stateVal) === false) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 function bfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, boolean>):boolean {
     if(!state.has(src)) {
