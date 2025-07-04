8a5affc117df3daf4f5adff0f9a19203dd3d7b5f
Aidar <stormymayday7@gmail.com>
2025-07-04 13:00:58 -0700

    Time: 3 ms (69.14%) | Memory: 58.1 MB (82.72%) - LeetSync

--- solutions/801-is-graph-bipartite/is-graph-bipartite.ts
+++ solutions/801-is-graph-bipartite/is-graph-bipartite.ts
@@ -2,13 +2,32 @@ function isBipartite(graph: number[][]): boolean {
     const state = new Map<number, boolean>();
     for(let i = 0; i < graph.length; i += 1) {
         if(!state.has(i)) {
-            if(bfs(graph, i, state) === false) {
+            // if(bfs(graph, i, state) === false) {
+            //     return false;
+            // }
+            if(dfs(graph, i, state, true) === false) {
                 return false;
             }
         }
     }
     return true;
 };
+
+function dfs(graph: number[][], src: number, state: Map<number, boolean>, stateVal: boolean):boolean {
+    if(state.has(src)) {
+        return state.get(src) === stateVal;
+    }
+
+    state.set(src, stateVal);
+
+    for(const neighbor of graph[src]) {
+        if(dfs(graph, neighbor, state, !stateVal) === false) {
+            return false;
+        }
+    }
+
+    return true;
+}
 
 function bfs(graph:number[][], src: number, state: Map<number, boolean>):boolean {
 
