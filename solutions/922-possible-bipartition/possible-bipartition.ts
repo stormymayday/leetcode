0ec0d174ccfaee7fa1b687214cdaf1c563211327
Aidar <stormymayday7@gmail.com>
2025-07-04 13:14:50 -0700

    Time: 49 ms (11.11%) | Memory: 65.3 MB (72.22%) - LeetSync

--- solutions/922-possible-bipartition/possible-bipartition.ts
+++ solutions/922-possible-bipartition/possible-bipartition.ts
@@ -3,10 +3,7 @@ function possibleBipartition(n: number, dislikes: number[][]): boolean {
     const state = new Map<number, boolean>();
     for(const node of adjList.keys()) {
         if(!state.has(node)) {
-            // if(bfs(adjList, node, state) === false) {
-            //     return false;
-            // }
-            if(dfs(adjList, node, state, true) === false) {
+            if(bfs(adjList, node, state) === false) {
                 return false;
             }
         }
@@ -14,24 +11,7 @@ function possibleBipartition(n: number, dislikes: number[][]): boolean {
     return true;
 };
 
-function dfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, boolean>, stateVal: boolean):boolean {
-    if(state.has(src)) {
-        return state.get(src) === stateVal;
-    }
-
-    state.set(src, stateVal);
-
-    for(const neighbor of adjList.get(src)) {
-        if(dfs(adjList, neighbor, state, !stateVal) === false) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
 function bfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, boolean>):boolean {
-
     if(!state.has(src)) {
         state.set(src, true);
     }
@@ -52,10 +32,9 @@ function bfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, 
     }
 
     return true;
-
 }
 
-function buildAdjList(n: number, edges: number[][]):Map<number, Set<number>> {
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
     const adjList = new Map();
     for(let i = 1; i <= n; i += 1) {
         adjList.set(i, new Set());
