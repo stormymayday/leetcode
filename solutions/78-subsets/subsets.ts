92c6147d7028a270d3558fa97114a5d8a4cb3767
Aidar <stormymayday7@gmail.com>
2025-07-08 10:05:30 -0700

    Time: 0 ms (100.00%) | Memory: 57.5 MB (49.24%) - LeetSync

--- solutions/78-subsets/subsets.ts
+++ solutions/78-subsets/subsets.ts
@@ -1,22 +1,24 @@
 function subsets(nums: number[]): number[][] {
-    const result: number[][] = [];
-    const curSet: number[] = [];
-    helper(0, nums, curSet, result);
+    const result = [];
+    const curSet = [];
+    function helper(index) {
+
+        if(index === nums.length) {
+            result.push([...curSet]);
+            return;
+        }
+
+        // include
+        curSet.push(nums[index]);
+        helper(index + 1);
+
+        // backtrack
+        curSet.pop();
+
+        // exclude
+        helper(index + 1);
+
+    }
+    helper(0);
     return result;
 };
-
-function helper(i: number, nums: number[], curSet: number[], result: number[][]):void {
-    // Base Case
-    if(i === nums.length) {
-        result.push([...curSet]);
-        return;
-    }
-
-    // include
-    curSet.push(nums[i]);
-    helper(i + 1, nums, curSet, result);
-    curSet.pop();
-
-    // exclude
-    helper(i + 1, nums, curSet, result);
-}
