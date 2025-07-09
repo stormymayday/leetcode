0c2c6446313a575e427bff24f8e8af1b70e57a21
Aidar <stormymayday7@gmail.com>
2025-07-09 13:42:09 -0700

    Time: 2 ms (71.84%) | Memory: 60.3 MB (65.79%) - LeetSync

--- solutions/46-permutations/permutations.ts
+++ solutions/46-permutations/permutations.ts
@@ -1,5 +1,6 @@
 function permute(nums: number[]): number[][] {
     function helper(index: number):number[][] {
+        // Base Case
         if(index === nums.length) {
             return [[]];
         }
@@ -7,9 +8,20 @@ function permute(nums: number[]): number[][] {
         const fullPerms = [];
         const partialPerms = helper(index + 1);
         for(const perm of partialPerms) {
+            // iterating over every index of 'perm'
             for(let i = 0; i <= perm.length; i += 1) {
-                const copy = [...perm];
+                // 1. Create copy of perm array
+                const copy = [...perm]; // using speard operator
+                // const copy = perm.slice(); // using Array.slice()
+                // const copy = Array.from(perm); // usiing Array.from()
+        
+                // 2. insert value at 'index' into every position of 'perm'
+                // - starting at 'i'
+                // - delete '0' items
+                // - insert 'items' value at 'index'
                 copy.splice(i, 0, nums[index]);
+
+                // 3. push result into 'fullPerms'
                 fullPerms.push(copy);
             }
         }
