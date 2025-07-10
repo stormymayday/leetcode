bd985c454e7ccfacbb416ca92cd8a4926cce5943
Aidar <stormymayday7@gmail.com>
2025-07-10 11:01:50 -0700

    Time: 0 ms (100.00%) | Memory: 56.6 MB (87.18%) - LeetSync

--- solutions/78-subsets/subsets.ts
+++ solutions/78-subsets/subsets.ts
@@ -1,18 +1,22 @@
 function subsets(nums: number[]): number[][] {
     const res = [];
     const subset = [];
-    function helper(index: number):void {
+    function helper(index) {
 
+        // Base Case
         if(index === nums.length) {
             res.push([...subset]);
             return;
         }
 
+        // Include
         subset.push(nums[index]);
         helper(index + 1);
 
+        // Backtrack
         subset.pop();
 
+        // Exclude
         helper(index + 1);
     }
     helper(0);
