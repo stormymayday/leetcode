90958188fd13055cc08ad0c345c9e844715c03ca
Aidar <stormymayday7@gmail.com>
2025-07-09 20:56:40 -0700

    Time: 4 ms (53.14%) | Memory: 60.3 MB (79.30%) - LeetSync

--- solutions/39-combination-sum/combination-sum.ts
+++ solutions/39-combination-sum/combination-sum.ts
@@ -1,37 +1,29 @@
 function combinationSum(candidates: number[], target: number): number[][] {
-    const result: number[][] = [];
-    const curCombo: number[] = [];
-    let curSum = 0;
-    function helper(index:number):void {
-        // Base Case 1: index out of bounds
+    const res = [];
+    const combo = [];
+    let curSum  = 0;
+    function helper(index) {
+        if(curSum === target) {
+            res.push([...combo]);
+            return;
+        }
+        if(curSum > target) {
+            return;
+        }
         if(index === candidates.length) {
             return;
         }
 
-        // Base Case 2: curSum is greater than target
-        if(curSum > target) {
-            return;
-        }
-        
-        // Base Case 3: found the combination!
-        if(curSum === target) {
-            // copy curCombo
-            result.push([...curCombo]);
-            return;
-        }
-
-        // 1. Including current candidate
-        curCombo.push(candidates[index]);
+        combo.push(candidates[index]);
         curSum += candidates[index];
-        helper(index); // index stays the same! (we can re-use candidates)
+        helper(index);
 
-        // 2. Backtracking
-        curCombo.pop();
+        combo.pop();
         curSum -= candidates[index];
 
-        // 3. Excluding current candidate
-        helper(index + 1); // index moves forward
+        helper(index + 1);
+
     }
     helper(0);
-    return result;
+    return res;
 };
