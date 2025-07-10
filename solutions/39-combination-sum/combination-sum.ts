92ff492542d5914721ece5833b9243a689f88914
Aidar <stormymayday7@gmail.com>
2025-07-10 13:48:32 -0700

    Time: 2 ms (86.82%) | Memory: 60.3 MB (79.37%) - LeetSync

--- solutions/39-combination-sum/combination-sum.ts
+++ solutions/39-combination-sum/combination-sum.ts
@@ -1,29 +1,33 @@
 function combinationSum(candidates: number[], target: number): number[][] {
     const res = [];
     const combo = [];
-    let curSum  = 0;
-    function helper(index) {
+    let curSum = 0;
+    (function helper(index: number):void {
+        // Base Case 1
         if(curSum === target) {
             res.push([...combo]);
             return;
         }
+        // Base Case 2
         if(curSum > target) {
             return;
         }
+        // Base Case 3
         if(index === candidates.length) {
             return;
         }
 
+        // Include
         combo.push(candidates[index]);
         curSum += candidates[index];
-        helper(index);
+        helper(index); // stay on current
 
+        // Backtrack
         combo.pop();
         curSum -= candidates[index];
 
+        // Exclude (move to the next)
         helper(index + 1);
-
-    }
-    helper(0);
+    }(0))
     return res;
 };
