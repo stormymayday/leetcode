c905d536c0ead94ff8f209abc25df923aa585e9f
Aidar <stormymayday7@gmail.com>
2025-07-11 20:33:15 -0700

    Time: 297 ms (61.02%) | Memory: 55.6 MB (86.62%) - LeetSync

--- solutions/79-word-search/word-search.ts
+++ solutions/79-word-search/word-search.ts
@@ -1,47 +1,49 @@
 function exist(board: string[][], word: string): boolean {
     for(let r = 0; r < board.length; r += 1) {
         for(let c = 0; c < board[0].length; c += 1) {
-            if(dfs(board, r, c, 0, word, new Set()) === true) {
-                return true;
+            if(board[r][c] === word[0]) {
+                if(dfs(board, r, c, 0, word) === true) {
+                    return true;
+                }
             }
         }
     }
     return false;
 };
 
-function dfs(grid: string[][], r: number, c: number, index: number, word: string, visited: Set<string>):boolean {
-    // Base Case 1: word found!
+function dfs(grid: string[][], r: number, c: number, index: number, word: string):boolean {
+    // Base Case: word found
     if(index === word.length) {
         return true;
     }
 
-    // Base Case 2: out of bounds
+    // Base Case: out of bounds
     const rowInBounds = 0 <= r && r < grid.length;
     const colInBounds = 0 <= c && c < grid[0].length;
     if(!rowInBounds || !colInBounds) {
         return false;
     }
 
-    // Base Case 3: char mismatch
+    // Base Case: char mismatch
     if(grid[r][c] !== word[index]) {
         return false;
     }
 
-    // Base Case 4: visited
-    const position = `${r},${c}`;
-    if(visited.has(position)) {
+    // Base Case: visited
+    if(grid[r][c] === '*') {
         return false;
     }
 
-    visited.add(position);
+    const char = grid[r][c];
+    grid[r][c] = '*';
 
-    const result = dfs(grid, r - 1, c, index + 1, word, visited) // up
-        || dfs(grid, r + 1, c, index + 1, word, visited) // down
-        || dfs(grid, r, c - 1, index + 1, word, visited) // left
-        || dfs(grid, r, c + 1, index + 1, word, visited); // right
+    const result = dfs(grid, r - 1, c, index + 1, word) ||
+        dfs(grid, r + 1, c, index + 1, word) ||
+        dfs(grid, r, c - 1, index + 1, word) ||
+        dfs(grid, r, c + 1, index + 1, word);
 
     // Backtrack
-    visited.delete(position);
+    grid[r][c] = char;
 
     return result;
-}
+} 
