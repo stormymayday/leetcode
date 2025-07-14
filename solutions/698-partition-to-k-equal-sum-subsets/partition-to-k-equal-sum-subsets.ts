d120391b6f3f6eb92dc737b13db8fbd5fe7e1f54
Aidar <stormymayday7@gmail.com>
2025-07-13 17:21:35 -0700

    Time: 723 ms (38.33%) | Memory: 55.5 MB (88.33%) - LeetSync

--- solutions/698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.ts
+++ solutions/698-partition-to-k-equal-sum-subsets/partition-to-k-equal-sum-subsets.ts
@@ -1,38 +1,61 @@
 function canPartitionKSubsets(nums: number[], k: number): boolean {
 
+    const n = nums.length;
+
     const sum = nums.reduce((acc, curr) => acc + curr, 0);
     if(sum % k !== 0) {
+        // If total sum of nums is not divisble by k, we can exit early
         return false;
     }
 
-    const bucketSize = sum / k;
+    // Optimization: sort in descending order
     const sorted = nums.sort((a, b) => b - a);
+    const bucketSize = sum / k;
     if(sorted[0] > bucketSize) {
+        // if the largest element is greater than the bucket size, we can exit early
         return false;
     }
 
-    const buckets = new Array(k).fill(0);
-
-    function helper(index) {
-        if(index === sorted.length) {
+    // to keep track which elemnt was already used
+    const used = new Array(n).fill(false);
+    function helper(index, k, currentBucket): boolean {
+        // Base Case 1: if we reduce k to 1 (slight optimization)
+        // we don't have to check for k === 0 because
+        // if k reaches 1 we know that it must be of valid 'bucket' size
+        if(k === 1) {
             return true;
         }
 
-        for(let i = 0; i < k; i += 1) {
-            if(buckets[i] + sorted[index] <= bucketSize) {
-                buckets[i] += sorted[index];
-                if(helper(index + 1) === true) {
+        // Base Case 2: if current buckets fills up
+        if(currentBucket === bucketSize) {
+            // we can reduce k by 1 and restart the recursion
+            return helper(0, k - 1, 0);
+        }
+
+        // i = index
+        for(let i = index; i < n; i += 1) {
+
+            if(used[i] === false && currentBucket + sorted[i] <= bucketSize) {
+                
+                // Choice: use current element
+                currentBucket += sorted[i];
+                used[i] = true;
+
+                // Explore with current element
+                if(helper(i + 1, k, currentBucket) === true) {
                     return true;
                 }
-                buckets[i] -= sorted[index];
-                if(buckets[i] === 0) {
-                    break;
-                }
+
+                // Backtrack
+                currentBucket -= sorted[i];
+                used[i] = false;
+
             }
+
         }
 
         return false;
     }
-    return helper(0);
 
+    return helper(0, k, 0);
 };
