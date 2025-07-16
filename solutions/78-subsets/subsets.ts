87ebcf408ccc249fbc71ca1ed22f33d393e78c42
Aidar <stormymayday7@gmail.com>
2025-07-15 19:10:36 -0700

    Time: 1 ms (39.32%) | Memory: 55 MB (98.62%) - LeetSync

--- solutions/78-subsets/subsets.ts
+++ solutions/78-subsets/subsets.ts
@@ -1,24 +1,24 @@
 function subsets(nums: number[]): number[][] {
-    const res = [];
-    const subset = [];
-    function helper(index) {
-
-        // Base Case
+    const res: number[][] = [];
+    const curr: number[] = [];
+    function helper(index: number): void {
         if(index === nums.length) {
-            res.push([...subset]);
+            res.push([...curr]); // O(n) for copying curr
             return;
         }
 
-        // Include
-        subset.push(nums[index]);
+        // Choose value at this index
+        curr.push(nums[index]);
+        // Explore with this value
         helper(index + 1);
-
         // Backtrack
-        subset.pop();
-
-        // Exclude
+        curr.pop();
+        // Explore without this value
         helper(index + 1);
     }
     helper(0);
     return res;
+    // Time: O(n x 2^n) - n for copying curr x 2 decisions (with and without) to the power of recursion depth of n
+    // Space: O(n + 2^n) - n for recursion depth + 2^n number of subsets
+    // Where n - is the length of nums
 };
