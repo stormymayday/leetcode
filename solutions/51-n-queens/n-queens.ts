bcb260dc645a3d83d1c0ad5d874e9f1cc10fba6d
Aidar <stormymayday7@gmail.com>
2025-07-17 23:38:32 -0700

    Time: 9 ms (67.34%) | Memory: 59.5 MB (56.85%) - LeetSync

--- solutions/51-n-queens/n-queens.ts
+++ solutions/51-n-queens/n-queens.ts
@@ -1,37 +1,35 @@
 function solveNQueens(n: number): string[][] {
-    const colSet = new Set<number>();
-    const posDiagSet = new Set<number>();
-    const negDiagSet = new Set<number>();
-    const result: string[][] = [];
-    const board: string[][] = [];
+    const res: string[][] = [];
+    const board: string[][] = new Array(n);
     for(let i = 0; i < n; i += 1) {
         board[i] = new Array(n).fill('.');
     }
-    function helper(row: number): void {
+    const colSet = new Set<number>();
+    const posDiagSet = new Set<number>(); // row + col
+    const negDiagSet = new Set<number>(); // row - col
+    function helper(row: number):void {
         if(row === n) {
-            result.push(board.map(row => row.join('')));
+            res.push(board.map((row) => row.join("")));
             return;
         }
         for(let col = 0; col < n; col += 1) {
             if(!colSet.has(col) && !posDiagSet.has(row + col) && !negDiagSet.has(row - col)) {
-                // Choose this position
+                board[row][col] = 'Q';
                 colSet.add(col);
                 posDiagSet.add(row + col);
                 negDiagSet.add(row - col);
-                board[row][col] = 'Q';
 
-                // Explore with this position
                 helper(row + 1);
 
-                // Backtrack
+                board[row][col] = '.';
                 colSet.delete(col);
                 posDiagSet.delete(row + col);
                 negDiagSet.delete(row - col);
-                board[row][col] = '.';
+
 
             }
         }
     }
     helper(0);
-    return result;
+    return res;
 };
