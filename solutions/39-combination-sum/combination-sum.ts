95ee4906844831961590ed333ae1055953ea35bf
Aidar <stormymayday7@gmail.com>
2025-07-19 15:45:39 -0700

    Time: 2 ms (86.30%) | Memory: 59.8 MB (92.73%) - LeetSync

--- solutions/39-combination-sum/combination-sum.ts
+++ solutions/39-combination-sum/combination-sum.ts
@@ -2,21 +2,18 @@ function combinationSum(candidates: number[], target: number): number[][] {
     const res: number[][] = [];
     const combo: number[] = [];
     function helper(index: number, sum: number): void {
+        if(sum > target) {
+            return;
+        }
+        
         if(sum === target) {
             res.push([...combo]);
             return;
         }
-        
+
         if(index === candidates.length) {
             return;
-        }
-
-        if(sum > target) {
-            return;
-        }
-
-        
-        
+        }        
 
         combo.push(candidates[index]);
 
