31dec5979434ade691251dc2ff5b1353ef0aadb5
Aidar <stormymayday7@gmail.com>
2025-07-19 14:15:47 -0700

    Time: 65 ms (36.07%) | Memory: 60.3 MB (50.82%) - LeetSync

--- solutions/865-robot-room-cleaner/robot-room-cleaner.ts
+++ solutions/865-robot-room-cleaner/robot-room-cleaner.ts
@@ -25,10 +25,11 @@ function cleanRoom(robot: Robot) {
         // [1, 0], // down
         // [0, -1] // left
         // counter clockwise
+        // [-1, 0], // up
         [-1, 0], // up
         [0, -1], // left
         [1, 0], // down
-        [0, 1] // right
+        [0, 1], // right
     ];
     const visited = new Set<string>();
     function helper(row: number, col: number, direction: number): void {
