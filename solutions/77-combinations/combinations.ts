0e29bd8819195edbf060b12bf07ff7c5a4e912ac
Aidar <stormymayday7@gmail.com>
2025-07-19 15:54:18 -0700

    Time: 63 ms (58.21%) | Memory: 129.9 MB (31.09%) - LeetSync

--- solutions/77-combinations/combinations.ts
+++ solutions/77-combinations/combinations.ts
@@ -1,27 +1,19 @@
 function combine(n: number, k: number): number[][] {
     const res: number[][] = [];
-    const curr: number[] = [];
+    const combo: number[] = [];
     function helper(num: number): void {
-        if(curr.length === k) {
-            res.push([...curr]);
+        if(combo.length === k) {
+            res.push([...combo]);
             return;
         }
         if(num > n) {
             return;
         }
 
-        // O(2^n)
-        // curr.push(num);
-        // helper(num + 1);
-        // curr.pop();
-        // helper(num + 1);
-
-        // O(n choose k)
-        for(let i = num; i <= n; i += 1) {
-            curr.push(i);
-            helper(i + 1);
-            curr.pop();
-        }
+        combo.push(num);
+        helper(num + 1);
+        combo.pop();
+        helper(num + 1);
     }
     helper(1);
     return res;
