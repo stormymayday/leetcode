92ee9df891eb0d37720b29f1b01b5ab25f04a9ed
Aidar <stormymayday7@gmail.com>
2025-07-19 21:55:02 -0700

    Time: 208 ms (41.51%) | Memory: 55.9 MB (90.57%) - LeetSync

--- solutions/473-matchsticks-to-square/matchsticks-to-square.ts
+++ solutions/473-matchsticks-to-square/matchsticks-to-square.ts
@@ -1,19 +1,13 @@
 function makesquare(matchsticks: number[]): boolean {
-    // 1. Get total length of all matchsticks
-    const totalLength = matchsticks.reduce((acc, curr) => acc + curr, 0);
-    // 2. Check if total lenght is evenly divible by 4
-    if(totalLength % 4 !== 0) {
+    const totaLength = matchsticks.reduce((acc, curr) => acc + curr, 0);
+    if(totaLength % 4 !== 0) {
         return false;
     }
-    // 3. Calculate length of a side
-    const sideLength = totalLength / 4;
-    // 4. Sort the matchsticks in descending order
     const sorted = matchsticks.sort((a, b) => b - a);
-    // 5. Check if the largest matchstick longer than sideLength
+    const sideLength = totaLength / 4;
     if(sorted[0] > sideLength) {
         return false;
     }
-    // 6. Sides array
     const sides = new Array(4).fill(0);
     function helper(index: number): boolean {
         if(index === sorted.length) {
@@ -21,7 +15,6 @@ function makesquare(matchsticks: number[]): boolean {
         }
 
         for(let i = 0; i < 4; i += 1) {
-
             if(sides[i] + sorted[index] <= sideLength) {
                 sides[i] += sorted[index];
                 if(helper(index + 1) === true) {
@@ -29,12 +22,9 @@ function makesquare(matchsticks: number[]): boolean {
                 }
                 sides[i] -= sorted[index];
             }
-
         }
 
         return false;
-
     }
     return helper(0);
-
 };
