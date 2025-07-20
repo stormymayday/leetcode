64a916270760124a3648033bd992c36a8751a82c
Aidar <stormymayday7@gmail.com>
2025-07-20 14:08:53 -0700

    Time: 4 ms (83.90%) | Memory: 58.3 MB (23.73%) - LeetSync

--- solutions/52-n-queens-ii/n-queens-ii.ts
+++ solutions/52-n-queens-ii/n-queens-ii.ts
@@ -1,27 +1,29 @@
 function totalNQueens(n: number): number {
     const colSet = new Set<number>();
-    const posDiagSet = new Set<number>();
     const negDiagSet = new Set<number>();
+    const posDiagSet = new Set<number>();
     let result = 0;
-    function helper(row: number): void {
+    function helper(row: number):void {
         if(row === n) {
             result += 1;
             return;
         }
         for(let col = 0; col < n; col += 1) {
-            if(!colSet.has(col) && !posDiagSet.has(row + col) && !negDiagSet.has(row - col)) {
-                // Choose this position
+            if(
+                !colSet.has(col) &&
+                !negDiagSet.has(row - col) &&
+                !posDiagSet.has(row + col)
+            ) {
                 colSet.add(col);
-                posDiagSet.add(row + col);
                 negDiagSet.add(row - col);
+                posDiagSet.add(row + col);
 
-                // Explore with this position
                 helper(row + 1);
 
-                // Backtrack
                 colSet.delete(col);
-                posDiagSet.delete(row + col);
                 negDiagSet.delete(row - col);
+                posDiagSet.delete(row + col);
+
             }
         }
     }
