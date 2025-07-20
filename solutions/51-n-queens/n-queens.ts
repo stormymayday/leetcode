2f0005d40336235505aaeb4ef9f60e65400694a0
Aidar <stormymayday7@gmail.com>
2025-07-20 11:11:29 -0700

    Time: 10 ms (59.26%) | Memory: 59.8 MB (48.56%) - LeetSync

--- solutions/51-n-queens/n-queens.ts
+++ solutions/51-n-queens/n-queens.ts
@@ -1,31 +1,34 @@
 function solveNQueens(n: number): string[][] {
+    const colSet = new Set<number>();
+    const negDiagSet = new Set<number>(); // row - col
+    const posDiagSet = new Set<number>(); // row + col
     const res: string[][] = [];
-    const board: string[][] = new Array(n);
+    const board: string[][] = [];
     for(let i = 0; i < n; i += 1) {
         board[i] = new Array(n).fill('.');
     }
-    const colSet = new Set<number>();
-    const posDiagSet = new Set<number>(); // row + col
-    const negDiagSet = new Set<number>(); // row - col
-    function helper(row: number):void {
+    function helper(row: number): void {
         if(row === n) {
             res.push(board.map((row) => row.join("")));
             return;
         }
         for(let col = 0; col < n; col += 1) {
-            if(!colSet.has(col) && !posDiagSet.has(row + col) && !negDiagSet.has(row - col)) {
+            if(
+                !colSet.has(col) &&
+                !negDiagSet.has(row - col) &&
+                !posDiagSet.has(row + col)
+            ) {
                 board[row][col] = 'Q';
                 colSet.add(col);
-                posDiagSet.add(row + col);
                 negDiagSet.add(row - col);
+                posDiagSet.add(row + col);
 
                 helper(row + 1);
 
                 board[row][col] = '.';
                 colSet.delete(col);
-                posDiagSet.delete(row + col);
                 negDiagSet.delete(row - col);
-
+                posDiagSet.delete(row + col);
 
             }
         }
