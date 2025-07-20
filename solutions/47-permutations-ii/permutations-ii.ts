3519bf62f32e8db8bf3a2f3def9cff49be80d550
Aidar <stormymayday7@gmail.com>
2025-07-19 19:04:21 -0700

    Time: 3 ms (71.43%) | Memory: 62.5 MB (32.26%) - LeetSync

--- solutions/47-permutations-ii/permutations-ii.ts
+++ solutions/47-permutations-ii/permutations-ii.ts
@@ -1,31 +1,26 @@
 function permuteUnique(nums: number[]): number[][] {
-    const n = nums.length;
-    const numCount = new Map();
-    for(const num of nums) {
-        if(!numCount.has(num)) {
-            numCount.set(num, 0);
-        }
-        numCount.set(num, numCount.get(num) + 1);
+    const numCount = new Map<number, number>();
+    for(let i = 0; i < nums.length; i += 1) {
+        const num = nums[i];
+        numCount.set(num, (numCount.get(num) || 0) + 1);
     }
     const res: number[][] = [];
-    const curr: number[] = [];
-    function helper() {
-        if(curr.length === n) {
-            res.push([...curr]);
+    const perm: number[] = [];
+    (function helper() {
+        if(perm.length === nums.length) {
+            res.push([...perm]);
             return;
         }
         for(const [num, count] of numCount.entries()) {
             if(count > 0) {
-                curr.push(num);
+                perm.push(num);
                 numCount.set(num, numCount.get(num) - 1);
-
                 helper();
 
-                curr.pop();
+                perm.pop();
                 numCount.set(num, numCount.get(num) + 1);
             }
         }
-    }
-    helper();
+    }());
     return res;
 };
