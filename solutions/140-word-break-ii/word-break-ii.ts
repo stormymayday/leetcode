40fde2b1d81f14f26e1fb6d6af434e8a4825d0d3
Aidar <stormymayday7@gmail.com>
2025-07-20 16:12:16 -0700

    Time: 0 ms (100.00%) | Memory: 55.4 MB (40.22%) - LeetSync

--- solutions/140-word-break-ii/word-break-ii.ts
+++ solutions/140-word-break-ii/word-break-ii.ts
@@ -1,20 +1,16 @@
 function wordBreak(s: string, wordDict: string[]): string[] {
     const res: string[] = [];
     const curr: string[] = [];
-    function helper(index: number): void {
+    function helper(index: number): void { 
         if(index === s.length) {
             res.push(curr.join(" "));
             return;
         }
         for(let i = 0; i < wordDict.length; i += 1) {
-            if(s.substring(index, index + wordDict[i].length) === wordDict[i]) {
-                // Choose this word
-                curr.push(wordDict[i]);
-
-                // Explore
-                helper(index + wordDict[i].length);
-
-                // Backtrack
+            const subStr = s.substring(index, index + wordDict[i].length);
+            if(subStr === wordDict[i]) {
+                curr.push(subStr);
+                helper(index + subStr.length);
                 curr.pop();
             }
         }
