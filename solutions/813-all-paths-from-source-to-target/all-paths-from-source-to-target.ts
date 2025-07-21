e1099cb83d4daddbb0060c0a73b7b5aea3d5090a
Aidar <stormymayday7@gmail.com>
2025-07-20 21:58:13 -0700

    Time: 3 ms (90.07%) | Memory: 64.7 MB (77.48%) - LeetSync

--- solutions/813-all-paths-from-source-to-target/all-paths-from-source-to-target.ts
+++ solutions/813-all-paths-from-source-to-target/all-paths-from-source-to-target.ts
@@ -1,20 +1,19 @@
 function allPathsSourceTarget(graph: number[][]): number[][] {
     const res: number[][] = [];
     const path: number[] = [];
-    function helper(node: number): void {
+    function helper(src) {
 
-        path.push(node);
+        path.push(src);
 
-        if(node === graph.length - 1) {
+        if(src === graph.length - 1) {
             res.push([...path]);
         }
 
-        for(const neighbor of graph[node]) {
+        for(const neighbor of graph[src]) {
             helper(neighbor);
         }
 
         path.pop();
-        
     }
     helper(0);
     return res;
