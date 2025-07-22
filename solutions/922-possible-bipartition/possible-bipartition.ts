dc545e0a3502d4e66125308e84342dbb76785ca1
Aidar <stormymayday7@gmail.com>
2025-07-21 22:35:52 -0700

    Time: 48 ms (14.63%) | Memory: 65.5 MB (41.46%) - LeetSync

--- solutions/922-possible-bipartition/possible-bipartition.ts
+++ solutions/922-possible-bipartition/possible-bipartition.ts
@@ -2,54 +2,31 @@ function possibleBipartition(n: number, dislikes: number[][]): boolean {
     const adjList = buildAdjList(n, dislikes);
     const state = new Map<number, boolean>();
     for(const node of adjList.keys()) {
-        if(!state.has(node)) {
-            // if(bfs(adjList, node, state) === false) {
-            //     return false;
-            // }
-            if(dfs(adjList, node, state, true) === false) {
-                return false;
-            }
+        if(bfs(adjList, node, state) === false) {
+            return false;
         }
     }
     return true;
 };
 
-function dfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, boolean>, stateVal: boolean):boolean {
-    if(state.has(src)) {
-        return state.get(src) === stateVal;
-    }
-
-    state.set(src, stateVal);
-
-    for(const neighbor of adjList.get(src)) {
-        if(dfs(adjList, neighbor, state, !stateVal) === false) {
-            return false;
-        }
-    }
-
-    return true;
-}
-
-function bfs(adjList: Map<number, Set<number>>, src: number, state: Map<number, boolean>):boolean {
+function bfs(graph: Map<number, Set<number>>, src: number, state: Map<number, boolean>): boolean {
     if(!state.has(src)) {
         state.set(src, true);
     }
-
     const queue: [number, boolean][] = [[src, state.get(src)]];
-
     while(queue.length > 0) {
         const [node, stateVal] = queue.shift();
-        for(const neighbor of adjList.get(node)) {
+        for(const neighbor of graph.get(node)) {
             if(!state.has(neighbor)) {
                 state.set(neighbor, !stateVal);
                 queue.push([neighbor, !stateVal]);
-            }
-            if(state.get(neighbor) === stateVal) {
-                return false;
+            } else {
+                if(state.get(neighbor) === stateVal) {
+                    return false;
+                }
             }
         }
     }
-
     return true;
 }
 
