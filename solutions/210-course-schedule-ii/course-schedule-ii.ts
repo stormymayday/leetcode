e57fa04b51ece22f2a7da375b90514cd7b02c89b
Aidar <stormymayday7@gmail.com>
2025-07-22 20:21:50 -0700

    Time: 20 ms (15.76%) | Memory: 64.4 MB (19.26%) - LeetSync

--- solutions/210-course-schedule-ii/course-schedule-ii.ts
+++ solutions/210-course-schedule-ii/course-schedule-ii.ts
@@ -1,80 +1,53 @@
 function findOrder(numCourses: number, prerequisites: number[][]): number[] {
     const adjList = buildAdjList(numCourses, prerequisites);
-    // return kahns(adjList);
-    return dfs(adjList);
+    return kahns(adjList);
 };
 
-function dfs(adjList: Map<number, Set<number>>):number[] {
+function kahns(adjList: Map<number, Set<number>>): number[] {
+    // child to parents mapping
+    const nodeParents = new Map<number, Set<number>>();
+    for(const parent of adjList.keys()) {
+        if(!nodeParents.has(parent)) {
+            nodeParents.set(parent, new Set());
+        }
+        for(const child of adjList.get(parent)) {
+            if(!nodeParents.has(child)) {
+                nodeParents.set(child, new Set());
+            }
+            nodeParents.get(child).add(parent);
+        }
+    }
+    // queue nodes with no parents
+    const queue: number[] = [];
+    for(const child of nodeParents.keys()) {
+        if(nodeParents.get(child).size === 0) {
+            queue.push(child);
+        }
+    }
+
+    // Topological Ordering
     const topOrder: number[] = [];
-    const visiting = new Set<number>();
-    const visited = new Set<number>();
-    function traverse(node: number):boolean {
-        if(visited.has(node)) {
-            return true;
-        }
-        if(visiting.has(node)) {
-            return false;
-        }
-        visiting.add(node);
-        for(const neighbor of adjList.get(node)) {
-            if(traverse(neighbor) === false) {
-                return false;
-            }
-        }
-        visiting.delete(node);
-        visited.add(node);
-        topOrder.push(node);
-        return true;
-    }
-    for(const node of adjList.keys()) {
-        if(!visited.has(node)) {
-            if(traverse(node) === false) {
-                return [];
-            }
-        }
-    }
-    return topOrder;
-}
-
-function kahns(adjList: Map<number, Set<number>>):number[] {
-    const numParents = new Map<number, number>();
-    for(const node of adjList.keys()) {
-        numParents.set(node, 0);
-    }
-    for(const node of adjList.keys()) {
-        for(const child of adjList.get(node)){
-            numParents.set(child, numParents.get(child) + 1);
-        }
-    }
-
-    const ready: number[] = [];
-    for(const node of numParents.keys()) {
-        if(numParents.get(node) === 0) {
-            ready.push(node);
-        }
-    }
-
-    const topOrder:number[] = [];
-    while(ready.length > 0) {
-        const current = ready.pop();
+    while(queue.length > 0) {
+        const current = queue.shift();
         topOrder.push(current);
         for(const child of adjList.get(current)) {
-            numParents.set(child, numParents.get(child) - 1);
-            if(numParents.get(child) === 0) {
-                ready.push(child);
+            if(nodeParents.has(child)) {
+                nodeParents.get(child).delete(current);
+                if(nodeParents.get(child).size === 0) {
+                    queue.push(child);
+                }
             }
         }
     }
-
-    if(topOrder.length !== adjList.size) {
-        return [];
-    } else {
+    if(topOrder.length === adjList.size) {
         return topOrder.reverse();
+    } else {
+        return [];
     }
 }
 
-function buildAdjList(n: number, edges: number[][]):Map<number, Set<number>> {
-    const adjList = new Map<number, Set<number>>();
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+    const adjList = new Map();
     for(let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
