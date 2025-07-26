de7a6bcea4e54f20a6d5fba6c1bf9ce6b2660065
Aidar <stormymayday7@gmail.com>
2025-07-25 17:19:01 -0700

    Time: 28 ms (10.44%) | Memory: 65.3 MB (10.44%) - LeetSync

--- solutions/210-course-schedule-ii/course-schedule-ii.ts
+++ solutions/210-course-schedule-ii/course-schedule-ii.ts
@@ -1,33 +1,40 @@
 function findOrder(numCourses: number, prerequisites: number[][]): number[] {
     const adjList = buildAdjList(numCourses, prerequisites);
-    const visiting = new Set<number>();
-    const visited = new Set<number>();
-    const topOrder: number[] = [];
-    for(const node of adjList.keys()) {
-        if(postOrderDFSWhiteGrayBlack(adjList, node, visiting, visited, topOrder) === true) {
-            return []; // found a cycle
-        }
-    }
-    return topOrder;
+    return kahnsAlgorithm(adjList);
 };
 
-function postOrderDFSWhiteGrayBlack(adjList: Map<number, Set<number>>, src: number, visiting: Set<number>, visited: Set<number>, topOrder: number[]): boolean {
-    if(visited.has(src)) {
-        return false; // no cycle
+function kahnsAlgorithm(adjList: Map<number, Set<number>>):number[] {
+    const inDegree = new Map<number, number>();
+    for(const node of adjList.keys()) {
+        inDegree.set(node, 0);
     }
-    if(visiting.has(src)) {
-        return true; // cycle
-    }
-    visiting.add(src);
-    for(const neighbor of adjList.get(src)) {
-        if(postOrderDFSWhiteGrayBlack(adjList, neighbor, visiting, visited, topOrder) === true) {
-            return true; // cycle
+    for(const prereq of adjList.keys()) {
+        for(const course of adjList.get(prereq)) {
+            inDegree.set(course, inDegree.get(course) + 1);
         }
     }
-    visiting.delete(src);
-    visited.add(src);
-    topOrder.push(src);
-    return false; // no cycle
+    const queue: number[] = [];
+    for(const [node, count] of inDegree.entries()) {
+        if(count === 0) {
+            queue.push(node);
+        }
+    }
+    const topOrder: number[] = [];
+    while(queue.length > 0) {
+        const current = queue.shift();
+        topOrder.push(current);
+        for(const course of adjList.get(current)) {
+            inDegree.set(course, inDegree.get(course) - 1);
+            if(inDegree.get(course) ===  0) {
+                queue.push(course);
+            }
+        }
+    }
+    if(topOrder.length === adjList.size) {
+        return topOrder;
+    } else {
+        return [];
+    }
 }
 
 function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
@@ -36,8 +43,8 @@ function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
         adjList.set(i, new Set());
     }
     for(const edge of edges) {
-        const [src, dst] = edge;
-        adjList.get(src).add(dst);
+        const [course, prereq] = edge;
+        adjList.get(prereq).add(course);
     }
     return adjList;
 }
