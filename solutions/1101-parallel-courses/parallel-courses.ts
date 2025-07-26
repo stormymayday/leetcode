1d35ac08ea841fd408353e6586889f1516312dd4
Aidar <stormymayday7@gmail.com>
2025-07-26 13:38:29 -0700

    Time: 28 ms (13.64%) | Memory: 66.5 MB (22.73%) - LeetSync

---
+++ solutions/1101-parallel-courses/parallel-courses.ts
@@ -0,0 +1,60 @@
+function minimumSemesters(n: number, relations: number[][]): number {
+    const adjList = buildAdjList(n, relations);
+    return kahnsAlgorithm(adjList);
+};
+
+function kahnsAlgorithm(adjList: Map<number, Set<number>>): number {
+    const inDegree = new Map<number, number>();
+    for(const node of adjList.keys()) {
+        inDegree.set(node, 0);
+    }
+    for(const parent of adjList.keys()) {
+        for(const child of adjList.get(parent)) {
+            inDegree.set(child, inDegree.get(child) + 1);
+        }
+    }
+    let queue: number[] = [];
+    for(const [node, count] of inDegree.entries()) {
+        if(count === 0) {
+            queue.push(node);
+        }
+    }
+    
+    let nodesRemaining = adjList.size;
+    let layers = 1;
+    while(true) {
+        nodesRemaining -= queue.length;
+        const nextLayer: number[] = [];
+        while(queue.length > 0) {
+            const current = queue.shift();
+            for(const child of adjList.get(current)) {
+                inDegree.set(child, inDegree.get(child) - 1);
+                if(inDegree.get(child) === 0) {
+                    nextLayer.push(child);
+                }
+            }
+        }
+        if(nextLayer.length === 0) {
+            break;
+        }
+        queue = nextLayer;
+        layers += 1;
+    }
+    if(nodesRemaining === 0) {
+        return layers;
+    } else {
+        return -1;
+    }
+}
+
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+    const adjList = new Map();
+    for(let i = 1; i <= n; i += 1) {
+        adjList.set(i, new Set());
+    }
+    for(const edge of edges) {
+        const [src, dst] = edge;
+        adjList.get(src).add(dst);
+    }
+    return adjList;
+}
