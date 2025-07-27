55a7e563997d4ccc044b0d0485833aaf51ee3546
Aidar <stormymayday7@gmail.com>
2025-07-26 21:02:39 -0700

    Time: 392 ms (6.67%) | Memory: 75.3 MB (11.67%) - LeetSync

---
+++ solutions/1558-course-schedule-iv/course-schedule-iv.ts
@@ -0,0 +1,42 @@
+function checkIfPrerequisite(numCourses: number, prerequisites: number[][], queries: number[][]): boolean[] {
+    const adjList = buildAdjList(numCourses, prerequisites);
+    let result: boolean[] = [];
+    for(const query of queries) {
+        const [src, dst] = query;
+        result.push(dfs(adjList, src, dst, new Set()));
+    }
+    return result;
+};
+
+function dfs(adjList: Map<number, Set<number>>, src: number, dst: number, visited: Set<number>):boolean {
+
+    if(visited.has(src)) {
+        return false;
+    }
+
+    if(src === dst) {
+        return true;
+    }
+
+    visited.add(src);
+
+    for(const neighbor of adjList.get(src)) {
+        if(dfs(adjList, neighbor, dst, visited) === true) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+    const adjList = new Map();
+    for(let i = 0; i < n; i += 1) {
+        adjList.set(i, new Set());
+    }
+    for(const edge of edges) {
+        const [src, dst] = edge;
+        adjList.get(src).add(dst);
+    }
+    return adjList;
+}
