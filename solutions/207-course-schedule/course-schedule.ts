6b2ab66ff8ab4793e7c61c2830548463e5dcf731
Aidar <stormymayday7@gmail.com>
2025-07-28 13:16:02 -0700

    Time: 25 ms (18.25%) | Memory: 62.3 MB (81.50%) - LeetSync

--- solutions/207-course-schedule/course-schedule.ts
+++ solutions/207-course-schedule/course-schedule.ts
@@ -1,62 +1,40 @@
 function canFinish(numCourses: number, prerequisites: number[][]): boolean {
     const adjList = buildAdjList(numCourses, prerequisites);
-    // return kahnsAlgorithm(adjList);
-    const visiting = new Set<number>();
-    const visited = new Set<number>();
-    for(const node of adjList.keys()) {
-        if(postOrderDFSWhiteGrayBlack(adjList, node, visiting, visited) === true) {
-            return false; // cycle therefore not possible
-        }
-    }
-    return true; // no cycle therefore possible
+    return kahns(adjList);
 };
 
-function postOrderDFSWhiteGrayBlack(adjList: Map<number, Set<number>>, src: number, visiting: Set<number>, visited: Set<number>):boolean {
-    if(visited.has(src)) {
-        return false; // no cycle
-    }
-    if(visiting.has(src)) {
-        return true; // cycle
-    }
-    visiting.add(src);
-    for(const neighbor of adjList.get(src)) {
-        if(postOrderDFSWhiteGrayBlack(adjList, neighbor, visiting, visited) === true) {
-            return true; // cycle
-        }
-    }
-    visiting.delete(src);
-    visited.add(src);
-    return false; // no cycle
-}
-
-function kahnsAlgorithm(adjList: Map<number, Set<number>>):boolean {
+function kahns(adjList: Map<number, Set<number>>): boolean {
     const inDegree = new Map<number, number>();
     for(const node of adjList.keys()) {
         inDegree.set(node, 0);
     }
-    for(const prereq of adjList.keys()) {
-        for(const course of adjList.get(prereq)) {
-            inDegree.set(course, inDegree.get(course) + 1);
+    for(const node of adjList.keys()) {
+        for(const neighbor of adjList.get(node)) {
+            inDegree.set(neighbor, inDegree.get(neighbor) + 1);
         }
     }
-    const queue: number[] = [];
+    const stack: number[] = [];
     for(const [node, count] of inDegree.entries()) {
         if(count === 0) {
-            queue.push(node);
+            stack.push(node);
         }
     }
-    let nodesVisited = 0;
-    while(queue.length > 0) {
-        const current = queue.shift();
-        nodesVisited += 1;
-        for(const course of adjList.get(current)) {
-            inDegree.set(course, inDegree.get(course) - 1);
-            if(inDegree.get(course) ===  0) {
-                queue.push(course);
+    let visited = 0;
+    while(stack.length > 0) {
+        const current = stack.pop();
+        visited += 1;
+        for(const neighbor of adjList.get(current)) {
+            inDegree.set(neighbor, inDegree.get(neighbor) - 1);
+            if(inDegree.get(neighbor) === 0) {
+                stack.push(neighbor);
             }
         }
     }
-    return nodesVisited === adjList.size;
+    if(visited === adjList.size) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
