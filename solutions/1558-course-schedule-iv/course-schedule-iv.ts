9bd6c73e8d3c4572133944841a7488de246e0738
Aidar <stormymayday7@gmail.com>
2025-07-29 09:46:03 -0700

    Time: 57 ms (49.06%) | Memory: 68 MB (49.06%) - LeetSync

--- solutions/1558-course-schedule-iv/course-schedule-iv.ts
+++ solutions/1558-course-schedule-iv/course-schedule-iv.ts
@@ -1,30 +1,38 @@
 function checkIfPrerequisite(numCourses: number, prerequisites: number[][], queries: number[][]): boolean[] {
     const adjList = buildAdjList(numCourses, prerequisites);
+
+    const prereqForCourses = new Map<number, Set<number>>();
+    for(const prereq of adjList.keys()) {
+        dfs(adjList, prereq, prereqForCourses);
+    }
+
     const result: boolean[] = [];
     for(const query of queries) {
         const [prereq, course] = query;
-        result.push(dfs(adjList, prereq, course, new Set<number>()));
+        result.push(prereqForCourses.get(prereq).has(course));
     }
     return result;
 };
 
-function dfs(adjList: Map<number, Set<number>>, src: number, dst: number, visited: Set<number>): boolean {
+function dfs(adjList: Map<number, Set<number>>, src: number, prereqForCourses: Map<number, Set<number>>): Set<number> {
     
-    visited.add(src);
-
-    if(src === dst) {
-        return true;
+    if(prereqForCourses.has(src)) {
+        return prereqForCourses.get(src);
     }
-    
-    for(const neighbor of adjList.get(src)) {
-        if(!visited.has(neighbor)) {
-            if(dfs(adjList, neighbor, dst, visited) === true) {
-                return true;
-            }
+
+    const allCourses = new Set<number>();
+
+    for(const directCourse of adjList.get(src)) {
+        allCourses.add(directCourse);
+        const indirectCourses = dfs(adjList, directCourse, prereqForCourses);
+        for(const indirectCourse of indirectCourses) {
+            allCourses.add(indirectCourse);
         }
     }
 
-    return false;
+    prereqForCourses.set(src, allCourses);
+    return allCourses;
+    
 }
 
 function buildAdjList(n: number, edges: number[][]):Map<number, Set<number>> {
