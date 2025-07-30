0a0e62357d569c6457b2af15e8493c4713657483
Aidar <stormymayday7@gmail.com>
2025-07-29 17:29:07 -0700

    Time: 2 ms (79.31%) | Memory: 57.3 MB (74.71%) - LeetSync

--- solutions/269-alien-dictionary/alien-dictionary.ts
+++ solutions/269-alien-dictionary/alien-dictionary.ts
@@ -1,69 +1,66 @@
 function alienOrder(words: string[]): string {
-    // Initialize the adjacency list:
-    const adjList = new Map<string, Set<string>>();
-    // Iterate over every word
+
+   const adjList = new Map<string, Set<string>>();
     for(const word of words) {
-        // Iterate over each character of the word
         for(const char of word) {
             if(!adjList.has(char)) {
-                // create a mapping of char to set 
-                adjList.set(char, new Set());
+                adjList.set(char, new Set<string>());
             }
         }
     }
-    
-    // Build the Adjacency List by going through words in pairs
     for(let i = 0; i < words.length - 1; i += 1) {
-        // Get adjacent pair of words
+        // Compare two adjacent words
         const word1 = words[i];
         const word2 = words[i + 1];
-        // Get the minimum length of the two words
-        const minLen = Math.min(word1.length, word2.length);
-        // Edge Case: if both words have the same prefix but the first word is longer than the second word
-        if(word1.substring(0, minLen) === word2.substring(0, minLen) && word1.length > word2.length) {
-            // invalid ordering
-            return ""; // exit early
+        const minLength = Math.min(word1.length, word2.length);
+        // Edge Case!
+        if(word1.substring(0, minLength) === word2.substring(0, minLength) && word1.length > word2.length) {
+            return ""; // early exit
         }
-        // Otherwise, compare words character by character
-        for(let j = 0; j < minLen; j += 1) {
+        // Compare character by character until first discrepency
+        for(let j = 0; j < minLength; j += 1) {
             const char1 = word1[j];
             const char2 = word2[j];
-            // Look for the first divergence
             if(char1 !== char2) {
-                // Since we know that word1 comes before word2
-                // char1 must come before char2
                 adjList.get(char1).add(char2);
-                break; // we are only looking for the first divergence
+                break;
             }
         }
     }
-    
-    // postOrder DFS with White Gray Black
-    function dfs(src: string): boolean {
-        if(visited.has(src)) {
-            return false; // no cycle
-        }
-        if(visiting.has(src)) {
-            return true; // cycle
-        }
-        visiting.add(src);
-        for(const neighbor of adjList.get(src)) {
-            if(dfs(neighbor) === true) {
-                return true; // cycle
-            }
-        }
-        visiting.delete(src);
-        visited.add(src);
-        topOrder.push(src);
-        return false; // no cycle
-    }
-    const visiting = new Set<string>();
-    const visited = new Set<string>();
-    const topOrder: string[] = [];
-    for(const node of adjList.keys()) {
-        if(dfs(node) === true) {
-            return "";
-        }
-    }
-    return topOrder.reverse().join("");
+
+    return kahns(adjList);
 };
+
+function kahns(adjList: Map<string, Set<string>>): string {
+    const inDegree = new Map<string, number>();
+    for(const char of adjList.keys()) {
+        inDegree.set(char, 0);
+    }
+    for(const char of adjList.keys()) {
+        for(const neighbor of adjList.get(char)) {
+            inDegree.set(neighbor, inDegree.get(neighbor) + 1);
+        }
+    }
+    const queue: string[] = [];
+    for(const [char, count] of inDegree.entries()) {
+        if(count === 0) {
+            queue.push(char);
+        }
+    }
+    const topOrder: string[] = [];
+    while(queue.length > 0) {
+        const current = queue.shift();
+        topOrder.push(current);
+        for(const neighbor of adjList.get(current)) {
+            inDegree.set(neighbor, inDegree.get(neighbor) - 1);
+            if(inDegree.get(neighbor) === 0) {
+                queue.push(neighbor);
+            }
+        }
+    }
+    if(topOrder.length === adjList.size) {
+        return topOrder.join("");
+    } else {
+        return "";
+    }
+}
