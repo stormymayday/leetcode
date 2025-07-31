21a1c74596023dcd35128424a3703b9b2c3d4d55
Aidar <stormymayday7@gmail.com>
2025-07-31 15:41:46 -0700

    Time: 12 ms (59.85%) | Memory: 60.6 MB (81.06%) - LeetSync

--- solutions/323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.ts
+++ solutions/323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.ts
@@ -1,16 +1,16 @@
 function countComponents(n: number, edges: number[][]): number {
     const roots: number[] = [];
-    const sizes: number[] = [];
+    const heights: number[] = [];
     for(let i = 0; i < n; i += 1) {
         roots.push(i);
-        sizes.push(1);
+        heights.push(1);
     }
     for(const edge of edges) {
         const [a, b] = edge;
-        union(roots, sizes, a, b);
+        union(roots, heights, a, b);
     }
     let count = 0;
-    for(let i = 0; i < roots.length; i += 1) {
+    for(let i = 0; i < n; i += 1) {
         if(roots[i] === i) {
             count += 1;
         }
@@ -18,28 +18,27 @@ function countComponents(n: number, edges: number[][]): number {
     return count;
 };
 
-function union(roots: number[], sizes: number[], nodeA: number, nodeB: number): void {
-    const rootA = find(roots, nodeA);
-    const rootB = find(roots, nodeB);
-
-    if(rootA === rootB) {
-        return;
-    }
-
-    if(sizes[rootA] >= sizes[rootB]) {
-        roots[rootB] = rootA;
-        sizes[rootA] += sizes[rootB];
-    } else {
-        roots[rootA] = rootB;
-        sizes[rootB] += sizes[rootA];
+// Union by Height
+function union(roots: number[], heights: number[], a: number, b: number):void {
+    const rootA = find(roots, a);
+    const rootB = find(roots, b);
+    if(rootA !== rootB) {
+        if(heights[rootA] > heights[rootB]) {
+            roots[rootB] = rootA;
+        } else if(heights[rootB] > heights[rootA]) {
+            roots[rootA] = rootB;
+        } else {
+            roots[rootA] = rootB;
+            heights[rootB] += 1;
+        }
     }
 }
 
-function find(roots: number[], src: number):number {
-    if(roots[src] === src) {
-        return src;
+function find(roots: number[], node: number): number {
+    if(roots[node] === node) {
+        return node;
     }
-    const found = find(roots, roots[src]);
-    roots[src] = found;
-    return found;
+    const root = find(roots, roots[node]);
+    roots[node] = root; // path compression
+    return root;
 }
