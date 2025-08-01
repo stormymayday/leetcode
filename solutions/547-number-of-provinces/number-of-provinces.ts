70890dbbf1c3d3a925145572efdc0debdaa34421
Aidar <stormymayday7@gmail.com>
2025-07-31 20:34:41 -0700

    Time: 6 ms (37.98%) | Memory: 58.8 MB (41.25%) - LeetSync

---
+++ solutions/547-number-of-provinces/number-of-provinces.ts
@@ -0,0 +1,56 @@
+function findCircleNum(isConnected: number[][]): number {
+    const n = isConnected.length;
+    const unionFind = new UnionFind(n);
+    for(let row = 0; row < n; row += 1) {
+        for(let col = 0; col < n; col += 1) {
+            if(isConnected[row][col] === 1) {
+                unionFind.union(row, col);
+            }
+        }
+    }
+    return unionFind.getNumComponents();
+};
+
+class UnionFind {
+    private roots: Map<number, number>;
+    private heights: Map<number, number>;
+    private numComponents: number;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.heights = new Map();
+        this.numComponents = n;
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.heights.set(i, 1);
+        }
+    }
+    find(x: number): number {
+        let root = this.roots.get(x);
+        while(root !== this.roots.get(root)) {
+            this.roots.set(root, this.roots.get(this.roots.get(root)));
+            root = this.roots.get(root);
+        }
+        return root;
+    }
+    union(x: number, y: number): boolean {
+        let rootX = this.find(x);
+        let rootY = this.find(y);
+        if(rootX !== rootY) {
+            if(this.heights.get(rootX) > this.heights.get(rootY)) {
+                this.roots.set(rootY, rootX);
+            } else if(this.heights.get(rootY) > this.heights.get(rootX)) {
+                this.roots.set(rootX, rootY);
+            } else {
+                this.roots.set(rootX, rootY);
+                this.heights.set(rootY, this.heights.get(rootY) + 1);
+            }
+            this.numComponents -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    getNumComponents(): number {
+        return this.numComponents;
+    }
+}
