d3b09ce682b40a7f6175ee14cc926bc85a061d3b
Aidar <stormymayday7@gmail.com>
2025-08-01 10:59:30 -0700

    Time: 10 ms (19.58%) | Memory: 57.5 MB (92.88%) - LeetSync

--- solutions/547-number-of-provinces/number-of-provinces.ts
+++ solutions/547-number-of-provinces/number-of-provinces.ts
@@ -1,54 +1,55 @@
 function findCircleNum(isConnected: number[][]): number {
     const n = isConnected.length;
-    const uf = new UnionFind(n);
-    for(let r = 0; r < n; r += 1) {
-        for(let c = 0; c < n; c += 1) {
-            if(isConnected[r][c] === 1) {
-                uf.union(r, c);
+    const unionFind = new UnionFind(n);
+    for(let row = 0; row < n; row += 1) {
+        for(let col = 0; col < n; col += 1) {
+            if(isConnected[row][col] === 1) {
+                unionFind.union(row, col);
             }
         }
     }
-    return uf.getNumComponents();
+    return unionFind.getNumComponents();
 };
 
 class UnionFind {
     private roots: Map<number, number>;
-    private sizes: Map<number, number>;
+    private heights: Map<number, number>;
     private numComponents: number;
     constructor(n: number) {
         this.roots = new Map();
-        this.sizes = new Map();
+        this.heights = new Map();
         this.numComponents = n;
         for(let i = 0; i < n; i += 1) {
             this.roots.set(i, i);
-            this.sizes.set(i, 1);
+            this.heights.set(i, 1);
         }
     }
-    union(x: number, y: number): boolean {
-        const rootX = this.find(x);
-        const rootY = this.find(y);
-        if(rootX === rootY) {
-            return false;
-        } else {
-            if(this.sizes.get(rootX) > this.sizes.get(rootY)) {
-                this.roots.set(rootY, rootX);
-                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
-            } else {
-                this.roots.set(rootX, rootY);
-                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
-            }
-            this.numComponents -= 1;
-            return true;
-        }
-    }
-    find(x: number):number {
+    find(x: number): number {
         const parent = this.roots.get(x);
-        if(parent !== x) {
+        if (parent !== x) {
             this.roots.set(x, this.find(parent));
         }
         return this.roots.get(x);
     }
-    getNumComponents():number {
+    union(x: number, y: number): boolean {
+        let rootX = this.find(x);
+        let rootY = this.find(y);
+        if(rootX !== rootY) {
+            if(this.heights.get(rootX) > this.heights.get(rootY)) {
+                this.roots.set(rootY, rootX);
+            } else if(this.heights.get(rootY) > this.heights.get(rootX)) {
+                this.roots.set(rootX, rootY);
+            } else {
+                this.roots.set(rootX, rootY);
+                this.heights.set(rootY, this.heights.get(rootY) + 1);
+            }
+            this.numComponents -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+    getNumComponents(): number {
         return this.numComponents;
     }
 }
