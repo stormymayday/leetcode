7a7c6148ac1531cd440d4ca792e871680570fc32
Aidar <stormymayday7@gmail.com>
2025-08-02 10:06:10 -0700

    Time: 4 ms (86.21%) | Memory: 58.4 MB (44.83%) - LeetSync

---
+++ solutions/1085-the-earliest-moment-when-everyone-become-friends/the-earliest-moment-when-everyone-become-friends.ts
@@ -0,0 +1,54 @@
+function earliestAcq(logs: number[][], n: number): number {
+    const uf = new UnionFind(n);
+    const sorted = logs.sort((a,b) => a[0] - b[0]);
+    for(const log of sorted) {
+        const [timestamp, a, b] = log;
+        uf.union(a, b);
+        if(uf.getNumComponents() === 1) {
+            return timestamp;
+        }
+    }
+    return -1;
+};
+
+class UnionFind {
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    private numComponents: number;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.sizes = new Map();
+        this.numComponents = n;
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.sizes.set(i, 1);
+        }
+    }
+    private find(x: number): number {
+        const parent = this.roots.get(x);
+        if(parent !== x) {
+            this.roots.set(x, this.find(parent));
+        }
+        return this.roots.get(x);
+    }
+    union(x: number, y: number):boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if(rootX === rootY) {
+            return false;
+        } else {
+            if(this.sizes.get(rootX) > this.sizes.get(rootY)) {
+                this.roots.set(rootY, rootX);
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+            } else {
+                this.roots.set(rootX, rootY);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
+            }
+            this.numComponents -= 1;
+            return true;
+        }
+    }
+    getNumComponents():number {
+        return this.numComponents;
+    }
+}
