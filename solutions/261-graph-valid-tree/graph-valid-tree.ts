3d63197c080a05e32ba2ce5763c57669ccccfe4d
Aidar <stormymayday7@gmail.com>
2025-08-01 21:48:18 -0700

    Time: 0 ms (100.00%) | Memory: 56.5 MB (97.89%) - LeetSync

--- solutions/261-graph-valid-tree/graph-valid-tree.ts
+++ solutions/261-graph-valid-tree/graph-valid-tree.ts
@@ -1,36 +1,52 @@
+class UnionFind {
+    private roots: Map<number, number>;
+    private heights: Map<number, number>;
+    private numComponents: number;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.heights = new Map();
+        this.numComponents = n;
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.heights.set(i, 1);
+        }
+    }
+    union(x: number, y: number): boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if(rootX === rootY) {
+            return false;
+        } else {
+            if(this.heights.get(rootX) > this.heights.get(rootY)) {
+                this.roots.set(rootY, rootX);
+            } else if(this.heights.get(rootY) > this.heights.get(rootX)) {
+                this.roots.set(rootX, rootY);
+            } else {
+                this.roots.set(rootX, rootY);
+                this.heights.set(rootY, this.heights.get(rootY) + 1);
+            }
+            this.numComponents -= 1;
+            return true;
+        }
+    }
+    find(x: number): number {
+        const parent = this.roots.get(x);
+        if(parent !== x) {
+            this.roots.set(x, this.find(parent));
+        }
+        return this.roots.get(x);
+    }
+}
 function validTree(n: number, edges: number[][]): boolean {
-    const adjList = buildAdjList(n, edges);
-    const visited = new Set<number>();
-    const result = dfs(adjList, 0, visited, -1);
-    return result && visited.size === adjList.size;
-};
-
-function dfs(adjList: Map<number, Set<number>>, src: number, visited: Set<number>, prev: number): boolean {
-    if(visited.has(src)) {
+    if(n - 1 !== edges.length) {
         return false;
     }
-
-    visited.add(src);
-
-    for(const neighbor of adjList.get(src)) {
-        if(neighbor !== prev) {
-            if(dfs(adjList, neighbor, visited, src) === false) {
-                return false;
-            }
+    const uf = new UnionFind(n);
+    for(const edge of edges) {
+        const [a, b] = edge;
+        if(uf.union(a, b) === false) {
+            return false;
         }
     }
     return true;
-}
-
-function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
-    const adjList = new Map();
-    for(let i = 0; i < n; i += 1) {
-        adjList.set(i, new Set());
-    }
-    for(const edge of edges) {
-        const [a, b] = edge;
-        adjList.get(a).add(b);
-        adjList.get(b).add(a);
-    }
-    return adjList;
-}
+};
