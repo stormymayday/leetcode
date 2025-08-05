9027799b4db7c7ba03b45937ce6902e364f929b4
Aidar <stormymayday7@gmail.com>
2025-08-04 19:24:46 -0700

    Time: 809 ms (37.28%) | Memory: 197.4 MB (28.19%) - LeetSync

--- solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
+++ solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
@@ -1,62 +1,27 @@
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
     const adjList = buildAdjList(n, edges);
     return dfs(adjList, source, destination, new Set());
-    // return bfs(adjList, source, destination, new Set());
 };
 
-function bfs(adjList, src, dst, visited) {
-
-    // adding source right away to prevent it from being re-queued if it appears as a neighbor of other nodes
-    visited.add(src);
-
-    const queue = [src];
-
-    while(queue.length > 0) {
-        const current = queue.shift();
-
-        if(current === dst) {
-            return true;
-        }
-
-        for(const neighbor of adjList.get(current)) {
-            if(!visited.has(neighbor)) {
-                visited.add(neighbor);
-                queue.push(neighbor);
-            }
-        }
-
-    }
-
-    return false;
-
-}
-
-function dfs(adjList, src, dst, visited) {
-
-    // Will cause and error!
-    // if(visited.has(src)) {
-    //     return false;
-    // }
-
+function dfs(adjList: Map<number, Set<number>>, src: number, dst: number, visited: Set<number>):boolean {
     if(src === dst) {
         return true;
     }
-
+    
     visited.add(src);
-
+    
     for(const neighbor of adjList.get(src)) {
         if(!visited.has(neighbor)) {
-            visited.add(neighbor);
             if(dfs(adjList, neighbor, dst, visited) === true) {
                 return true;
             }
         }
     }
-
+    
     return false;
 }
 
-function buildAdjList(n: number, edges:number[][]):Map<number, Set<number>> {
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
     const adjList = new Map();
     for(let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
