373b1c0271711d2207f81378a4ffa863185e6b82
Aidar <stormymayday7@gmail.com>
2025-08-06 13:41:42 -0700

    Time: 359 ms (85.00%) | Memory: 151.1 MB (80.91%) - LeetSync

--- solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
+++ solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
@@ -1,37 +1,47 @@
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
-    const adjList = buildAdjList(n, edges);
-    return dfs(adjList, source, destination, new Set());
-};
-
-function dfs(adjList: Map<number, Set<number>>, src: number, dst: number, visited: Set<number>): boolean {
-    
-    if(src === dst) {
-        return true;
-    }
-
-    visited.add(src);
-
-    for(const neighbor of adjList.get(src)) {
-        if(!visited.has(neighbor)) {
-            if(dfs(adjList, neighbor, dst, visited) === true) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-
-}
-
-function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
-    const adjList = new Map();
-    for(let i = 0; i < n; i += 1) {
-        adjList.set(i, new Set());
-    }
+    const uf = new UnionFind(n);
     for(const edge of edges) {
         const [src, dst] = edge;
-        adjList.get(src).add(dst);
-        adjList.get(dst).add(src);
+        uf.union(src, dst);
     }
-    return adjList;
+    return uf.isSameComponent(source, destination);
+};
+
+class UnionFind {
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.sizes = new Map();
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.sizes.set(i, 1);
+        }
+    }
+    find(x: number):number {
+        const root = this.roots.get(x);
+        if(root !== x) {
+            this.roots.set(x, this.find(root));
+        }
+        return this.roots.get(x);
+    }
+    union(x: number, y: number): boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if(rootX === rootY) {
+            return false;
+        } else {
+            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
+                this.roots.set(rootY, rootX);
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+            } else {
+                this.roots.set(rootX, rootY);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
+            }
+            return true;
+        }
+    }
+    isSameComponent(x: number, y: number):boolean {
+        return this.find(x) === this.find(y);
+    }
 }
