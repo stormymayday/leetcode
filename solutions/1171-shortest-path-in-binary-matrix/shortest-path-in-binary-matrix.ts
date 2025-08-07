bc6ba819ac5d9a336575a3abb0fd1c7d4b8bf6cf
Aidar <stormymayday7@gmail.com>
2025-08-07 15:44:45 -0700

    Time: 149 ms (27.13%) | Memory: 74.5 MB (12.79%) - LeetSync

--- solutions/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.ts
+++ solutions/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.ts
@@ -1,50 +1,40 @@
 function shortestPathBinaryMatrix(grid: number[][]): number {
-    const visited = new Set<string>();
-    return bfs(grid, 0, 0, visited);
-};
-
-function bfs(grid: number[][], r: number, c: number, visited: Set<string>):number {
-    // Edge Case: starting position is a wall
-    if(grid[r][c] === 1) {
+    // Edge Case: starting cell is 1
+    if(grid[0][0] === 1) {
         return -1;
     }
+    return matrixBFS(grid);
+};
 
-    visited.add(`${r},${c}`);
-    const queue = [[r, c, 1]];
+function matrixBFS(grid:number[][]):number {
+    const visited = new Set<string>();
+    visited.add(`${0},${0}`);
+    const queue: [number, number, number][] = [[0, 0, 1]]; // row, col, distance
     while(queue.length > 0) {
         const [row, col, distance] = queue.shift();
-        if(row === grid.length -1 && col === grid[0].length - 1) {
+        if(row === grid.length - 1 && col === grid[0].length - 1) {
             return distance;
         }
         const deltas = [
-            [-1, -1], [-1, 0], [-1, 1],
-            [0, -1],           [0, 1],
-            [1, -1], [1, 0],   [1, 1]
+            [-1, -1],[-1, 0],[-1,1],
+            [0, -1],         [0, 1],
+            [1, -1], [1, 0], [1, 1]
         ];
         for(const delta of deltas) {
             const [rowDelta, colDelta] = delta;
-            const neighborRow = row + rowDelta;
-            const neighborCol = col + colDelta;
+            const neighborRow = rowDelta + row;
+            const neighborCol = colDelta + col;
             const neighborPosition = `${neighborRow},${neighborCol}`;
             if(
-                isInBounds(grid, neighborRow, neighborCol) === true
-                && !visited.has(neighborPosition)
-                && grid[neighborRow][neighborCol] !== 1
+                0 <= neighborRow && neighborRow < grid.length &&
+                0 <= neighborCol && neighborCol < grid[0].length &&
+                !visited.has(neighborPosition) &&
+                grid[neighborRow][neighborCol] !== 1
             ) {
                 visited.add(neighborPosition);
-                queue.push([
-                    neighborRow,
-                    neighborCol,
-                    distance + 1
-                ]);
+                queue.push([neighborRow, neighborCol, distance + 1]);
             }
         }
     }
-    return -1;
-}
-
-function isInBounds(grid:number[][], r: number, c: number):boolean {
-    const rowInBounds:boolean = 0 <= r && r < grid.length;
-    const colInBounds:boolean = 0 <= c && c < grid[0].length;
-    return rowInBounds === true && colInBounds === true;
+    return -1; // not found
 }
