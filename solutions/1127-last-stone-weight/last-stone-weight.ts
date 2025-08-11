d3051a0d9d0a8eda6af6a9db14131dded4137d9c
Aidar <stormymayday7@gmail.com>
2025-08-10 22:09:38 -0700

    Time: 2 ms (68.44%) | Memory: 55.7 MB (89.34%) - LeetSync

--- solutions/1127-last-stone-weight/last-stone-weight.ts
+++ solutions/1127-last-stone-weight/last-stone-weight.ts
@@ -1,79 +1,88 @@
-class MaxHeap {
-    values: number[];
-    constructor() {
-        this.values = [];
-    }
-    push(val: number):void {
-        this.values.push(val);
-        let current = this.size() - 1;
-        while(current > 0) {
-            const parentIndex = Math.floor((current - 1)/2);
-            if(this.values[current] > this.values[parentIndex]) {
-                this.swap(current, parentIndex);
-                current = parentIndex;
-            } else {
-                break;
-            }
-        }
-    }
-    pop(): number | null {
-        if(this.size() === 0) {
-            return null;
-        }
-        if(this.size() === 1) {
-            return this.values.pop();
-        }
-        const max = this.values[0];
-        this.values[0] = this.values.pop();
-        this.siftDown(0);
-        return max;
-    }
-    heapify(nums: number[]):void {
-        this.values = nums;
-        let current = Math.floor((this.size() - 2)/2);
-        while(current >= 0) {
-            this.siftDown(current);
-            current -= 1;
-        }
-    }
-    siftDown(index: number):void {
-        let current = index;
-        while(current < this.size() - 1) {
-            const leftChildIndex = 2 * current + 1;
-            const rightChildIndex = 2 * current + 2;
-            const leftChildValue = this.values[leftChildIndex] === undefined ? -Infinity : this.values[leftChildIndex];
-            const rightChildValue = this.values[rightChildIndex] === undefined ? -Infinity : this.values[rightChildIndex];
-            const largerChildIndex = leftChildValue > rightChildValue ? leftChildIndex : rightChildIndex;
-            const largetChildValue = leftChildValue > rightChildValue ? leftChildValue : rightChildValue;
-            if(this.values[current] < largetChildValue) {
-                this.swap(current, largerChildIndex);
-                current = largerChildIndex;
-            } else {
-                break;
-            }
-        }
-    }
-    swap(index1: number, index2: number):void {
-        const temp = this.values[index1];
-        this.values[index1] = this.values[index2];
-        this.values[index2] = temp;
-    }
-    size():number {
-        return this.values.length;
-    }
-}
 function lastStoneWeight(stones: number[]): number {
     const maxHeap = new MaxHeap();
     maxHeap.heapify(stones);
-    while(maxHeap.size() > 0) {
-        if(maxHeap.size() > 1) {
-            const x = maxHeap.pop();
-            const y = maxHeap.pop();
-            maxHeap.push(Math.abs(x - y));
-        } else if(maxHeap.size() === 1) {
-            return maxHeap.pop();
+    while(maxHeap.length > 0) {
+        if(maxHeap.length > 1) {
+            const stone1 = maxHeap.pop();
+            const stone2 = maxHeap.pop();
+            const stone3 = stone1 - stone2;
+            if(stone3 > 0) {
+                maxHeap.push(stone3);
+                if(maxHeap.length === 1) {
+                    return maxHeap.peek();
+                }
+            }
         } else {
-            return 0;
+            return maxHeap.peek();
         }
     }
+    return 0;
 };
+
+class MaxHeap {
+    private data: number[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val:number):void {
+        this.data.push(val);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1)/2);
+        while(currIdx > 0 && this.data[currIdx] > this.data[parentIdx]) {
+            const temp = this.data[currIdx];
+            this.data[currIdx] = this.data[parentIdx];
+            this.data[parentIdx] = temp;
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1)/2);
+        }
+    }
+    pop():number | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length -= 1;
+            return this.data.pop();
+        }
+        const max = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return max;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildVal = this.data[leftChildIdx] === undefined ? -Infinity : this.data[leftChildIdx];
+            const rightChildVal = this.data[rightChildIdx] === undefined ? -Infinity : this.data[rightChildIdx];
+            const biggerChildIdx = leftChildVal > rightChildVal ? leftChildIdx : rightChildIdx;
+            const biggerChildVal = leftChildVal > rightChildVal ? leftChildVal : rightChildVal;
+            if(this.data[currIdx] < biggerChildVal) {
+                const temp = this.data[currIdx];
+                this.data[currIdx] = this.data[biggerChildIdx];
+                this.data[biggerChildIdx] = temp;
+                currIdx = biggerChildIdx;
+            } else {
+                break;
+            }
+        }
+    }
+    heapify(nums: number[]):void {
+        const n = nums.length;
+        this.data = [...nums];
+        this.length = n;
+        let currIdx = Math.floor((n - 2)/2);
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
+    }
+    peek():number | null {
+        return this.length > 0 ? this.data[0] : null;
+    }
+}
