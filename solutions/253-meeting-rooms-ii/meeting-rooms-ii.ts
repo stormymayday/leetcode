37de6ac9519a169e01439b9a77c313a5cfc6c8ee
Aidar <stormymayday7@gmail.com>
2025-08-11 21:07:46 -0700

    Time: 9 ms (64.74%) | Memory: 61.5 MB (17.90%) - LeetSync

---
+++ solutions/253-meeting-rooms-ii/meeting-rooms-ii.ts
@@ -0,0 +1,70 @@
+function minMeetingRooms(intervals: number[][]): number {
+    intervals.sort((a, b) => a[0] - b[0]);
+    const minHeap = new MinHeap();
+    for(const interval of intervals) {
+        const [start, end] = interval;
+        if(minHeap.length !== 0 && start >= minHeap.peek()) {
+            minHeap.pop();
+        }
+        minHeap.push(end);
+    }
+    return minHeap.length;
+};
+
+class MinHeap {
+    private data: number[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val:number):void {
+        this.data.push(val);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx] < this.data[parentIdx]) {
+            const temp = this.data[currIdx];
+            this.data[currIdx] = this.data[parentIdx];
+            this.data[parentIdx] = temp;
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop():number | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length -= 1;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number):void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildVal = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx];
+            const rightChildVal = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx];
+            const smallerChildIdx = leftChildVal < rightChildVal ? leftChildIdx : rightChildIdx;
+            const smallerChildVal = leftChildVal < rightChildVal ? leftChildVal : rightChildVal;
+            if(this.data[currIdx] > smallerChildVal) {
+                const temp = this.data[currIdx];
+                this.data[currIdx] = this.data[smallerChildIdx];
+                this.data[smallerChildIdx] = temp;
+                currIdx = smallerChildIdx;
+            } else {
+                break;
+            }
+        }
+    }
+    peek():number | null {
+        return this.length > 0 ? this.data[0] : null;
+    }
+}
