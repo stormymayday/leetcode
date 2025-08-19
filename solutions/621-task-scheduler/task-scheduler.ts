8c3574951339f0d4a131034a4b6cca72206dd6bf
Aidar <stormymayday7@gmail.com>
2025-08-18 19:04:06 -0700

    Time: 43 ms (52.94%) | Memory: 67.2 MB (21.18%) - LeetSync

--- solutions/621-task-scheduler/task-scheduler.ts
+++ solutions/621-task-scheduler/task-scheduler.ts
@@ -1,172 +1,126 @@
-class MaxHeap {
-    values: number[];
-    constructor() {
-        this.values = [];
-    }
-    push(val:number):void {
-        this.values.push(val);
-        let current = this.values.length - 1;
-        // sift up
-        while(current > 0) {
-            const parentIndex = Math.floor((current - 1)/2);
-            if(this.values[current] > this.values[parentIndex]) {
-                this.swap(current, parentIndex);
-                current = parentIndex;
-            } else {
-                break;
-            }
-        }
-    }
-    pop(): number | undefined {
-        if(this.values.length === 0) {
-            return undefined;
-        }
-        if(this.values.length === 1) {
-            return this.values.pop();
-        }
-        const max = this.values[0];
-        this.values[0] = this.values.pop();
-        this.siftDown(0);
-        return max;
-    }
-    heapify(nums: number[]):void {
-        this.values = nums;
-        let current = Math.floor((this.values.length - 2)/2);
-        while(current >= 0) {
-            this.siftDown(current);
-            current -= 1;
-        }
-    }
-    siftDown(index: number):void {
-        let current = index;
-        while(current < this.values.length - 1) {
-            const leftChildIndex = 2 * current + 1;
-            const rightChildIndex = 2 * current + 2;
-            const leftChildValue = this.values[leftChildIndex] === undefined ? -Infinity : this.values[leftChildIndex];
-            const rightChildValue = this.values[rightChildIndex] === undefined ? -Infinity : this.values[rightChildIndex];
-            const largerChildIndex = leftChildValue > rightChildValue ? leftChildIndex : rightChildIndex;
-            const largerChildValue = leftChildValue > rightChildValue ? leftChildValue : rightChildValue;
-            if(this.values[current] < largerChildValue) {
-                this.swap(current, largerChildIndex);
-                current = largerChildIndex;
-            } else {
-                break;
-            }
-        }
-    }
-    swap(index1:number, index2:number):void {
-        const temp = this.values[index1];
-        this.values[index1] = this.values[index2];
-        this.values[index2] = temp;
-    }
-    size():number {
-        return this.values.length;
-    }
-}
-
-type Node<T> = {
-    value: T;
-    next?: Node<T>;
-};
-
-class CustomQueue<T> {
-    public length: number;
-
-    private head?: Node<T>;
-    private tail?: Node<T>;
-
-    constructor() {
-        this.head = this.tail = undefined;
-        this.length = 0;
-    }
-
-    enqueue(item: T): void {
-        const node = { value: item } as Node<T>;
-        this.length++;
-        if (!this.tail) {
-            this.tail = this.head = node;
-            return;
-        }
-
-        this.tail.next = node;
-        this.tail = node;
-    }
-
-    deque(): T | undefined {
-        if (!this.head) {
-            return undefined;
-        }
-
-        this.length--;
-
-        const head = this.head;
-        this.head = this.head.next;
-
-        // free
-        head.next = undefined;
-
-        if (this.length === 0) {
-            this.tail = undefined;
-        }
-
-        return head.value;
-    }
-
-    peek(): T | undefined {
-        return this.head?.value;
-    }
-
-    size(): number {
-        return this.length;
-    }
-}
-
 function leastInterval(tasks: string[], n: number): number {
-    // 1. Create a task to count map
-    const taskCount = new Map();
-    for(const task of tasks) {
-        if(!taskCount.has(task)) {
-            taskCount.set(task, 1);
-        } else {
-            taskCount.set(task, taskCount.get(task) + 1);
+    // 1. Create a frequency count map
+    const freqCount = new Map<string, number>();
+    for(let i = 0; i < tasks.length; i += 1) {
+        const task = tasks[i];
+        if(!freqCount.has(task)) {
+            freqCount.set(task, 0);
         }
+        freqCount.set(task, freqCount.get(task) + 1);
     }
 
-    // 2. Initialize a priority queue and push task count values
-    const prioQueue = new MaxHeap();
-    for(const [task, count] of taskCount.entries()) {
-        prioQueue.push(count);
+    // 2. Initialze priority queue
+    const maxPQ = new CustomMaxPriorityQueue<string>();
+    const queueNodes: QueueNode<string>[] = [];
+    for(const [char, count] of freqCount.entries()) {
+        const newNode = new QueueNode<string>(char, count);
+        queueNodes.push(newNode);
     }
+    maxPQ.heapify(queueNodes);
 
-    // 3. Set up a time count and a cooldown queue
+    // 3. Main Logic
+    const cooldownQueue: [string, number, number][] = [];
     let time = 0;
-    const cooldownQueue = new CustomQueue<[number, number]>(); // [count, nextAvailableTime (time + n)]
+    while(maxPQ.length > 0 || cooldownQueue.length > 0) {
 
-    // 4. Iterate while either priority queue or cooldown queue are not empty
-    while(prioQueue.size() > 0 || cooldownQueue.size() > 0) {
-
-        // 4.1 increment total time
+        // 1. start execution
         time += 1;
 
-        // 4.2 check if there are items in the prioQueue
-        if(prioQueue.size() > 0) {
-            // 4.2.1 pop and decrement the count by 1 (task processed)
-            const count = prioQueue.pop() - 1;
-            // 4.2.2 if the count is not zero push it to the cooldown queue together with it's next available time (time + n)
+        // 2. pop the pq if it is not empty
+        if(maxPQ.length > 0) {
+            let {val: char, prio: count} = maxPQ.pop();
+            // 2.1. decrement the count
+            count -= 1;
+            // 2.2 put char into the cooldown queue (if count is greater than 0)
             if(count > 0) {
-                cooldownQueue.enqueue([count, time + n]);
+                cooldownQueue.push([char, count, time + n]);
             }
         }
 
-        // 4.3 check if cooldownQueue is not empty AND the next item is off cooldown (next available time === current time)
-        if(cooldownQueue.size() > 0 && cooldownQueue.peek()[1] === time) {
-            // 4.3.1 pop the item and push 'count' to the prioQueue
-            const [count, nextAvailableTime] = cooldownQueue.deque();
-            prioQueue.push(count);
+        // 3. check the cooldown queue
+        if(cooldownQueue.length > 0 && cooldownQueue[0][2] <= time) {
+            const [char, count, availableTime] = cooldownQueue.shift();
+            // put item back into the priority queue
+            maxPQ.push(char, count);
         }
-
     }
-
-    // 5. return total time
     return time;
 };
+
+class QueueNode<T> {
+    val: T;
+    prio: number;
+    constructor(val: T, prio: number) {
+        this.val = val;
+        this.prio = prio;
+    }
+}
+
+class CustomMaxPriorityQueue<T> {
+    private data: QueueNode<T>[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: T, prio: number): void {
+        const newNode = new QueueNode<T>(val, prio);
+        this.data.push(newNode);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx].prio > this.data[parentIdx].prio) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): QueueNode<T> | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildPrio = this.data[leftChildIdx] === undefined ? -Infinity : this.data[leftChildIdx].prio;
+            const rightChildPrio = this.data[rightChildIdx] === undefined ? -Infinity : this.data[rightChildIdx].prio;
+            const largerChildIdx = leftChildPrio > rightChildPrio ? leftChildIdx : rightChildIdx;
+            const largerChildPrio = leftChildPrio > rightChildPrio ? leftChildPrio : rightChildPrio;
+            if(this.data[currIdx].prio < largerChildPrio) {
+                this.swap(currIdx, largerChildIdx);
+                currIdx = largerChildIdx;
+            } else {
+                break;
+            }
+        }
+    }
+    top(): number | null {
+        return this.length > 0 ? this.data[0].prio : null;
+    }
+    heapify(values: QueueNode<T>[]): void {
+        this.data = [...values];
+        this.length = values.length;
+        let currIdx = Math.floor((this.length - 2) / 2);
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
+    }
+}
