78fa3b1f81aa18631dfb945051ac3bea02b2a4c7
Aidar <stormymayday7@gmail.com>
2025-08-21 17:45:42 -0700

    Time: 1406 ms (24.64%) | Memory: 167.3 MB (7.24%) - LeetSync

--- solutions/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.ts
+++ solutions/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.ts
@@ -11,20 +11,27 @@ function minCostConnectPoints(points: number[][]): number {
         }
     }
 
-    // sort by weight
-    edges.sort((a, b) => a[2] - b[2]);
-
     // Initalize Union Find
     const uf = new UnionFind(n);
 
-    let minCost = 0;
+    // Initialize Priority Queue
+    const minPQ = new CustomMinPriorityQueue<[number, number]>(); // val: [src, dst], prio: weight
+    const pqNodes: PriorityQueueNode<[number, number]>[] = [];
     for(const edge of edges) {
         const [src, dst, weight] = edge;
+        const newNode = new PriorityQueueNode([src, dst] as [number, number], weight);
+        pqNodes.push(newNode);
+    }
+    minPQ.heapify(pqNodes);
+
+    let minCost = 0;
+    let edgesUsed = 0;
+    while(edgesUsed !== n - 1) {
+        const {val, prio: weight} = minPQ.pop();
+        const [src, dst] = val;
         if(uf.union(src, dst) === true) {
             minCost += weight;
-            if(uf.getNumComponents() === 1) {
-                break;
-            }
+            edgesUsed += 1 ;
         }
     }
     return minCost;
@@ -72,5 +79,83 @@ class UnionFind {
     }
     getNumComponents(): number {
         return this.numComponents;
+    }
+}
+
+class PriorityQueueNode<T> {
+    val: T;
+    prio: number;
+    constructor(val: T, prio: number) {
+        this.val = val;
+        this.prio = prio;
+    }
+}
+
+class CustomMinPriorityQueue<T> {
+    private data: PriorityQueueNode<T>[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: T, prio: number): void {
+        const newNode = new PriorityQueueNode<T>(val, prio);
+        this.data.push(newNode);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): PriorityQueueNode<T> | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildPrio = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx].prio;
+            const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
+            const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
+            const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
+            if(this.data[currIdx].prio > smallerChildPrio) {
+                this.swap(currIdx, smallerChildIdx);
+                currIdx = smallerChildIdx; 
+            } else {
+                break;
+            }
+        }
+    }
+    heapify(vals: PriorityQueueNode<T>[]): void {
+        this.data = [...vals];
+        this.length = vals.length;
+        let currIdx = Math.floor((this.length - 2) / 2);
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
+    }
+    top(): number | null {
+        return this.length > 0 ? this.data[0].prio : null;
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
     }
 }
