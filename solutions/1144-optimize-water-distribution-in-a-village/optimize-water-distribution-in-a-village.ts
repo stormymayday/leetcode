678324f21205b89ca72db5da2d9ce1cca13f44d7
Aidar <stormymayday7@gmail.com>
2025-08-22 19:23:58 -0700

    Time: 153 ms (0.00%) | Memory: 83.1 MB (0.00%) - LeetSync

--- solutions/1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.ts
+++ solutions/1144-optimize-water-distribution-in-a-village/optimize-water-distribution-in-a-village.ts
@@ -11,68 +11,130 @@ function minCostToSupplyWater(n: number, wells: number[], pipes: number[][]): nu
         edges.push([src, dst, weight]);
     }
 
-    // Sort edges by cost
-    edges.sort((a, b) => a[2] - b[2]);
+    // Create and Adjacency List
+    const adjList = new Map<number, [number, number][]>(); // src -> [dst, weight]
+    for(let i = 0; i < n + 1; i += 1) {
+        adjList.set(i, []);
+    }
+    for(const edge of edges) {
+        const [src, dst, weight] = edge;
+        adjList.get(src).push([dst, weight]);
+        adjList.get(dst).push([src, weight]);
+    }
 
-    const uf = new UnionFind(n);
+    // Mark first node as visited and queue it's neighbors
+    const visited = new Set<number>();
+    visited.add(0);
+    const minPQ = new CustomMinPriorityQueue<[number, number]>(); // val: [src, dst], prio: weight
+    for(const neighbor of adjList.get(0)) {
+        const [neighborNode, weight] = neighbor;
+        minPQ.push([0, neighborNode], weight);
+    }
+
+    // Variables
     let edgesUsed = 0;
     let mstCost = 0;
 
-    for(const edge of edges) {
-        const [src, dst, weight] = edge;
-        if(uf.union(src, dst) === true) {
-            mstCost += weight;
-            edgesUsed += 1;
-            if(edgesUsed === n) { // Need n edges to connect (including virtual)
-                return mstCost;
+    while(minPQ.length > 0) {
+        const { val: [src, currNode], prio: weight } = minPQ.pop();
+
+        if(visited.has(currNode)) {
+            continue;
+        }
+
+        visited.add(currNode);
+        mstCost += weight;
+        edgesUsed += 1;
+        if(edgesUsed === n) { // including the virtual node 0
+            return mstCost;
+        }
+
+        for(const neighbor of adjList.get(currNode)) {
+            const [neighborNode, neighborWeight] = neighbor;
+            if(!visited.has(neighborNode)) {
+                minPQ.push([currNode, neighborNode], neighborWeight);
             }
         }
     }
 
-    return -1; // Should not happen with valid input
+    return -1; // should not happend with valid input
+
 };
 
-class UnionFind {
-    roots: Map<number, number>;
-    sizes: Map<number, number>;
-    numComponents: number;
-    constructor(n: number) {
-        this.roots = new Map();
-        this.sizes = new Map();
-        this.numComponents = n;
-        for(let i = 0; i <= n; i += 1) {
-            this.roots.set(i, i);
-            this.sizes.set(i, 1);
+class PriorityQueueNode<T> {
+    val: T;
+    prio: number;
+    constructor(val: T, prio: number) {
+        this.val = val;
+        this.prio = prio;
+    }
+}
+
+class CustomMinPriorityQueue<T> {
+    private data: PriorityQueueNode<T>[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: T, prio: number): void {
+        const newNode = new PriorityQueueNode<T>(val, prio);
+        this.data.push(newNode);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
         }
     }
-    find(x: number): number {
-        const root = this.roots.get(x);
-        if(root !== x) {
-            this.roots.set(x, this.find(root));
+    pop(): PriorityQueueNode<T> | null {
+        if(this.length === 0) {
+            return null;
         }
-        return this.roots.get(x);
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
     }
-    union(x: number, y: number): boolean {
-        const rootX = this.find(x);
-        const rootY = this.find(y);
-        if(rootX === rootY) {
-            return false;
-        } else {
-            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
-                this.roots.set(rootY, rootX);
-                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildPrio = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx].prio;
+            const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
+            const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
+            const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
+            if(this.data[currIdx].prio > smallerChildPrio) {
+                this.swap(currIdx, smallerChildIdx);
+                currIdx = smallerChildIdx; 
             } else {
-                this.roots.set(rootX, rootY);
-                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
+                break;
             }
-            this.numComponents -= 1;
-            return true;
         }
     }
-    isSameComponent(x: number, y: number): boolean {
-        return this.find(x) === this.find(y);
+    heapify(vals: PriorityQueueNode<T>[]): void {
+        this.data = [...vals];
+        this.length = vals.length;
+        let currIdx = Math.floor((this.length - 2) / 2);
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
     }
-    getNumComponents(): number {
-        return this.numComponents;
+    top(): number | null {
+        return this.length > 0 ? this.data[0].prio : null;
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
     }
 }
