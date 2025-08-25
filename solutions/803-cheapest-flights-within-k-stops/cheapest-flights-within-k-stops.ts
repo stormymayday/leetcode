68bb3c41b4bfc1e379687c4066db99e08224a0c6
Aidar <stormymayday7@gmail.com>
2025-08-25 12:19:40 -0700

    Time: 22 ms (66.67%) | Memory: 65.8 MB (35.83%) - LeetSync

--- solutions/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.ts
+++ solutions/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.ts
@@ -26,7 +26,7 @@ function findCheapestPrice(n: number, flights: number[][], src: number, dst: num
         }
 
         // If node has been visited AND it took less edges last time, skip
-        if(visited.has(currNode) && visited.get(currNode) <= edgesUsed) {
+        if(visited.has(currNode) && visited.get(currNode) <= edgesUsed || edgesUsed > k) {
             continue;
         }
 
@@ -34,13 +34,15 @@ function findCheapestPrice(n: number, flights: number[][], src: number, dst: num
         visited.set(currNode, edgesUsed);
 
         // We can use no more than k + 1 edges
-        if(edgesUsed > k) {
-            continue;
-        }
+        // if(edgesUsed > k) {
+        //     continue;
+        // }
 
         // Otherwise, visit neighbors
         for(const [neighbor, neighborCost] of adjList.get(currNode)) {
-            minPQ.push([neighbor, edgesUsed + 1], currCost + neighborCost);
+            // if(!visited.has(neighbor) && edgesUsed < k + 1) {
+                minPQ.push([neighbor, edgesUsed + 1], currCost + neighborCost);
+            //  }
         }
 
     }
