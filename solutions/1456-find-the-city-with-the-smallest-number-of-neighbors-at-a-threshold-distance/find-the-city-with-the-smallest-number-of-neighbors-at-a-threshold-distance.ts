a4169cd35849f19aa3d4a6288c74ab419d59ba22
Aidar <stormymayday7@gmail.com>
2025-08-26 14:46:58 -0700

    Time: 137 ms (10.00%) | Memory: 67 MB (15.00%) - LeetSync

---
+++ solutions/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.ts
@@ -0,0 +1,122 @@
+function findTheCity(n: number, edges: number[][], distanceThreshold: number): number {
+    
+    // 1. Create a weighted adjacency list
+    const adjList = new Map<number, [number, number][]>(); // src -> [[dst, cost], ...]
+    for(let i = 0; i < n; i += 1) {
+        adjList.set(i, []);
+    }
+    for(const [src, dst, cost] of edges) {
+        adjList.get(src).push([dst, cost]);
+        adjList.get(dst).push([src, cost]);
+    }
+
+    // 2. Dijkstra's
+    function dijkstra(src: number): number {
+        const minPQ = new CustomMinPriorityQueue<number>(); // val: node, prio: cost / weight
+        minPQ.push(src, 0);
+        const visited = new Set<number>();
+        while(minPQ.length > 0) {
+            const { val: currNode, prio: currWeight } = minPQ.pop();
+            if(visited.has(currNode)) {
+                continue;
+            }
+            visited.add(currNode);
+            for(const [neighbor, neighborWeight] of adjList.get(currNode)) {
+                if(!visited.has(neighbor) && currWeight + neighborWeight <= distanceThreshold) {
+                    minPQ.push(neighbor, currWeight + neighborWeight);
+                }
+            }
+        }
+        return visited.size - 1;
+    }
+
+    // 3. Run Dijkstra's on each node
+    let resultNode: number = -1;
+    let minNodesReached: number = Infinity;
+    for(let i = 0; i < n; i += 1) {
+        const nodesReached = dijkstra(i);
+        if(nodesReached <= minNodesReached) {
+            resultNode = i;
+            minNodesReached = nodesReached;
+        }
+    }
+    return resultNode;
+};
+
+class PriorityQueueNode<T> {
+    val: T;
+    prio: number;
+    constructor(val: T, prio: number) {
+        this.val = val;
+        this.prio = prio;
+    }
+}
+
+class CustomMinPriorityQueue<T> {
+    private data: PriorityQueueNode<T>[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: T, prio: number): void {
+        const newNode = new PriorityQueueNode<T>(val, prio);
+        this.data.push(newNode);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): PriorityQueueNode<T> | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildPrio = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx].prio;
+            const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
+            const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
+            const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
+            if(this.data[currIdx].prio > smallerChildPrio) {
+                this.swap(currIdx, smallerChildIdx);
+                currIdx = smallerChildIdx; 
+            } else {
+                break;
+            }
+        }
+    }
+    heapify(vals: PriorityQueueNode<T>[]): void {
+        this.data = [...vals];
+        this.length = vals.length;
+        let currIdx = Math.floor((this.length - 2) / 2);
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
+    }
+    top(): number | null {
+        return this.length > 0 ? this.data[0].prio : null;
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
+    }
+}
