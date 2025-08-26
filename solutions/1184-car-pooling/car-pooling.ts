885924c2b06dc45cd7abec3360a8e60e9a264300
Aidar <stormymayday7@gmail.com>
2025-08-25 22:05:32 -0700

    Time: 8 ms (56.86%) | Memory: 60 MB (43.14%) - LeetSync

--- solutions/1184-car-pooling/car-pooling.ts
+++ solutions/1184-car-pooling/car-pooling.ts
@@ -1,25 +1,93 @@
 function carPooling(trips: number[][], capacity: number): boolean {
-
-    trips.sort((a, b) => a[1] - b[1]);
     
+    trips.sort((a, b) => a[1] - b[1]);
+
+    const minPQ = new CustomMinPriorityQueue<number>(); // val: numPassengers , prio: to
+
+    let currCap = 0;
     for(let i = 0; i < trips.length; i += 1) {
 
-        let currPassengers = trips[i][0];
-
-        for(let j = 0; j < i; j += 1) {
-
-            if(trips[j][2] > trips[i][1]) {
-                currPassengers += trips[j][0];
-            }
-
+        const [numPass, from, to] = trips[i];
+        currCap += numPass;
+        minPQ.push(numPass, to);
+        while(minPQ.length > 0 && from >= minPQ.top()) {
+            const { val: prevPassengers } = minPQ.pop();
+            currCap -= prevPassengers;
         }
-
-        if(currPassengers > capacity) {
+        if(currCap > capacity) {
             return false;
         }
 
     }
-
     return true;
 
 };
+
+class QueueNode<T> {
+    val: T;
+    prio: number;
+    constructor(val: T, prio: number) {
+        this.val = val;
+        this.prio = prio;
+    }
+}
+
+class CustomMinPriorityQueue<T> {
+    private data: QueueNode<T>[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: T, prio: number): void {
+        const newNode = new QueueNode<T>(val, prio);
+        this.data.push(newNode);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): QueueNode<T> | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildPrio = this.data[leftChildIdx] === undefined? Infinity : this.data[leftChildIdx].prio;
+            const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
+            const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
+            const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
+            if(this.data[currIdx].prio > smallerChildPrio) {
+                this.swap(currIdx, smallerChildIdx);
+                currIdx = smallerChildIdx;
+            } else {
+                break;
+            }
+        }
+    }
+    top(): number | null {
+        return this.length > 0 ? this.data[0].prio : null;
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
+    }
+}
