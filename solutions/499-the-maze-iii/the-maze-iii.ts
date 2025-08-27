9b20318835a6c22d4e7358491dc52e6e4d289ea5
Aidar <stormymayday7@gmail.com>
2025-08-27 16:50:11 -0700

    Time: 20 ms (100.00%) | Memory: 63.8 MB (0.00%) - LeetSync

--- solutions/499-the-maze-iii/the-maze-iii.ts
+++ solutions/499-the-maze-iii/the-maze-iii.ts
@@ -1,17 +1,19 @@
 function findShortestWay(maze: number[][], ball: number[], hole: number[]): string {
-    
+
     const ROWS = maze.length;
     const COLS = maze[0].length;
 
     // [distance, path, row, col]
     const heap: [number, string, number, number][] = [[0, "", ball[0], ball[1]]];
 
-    const visited = new Set<string>();
+    const visited = new Map<string, [number, string]>(); // key: `${row},${col}` -> val: [distance, path]
+    // IMPORTANT! mark ball as visited
+    visited.set(`${ball[0]},${ball[1]}`, [0, ""]);
 
-    while(heap.length > 0) {
+    while (heap.length > 0) {
 
         heap.sort((a, b) => {
-            if(a[0] !== b[0]) {
+            if (a[0] !== b[0]) {
                 return a[0] - b[0];
             } else {
                 return a[1].localeCompare(b[1]);
@@ -20,52 +22,56 @@ function findShortestWay(maze: number[][], ball: number[], hole: number[]): stri
 
         const [distance, path, row, col] = heap.shift();
 
-        if(row === hole[0] && col === hole[1]) {
+        if (row === hole[0] && col === hole[1]) {
             return path;
         }
-
-        const currPosition = `${row},${col}`;
-        if(visited.has(currPosition)) {
-            continue;
-        }
-        visited.add(currPosition);
 
         const directions: [number, number, string][] = [
             [-1, 0, "u"], // up
             [0, 1, "r"], // right
             [1, 0, "d"], // down
-            [0, -1, "l"] // left
+            [0, -1, "l"], // left
         ];
-        for(const [rowDelta, colDelta, direction] of directions) {
+        for (const [rowDelta, colDelta, direction] of directions) {
 
             let currRow = row;
             let currCol = col;
             let currDist = 0;
 
-            while(
+            while (
                 // out of bounds check
                 0 <= currRow + rowDelta && currRow + rowDelta < ROWS &&
                 0 <= currCol + colDelta && currCol + colDelta < COLS &&
                 // wall check
                 maze[currRow + rowDelta][currCol + colDelta] !== 1
-            ){
+            ) {
+
                 currRow += rowDelta;
                 currCol += colDelta;
                 currDist += 1;
 
-                // Hole check
-                if(currRow === hole[0] && currCol === hole[1]) {
+                if (currRow === hole[0] && currCol === hole[1]) {
                     break;
                 }
 
             }
 
-            heap.push([currDist + distance, path + direction, currRow, currCol]);
+            const currPosition = `${currRow},${currCol}`;
+            const newDist = currDist + distance;
+            const newPath = path + direction;
+
+            if (
+                !visited.has(currPosition) ||
+                visited.get(currPosition)[0] > newDist ||
+                (visited.get(currPosition)[0] === newDist && visited.get(currPosition)[1] > newPath)
+            ) {
+                visited.set(currPosition, [newDist, newPath]);
+                heap.push([newDist, newPath, currRow, currCol]);
+            }
 
         }
 
     }
 
     return "impossible";
-
 };
