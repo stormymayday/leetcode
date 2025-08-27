bb7a814e9445ea649220ba8cecda5649fdd0db28
Aidar <stormymayday7@gmail.com>
2025-08-27 13:50:22 -0700

    Time: 89 ms (40.00%) | Memory: 63.3 MB (90.00%) - LeetSync

--- solutions/505-the-maze-ii/the-maze-ii.ts
+++ solutions/505-the-maze-ii/the-maze-ii.ts
@@ -6,55 +6,58 @@ function shortestDistance(maze: number[][], start: number[], destination: number
     // [distance, row, col]
     const heap: [number, number, number][] = [[0, start[0], start[1]]];
 
-    // Track visited positions
-    const visited = new Set<string>();
+    const visited = new Set<string>(); // key -> `${row},${col}`
 
-    while (heap.length > 0) {
+    // Dijkstra's
+    while(heap.length > 0) {
 
         heap.sort((a, b) => {
             return a[0] - b[0];
         });
 
-        const [startingDistance, startingRow, startingCol] = heap.shift();
+        const [distance, row, col] = heap.shift();
 
-        if (startingRow === destination[0] && startingCol === destination[1]) {
-            return startingDistance;
+        if(row === destination[0] && col === destination[1]) {
+            return distance;
         }
 
-        const currentPosition = `${startingRow},${startingCol}`;
-        if (visited.has(currentPosition)) {
+        const currPosition = `${row},${col}`;
+        if(visited.has(currPosition)) {
             continue;
         }
-        visited.add(currentPosition);
+        visited.add(currPosition);
 
-        const deltas = [
+        const directions: [number, number][] = [
             [-1, 0], // up
             [0, 1], // right
             [1, 0], // down
             [0, -1], // left
         ];
-        for (const [rowDelta, colDelta] of deltas) {
+        for(const [rowDelta, colDelta] of directions) {
 
-            let currRow = startingRow;
-            let currCol = startingCol;
-            let distance = 0;
+            let currRow = row;
+            let currCol = col;
+            let currDist = 0;
 
-            while (
-                // out of bounds check
+            while(
+                // Out of bounds check
                 0 <= currRow + rowDelta && currRow + rowDelta < ROWS &&
                 0 <= currCol + colDelta && currCol + colDelta < COLS &&
-                // wall check
+                // Walls check
                 maze[currRow + rowDelta][currCol + colDelta] !== 1
             ) {
                 currRow += rowDelta;
                 currCol += colDelta;
-                distance += 1;
+                currDist += 1;
             }
-            heap.push([startingDistance + distance, currRow, currCol]);
+
+            heap.push([currDist + distance, currRow, currCol]);
+
         }
 
     }
 
+    // Edge Case: ball cannot stop at destination
     return -1;
-
+    
 };
