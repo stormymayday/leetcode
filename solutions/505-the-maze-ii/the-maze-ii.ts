4856d8c53ca2c0df930cb897ef1c3d1dd6d66922
Aidar <stormymayday7@gmail.com>
2025-08-27 16:26:09 -0700

    Time: 34 ms (80.00%) | Memory: 65.5 MB (70.00%) - LeetSync

--- solutions/505-the-maze-ii/the-maze-ii.ts
+++ solutions/505-the-maze-ii/the-maze-ii.ts
@@ -1,14 +1,13 @@
 function shortestDistance(maze: number[][], start: number[], destination: number[]): number {
-
+    
     const ROWS = maze.length;
     const COLS = maze[0].length;
 
     // [distance, row, col]
     const heap: [number, number, number][] = [[0, start[0], start[1]]];
 
-    const visited = new Set<string>(); // key -> `${row},${col}`
+    const visited = new Map<string, number>(); // key: `${row},${col}` -> val: distance
 
-    // Dijkstra's
     while(heap.length > 0) {
 
         heap.sort((a, b) => {
@@ -20,12 +19,6 @@ function shortestDistance(maze: number[][], start: number[], destination: number
         if(row === destination[0] && col === destination[1]) {
             return distance;
         }
-
-        const currPosition = `${row},${col}`;
-        if(visited.has(currPosition)) {
-            continue;
-        }
-        visited.add(currPosition);
 
         const directions: [number, number][] = [
             [-1, 0], // up
@@ -41,23 +34,32 @@ function shortestDistance(maze: number[][], start: number[], destination: number
 
             while(
                 // Out of bounds check
-                0 <= currRow + rowDelta && currRow + rowDelta < ROWS &&
-                0 <= currCol + colDelta && currCol + colDelta < COLS &&
-                // Walls check
-                maze[currRow + rowDelta][currCol + colDelta] !== 1
+                0 <= currRow && currRow < ROWS &&
+                0 <= currCol && currCol < COLS &&
+                // walls check
+                maze[currRow][currCol] !== 1
             ) {
+
                 currRow += rowDelta;
                 currCol += colDelta;
                 currDist += 1;
             }
 
-            heap.push([currDist + distance, currRow, currCol]);
+            // Rollback
+            currRow -= rowDelta;
+            currCol -= colDelta;
+            currDist -= 1;
+
+            const currPosition = `${currRow},${currCol}`;
+
+            if(!visited.has(currPosition) || visited.get(currPosition) > currDist + distance) {
+                visited.set(currPosition, currDist + distance);
+                heap.push([currDist + distance, currRow, currCol]);
+            }
 
         }
 
     }
 
-    // Edge Case: ball cannot stop at destination
     return -1;
-    
 };
