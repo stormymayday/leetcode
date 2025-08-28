1d5b2774b7bad49179da24064113bdad62b33796
Aidar <stormymayday7@gmail.com>
2025-08-28 09:51:00 -0700

    Time: 154 ms (44.00%) | Memory: 71.3 MB (30.00%) - LeetSync

--- solutions/1753-path-with-minimum-effort/path-with-minimum-effort.ts
+++ solutions/1753-path-with-minimum-effort/path-with-minimum-effort.ts
@@ -3,57 +3,58 @@ function minimumEffortPath(heights: number[][]): number {
     const ROWS = heights.length;
     const COLS = heights[0].length;
 
-    // 1. Queue-up stating position with it's effort
-    const minPQ = new CustomMinPriorityQueue<[number, number]>(); // val: [row, col], prio: effort / maxAbsDiff
-    minPQ.push([0,0], 0);
+    // 1. Initialize a priority queue
+    const minPQ = new CustomMinPriorityQueue<[number, number]>(); // val: [row, col], prio: maxAbsDiff
+    minPQ.push([0, 0], 0); // starting off with row = 0, col = 0, maxAbsDiff = 0
 
     // 2. Set up a visited set
-    const visited = new Set<string>(); // key: `${row},${col}`
+    const visited = new Set<string>(); // key -> `${row},${col}`
 
     // 3. Perform Dijkstra's on a matrix
     while(minPQ.length > 0) {
-
+        // Getting the top element from priority queue
         const { val: [row, col], prio: maxAbsDiff } = minPQ.pop();
 
+        // Check if we have reached the end node
         if(row === ROWS - 1 && col === COLS - 1) {
             return maxAbsDiff;
         }
 
-        const currPosition = `${row},${col}`;
-        if(visited.has(currPosition)) {
-            continue;
+        // Check if current node has been visited
+        if(visited.has(`${row},${col}`)) {
+            continue; // skip
         }
-        visited.add(currPosition);
+        // Otherwise, mark it as visited
+        visited.add(`${row},${col}`);
 
-        const directions: [number, number][] = [
+        // Visit neighbors (up, right, down, left)
+        const deltas = [
             [-1, 0], // up
             [0, 1], // right
             [1, 0], // down
-            [0, -1] // left
+            [0, -1], // left
         ];
-        for(const [rowDelta, colDelta] of directions) {
-
+        for(const delta of deltas) {
+            const [rowDelta, colDelta] = delta;
             const neighborRow = rowDelta + row;
             const neighborCol = colDelta + col;
             const neighborPosition = `${neighborRow},${neighborCol}`;
-
             if(
-                // out of bounds check
+                // Out of bounds check
                 0 <= neighborRow && neighborRow < ROWS &&
                 0 <= neighborCol && neighborCol < COLS &&
                 // visited check
                 !visited.has(neighborPosition)
             ) {
+                // Calculate current absolute difference
                 const currAbsDiff = Math.abs(heights[row][col] - heights[neighborRow][neighborCol]);
                 const newMaxAbsDiff = Math.max(maxAbsDiff, currAbsDiff);
                 minPQ.push([neighborRow, neighborCol], newMaxAbsDiff);
             }
-
         }
-
     }
-    // return minEffort;
-    
+
+    return -1; // should not trigger
 };
 
 class PriorityQueueNode<T> {
