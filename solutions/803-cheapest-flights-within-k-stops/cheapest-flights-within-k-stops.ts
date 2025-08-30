df65af3a2773d6313628ba5639595b5846036523
Aidar <stormymayday7@gmail.com>
2025-08-29 20:27:24 -0700

    Time: 214 ms (5.44%) | Memory: 66.6 MB (21.09%) - LeetSync

--- solutions/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.ts
+++ solutions/803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.ts
@@ -1,4 +1,5 @@
 function findCheapestPrice(n: number, flights: number[][], src: number, dst: number, k: number): number {
+
     // 1. Create a weighted adjacency list
     const adjList = new Map<number, [number, number][]>(); // key: src -> val: [[dst, cost], ...]
     for (let i = 0; i < n; i += 1) {
@@ -8,34 +9,39 @@ function findCheapestPrice(n: number, flights: number[][], src: number, dst: num
         adjList.get(src).push([dst, cost]);
     }
 
-    // 2. Initialize a priority queue
+    // 2. Initialze a (naive) priority queue
     const minPQ: [number, number, number][] = []; // [cost, edgesUsed, node]
     minPQ.push([0, 0, src]);
 
-    const visited = new Map<number, number>(); // key: node, val: edgesUsed
+    // 3. visited hash map
+    const visited = new Map<number, number>(); // key: node -> val: edgesUsed
 
-    while (minPQ.length > 0) {
+    // 4. Perform Dijkstra
+    while(minPQ.length > 0) {
 
         minPQ.sort((a, b) => a[0] - b[0]);
 
-        const [currCost, edgesUsed, currNode] = minPQ.shift();
+        const [currCost, currEdgesUsed, currNode] = minPQ.shift();
 
-        if (currNode === dst) {
+        if(currNode === dst) {
             return currCost;
         }
 
-        if (visited.has(currNode) && visited.get(currNode) <= edgesUsed || edgesUsed >= k + 1) {
+        if(visited.has(currNode) && 
+        visited.get(currNode) <= currEdgesUsed ||
+        currEdgesUsed >= k + 1
+        ) {
             continue;
         }
 
-        visited.set(currNode, edgesUsed);
+        visited.set(currNode, currEdgesUsed);
 
-        for (const [neighbor, neighborCost] of adjList.get(currNode)) {
-            minPQ.push([currCost + neighborCost, edgesUsed + 1, neighbor]);
+        for(const [neighbor, neighborCost] of adjList.get(currNode)) {
+            minPQ.push([currCost + neighborCost, currEdgesUsed + 1, neighbor]);
         }
+
     }
 
-    // No path found within k stops
     return -1;
 
 };
