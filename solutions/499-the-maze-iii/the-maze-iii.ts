24e82b06e6a843f7b8a98f14f4583f2f7ccf9754
Aidar <stormymayday7@gmail.com>
2025-08-30 11:43:39 -0700

    Time: 22 ms (68.18%) | Memory: 65 MB (68.18%) - LeetSync

--- solutions/499-the-maze-iii/the-maze-iii.ts
+++ solutions/499-the-maze-iii/the-maze-iii.ts
@@ -1,20 +1,20 @@
 function findShortestWay(maze: number[][], ball: number[], hole: number[]): string {
-
+    
     const ROWS = maze.length;
     const COLS = maze[0].length;
 
-    // 1. Initialize a priotiy queue
+    // 1. Initalize a (naive) priority queue
     const minPQ: [number, string, number, number][] = []; // [distance, path, row, col]
     minPQ.push([0, "", ball[0], ball[1]]);
 
-    // 2. Inialize visited set
+    // 2. Simple visited set
     const visited = new Set<string>();
 
-    // 3. Run Dijkstra's on matrix
-    while (minPQ.length > 0) {
+    // 3. Run Dijkstra's
+    while(minPQ.length > 0) {
 
         minPQ.sort((a, b) => {
-            if (a[0] !== b[0]) {
+            if(a[0] !== b[0]) {
                 return a[0] - b[0];
             } else {
                 return a[1].localeCompare(b[1]);
@@ -23,12 +23,12 @@ function findShortestWay(maze: number[][], ball: number[], hole: number[]): stri
 
         const [distance, path, row, col] = minPQ.shift();
 
-        if (row === hole[0] && col === hole[1]) {
+        if(row === hole[0] && col === hole[1]) {
             return path;
         }
 
         const position = `${row},${col}`;
-        if (visited.has(position)) {
+        if(visited.has(position)) {
             continue;
         }
         visited.add(position);
@@ -39,13 +39,13 @@ function findShortestWay(maze: number[][], ball: number[], hole: number[]): stri
             [1, 0, "d"], // down
             [0, -1, "l"], // left
         ];
-        for (const [rowDelta, colDelta, direction] of directions) {
+        for(const [rowDelta, colDelta, direction] of directions) {
 
             let currRow = row;
             let currCol = col;
             let currDist = 0;
 
-            while (
+            while(
                 // out of bounds check
                 0 <= currRow + rowDelta && currRow + rowDelta < ROWS &&
                 0 <= currCol + colDelta && currCol + colDelta < COLS &&
@@ -53,22 +53,17 @@ function findShortestWay(maze: number[][], ball: number[], hole: number[]): stri
                 maze[currRow + rowDelta][currCol + colDelta] !== 1
             ) {
 
-                // Advance
                 currRow += rowDelta;
                 currCol += colDelta;
                 currDist += 1;
 
-                // Hole check
-                if (currRow === hole[0] && currCol === hole[1]) {
+                if(currRow === hole[0] && currCol === hole[1]) {
                     break;
                 }
             }
 
-            const newDistance = distance + currDist;
-            const newPath = path + direction;
-            if(!visited.has(`${currRow},${currCol}`)) {
-                minPQ.push([newDistance, newPath, currRow, currCol]);
-            }
+            minPQ.push([currDist + distance, path + direction, currRow, currCol]);
+
         }
 
     }
