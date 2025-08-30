4c90fc3d3991d69f75849ecbda44f4e3fbeff894
Aidar <stormymayday7@gmail.com>
2025-08-30 13:13:12 -0700

    Time: 934 ms (67.82%) | Memory: 115.2 MB (35.63%) - LeetSync

--- solutions/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.ts
+++ solutions/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.ts
@@ -1,43 +1,48 @@
 function minCostConnectPoints(points: number[][]): number {
 
-    if(points.length === 1) {
-        return 0;
-    }
-
     const n = points.length;
 
-    // 1. Create weighted edge List
-    const edges: [number, number, number][] = []; // [[src, dst, weight], [src, dst, weight], ...]
-    for(let i = 0; i < n - 1; i += 1) {
-        for(let j = i + 1; j < n; j += 1) {
+    // 1. Create a weighted edge list
+    const edges: [number, number, number][] = []; // [src, dst, cost]
+    for(let i = 0; i < points.length - 1; i += 1) {
+        for(let j = i + 1; j < points.length; j += 1) {
+
+            const src = i;
+            const dst = j;
             const weight = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-            edges.push([i, j, weight]);
+
+            edges.push([src, dst, weight]);
+
         }
     }
 
-    // 2. sort edges by weight
+    // 2. Sort edges by weight
     edges.sort((a, b) => a[2] - b[2]);
 
+    // 3. Initalize a Union Find
     const uf = new UnionFind(n);
+
+    // 4. Run Kruskal's
     let mstCost = 0;
     let edgesUsed = 0;
-    for(const [src, dst, weight] of edges) {
+    for(const [src, dst, cost] of edges) {
         if(uf.union(src, dst) === true) {
-            mstCost += weight;
+            mstCost += cost;
             edgesUsed += 1;
             if(edgesUsed === n - 1) {
                 return mstCost;
             }
         }
     }
-    return -1; // graph is not connected or has cycles
+
+    // Edge Case: 1 node 
+    return 0;
 };
 
-
 class UnionFind {
-    roots: Map<number, number>;
-    sizes: Map<number, number>;
-    numComponents: number;
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    private numComponents: number;
     constructor(n: number) {
         this.roots = new Map();
         this.sizes = new Map();
