c5be70df7fe89ec266ece8551ed685a81721439a
Aidar <stormymayday7@gmail.com>
2025-08-31 15:59:06 -0700

    Time: 4 ms (55.56%) | Memory: 58.1 MB (55.56%) - LeetSync

--- solutions/1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.ts
+++ solutions/1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.ts
@@ -4,7 +4,7 @@ function kWeakestRows(mat: number[][], k: number): number[] {
     const COLS = mat[0].length;
 
     // 1. Initalize a max Priority Queue
-    const maxPQ = new CustomMaxPriorityQueue<number>(); // val: row index, prio: 1s count * ROWS + row Index (tie-breaker)
+    const maxPQ: [number, number][] = []; // val: row index, prio: 1s count * ROWS + row Index (tie-breaker)
     for(let row = 0; row < ROWS; row += 1) {
         let soldierCount = 0;
         for(let col = 0; col < COLS; col += 1) {
@@ -15,18 +15,20 @@ function kWeakestRows(mat: number[][], k: number): number[] {
             }
         }
         if(maxPQ.length < k) {
-            maxPQ.push(row, soldierCount * ROWS + row);
+            maxPQ.push([row, soldierCount * ROWS + row]);
         } else {
-            if(soldierCount * ROWS + row < maxPQ.top()) {
-                maxPQ.pop();
-                maxPQ.push(row, soldierCount * ROWS + row);
+            maxPQ.sort((a, b) => b[1] - a[1]);
+            if(soldierCount * ROWS + row < maxPQ[0][1]) {
+                maxPQ.shift();
+                maxPQ.push([row, soldierCount * ROWS + row]);
             }
         }
     }
 
+    maxPQ.sort((a, b) => b[1] - a[1]);
     const res: number[] = [];
     while(maxPQ.length > 0) {
-        const {val: row} = maxPQ.pop();
+        const [row, tieBreaker] = maxPQ.shift();
         res.push(row);
     }
     return res.reverse();
