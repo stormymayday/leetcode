3f227abb9596144f00d9426fc663f4c0b4ae46c4
Aidar <stormymayday7@gmail.com>
2025-09-01 15:42:18 -0700

    Time: 15 ms (41.48%) | Memory: 64 MB (30.72%) - LeetSync

--- solutions/210-course-schedule-ii/course-schedule-ii.ts
+++ solutions/210-course-schedule-ii/course-schedule-ii.ts
@@ -1,43 +1,59 @@
 function findOrder(numCourses: number, prerequisites: number[][]): number[] {
     const adjList = buildAdjList(numCourses, prerequisites);
-    const visiting = new Set<number>();
-    const visited = new Set<number>();
-    const topOrder: number[] = [];
-    for(const node of adjList.keys()) {
-        if(postOrderDFS_WhiteGrayBlack(adjList, node, visiting, visited, topOrder) === true) {
-            return []; // cycle => topological ordering is not possible
-        }
-    }
-    return topOrder.reverse();
+    return kahns(numCourses, adjList);
 };
 
-function postOrderDFS_WhiteGrayBlack(adjList: Map<number, Set<number>>, src: number, visiting: Set<number>, visited: Set<number>, topOrder: number[]): boolean {
-    if(visited.has(src)) {
-        return false; // no cycle
+function kahns(n: number, adjList: Map<number, Set<number>>): number[] {
+    // 1. Build and inDegree hash map
+    const inDegree = new Map<number, number>();
+    for (let i = 0; i < n; i += 1) {
+        inDegree.set(i, 0);
+        // for(const neighbor of adjList.get(i)) {
+        //     inDegree.set(neighbor, (inDegree.get(neighbor) || 0) + 1);
+        // }
     }
-    if(visiting.has(src)) {
-        return true; // cycle
-    }
-    visiting.add(src);
-    for(const neighbor of adjList.get(src)) {
-        if(postOrderDFS_WhiteGrayBlack(adjList, neighbor, visiting, visited, topOrder) === true) {
-            return true; // cycle => exit
+    for (let i = 0; i < n; i += 1) {
+        for (const neighbor of adjList.get(i)) {
+            inDegree.set(neighbor, (inDegree.get(neighbor) || 0) + 1);
         }
     }
-    visiting.delete(src);
-    visited.add(src);
-    topOrder.push(src);
-    return false; // no cycle
+
+    // 2. Initialze 'ready' queue
+    const queue: number[] = [];
+    for (const [node, inDegreeCount] of inDegree.entries()) {
+        if (inDegreeCount === 0) {
+            queue.push(node);
+        }
+    }
+
+    // 3. BFS
+    const topOrder: number[] = [];
+    while (queue.length > 0) {
+        const currNode = queue.shift();
+        topOrder.push(currNode);
+        for (const neighbor of adjList.get(currNode)) {
+            inDegree.set(neighbor, inDegree.get(neighbor) - 1);
+            if (inDegree.get(neighbor) === 0) {
+                queue.push(neighbor);
+            }
+        }
+    }
+
+    // 4. Cycle check
+    if (topOrder.length === n) {
+        return topOrder;
+    } else {
+        return [];
+    }
 }
 
-function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+function buildAdjList(n, edges): Map<number, Set<number>> {
     const adjList = new Map();
-    for(let i = 0; i < n; i += 1){
+    for (let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const edge of edges) {
-        const [course, prereq] = edge;
-        adjList.get(prereq).add(course);
+    for (const [dst, src] of edges) {
+        adjList.get(src).add(dst);
     }
     return adjList;
-}
+};
