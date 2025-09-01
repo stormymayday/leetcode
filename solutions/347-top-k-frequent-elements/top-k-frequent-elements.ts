9d966d69629329e93069b903be5cbffe5429b670
Aidar <stormymayday7@gmail.com>
2025-08-31 19:36:21 -0700

    Time: 14 ms (45.51%) | Memory: 60.8 MB (51.06%) - LeetSync

--- solutions/347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ solutions/347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -6,17 +6,15 @@ function topKFrequent(nums: number[], k: number): number[] {
         freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
     }
 
-    // 2. Initialize a (naive) min priority queue
-    const minHeap: [number, number][] = [];
+    // 2. Initialize a min priority queue
+    const minHeap = new CustomMinPriorityQueue<number>(); // val: num, prio: count
     for(const [num, count] of freqMap.entries()) {
         if(minHeap.length < k) {
-            minHeap.push([num, count]);
-            minHeap.sort((a, b) => a[1] - b[1]);
+            minHeap.push(num, count);
         } else {
-            if(count > minHeap[0][1]) {
-                minHeap.shift();
-                minHeap.push([num, count]);
-                minHeap.sort((a, b) => a[1] - b[1]);
+            if(count > minHeap.top()) {
+                minHeap.pop();
+                minHeap.push(num, count);
             }
         }
         
@@ -25,8 +23,86 @@ function topKFrequent(nums: number[], k: number): number[] {
     // 3. Get Top K Frequent Elements
     const res: number[] = [];
     while(minHeap.length > 0) {
-        const [topNum, count] = minHeap.shift();
-        res.push(topNum);
+        const {val} = minHeap.pop();
+        res.push(val);
     }
     return res;
 };
+
+class PriorityQueueNode<T> {
+    val: T;
+    prio: number;
+    constructor(val: T, prio: number) {
+        this.val = val;
+        this.prio = prio;
+    }
+}
+
+class CustomMinPriorityQueue<T> {
+    private data: PriorityQueueNode<T>[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: T, prio: number): void {
+        const newNode = new PriorityQueueNode<T>(val, prio);
+        this.data.push(newNode);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): PriorityQueueNode<T> | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildPrio = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx].prio;
+            const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
+            const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
+            const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
+            if(this.data[currIdx].prio > smallerChildPrio) {
+                this.swap(currIdx, smallerChildIdx);
+                currIdx = smallerChildIdx; 
+            } else {
+                break;
+            }
+        }
+    }
+    heapify(vals: PriorityQueueNode<T>[]): void {
+        this.data = [...vals];
+        this.length = vals.length;
+        let currIdx = Math.floor((this.length - 2) / 2);
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
+    }
+    top(): number | null {
+        return this.length > 0 ? this.data[0].prio : null;
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
+    }
+}
