cba2c9a9867e18263117e9f15c2d4e691b0a5683
Aidar <stormymayday7@gmail.com>
2025-08-31 19:34:10 -0700

    Time: 34 ms (8.42%) | Memory: 65.2 MB (7.98%) - LeetSync

--- solutions/347-top-k-frequent-elements/top-k-frequent-elements.ts
+++ solutions/347-top-k-frequent-elements/top-k-frequent-elements.ts
@@ -6,21 +6,27 @@ function topKFrequent(nums: number[], k: number): number[] {
         freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
     }
 
-    // 2. Initialize a (naive) max priority queue
-    const maxHeap: [number, number][] = [];
+    // 2. Initialize a (naive) min priority queue
+    const minHeap: [number, number][] = [];
     for(const [num, count] of freqMap.entries()) {
-        maxHeap.push([num, count]);
+        if(minHeap.length < k) {
+            minHeap.push([num, count]);
+            minHeap.sort((a, b) => a[1] - b[1]);
+        } else {
+            if(count > minHeap[0][1]) {
+                minHeap.shift();
+                minHeap.push([num, count]);
+                minHeap.sort((a, b) => a[1] - b[1]);
+            }
+        }
+        
     }
-    maxHeap.sort((a, b) => b[1] - a[1]);
 
     // 3. Get Top K Frequent Elements
     const res: number[] = [];
-    while(maxHeap.length > 0) {
-        const [topNum, count] = maxHeap.shift();
+    while(minHeap.length > 0) {
+        const [topNum, count] = minHeap.shift();
         res.push(topNum);
-        if(res.length === k) {
-            break;
-        }
     }
     return res;
 };
