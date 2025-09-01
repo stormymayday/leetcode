e44ba976a31c0a45ebf5c38df5b6f5a3a0516538
Aidar <stormymayday7@gmail.com>
2025-08-31 19:19:56 -0700

    Time: 22 ms (90.23%) | Memory: 68.1 MB (89.10%) - LeetSync

--- solutions/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.ts
+++ solutions/789-kth-largest-element-in-a-stream/kth-largest-element-in-a-stream.ts
@@ -1,30 +1,99 @@
 class KthLargest {
-    private minHeap: number[];
+    private minHeap: MinHeap;
     private k: number;
     constructor(k: number, nums: number[]) {
         this.k = k;
-        this.minHeap = [...nums];
-        if (this.minHeap.length > 0) {
-            this.minHeap.sort((a, b) => a - b);
-            while (this.minHeap.length > this.k) {
-                this.minHeap.shift();
+        this.minHeap = new MinHeap();
+        for(let i = 0; i < nums.length; i += 1) {
+            if(this.minHeap.length < this.k) {
+                this.minHeap.push(nums[i]);
+            } else {
+                if(nums[i] > this.minHeap.peek()) {
+                    this.minHeap.pop();
+                    this.minHeap.push(nums[i]);
+                }
             }
         }
 
     }
 
     add(val: number): number {
-        if (this.minHeap.length < this.k) {
-            this.minHeap.push(val);
-            this.minHeap.sort((a, b) => a - b);
-        } else {
-            if (val > this.minHeap[0]) {
-                this.minHeap.shift();
+         if(this.minHeap.length < this.k) {
                 this.minHeap.push(val);
-                this.minHeap.sort((a, b) => a - b);
+            } else {
+                if(val > this.minHeap.peek()) {
+                    this.minHeap.pop();
+                    this.minHeap.push(val);
+                }
+            }
+        return this.minHeap.peek();
+    }
+}
+
+class MinHeap {
+    private data: number[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length  = 0;
+    }
+    push(val: number):void {
+        this.data.push(val);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1)/2);
+        while(currIdx > 0 && this.data[currIdx] < this.data[parentIdx]) {
+            const temp = this.data[currIdx];
+            this.data[currIdx] = this.data[parentIdx];
+            this.data[parentIdx] = temp;
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): number | null {
+        if(this.length === 0) {
+            return null;
+        }
+        if(this.length === 1) {
+            this.length -= 1;
+            return this.data.pop();
+        }
+        const min = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return min;
+    }
+    siftDown(idx: number):void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildVal = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx];
+            const rightChildVal = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx];
+            const smallerChildIdx = leftChildVal < rightChildVal ? leftChildIdx : rightChildIdx;
+            const smallerChildVal = leftChildVal < rightChildVal ? leftChildVal : rightChildVal;
+            if(this.data[currIdx] > smallerChildVal) {
+                const temp = this.data[currIdx];
+                this.data[currIdx] = this.data[smallerChildIdx];
+                this.data[smallerChildIdx] = temp;
+                currIdx = smallerChildIdx;
+            } else {
+                break;
             }
         }
-        return this.minHeap[0];
+    }
+    heapify(nums: number[]):void {
+        this.data = [...nums];
+        this.length = nums.length;
+        let currIdx = Math.floor((this.length - 2) / 2); // skipping leaves
+        while(currIdx >= 0) {
+            this.siftDown(currIdx);
+            currIdx -= 1;
+        }
+    }
+    peek():number | null {
+        return this.length > 0 ? this.data[0] : null;
     }
 }
 
