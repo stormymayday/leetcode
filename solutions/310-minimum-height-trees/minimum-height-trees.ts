eacdeb012972829e60fce0edabfc5096638832e9
Aidar <stormymayday7@gmail.com>
2025-09-02 12:18:20 -0700

    Time: 166 ms (16.21%) | Memory: 85.7 MB (13.51%) - LeetSync

--- solutions/310-minimum-height-trees/minimum-height-trees.ts
+++ solutions/310-minimum-height-trees/minimum-height-trees.ts
@@ -1,13 +1,19 @@
 function findMinHeightTrees(n: number, edges: number[][]): number[] {
-    // Edge Case: single node => no edges
+    
+    // 1. Edge case: single node
     if(n === 1 || edges.length === 0) {
         return [0];
     }
+
     const adjList = buildAdjList(n, edges);
+
     return kahns(adjList);
+
 };
 
 function kahns(adjList: Map<number, Set<number>>): number[] {
+
+    // 1. Build inDegree (leaves) hash map
     const inDegree = new Map<number, number>();
     for(const node of adjList.keys()) {
         inDegree.set(node, 0);
@@ -17,39 +23,52 @@ function kahns(adjList: Map<number, Set<number>>): number[] {
             inDegree.set(neighbor, inDegree.get(neighbor) + 1);
         }
     }
+
+    // 2. Queue up the "leaves" inDegree of 1
     let queue: number[] = [];
-    for(const [node, count] of inDegree.entries()) {
-        if(count === 1) {
+    for(const [node, inDegreeCount] of inDegree.entries()) {
+        if(inDegreeCount === 1) {
             queue.push(node);
         }
     }
-    let nodesRemaining = adjList.size;
-    while(nodesRemaining > 2) {
-        nodesRemaining -= queue.length;
-        const nextLayerLeaves: number[] = [];
-        // process current layer leaves
-        for(const node of queue) {
-            for(const neighbor of adjList.get(node)) {
+
+    // 3. Kahn's BFS
+    let nodesLeft = adjList.size;
+    while(nodesLeft > 2) {
+        
+        const queueLength = queue.length;
+        nodesLeft -= queueLength;
+        const nextLayer: number[] = [];
+
+        for(let i = 0; i < queueLength; i += 1) {
+            const currNode = queue.shift();
+            for(const neighbor of adjList.get(currNode)) {
                 inDegree.set(neighbor, inDegree.get(neighbor) - 1);
+                if(inDegree.get(neighbor) === 0) {
+                    inDegree.delete(neighbor);
+                }
                 if(inDegree.get(neighbor) === 1) {
-                    nextLayerLeaves.push(neighbor);
+                    nextLayer.push(neighbor);
                 }
             }
         }
-        queue = nextLayerLeaves;
+
+        queue = nextLayer;
+
     }
+
     return queue;
+
 }
 
 function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
-    const adjList = new Map();
+    const adjList = new Map<number, Set<number>>();
     for(let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const edge of edges) {
-        const [a, b] = edge;
-        adjList.get(a).add(b);
-        adjList.get(b).add(a);
+    for(const [src, dst] of edges) {
+        adjList.get(src).add(dst);
+        adjList.get(dst).add(src);
     }
     return adjList;
 }
