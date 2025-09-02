b18b50fc930b2d0ae3615a4c24faa6b4ecfa1560
Aidar <stormymayday7@gmail.com>
2025-09-01 20:39:15 -0700

    Time: 37 ms (16.67%) | Memory: 68.2 MB (16.67%) - LeetSync

--- solutions/1101-parallel-courses/parallel-courses.ts
+++ solutions/1101-parallel-courses/parallel-courses.ts
@@ -1,58 +1,79 @@
 function minimumSemesters(n: number, relations: number[][]): number {
+
     const adjList = buildAdjList(n, relations);
+
     return kahns(adjList);
+
 };
 
 function kahns(adjList: Map<number, Set<number>>): number {
+
+    // 1. Initialzie the inDegree hash map
     const inDegree = new Map<number, number>();
-    for(const node of adjList.keys()) {
+    for (const node of adjList.keys()) {
         inDegree.set(node, 0);
     }
-    for(const node of adjList.keys()) {
-        for(const neighbor of adjList.get(node)) {
+    for (const node of adjList.keys()) {
+        for (const neighbor of adjList.get(node)) {
             inDegree.set(neighbor, inDegree.get(neighbor) + 1);
         }
     }
+
+    // 2. Initialze the 'ready' queue
     let queue: number[] = [];
-    for(const [node, count] of inDegree.entries()) {
-        if(count === 0) {
+    for (const [node, inDegreeCount] of inDegree.entries()) {
+        if (inDegreeCount === 0) {
             queue.push(node);
         }
     }
-    let nodesRemaining = adjList.size;
-    let result = 1; // one layer is already in the queue
-    while(true) {
-        nodesRemaining -= queue.length;
-        const nextLayerNodes: number[] = [];
-        // process all nodes at the current layer
-        for(const node of queue) {
-            for(const neighbor of adjList.get(node)) {
+
+    // 3. BFS
+    const topOrder: number[] = [];
+    let levels = 1; // counting enqueued nodes
+    while (queue.length > 0) {
+
+        const queueLength = queue.length;
+        const nextLevelNodes: number[] = [];
+
+        for (let i = 0; i < queueLength; i += 1) {
+
+            const currNode = queue.shift();
+            topOrder.push(currNode);
+
+            for(const neighbor of adjList.get(currNode)) {
+
                 inDegree.set(neighbor, inDegree.get(neighbor) - 1);
                 if(inDegree.get(neighbor) === 0) {
-                    nextLayerNodes.push(neighbor);
+                    nextLevelNodes.push(neighbor);
                 }
+
             }
+
         }
-        if(nextLayerNodes.length === 0) {
+
+        if (nextLevelNodes.length > 0) {
+            queue = nextLevelNodes;
+            levels += 1;
+        } else {
             break;
         }
-        result += 1;
-        queue = nextLayerNodes;
+
     }
-    if(nodesRemaining === 0) {
-        return result;
+
+    if (topOrder.length === adjList.size) {
+        return levels;
     } else {
-        return -1; // cycle
+        return -1;
     }
+
 }
 
-function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+function buildAdjList(n, edges): Map<number, Set<number>> {
     const adjList = new Map();
-    for(let i = 1; i <= n; i += 1) {
+    for (let i = 1; i <= n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const edge of edges) {
-        const [src, dst] = edge;
+    for (const [src, dst] of edges) {
         adjList.get(src).add(dst);
     }
     return adjList;
