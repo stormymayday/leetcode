36da8923f32aa0bc89f78838f33b421cb91d19d0
Aidar <stormymayday7@gmail.com>
2025-09-02 09:20:22 -0700

    Time: 80 ms (45.71%) | Memory: 72.2 MB (45.71%) - LeetSync

--- solutions/1558-course-schedule-iv/course-schedule-iv.ts
+++ solutions/1558-course-schedule-iv/course-schedule-iv.ts
@@ -1,71 +1,85 @@
 function checkIfPrerequisite(numCourses: number, prerequisites: number[][], queries: number[][]): boolean[] {
 
     const adjList = buildAdjList(numCourses, prerequisites);
-    const nodePrereqs = kahns(adjList);
+
+    const nodePrereqs: Map<number, Set<number>> | undefined = kahns(adjList);
 
     const res: boolean[] = [];
-    for(const [prereq, node] of queries) {
+    // Cycle Detection (should not be necessary)
+    // if(nodePrereqs !== undefined) {
+    for (const [prereq, node] of queries) {
         res.push(nodePrereqs.get(node).has(prereq));
     }
+    // } else {
     return res;
+    // }
+
 };
 
-function kahns(adjList: Map<number, Set<number>>): Map<number, Set<number>> {
+function kahns(adjList: Map<number, Set<number>>): Map<number, Set<number>> | undefined {
 
-    // 1. Build inDegree hash map & Initialze nodePrereqs hash map
+    // 1. Initialze inDegree and nodePrereqs hash maps
     const inDegree = new Map<number, number>();
     const nodePrereqs = new Map<number, Set<number>>();
-    for(const node of adjList.keys()) {
+    for (const node of adjList.keys()) {
         inDegree.set(node, 0);
         nodePrereqs.set(node, new Set());
     }
-    for(const node of adjList.keys()) {
-        for(const neighbor of adjList.get(node)) {
+    for (const node of adjList.keys()) {
+        for (const neighbor of adjList.get(node)) {
             inDegree.set(neighbor, inDegree.get(neighbor) + 1);
         }
     }
 
-    // 2. Initialize 'ready' stack
-    const stack: number[] = [];
-    for(const [node, inDegreeCount] of inDegree.entries()) {
-        if(inDegreeCount === 0) {
-            stack.push(node);
+    // 2. Initialze the 'ready' queue
+    const queue: number[] = [];
+    for (const [node, inDegreeCount] of inDegree.entries()) {
+        if (inDegreeCount === 0) {
+            queue.push(node);
         }
     }
 
-    // 3. BFS
-    while(stack.length > 0) {
-        const currNode = stack.pop();
-        for(const neighbor of adjList.get(currNode)) {
-            
-            // *********** Prereq Logic ***********
-            // Store direct prereq
-            nodePrereqs.get(neighbor).add(currNode);
+    // 3. Kahn's (BFS)
+    const topOrder: number[] = [];
+    while (queue.length > 0) {
 
-            // Store all prereqs of the prereq (currNode)
-            for(const prereq of nodePrereqs.get(currNode)) {
-                nodePrereqs.get(neighbor).add(prereq)
+        const currNode = queue.shift();
+        topOrder.push(currNode);
+
+        for (const neighbor of adjList.get(currNode)) {
+
+            // Direct prereq
+            nodePrereqs.get(neighbor).add(currNode)
+
+            // Prereqs of a prereq (currNode)
+            for (const prereq of nodePrereqs.get(currNode)) {
+                nodePrereqs.get(neighbor).add(prereq);
             }
-            // *********** Prereq Logic ***********
 
             inDegree.set(neighbor, inDegree.get(neighbor) - 1);
-            if(inDegree.get(neighbor) === 0) {
-                stack.push(neighbor);
+            if (inDegree.get(neighbor) === 0) {
+                queue.push(neighbor);
             }
 
         }
+
     }
 
+    // 4. Cycle check (should not be necessary)
+    // if(topOrder.length === adjList.size) {
     return nodePrereqs;
+    // } else {
+    //     return undefined;
+    // }
 
 }
 
 function buildAdjList(n, edges): Map<number, Set<number>> {
-    const adjList = new Map();
-    for(let i = 0; i < n; i += 1) {
+    const adjList = new Map<number, Set<number>>();
+    for (let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const [src, dst] of edges) {
+    for (const [src, dst] of edges) {
         adjList.get(src).add(dst);
     }
     return adjList;
