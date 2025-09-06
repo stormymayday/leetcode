102ee43c0623822cff1d66ca978acdacc2a825b9
Aidar <stormymayday7@gmail.com>
2025-09-06 09:58:52 -0700

    Time: 18 ms (27.11%) | Memory: 64 MB (32.89%) - LeetSync

--- solutions/210-course-schedule-ii/course-schedule-ii.ts
+++ solutions/210-course-schedule-ii/course-schedule-ii.ts
@@ -1,45 +1,60 @@
 function findOrder(numCourses: number, prerequisites: number[][]): number[] {
     const adjList = buildAdjList(numCourses, prerequisites);
-    
-    const visiting = new Set<number>();
-    const visited = new Set<number>();
+    return kahns(adjList);
+};
+
+function kahns(adjList: Map<number, Set<number>>): number[] {
+
+    // 1. inDegree map
+    const inDegreeMap = new Map<number, number>();
+    for(const node of adjList.keys()) { // O(V)
+        inDegreeMap.set(node, 0);
+    }
+    for(const node of adjList.keys()) { // O(V + E)
+        for(const neighbor of adjList.get(node)) {
+            inDegreeMap.set(neighbor, inDegreeMap.get(neighbor) + 1);
+        }
+    }
+
+    // 2. 'ready' queue / stack
+    const ready: number[] = [];
+    for(const [node, inDegreeCount] of inDegreeMap.entries()) {
+        if(inDegreeCount === 0) {
+            ready.push(node);
+        }
+    }
+
+    // 3. Kahn's BFS
     const topOrder: number[] = [];
-    for(let i = 0; i < numCourses; i += 1) {
-        if(!visited.has(i)) {
-            if(postOrderDFS(i, adjList, visiting, visited, topOrder) == false) {
-                return [];
+    while(ready.length > 0) {
+        const currNode = ready.pop();
+        topOrder.push(currNode);
+        for(const neighbor of adjList.get(currNode)) {
+            inDegreeMap.set(neighbor, inDegreeMap.get(neighbor) - 1);
+            if(inDegreeMap.get(neighbor) === 0) {
+                ready.push(neighbor);
             }
         }
     }
-    return topOrder.reverse();
-};
 
-function postOrderDFS(node: number, adjList: Map<number, Set<number>>, visiting: Set<number>, visited :Set<number>, topOrder: number[]): boolean {
-    if(visited.has(node)) {
-        return true;
+    // 4. Cycle check & return
+    if(topOrder.length === adjList.size) {
+        return topOrder;
+    } else {
+        return [];
     }
-    if(visiting.has(node)) {
-        return false;
-    }
-    visiting.add(node);
-    for(const neighbor of adjList.get(node)) {
-        if(postOrderDFS(neighbor, adjList, visiting, visited, topOrder) == false) {
-            return false;
-        }
-    }
-    visiting.delete(node);
-    visited.add(node);
-    topOrder.push(node);
-    return true;
+
 }
 
-function buildAdjList(n, edges): Map<number, Set<number>> {
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
     const adjList = new Map();
-    for (let i = 0; i < n; i += 1) {
+    for(let i = 0; i < n; i += 1) { // O(V)
         adjList.set(i, new Set());
     }
-    for (const [dst, src] of edges) {
+    for(const [dst, src] of edges) { // O(E)
         adjList.get(src).add(dst);
     }
     return adjList;
-};
+    // Time: O(V + E)
+    // Space: O(V + E)
+}
