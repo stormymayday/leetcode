a17e3cfa77fbf479d2f895bc2febb3b097d6f0bf
Aidar <stormymayday7@gmail.com>
2025-09-07 11:20:28 -0700

    Time: 1 ms (53.03%) | Memory: 53.8 MB (98.48%) - LeetSync

--- solutions/990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.ts
+++ solutions/990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.ts
@@ -1,36 +1,34 @@
 function isAlienSorted(words: string[], order: string): boolean {
-    // Create index map once for O(1) character lookups
     const charIndex = new Map<string, number>();
-    for (let i = 0; i < order.length; i++) {
+    for (let i = 0; i < order.length; i += 1) {
         charIndex.set(order[i], i);
     }
-    
-    for (let i = 0; i < words.length - 1; i++) {
+
+    for (let i = 0; i < words.length - 1; i += 1) {
+
         const word1 = words[i];
-        const word2 = words[i + 1]
-        if (compare(word1, word2, charIndex) === false) {
+        const word2 = words[i + 1];
+
+        let differentCharFound = false;
+        for (let j = 0; j < Math.min(word1.length, word2.length); j += 1) {
+            const char1 = word1[j];
+            const char2 = word2[j];
+            if (char1 !== char2) {
+                if (charIndex.get(char1) > charIndex.get(char2)) {
+                    return false;
+                } else {
+                    differentCharFound = true;
+                    break;
+                }
+            }
+
+        }
+
+        if (differentCharFound === false && word1.length > word2.length) {
             return false;
         }
+
     }
 
     return true;
-}
-
-function compare(word1: string, word2: string, charIndex: Map<string, number>): boolean {
-
-    const minLength = Math.min(word1.length, word2.length);
-    
-    for (let i = 0; i < minLength; i++) {
-
-        const char1 = word1[i];
-        const char2 = word2[i];
-        
-        if (char1 !== char2) {
-            // char2 should have larger index -> true. false otherwie
-            return charIndex.get(char1) < charIndex.get(char2);
-        }
-    }
-    
-    // If all compared characters are equal, shorter word should come first
-    return word1.length <= word2.length;
-}
+};
