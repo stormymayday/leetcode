96c151c4708a1cc6684d5dcca61ff2d58e6ac78e
Aidar <stormymayday7@gmail.com>
2025-09-08 17:57:14 -0700

    Time: 12 ms (47.52%) | Memory: 62 MB (44.55%) - LeetSync

--- solutions/323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.ts
+++ solutions/323-number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.ts
@@ -1,44 +1,53 @@
 function countComponents(n: number, edges: number[][]): number {
-    const roots: number[] = [];
-    const heights: number[] = [];
-    for(let i = 0; i < n; i += 1) {
-        roots.push(i);
-        heights.push(1);
+
+    const uf = new UnionFind(n);
+
+    for(const [src, dst] of edges) {
+        uf.union(src, dst);
     }
-    for(const edge of edges) {
-        const [a, b] = edge;
-        union(roots, heights, a, b);
-    }
-    let count = 0;
-    for(let i = 0; i < n; i += 1) {
-        if(roots[i] === i) {
-            count += 1;
-        }
-    }
-    return count;
+
+    return uf.getNumComponents();
+    
 };
 
-// Union by Height
-function union(roots: number[], heights: number[], a: number, b: number):void {
-    const rootA = find(roots, a);
-    const rootB = find(roots, b);
-    if(rootA !== rootB) {
-        if(heights[rootA] > heights[rootB]) {
-            roots[rootB] = rootA;
-        } else if(heights[rootB] > heights[rootA]) {
-            roots[rootA] = rootB;
-        } else {
-            roots[rootA] = rootB;
-            heights[rootB] += 1;
+class UnionFind {
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    private numComponents: number;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.sizes = new Map();
+        this.numComponents = n;
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.sizes.set(i, 1);
         }
     }
-}
-
-function find(roots: number[], node: number): number {
-    if(roots[node] === node) {
-        return node;
+    find(x: number): number {
+        const root = this.roots.get(x);
+        if(root !== x) {
+            this.roots.set(x, this.find(root));
+        }
+        return this.roots.get(x);
     }
-    const root = find(roots, roots[node]);
-    roots[node] = root; // path compression
-    return root;
+    union(x: number, y: number): boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if(rootX === rootY) {
+            return false;
+        } else {
+            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
+                this.roots.set(rootY, rootX);
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY)); 
+            } else {
+                this.roots.set(rootX, rootY);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
+            }
+            this.numComponents -= 1;
+            return true;
+        }
+    }
+    getNumComponents(): number {
+        return this.numComponents;
+    }
 }
