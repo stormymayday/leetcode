c2bcb7cc6291ac2066e070b63acec363085dc7e3
Aidar <stormymayday7@gmail.com>
2025-09-09 20:15:30 -0700

    Time: 479 ms (16.67%) | Memory: 90.4 MB (16.67%) - LeetSync

---
+++ solutions/989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.ts
@@ -0,0 +1,111 @@
+function largestComponentSize(nums: number[]): number {
+
+    const uf = new UnionFind(nums);
+
+    // 1. Get factors for each number and map numbers to a factor
+    const factorToNums = new Map<number, number[]>(); // key: factor -> val: numbers
+    for (const num of nums) {
+
+        const factors = getFactors(num);
+
+        for (const factor of factors) {
+
+            if (!factorToNums.has(factor)) {
+                factorToNums.set(factor, []);
+            }
+
+            factorToNums.get(factor).push(num);
+
+        }
+
+    }
+
+    // 2. Union numbers for each factor
+    for (const nums of factorToNums.values()) {
+
+        for (let i = 0; i < nums.length - 1; i += 1) {
+            const num1 = nums[i];
+            const num2 = nums[i + 1];
+            uf.union(num1, num2);
+        }
+
+    }
+
+    return uf.getLargestComponentSize();
+
+};
+
+function getFactors(num: number): number[] {
+    const factors = new Set<number>();
+    let factor = 2;
+    while (factor * factor <= num) {
+        if (num % factor === 0) {
+            factors.add(factor);
+            factors.add(num / factor);
+        }
+        factor += 1;
+    }
+
+    // If num > 1, add num itself as a factor (handles primes and remaining factors)
+    if (num > 1) {
+        factors.add(num);
+    }
+
+    return [...factors];
+}
+
+class UnionFind {
+
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    private numComponents: number;
+
+    constructor(nums: number[]) {
+        this.roots = new Map();
+        this.sizes = new Map();
+        this.numComponents = nums.length;
+        for (let i = 0; i < nums.length; i += 1) {
+            this.roots.set(nums[i], nums[i]);
+            this.sizes.set(nums[i], 1);
+        }
+    }
+
+    find(x: number): number {
+        const root = this.roots.get(x);
+        if (root !== x) {
+            this.roots.set(x, this.find(root));
+        }
+        return this.roots.get(x);
+    }
+
+    union(x: number, y: number): boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if (rootX === rootY) {
+            return false;
+        } else {
+            if (this.sizes.get(rootX) >= this.sizes.get(rootY)) {
+                this.roots.set(rootY, rootX);
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+            } else {
+                this.roots.set(rootX, rootY);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
+            }
+            this.numComponents -= 1;
+            return true;
+        }
+    }
+
+    getLargestComponentSize(): number {
+        let maxSize = -Infinity;
+        for (const size of this.sizes.values()) {
+            maxSize = Math.max(maxSize, size);
+        }
+        return maxSize;
+    }
+
+    getNumComponents(): number {
+        return this.numComponents;
+    }
+
+}
