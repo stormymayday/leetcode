2efccc14b8e5def96069eaf78d0e0ec85dba11b1
Aidar <stormymayday7@gmail.com>
2025-09-10 18:18:34 -0700

    Time: 6 ms (30.00%) | Memory: 60 MB (25.38%) - LeetSync

--- solutions/684-redundant-connection/redundant-connection.ts
+++ solutions/684-redundant-connection/redundant-connection.ts
@@ -1,27 +1,31 @@
 function findRedundantConnection(edges: number[][]): number[] {
+    
     const uf = new UnionFind(edges.length);
-    for(const edge of edges) {
-        const [a, b] = edge;
-        if(uf.union(a, b) === false) {
-            return edge;
+
+    for(const [src, dst] of edges) {
+        if(uf.union(src, dst) === false) {
+            return [src, dst];
         }
     }
+
 };
 
 class UnionFind {
     private roots: Map<number, number>;
     private sizes: Map<number, number>;
+    private numComponents: number;
     constructor(n: number) {
         this.roots = new Map();
         this.sizes = new Map();
-        for(let i = 1; i <= n; i += 1) {
+        this.numComponents = n;
+        for (let i = 1; i <= n; i += 1) {
             this.roots.set(i, i);
             this.sizes.set(i, 1);
         }
     }
-    find(x: number):number {
+    find(x: number): number {
         const root = this.roots.get(x);
-        if(root !== x) {
+        if (root !== x) {
             this.roots.set(x, this.find(root));
         }
         return this.roots.get(x);
@@ -29,17 +33,21 @@ class UnionFind {
     union(x: number, y: number): boolean {
         const rootX = this.find(x);
         const rootY = this.find(y);
-        if(rootX === rootY) {
+        if (rootX === rootY) {
             return false;
         } else {
-            if(this.sizes.get(rootX) > this.sizes.get(rootY)) {
+            if (this.sizes.get(rootX) >= this.sizes.get(rootY)) {
                 this.roots.set(rootY, rootX);
                 this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
             } else {
                 this.roots.set(rootX, rootY);
                 this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
             }
+            this.numComponents -= 1;
             return true;
         }
+    }
+    getLargestComponent(): number {
+        return Math.max(...this.sizes.values());
     }
 }
