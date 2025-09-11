392cf5a78638d829cb28cfa9d29c9e107042aac8
Aidar <stormymayday7@gmail.com>
2025-09-10 21:00:16 -0700

    Time: 11 ms (74.16%) | Memory: 65.7 MB (13.48%) - LeetSync

--- solutions/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.ts
+++ solutions/1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.ts
@@ -1,17 +1,16 @@
 function removeDuplicates(s: string): string {
     const stack: string[] = [];
-    for(let i = 0; i < s.length; i += 1) {
 
+    for (let i = 0; i < s.length; i += 1) {
         const char = s[i];
 
-        if(stack.length === 0 || stack[stack.length - 1] !== char) {
+        if (stack.length === 0 || stack[stack.length - 1] !== char) {
             stack.push(char);
         } else {
-            if(stack[stack.length - 1] === char) {
-                stack.pop();
-            }
+            // We know stack[stack.length - 1] === char
+            stack.pop();
         }
-
     }
+
     return stack.join("");
-};
+}
