60e4c5d2245727d4e83bad5b1bf1d36270c480ef
Aidar <stormymayday7@gmail.com>
2025-09-12 10:20:32 -0700

    Time: 46 ms (66.96%) | Memory: 69.4 MB (65.18%) - LeetSync

--- solutions/721-accounts-merge/accounts-merge.ts
+++ solutions/721-accounts-merge/accounts-merge.ts
@@ -1,78 +1,87 @@
 function accountsMerge(accounts: string[][]): string[][] {
 
-    // 1. Create a bi-directional adjacency List using Hub/Star method
-    const adjList = buildAdjList(accounts);
+    // 1. Initialzing UnionFind using the accounts.length
+    const n = accounts.length;
+    const uf = new UnionFind(n);
 
-    // 2. Run BFS and fill in the result
-    const visited = new Set<string>();
-    const result: string[][] = [];
-    for (const account of accounts) {
-        const name = account[0];
-        const firstEmail = account[1];
-        if (!visited.has(firstEmail)) {
-
-            const mergedEmails: string[] = [];
-            bfs(firstEmail, adjList, visited, mergedEmails);
-
-            // 2.1. Sort the emails
-            mergedEmails.sort();
-            // 2.2. Add name in the beginning and push to result
-            result.push([name, ...mergedEmails]);
-
+    // 2. Map each email to account index and perform 'union' (MERGE) where necessary
+    const emailToAccIdx = new Map<string, number>();
+    // 2.1. Iterating over every account
+    for(let accIdx = 0; accIdx < n; accIdx += 1) {
+        // 2.2 For each account, iterating over every email (skipping index 0 'name')
+        for(let emailIdx = 1; emailIdx < accounts[accIdx].length; emailIdx += 1) {
+            const currEmail = accounts[accIdx][emailIdx];
+            // 2.2.1. If we see the email for the first time
+            // Set it's account index to current 'accIdx'
+            if(!emailToAccIdx.has(currEmail)) {
+                emailToAccIdx.set(currEmail, accIdx);
+            }
+            // Otherwise, perform a union between current 'accIdx' and the the account index the emails belongs to
+            // THIS IS THE MERGE STEP
+            else {
+                uf.union(accIdx, emailToAccIdx.get(currEmail));
+            }
         }
     }
-    return result;
+
+    // 3. Using 'emailToAccIdx' map, create a reverse mapping of account index to all all (MEGED) emails
+    const accIdxToMergedEmails = new Map<number, string[]>;
+    for(const [email, accIdx] of emailToAccIdx.entries()) {
+        // PREVIOUS MISTAKE: need to get root
+        const root = uf.find(accIdx);
+        if(!accIdxToMergedEmails.has(root)) {
+            accIdxToMergedEmails.set(root, []);
+        }
+        accIdxToMergedEmails.get(root).push(email);
+    }
+
+    // 4. Using 'accIdxToMergedEmails', Create the result
+    const res: string[][] = [];
+    for(const [accIdx, emails] of accIdxToMergedEmails.entries()) {
+        // 4.1. Grab the account name, list of emails and sort the emails
+        const accName = accounts[accIdx][0];
+        const listOfEmails = emails.sort();
+        // 4.2 Push both to the result
+        res.push([accName, ...listOfEmails]);
+    }
+    return res;
 };
 
-function bfs(
-    src: string,
-    adjList: Map<string, Set<string>>,
-    visited: Set<string>,
-    merged: string[]
-): void {
-    const queue: string[] = [];
-    queue.push(src);
-    visited.add(src);
-    while (queue.length > 0) {
-        const currNode = queue.shift();
-        merged.push(currNode);
-        for (const neighbor of adjList.get(currNode)) {
-            if (!visited.has(neighbor)) {
-                visited.add(neighbor);
-                queue.push(neighbor);
-            }
+class UnionFind {
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    private numComponents: number;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.sizes = new Map();
+        this.numComponents = n;
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.sizes.set(i, 1);
         }
     }
-
-}
-
-// Hub/Star - First email connects to all others (connection is bi-directional)
-function buildAdjList(accounts: string[][]): Map<string, Set<string>> {
-
-    const adjList = new Map();
-
-    for (const account of accounts) {
-        // Hub email
-        // Start from index 1 (first email), not 0 (name)
-        const firstEmail = account[1];
-        if (!adjList.has(firstEmail)) {
-            adjList.set(firstEmail, new Set());
+    find(x: number): number {
+        const root = this.roots.get(x);
+        if(root !== x) {
+            this.roots.set(x, this.find(root));
         }
-        // Connect first email to all others
-        // Starting from index 2 (other emails)
-        for (let i = 2; i < account.length; i += 1) {
-
-            const otherEmail = account[i];
-
-            // Initialize otherEmail if needed
-            if (!adjList.has(otherEmail)) {
-                adjList.set(otherEmail, new Set());
+        return this.roots.get(x);
+    }
+    union(x: number, y: number): boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if(rootX === rootY) {
+            return false;
+        } else {
+            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
+                this.roots.set(rootY, rootX);
+                this.sizes.set(rootX, this.sizes.get(rootY) + this.sizes.get(rootX));
+            } else {
+                this.roots.set(rootX, rootY);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
             }
-
-            // Create bidirectional connection
-            adjList.get(firstEmail).add(otherEmail);
-            adjList.get(otherEmail).add(firstEmail);
+            this.numComponents -= 1;
+            return true;
         }
     }
-    return adjList;
 }
