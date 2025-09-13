c3d15be239a660509bba1e360ee24f548bc739f8
Aidar <stormymayday7@gmail.com>
2025-09-13 11:01:27 -0700

    Time: 3 ms (100.00%) | Memory: 57.6 MB (88.89%) - LeetSync

--- solutions/1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.ts
+++ solutions/1058-lexicographically-smallest-equivalent-string/lexicographically-smallest-equivalent-string.ts
@@ -2,17 +2,17 @@ function smallestEquivalentString(s1: string, s2: string, baseStr: string): stri
 
     const uf = new UnionFind();
 
-    for(let i = 0; i < s1.length; i += 1) {
+    for (let i = 0; i < s1.length; i += 1) {
         uf.union(s1[i], s2[i]);
     }
 
     const res: string[] = [];
-    for(let i = 0; i < baseStr.length; i += 1) {
+    for (let i = 0; i < baseStr.length; i += 1) {
         res.push(uf.find(baseStr[i]));
     }
     return res.join("");
 
-    
+
 };
 
 class UnionFind {
@@ -21,32 +21,30 @@ class UnionFind {
         this.roots = new Map();
     }
     add(x: string): void {
-        if(!this.roots.has(x)) {
+        if (!this.roots.has(x)) {
             this.roots.set(x, x);
         }
     }
     find(x: string): string {
 
-        if(!this.roots.has(x)) {
+        if (!this.roots.has(x)) {
             this.roots.set(x, x);
             return x;
         }
 
         const root = this.roots.get(x);
-        if(root !== x) {
+        if (root !== x) {
             this.roots.set(x, this.find(root));
         }
         return this.roots.get(x);
     }
-    union(x: string, y: string): boolean {
+    union(x: string, y: string): void {
 
         const rootX = this.find(x);
         const rootY = this.find(y);
 
-        if(rootX === rootY) {
-            return false;
-        } else {
-            if(this.roots.get(rootX) < this.roots.get(rootY)) {
+        if (rootX !== rootY) {
+            if (rootX < rootY) {
                 // If rootX is rootX is lexiographically smaller than rootY
                 // Set rootY be rootX
                 this.roots.set(rootY, rootX);
@@ -54,8 +52,6 @@ class UnionFind {
                 // Otherwise, set rootX be rootY
                 this.roots.set(rootX, rootY)
             }
-            return true;
         }
-
     }
 }
