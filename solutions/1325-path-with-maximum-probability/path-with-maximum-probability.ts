a3c977fc3d69e0ee47370d09c0c384a7334e7c3f
Aidar <stormymayday7@gmail.com>
2025-09-15 13:45:05 -0700

    Time: 169 ms (47.62%) | Memory: 90.6 MB (14.29%) - LeetSync

--- solutions/1325-path-with-maximum-probability/path-with-maximum-probability.ts
+++ solutions/1325-path-with-maximum-probability/path-with-maximum-probability.ts
@@ -1,52 +1,60 @@
 function maxProbability(n: number, edges: number[][], succProb: number[], start_node: number, end_node: number): number {
-    // 1. Create a a weighted adjacency list
-    const adjList = new Map<number, [number, number][]>(); // key: src -> val: [[dst, cost], ...]
-    for(let i = 0; i < n; i += 1) {
-        adjList.set(i, []);
-    }
+
+    const edgeList: [number, number, number][] = [];
     for(let i = 0; i < edges.length; i += 1) {
         const [src, dst] = edges[i];
-        const prob = succProb[i];
-        adjList.get(src).push([dst, prob]);
-        adjList.get(dst).push([src, prob]);
+        const cost = succProb[i];
+        edgeList.push([src, dst, cost]);
     }
 
-    // 2. Initalize a priority queue
-    const maxPQ = new CustomMaxPriorityQueue<number>(); // val: node, prio: prob
-    maxPQ.push(start_node, 1); // starting prob is 1
+    const adjList = buildAdjList(n, edgeList);
 
-    // 3. Visited set
+    const maxPQ = new CustomMaxPriorityQueue<number>(); // val: node (number)
+    maxPQ.push(start_node, 1);
+
     const visited = new Set<number>();
 
-    // 4. Run Dijkstra's
     while(maxPQ.length > 0) {
 
-        const { val: currNode, prio: currProb } = maxPQ.pop();
+        const { val: currNode, prio: currCost } = maxPQ.pop();
 
         if(currNode === end_node) {
-            return currProb;
+            return currCost;
         }
 
         if(visited.has(currNode)) {
             continue;
         }
+
         visited.add(currNode);
 
-        for(const [neighbor, neighborProb] of adjList.get(currNode)) {
+        for(const [neighbor, neighborCost] of adjList.get(currNode)) {
             if(!visited.has(neighbor)) {
-                maxPQ.push(neighbor, currProb * neighborProb);
+                maxPQ.push(neighbor, currCost * neighborCost);
             }
         }
 
     }
 
-    // there is no path from start to end
     return 0;
+    
 };
 
+function buildAdjList(n: number, edges: [number, number, number][]): Map<number, [number, number][]> {
+    const adjList = new Map();
+    for(let i = 0; i < n; i += 1) {
+        adjList.set(i, []);
+    }
+    for(const [src, dst, cost] of edges) {
+        adjList.get(src).push([dst, cost]);
+        adjList.get(dst).push([src, cost]);
+    }
+    return adjList;
+}
+
 class PriorityQueueNode<T> {
-    val: T;
-    prio: number;
+    public val: T;
+    public prio: number;
     constructor(val: T, prio: number) {
         this.val = val;
         this.prio = prio;
@@ -61,7 +69,7 @@ class CustomMaxPriorityQueue<T> {
         this.length = 0;
     }
     push(val: T, prio: number): void {
-        const newNode = new PriorityQueueNode<T>(val, prio);
+        const newNode = new PriorityQueueNode(val, prio);
         this.data.push(newNode);
         this.length += 1;
         let currIdx = this.length - 1;
@@ -72,9 +80,9 @@ class CustomMaxPriorityQueue<T> {
             parentIdx = Math.floor((currIdx - 1) / 2);
         }
     }
-    pop(): PriorityQueueNode<T> | null {
+    pop(): PriorityQueueNode<T> | undefined {
         if(this.length === 0) {
-            return null;
+            return undefined;
         }
         if(this.length === 1) {
             this.length = 0;
@@ -103,17 +111,17 @@ class CustomMaxPriorityQueue<T> {
             }
         }
     }
-    top(): number | null {
-        return this.length > 0 ? this.data[0].prio : null;
-    }
-    heapify(values: PriorityQueueNode<T>[]): void {
-        this.data = [...values];
-        this.length = values.length;
+    heapify(vals: PriorityQueueNode<T>[]): void {
+        this.data = [...vals];
+        this.length = vals.length;
         let currIdx = Math.floor((this.length - 2) / 2);
         while(currIdx >= 0) {
             this.siftDown(currIdx);
             currIdx -= 1;
         }
+    }
+    top(): number | undefined {
+        return this.length > 0 ? this.data[0].prio : undefined;
     }
     swap(idx1: number, idx2: number): void {
         const temp = this.data[idx1];
