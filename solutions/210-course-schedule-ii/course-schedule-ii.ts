2af62332b166b4a9d53943dc98140438a1362456
Aidar <stormymayday7@gmail.com>
2025-09-15 15:32:28 -0700

    Time: 22 ms (16.74%) | Memory: 64.3 MB (27.01%) - LeetSync

--- solutions/210-course-schedule-ii/course-schedule-ii.ts
+++ solutions/210-course-schedule-ii/course-schedule-ii.ts
@@ -5,18 +5,16 @@ function findOrder(numCourses: number, prerequisites: number[][]): number[] {
 
 function kahns(adjList: Map<number, Set<number>>): number[] {
 
-    // 1. inDegree map
     const inDegreeMap = new Map<number, number>();
-    for(const node of adjList.keys()) { // O(V)
+    for(const node of adjList.keys()) {
         inDegreeMap.set(node, 0);
     }
-    for(const node of adjList.keys()) { // O(V + E)
+    for(const node of adjList.keys()) {
         for(const neighbor of adjList.get(node)) {
             inDegreeMap.set(neighbor, inDegreeMap.get(neighbor) + 1);
         }
     }
 
-    // 2. 'ready' queue / stack
     const ready: number[] = [];
     for(const [node, inDegreeCount] of inDegreeMap.entries()) {
         if(inDegreeCount === 0) {
@@ -24,7 +22,6 @@ function kahns(adjList: Map<number, Set<number>>): number[] {
         }
     }
 
-    // 3. Kahn's BFS
     const topOrder: number[] = [];
     while(ready.length > 0) {
         const currNode = ready.pop();
@@ -37,7 +34,6 @@ function kahns(adjList: Map<number, Set<number>>): number[] {
         }
     }
 
-    // 4. Cycle check & return
     if(topOrder.length === adjList.size) {
         return topOrder;
     } else {
@@ -48,13 +44,11 @@ function kahns(adjList: Map<number, Set<number>>): number[] {
 
 function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
     const adjList = new Map();
-    for(let i = 0; i < n; i += 1) { // O(V)
+    for(let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const [dst, src] of edges) { // O(E)
+    for(const [dst, src] of edges) {
         adjList.get(src).add(dst);
     }
     return adjList;
-    // Time: O(V + E)
-    // Space: O(V + E)
 }
