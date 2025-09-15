c9617cd11019f16e28b60d175410e094bbd9b8df
Aidar <stormymayday7@gmail.com>
2025-09-15 12:50:58 -0700

    Time: 833 ms (6.70%) | Memory: 76 MB (7.22%) - LeetSync

--- solutions/744-network-delay-time/network-delay-time.ts
+++ solutions/744-network-delay-time/network-delay-time.ts
@@ -2,13 +2,17 @@ function networkDelayTime(times: number[][], n: number, k: number): number {
 
     const adjList = buildAdjList(n, times);
 
-    const minPQ = new CustomMinPriorityQueue<number>(); // val: node (number)
-    minPQ.push(k, 0);
+    const naivePQ: [number, number][] = [];
+    naivePQ.push([k, 0]);
 
     let minTime = 0;
     const visited = new Set<number>();
-    while(minPQ.length > 0) {
-        const { val: currNode, prio: currCost } = minPQ.pop();
+    while(naivePQ.length > 0) {
+
+        naivePQ.sort((a, b) => a[1] - b[1]);
+
+        const [ currNode, currCost ] = naivePQ.shift();
+
         if(visited.has(currNode)) {
             continue;
         }
@@ -16,7 +20,7 @@ function networkDelayTime(times: number[][], n: number, k: number): number {
         minTime = currCost;
         for(const [neighbor, neighborCost] of adjList.get(currNode)) {
             if(!visited.has(neighbor)) {
-                minPQ.push(neighbor, currCost + neighborCost);
+                naivePQ.push([neighbor, currCost + neighborCost]);
             }
         }
     }
@@ -49,71 +53,71 @@ class PriorityQueueNode<T> {
     }
 }
 
-class CustomMinPriorityQueue<T> {
-    private data: PriorityQueueNode<T>[];
-    public length: number;
-    constructor() {
-        this.data = [];
-        this.length = 0;
-    }
-    push(val: T, prio: number): void {
-        const newNode = new PriorityQueueNode(val, prio);
-        this.data.push(newNode);
-        this.length += 1;
-        let currIdx = this.length - 1;
-        let parentIdx = Math.floor((currIdx - 1) / 2);
-        while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
-            this.swap(currIdx, parentIdx);
-            currIdx = parentIdx;
-            parentIdx = Math.floor((currIdx - 1) / 2);
-        }
-    }
-    pop(): PriorityQueueNode<T> | undefined {
-        if(this.length === 0) {
-            return undefined;
-        }
-        if(this.length === 1) {
-            this.length = 0;
-            return this.data.pop();
-        }
-        const root = this.data[0];
-        this.data[0] = this.data.pop();
-        this.length -= 1;
-        this.siftDown(0);
-        return root;
-    }
-    siftDown(idx: number): void {
-        let currIdx = idx;
-        while(currIdx < this.length - 1) {
-            const leftChildIdx = currIdx * 2 + 1;
-            const rightChildIdx = currIdx * 2 + 2;
-            const leftChildPrio = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx].prio;
-            const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
-            const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
-            const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
-            if(this.data[currIdx].prio > smallerChildPrio) {
-                this.swap(currIdx, smallerChildIdx);
-                currIdx = smallerChildIdx;
-            } else {
-                break;
-            }
-        }
-    }
-    heapify(vals: PriorityQueueNode<T>[]): void {
-        this.data = [...vals];
-        this.length = vals.length;
-        let currIdx = Math.floor((this.length - 2) / 2);
-        while(currIdx >= 0) {
-            this.siftDown(currIdx);
-            currIdx -= 1;
-        }
-    }
-    top(): number | undefined {
-        return this.length > 0 ? this.data[0].prio : undefined;
-    }
-    swap(idx1: number, idx2: number): void {
-        const temp = this.data[idx1];
-        this.data[idx1] = this.data[idx2];
-        this.data[idx2] = temp;
-    }
-}
+// class CustomMinPriorityQueue<T> {
+//     private data: PriorityQueueNode<T>[];
+//     public length: number;
+//     constructor() {
+//         this.data = [];
+//         this.length = 0;
+//     }
+//     push(val: T, prio: number): void {
+//         const newNode = new PriorityQueueNode(val, prio);
+//         this.data.push(newNode);
+//         this.length += 1;
+//         let currIdx = this.length - 1;
+//         let parentIdx = Math.floor((currIdx - 1) / 2);
+//         while(currIdx > 0 && this.data[currIdx].prio < this.data[parentIdx].prio) {
+//             this.swap(currIdx, parentIdx);
+//             currIdx = parentIdx;
+//             parentIdx = Math.floor((currIdx - 1) / 2);
+//         }
+//     }
+//     pop(): PriorityQueueNode<T> | undefined {
+//         if(this.length === 0) {
+//             return undefined;
+//         }
+//         if(this.length === 1) {
+//             this.length = 0;
+//             return this.data.pop();
+//         }
+//         const root = this.data[0];
+//         this.data[0] = this.data.pop();
+//         this.length -= 1;
+//         this.siftDown(0);
+//         return root;
+//     }
+//     siftDown(idx: number): void {
+//         let currIdx = idx;
+//         while(currIdx < this.length - 1) {
+//             const leftChildIdx = currIdx * 2 + 1;
+//             const rightChildIdx = currIdx * 2 + 2;
+//             const leftChildPrio = this.data[leftChildIdx] === undefined ? Infinity : this.data[leftChildIdx].prio;
+//             const rightChildPrio = this.data[rightChildIdx] === undefined ? Infinity : this.data[rightChildIdx].prio;
+//             const smallerChildIdx = leftChildPrio < rightChildPrio ? leftChildIdx : rightChildIdx;
+//             const smallerChildPrio = leftChildPrio < rightChildPrio ? leftChildPrio : rightChildPrio;
+//             if(this.data[currIdx].prio > smallerChildPrio) {
+//                 this.swap(currIdx, smallerChildIdx);
+//                 currIdx = smallerChildIdx;
+//             } else {
+//                 break;
+//             }
+//         }
+//     }
+//     heapify(vals: PriorityQueueNode<T>[]): void {
+//         this.data = [...vals];
+//         this.length = vals.length;
+//         let currIdx = Math.floor((this.length - 2) / 2);
+//         while(currIdx >= 0) {
+//             this.siftDown(currIdx);
+//             currIdx -= 1;
+//         }
+//     }
+//     top(): number | undefined {
+//         return this.length > 0 ? this.data[0].prio : undefined;
+//     }
+//     swap(idx1: number, idx2: number): void {
+//         const temp = this.data[idx1];
+//         this.data[idx1] = this.data[idx2];
+//         this.data[idx2] = temp;
+//     }
+// }
