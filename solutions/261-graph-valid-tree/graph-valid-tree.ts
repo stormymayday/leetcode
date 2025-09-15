96f870666e6e588c7d8fb5189354a6d7b6f63a4e
Aidar <stormymayday7@gmail.com>
2025-09-14 21:19:07 -0700

    Time: 2 ms (76.25%) | Memory: 56.3 MB (98.75%) - LeetSync

--- solutions/261-graph-valid-tree/graph-valid-tree.ts
+++ solutions/261-graph-valid-tree/graph-valid-tree.ts
@@ -1,15 +1,41 @@
+function validTree(n: number, edges: number[][]): boolean {
+    // A tree must have exactly n-1 edges
+    if (edges.length !== n - 1) {
+        return false;
+    }
+    
+    const uf = new UnionFind(n);
+    
+    // Check for cycles
+    for (const [src, dst] of edges) {
+        if (uf.union(src, dst) === false) {
+            return false;
+        }
+    }
+    
+    // If we have n-1 edges and no cycles, we have a valid tree
+    return true;
+};
+
 class UnionFind {
     private roots: Map<number, number>;
-    private heights: Map<number, number>;
+    private sizes: Map<number, number>;
     private numComponents: number;
     constructor(n: number) {
         this.roots = new Map();
-        this.heights = new Map();
+        this.sizes = new Map();
         this.numComponents = n;
         for(let i = 0; i < n; i += 1) {
             this.roots.set(i, i);
-            this.heights.set(i, 1);
+            this.sizes.set(i, 1);
         }
+    }
+    find(x: number): number {
+        const root = this.roots.get(x);
+        if(root !== x) {
+            this.roots.set(x, this.find(root));
+        }
+        return this.roots.get(x);
     }
     union(x: number, y: number): boolean {
         const rootX = this.find(x);
@@ -17,36 +43,18 @@ class UnionFind {
         if(rootX === rootY) {
             return false;
         } else {
-            if(this.heights.get(rootX) > this.heights.get(rootY)) {
+            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
                 this.roots.set(rootY, rootX);
-            } else if(this.heights.get(rootY) > this.heights.get(rootX)) {
-                this.roots.set(rootX, rootY);
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY)); 
             } else {
                 this.roots.set(rootX, rootY);
-                this.heights.set(rootY, this.heights.get(rootY) + 1);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
             }
             this.numComponents -= 1;
             return true;
         }
     }
-    find(x: number): number {
-        const parent = this.roots.get(x);
-        if(parent !== x) {
-            this.roots.set(x, this.find(parent));
-        }
-        return this.roots.get(x);
+    getNumComponents(): number {
+        return this.numComponents;
     }
 }
-function validTree(n: number, edges: number[][]): boolean {
-    if(n - 1 !== edges.length) {
-        return false;
-    }
-    const uf = new UnionFind(n);
-    for(const edge of edges) {
-        const [a, b] = edge;
-        if(uf.union(a, b) === false) {
-            return false;
-        }
-    }
-    return true;
-};
