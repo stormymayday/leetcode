a5762cce3b9e167a09169a6da4a5705e9cb4a890
Aidar <stormymayday7@gmail.com>
2025-09-15 20:54:08 -0700

    Time: 8 ms (17.32%) | Memory: 60 MB (26.77%) - LeetSync

--- solutions/684-redundant-connection/redundant-connection.ts
+++ solutions/684-redundant-connection/redundant-connection.ts
@@ -18,14 +18,14 @@ class UnionFind {
         this.roots = new Map();
         this.sizes = new Map();
         this.numComponents = n;
-        for (let i = 1; i <= n; i += 1) {
+        for(let i = 1; i <= n; i += 1) {
             this.roots.set(i, i);
             this.sizes.set(i, 1);
         }
     }
     find(x: number): number {
         const root = this.roots.get(x);
-        if (root !== x) {
+        if(root !== x) {
             this.roots.set(x, this.find(root));
         }
         return this.roots.get(x);
@@ -33,12 +33,12 @@ class UnionFind {
     union(x: number, y: number): boolean {
         const rootX = this.find(x);
         const rootY = this.find(y);
-        if (rootX === rootY) {
+        if(rootX === rootY) {
             return false;
         } else {
-            if (this.sizes.get(rootX) >= this.sizes.get(rootY)) {
+            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
                 this.roots.set(rootY, rootX);
-                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY)); 
             } else {
                 this.roots.set(rootX, rootY);
                 this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
@@ -47,7 +47,7 @@ class UnionFind {
             return true;
         }
     }
-    getLargestComponent(): number {
-        return Math.max(...this.sizes.values());
+    getNumComponents(): number {
+        return this.numComponents;
     }
 }
