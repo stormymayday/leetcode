071f03243fbb240b8d707c98fa9a770f60a42b17
Aidar <stormymayday7@gmail.com>
2025-09-15 19:02:03 -0700

    Time: 479 ms (36.36%) | Memory: 87.8 MB (18.18%) - LeetSync

--- solutions/989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.ts
+++ solutions/989-largest-component-size-by-common-factor/largest-component-size-by-common-factor.ts
@@ -2,65 +2,40 @@ function largestComponentSize(nums: number[]): number {
 
     const uf = new UnionFind(nums);
 
-    // 1. Get factors for each number and create a mapping of factor to numbers
-    const factorToNums = new Map<number, number[]>();
-    for (const num of nums) {
-        // 1.1. Get all factors for the current num
+    const factorToNums = new Map<number, number[]>;
+    for(const num of nums) {
         const factors = getFactors(num);
-        for (const factor of factors) {
-            // 1.2. Create a mapping: factor to the nums
-            if (!factorToNums.has(factor)) {
+        for(const factor of factors) {
+            if(!factorToNums.has(factor)) {
                 factorToNums.set(factor, []);
             }
             factorToNums.get(factor).push(num);
         }
     }
 
-    // 2. Perform union on numbers for each factor
     for(const nums of factorToNums.values()) {
-        for(let i = 0; i < nums.length -1; i += 1) {
-            const num1 = nums[i];
-            const num2 = nums[i + 1];
-            uf.union(num1, num2);
+        for(let i = 0; i < nums.length - 1; i += 1) {
+            uf.union(nums[i], nums[i + 1]);
         }
     }
 
-
-    // 3. Get the largest component size
     return uf.getLargestComponent();
 
 };
 
 function getFactors(num: number): number[] {
-
-    // Use a Set to store factors to avoid duplicates
     const factors = new Set<number>();
-
-    // Start checking from 2, because 1 is common factor for all the numbers
-    // Therefore, if 1 is included 1 as a factor, then every number would be “connected” through 1, because 1 divides all numbers.
     let factor = 2;
-
-    // Only need to check divisors up to the square root of num
-    // Any factor greater than sqrt(num) has a complementary factor less than sqrt(num)
-    while (factor * factor <= num) {
-        // If factor divides num exactly
-        if (num % factor === 0) {
-            // Add the small factor
+    while(factor * factor <= num) {
+        if(num % factor === 0) {
             factors.add(factor);
-            // Add the complementary (larger) factor
             factors.add(num / factor);
         }
-        // Move to the next potential factor
         factor += 1;
     }
-
-    // Edge Case: Include the number itself to ensure primes are unioned properly
     factors.add(num);
-
-    // Convert Set to array and return all factors
-    return [...factors];
+    return [...factors]
 }
-
 
 class UnionFind {
     private roots: Map<number, number>;
