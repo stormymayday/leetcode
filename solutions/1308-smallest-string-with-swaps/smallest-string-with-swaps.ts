aa2553b7b1d7e7b8462a7401d8529493266b270f
Aidar <stormymayday7@gmail.com>
2025-09-15 18:39:44 -0700

    Time: 153 ms (28.00%) | Memory: 103.5 MB (8.00%) - LeetSync

--- solutions/1308-smallest-string-with-swaps/smallest-string-with-swaps.ts
+++ solutions/1308-smallest-string-with-swaps/smallest-string-with-swaps.ts
@@ -1,47 +1,29 @@
 function smallestStringWithSwaps(s: string, pairs: number[][]): string {
 
-    // 1. Union all edges (pairs)
-    const n = s.length;
-    const uf = new UnionFind(n);
-    for(const [src ,dst] of pairs) {
+    const uf = new UnionFind(s.length);
+    for(const [src, dst] of pairs) {
         uf.union(src, dst);
     }
 
-    // 2. Create a mapping (key: root (index) -> val: [characters])
-    const rootToChars = new Map<number, string[]>();
-    for(let i = 0; i < n; i += 1) {
-        // 2.1 find root of the current index value
+    const rootToComponent = new Map<number, string[]>();
+    for(let i = 0; i < s.length; i += 1) {
+        rootToComponent.set(i, []);
+    }
+    for(let i = 0; i < s.length; i += 1) {
         const root = uf.find(i);
-        // 2.2 Create a map entry for the root if it does not exist
-        if(!rootToChars.has(root)) {
-            rootToChars.set(root, []);
-        }
-        // 2.3 push character at current index into the array
-        rootToChars.get(root).push(s[i]);
+        const char = s[i];
+        rootToComponent.get(root).push(char);
+    }
+    for(const component of rootToComponent.values()) {
+        // sorting in descending order 
+        // lexiographically smaller characters will be at the end
+        component.sort((a, b) => b.localeCompare(a));
     }
 
-    // 3. Sort the string arrays (map values) for each root in an descending order
-    // Such that they can be 'popped' into the 'result' at O(1) time
-    for(const chars of rootToChars.values()) {
-        chars.sort((a, b) => b.localeCompare(a));
-    }
-
-    // 4. Creating the result
     const res: string[] = [];
-    // Loop over indicies of the input string
-    for(let i = 0; i < n; i += 1) {
-
-        // 4.1 Get root of the current index
-        const root = uf.find(i);
-
-        // 4.2 Get the char from the 'rootToChars' map
-        // The array is sorted in descending order
-        // Therefore, last char is lexicographically smallest
-        const char = rootToChars.get(root).pop();
-
-        // 4.3 push char into the result
+    for(let i = 0; i < s.length; i += 1) {
+        const char: string = rootToComponent.get(uf.find(i)).pop();
         res.push(char);
-
     }
     return res.join("");
 
@@ -50,11 +32,9 @@ function smallestStringWithSwaps(s: string, pairs: number[][]): string {
 class UnionFind {
     private roots: Map<number, number>;
     private sizes: Map<number, number>;
-    private numComponents: number;
     constructor(n: number) {
         this.roots = new Map();
         this.sizes = new Map();
-        this.numComponents = n;
         for(let i = 0; i < n; i += 1) {
             this.roots.set(i, i);
             this.sizes.set(i, 1);
@@ -75,12 +55,11 @@ class UnionFind {
         } else {
             if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
                 this.roots.set(rootY, rootX);
-                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY)); 
             } else {
                 this.roots.set(rootX, rootY);
                 this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
             }
-            this.numComponents -= 1;
             return true;
         }
     }
