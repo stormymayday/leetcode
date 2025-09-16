fabb90798604b35f0446c86c93de1a311bba5574
Aidar <stormymayday7@gmail.com>
2025-09-15 20:03:11 -0700

    Time: 1 ms (67.13%) | Memory: 54.5 MB (82.41%) - LeetSync

--- solutions/399-evaluate-division/evaluate-division.ts
+++ solutions/399-evaluate-division/evaluate-division.ts
@@ -1,92 +1,65 @@
 function calcEquation(equations: string[][], values: number[], queries: string[][]): number[] {
 
-    const n = equations.length;
-    
-    // 1. Initialize UnionFind
-    const uf = new ModifiedUnionFind();
-    for(let i = 0; i < n; i += 1) {
-        const [dividend, divisor] = equations[i];
-        const quotient = values[i];
-        uf.union(dividend, divisor, quotient);
+    const edgeList: [string, string, number][] = [];
+    for (let i = 0; i < equations.length; i += 1) {
+        const [src, dst] = equations[i];
+        const weight = values[i];
+        edgeList.push([src, dst, weight]);
     }
 
-    // 2. Process queries and get the result
+    const adjList = buildAdjList(edgeList);
+
     const res: number[] = [];
-    for(const [dividend, divisor] of queries) {
-        res.push(uf.getRatio(dividend, divisor));
+    for (const [src, dst] of queries) {
+        if (!adjList.has(src) || !adjList.has(dst)) {
+            res.push(-1);
+        } else if (src === dst) {
+            res.push(1);
+        } else {
+            res.push(dfs(src, dst, adjList, new Set<string>()));
+        }
     }
     return res;
-
 };
 
-class ModifiedUnionFind {
+function dfs(src: string, dst: string, adjList: Map<string, [string, number][]>, visited: Set<string>): number {
 
-    // standard node to root mapping
-    private roots: Map<string, string>;
-
-    // Ratio of node / parent (eventually root)
-    // Initially, this stores ratio to node's immediate parent
-    // However, after running 'find' method it will update ratio towards root
-    private ratioToParent: Map<string, number>;
-
-    constructor() {
-        this.roots = new Map();
-        this.ratioToParent = new Map();
+    // Base Case: reached destination
+    if (src === dst) {
+        return 1;
     }
 
-    add(x: string): void {
-        if(!this.roots.has(x)) {
-            // Setting node as it's own root
-            this.roots.set(x, x);
-            // Initial ratio is 1 ( x / x = 1)
-            this.ratioToParent.set(x, 1);
-        }
+    // Cycle ? 
+    if (visited.has(src)) {
+        return -1;
     }
 
-    find(x: string): string {
-        const root = this.roots.get(x);
-        if(root !== x) {
-            // Path compression
-            this.roots.set(x, this.find(root));
-            // Update ratio
-            this.ratioToParent.set(x, this.ratioToParent.get(x) * this.ratioToParent.get(root));
-        }
-        return this.roots.get(x);
-    }
+    visited.add(src);
 
-    union(dividend: string, divisor: string, quotioent: number): void {
-
-        // 1. Adding nodes
-        this.add(dividend);
-        this.add(divisor);
-
-        // 2. Fetching roots
-        const dividendRoot = this.find(dividend);
-        const divisorRoot = this.find(divisor);
-
-        // 3. Check if roots are not already connected
-        if(dividendRoot !== divisorRoot) {
-            // 3.1. Connecting roots
-            this.roots.set(dividendRoot, divisorRoot);
-            // 3.2. Setting the ratio (don't understand this)
-            this.ratioToParent.set(dividendRoot, (quotioent * this.ratioToParent.get(divisor) / this.ratioToParent.get(dividend)));
+    for (const [neighbor, weight] of adjList.get(src)) {
+        if (!visited.has(neighbor)) {
+            const res: number = dfs(neighbor, dst, adjList, visited);
+            if (res !== - 1) {
+                return res * weight;
+            }
         }
 
     }
 
-    getRatio(dividend: string, divisor: string): number {
-        // Check if either doesn't exist
-        if(!this.roots.has(dividend) || !this.roots.has(divisor)) {
-            return -1;
-        }
+    return -1;
+}
 
-        // Check if their root is not the same AKA they are not connected
-        if(this.find(dividend) !== this.find(divisor)) {
-            return -1;
+function buildAdjList(edgeList: [string, string, number][]): Map<string, [string, number][]> {
+    const adjList = new Map();
+    for (const [src, dst, weight] of edgeList) {
+        if (!adjList.has(src)) {
+            adjList.set(src, []);
         }
-
-        // Otherwise, get the ratio
-        return this.ratioToParent.get(dividend) / this.ratioToParent.get(divisor);
+        if (!adjList.has(dst)) {
+            adjList.set(dst, []);
+        }
+        adjList.get(src).push([dst, weight]);
+        adjList.get(dst).push([src, 1 / weight]);
     }
-
+    return adjList;
 }
