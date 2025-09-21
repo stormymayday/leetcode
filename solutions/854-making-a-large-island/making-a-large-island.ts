0fb48ef21851d528a6daefbb1b0bac972103144e
Aidar <stormymayday7@gmail.com>
2025-09-20 17:33:18 -0700

    Time: 699 ms (8.69%) | Memory: 113.5 MB (5.43%) - LeetSync

--- solutions/854-making-a-large-island/making-a-large-island.ts
+++ solutions/854-making-a-large-island/making-a-large-island.ts
@@ -1,101 +1,128 @@
 function largestIsland(grid: number[][]): number {
 
     const n = grid.length;
-
-    // 1. DFS the matrix finding each island, labeling it and recording it's size
-    const islandToSize = new Map<number, number>(); // key: islandId, value: size
-    const visited: boolean[][] = new Array(n);
-    let islandId = 2; // starting with ID of 2
-    for (let i = 0; i < n; i += 1) {
-        visited[i] = new Array(n).fill(false);
-    }
-    for (let row = 0; row < n; row += 1) {
-        for (let col = 0; col < n; col += 1) {
-            if (grid[row][col] === 1 && visited[row][col] === false) {
-                islandToSize.set(islandId, matrixDFS(row, col, grid, islandId, visited));
-                islandId += 1;
-            }
-        }
-    }
-
-    // Edge Case: no islands were found (entire grid is water)
-    if (islandToSize.size === 0) {
-        return 1; // Can only flip one zero
-    }
-
-    // Edge Case: entire grid is one island
-    if (islandToSize.size === 1 && islandToSize.get(2) === n * n) {
-        return islandToSize.get(2);
-    }
-
-    // 2. Making A Large Island
-    let largest = 1; // to track max (combined) island size
-    for (let row = 0; row < n; row += 1) {
-        for (let col = 0; col < n; col += 1) {
-            if (grid[row][col] === 0) {
-                const usedIslands = new Set<number>(); // to avoid re-adding an island
-                let currentLargest = 1; // flipping 0 to 1
-                // check all directions for islands and add their size
+    
+    // Step 1: Initialize UnionFind and Union adjacent 1s in the grid
+    const uf = new UnionFind(n*n);
+    for(let row = 0; row < n; row += 1) {
+        for(let col = 0; col < n; col += 1) {
+            if(grid[row][col] === 1) {
                 const directions: [number, number][] = [
                     [-1, 0], // up
                     [0, 1], // right
                     [1, 0], // down
                     [0, -1], // left
                 ];
-                for (const [rowDelta, colDelta] of directions) {
+                for(const [rowDelta, colDelta] of directions) {
                     const neighborRow = row + rowDelta;
                     const neighborCol = col + colDelta;
-                    if (
+                    if(
                         // out of bounds check
                         0 <= neighborRow && neighborRow < n &&
                         0 <= neighborCol && neighborCol < n &&
-                        // island check
-                        grid[neighborRow][neighborCol] !== 0 &&
-                        // used island check
-                        !usedIslands.has(grid[neighborRow][neighborCol])
+                        // land check
+                        grid[neighborRow][neighborCol] === 1
                     ) {
-                        usedIslands.add(grid[neighborRow][neighborCol]);
-                        currentLargest += islandToSize.get(grid[neighborRow][neighborCol]);
+                        // Flatten 2D index to 1D using:
+                        // Row Major Order formula: index = (row * numCols) + col
+                        const currCellIdx = (row * n) + col;
+                        const neighborCellIdx = (neighborRow * n) + neighborCol;
+                        uf.union(currCellIdx, neighborCellIdx);
                     }
                 }
-                largest = Math.max(largest, currentLargest);
             }
         }
     }
-    return largest;
+
+    // Step 2: Calculate the maximum possible island size
+    let maxIslandSize = 0;
+    let hasZeros = false; // handle the edge case where there are no zeroes in the grid
+    for(let row = 0; row < n; row += 1) {
+        for(let col = 0; col < n; col += 1) {
+            if(grid[row][col] === 0) {
+                hasZeros = true;
+                let currentIslandSize = 1; // flipping zero to 1 at this cell
+                const rootsUsed = new Set<number>(); // to avoid re-adding same islands
+                const directions: [number, number][] = [
+                    [-1, 0], // up
+                    [0, 1], // right
+                    [1, 0], // down
+                    [0, -1], // left
+                ];
+                for(const [rowDelta, colDelta] of directions) {
+                    const neighborRow = row + rowDelta;
+                    const neighborCol = col + colDelta;
+                    // Using Row Major Order formula: index = (row * numCols) + col to flatten 2D index to 1D
+                    const neighborCellIdx = (neighborRow * n) + neighborCol;
+                    const neighborCellRoot = uf.find(neighborCellIdx);
+                    if(
+                        // out of bounds check
+                        0 <= neighborRow && neighborRow < n &&
+                        0 <= neighborCol && neighborCol < n &&
+                        // land check
+                        grid[neighborRow][neighborCol] === 1 &&
+                        // check if we added this island already
+                        !rootsUsed.has(neighborCellRoot)
+                    ) {
+                        rootsUsed.add(neighborCellRoot);
+                        currentIslandSize += uf.getSize(neighborCellRoot);
+                    }
+                }
+                maxIslandSize = Math.max(maxIslandSize, currentIslandSize);
+            }
+        }
+    }
+
+    // If there are no zeros, the largest island is the entire grid
+    if(hasZeros === false) {
+        return n*n;
+    } else {
+        return maxIslandSize;
+    }
+
 };
 
-function matrixDFS(row: number, col: number, grid: number[][], islandId: number, visited: boolean[][]): number {
-
-    // Base Case 1: Out of bounds
-    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
-        return 0;
+class UnionFind {
+    private roots: Map<number, number>;
+    private sizes: Map<number, number>;
+    private numComponents: number;
+    constructor(n: number) {
+        this.roots = new Map();
+        this.sizes = new Map();
+        this.numComponents = n;
+        for(let i = 0; i < n; i += 1) {
+            this.roots.set(i, i);
+            this.sizes.set(i, 1);
+        }
     }
-
-    // Base Case 2: visited
-    if (visited[row][col] === true) {
-        return 0;
+    find(x: number): number {
+        const root = this.roots.get(x);
+        if(root !== x) {
+            this.roots.set(x, this.find(root));
+        }
+        return this.roots.get(x);
     }
-
-    // Base Case 3: not a land
-    if (grid[row][col] !== 1) {
-        return 0;
+    union(x: number, y: number): boolean {
+        const rootX = this.find(x);
+        const rootY = this.find(y);
+        if(rootX === rootY) {
+            return false;
+        } else {
+            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
+                this.roots.set(rootY, rootX);
+                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
+            } else {
+                this.roots.set(rootX, rootY);
+                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
+            }
+            this.numComponents -= 1;
+            return true;
+        }
     }
-
-    // Recursive Step
-    visited[row][col] = true; // mark cell as visited
-    grid[row][col] = islandId; // change cell value to islandId
-    let area = 1; // counting current land cell
-    const directions: [number, number][] = [
-        [-1, 0], // up
-        [0, 1], // right
-        [1, 0], // down
-        [0, -1], // left
-    ];
-    for (const [rowDelta, colDelta] of directions) {
-        const neighborRow = row + rowDelta;
-        const neighborCol = col + colDelta;
-        area += matrixDFS(neighborRow, neighborCol, grid, islandId, visited);
+    getNumComponents() {
+        return this.numComponents;
     }
-    return area;
+    getSize(x: number): number {
+        return this.sizes.get(x);
+    }
 }
