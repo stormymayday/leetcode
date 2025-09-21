caf13d8d9d3161468b6906f4b0cc1dd7e63a3556
Aidar <stormymayday7@gmail.com>
2025-09-21 10:56:37 -0700

    Time: 8 ms (8.78%) | Memory: 59 MB (81.31%) - LeetSync

--- solutions/733-flood-fill/flood-fill.ts
+++ solutions/733-flood-fill/flood-fill.ts
@@ -1,10 +1,48 @@
 function floodFill(image: number[][], sr: number, sc: number, color: number): number[][] {
 
-    matrixBFS(sr, sc, image, color);
+    const visited: boolean[][] = new Array(image.length);
+    for(let row = 0; row < image.length; row += 1) {
+        visited[row] = new Array(image[0].length).fill(false);
+    }
+    matrixDFS(sr, sc, image, visited, image[sr][sc], color);
 
     return image;
 
 };
+
+function matrixDFS(row: number, col: number, grid: number[][], visited: boolean[][], original: number, color: number): void {
+
+    // Base Case: out of bounds
+    if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+        return;
+    }
+
+    // Base Case: visited
+    if(visited[row][col] === true) {
+        return;
+    }
+
+    // Base Case: value check
+    if(grid[row][col] !== original) {
+        return;
+    }
+
+    visited[row][col] = true;
+    grid[row][col] = color;
+
+    const directions: [number, number][] = [
+        [-1, 0], // up
+        [0, 1], // right
+        [1, 0], // down
+        [0, -1], // left
+    ];
+    for(const [rowDelta, colDelta] of directions) {
+        const neighborRow = row + rowDelta;
+        const neighborCol = col + colDelta;
+        matrixDFS(neighborRow, neighborCol, grid, visited, original, color);
+    }
+
+}
 
 function matrixBFS(row: number, col: number, grid: number[][], color: number): void {
 
