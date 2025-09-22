094d99ae008193b840c463b5097300ae581a7201
Aidar <stormymayday7@gmail.com>
2025-09-22 12:48:48 -0700

    Time: 32 ms (45.24%) | Memory: 65.5 MB (69.05%) - LeetSync

--- solutions/820-find-eventual-safe-states/find-eventual-safe-states.ts
+++ solutions/820-find-eventual-safe-states/find-eventual-safe-states.ts
@@ -1,48 +1,35 @@
 function eventualSafeNodes(graph: number[][]): number[] {
+    
+    const visiting = new Set<number>();
+    const visited = new Set<number>();
 
-    const revAdjList = buildRevAdjList(graph);
+    for(let i = 0; i < graph.length; i += 1) {
+        dfs(i, graph, visiting, visited);
+    }
 
-    return reverseKahns(graph, revAdjList);
+    return [...visited].sort((a, b) => a - b);
 
 };
 
-function buildRevAdjList(graph: number[][]): Map<number, Set<number>> {
-    const revAdjList = new Map();
-    for(let i = 0; i < graph.length; i += 1) {
-        revAdjList.set(i, new Set());
-    }
-    for(let node = 0; node < graph.length; node += 1) {
-        for(const neighbor of graph[node]) {
-            revAdjList.get(neighbor).add(node);
-        }
-    }
-    return revAdjList;
-}
+function dfs(node: number, graph: number[][], visiting: Set<number>, visited: Set<number>): boolean {
 
-function reverseKahns(graph: number[][], revAdjList: Map<number, Set<number>>): number[] {
-
-    const outDegreeMap = new Map<number, number>();
-    for(let i = 0; i < graph.length; i += 1) {
-        outDegreeMap.set(i, graph[i].length);
+    if(visited.has(node)) {
+        return false; // no cycle
     }
 
-    const ready: number[] = [];
-    for(const [node, outDegreeCount] of outDegreeMap.entries()) {
-        if(outDegreeCount === 0) {
-            ready.push(node);
+    if(visiting.has(node)) {
+        return true; // cycle
+    }
+
+    visiting.add(node);
+
+    for(const neighbor of graph[node]) {
+        if(dfs(neighbor, graph, visiting, visited) === true) {
+            return true; // cycle
         }
     }
 
-    const topOrder: number[] = [];
-    while(ready.length > 0) {
-        const currNode = ready.pop();
-        topOrder.push(currNode);
-        for(const parent of revAdjList.get(currNode)) {
-            outDegreeMap.set(parent, outDegreeMap.get(parent) - 1);
-            if(outDegreeMap.get(parent) === 0) {
-                ready.push(parent);
-            }
-        }
-    }
-    return topOrder.sort((a, b) => a - b);
+    visiting.delete(node);
+    visited.add(node);
+    return false; // no cycle
 }
