577aaf1b1e551837ddd50e43fc894a2055348bde
Aidar <stormymayday7@gmail.com>
2025-09-22 19:21:38 -0700

    Time: 285 ms (35.00%) | Memory: 107 MB (20.00%) - LeetSync

---
+++ solutions/1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.ts
@@ -0,0 +1,61 @@
+function numOfMinutes(n: number, headID: number, manager: number[], informTime: number[]): number {
+    
+    const adjList = buildAdjList(n, manager);
+
+    return bfs(headID, adjList, informTime);
+
+};
+
+function bfs(src: number, adjList: Map<number, Set<number>>, informTime: number[]): number {
+
+    let queue: [number, number][] = []; // [node, informTime]
+    queue.push([src, 0]);
+
+    let totalInformTime = 0;
+
+    while(queue.length > 0) {
+
+        const nextQueue: [number, number][] = [];
+        // let layerMax = 0;
+
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const [currNode, currInformTime] = queue[i];
+            // layerMax = Math.max(layerMax, currInformTime);
+            totalInformTime = Math.max(totalInformTime, currInformTime);
+
+            for(const neighbor of adjList.get(currNode)) {
+                // nextQueue.push([neighbor, informTime[currNode]]);
+                nextQueue.push([neighbor, currInformTime + informTime[currNode]]);
+            }
+
+        }
+
+        // totalInformTime += layerMax;
+
+        if(nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
+        }
+
+    }
+
+    return totalInformTime;
+}
+
+function buildAdjList(n: number, manager: number[]): Map<number, Set<number>> {
+    const adjList = new Map();
+    for(let i = 0; i < n; i += 1) {
+        adjList.set(i, new Set());
+    }
+    for(let i = 0; i < manager.length; i += 1) {
+        const src = manager[i];
+        const dst = i;
+        if(src === -1) {
+            continue;
+        }
+        adjList.get(src).add(dst);
+    }
+    return adjList;
+}
