75543814a96ff3f37012cb6005e38f376b60ca67
Aidar <stormymayday7@gmail.com>
2025-09-25 14:15:40 -0700

    Time: 81 ms (6.35%) | Memory: 90.6 MB (6.35%) - LeetSync

--- solutions/1428-jump-game-iii/jump-game-iii.ts
+++ solutions/1428-jump-game-iii/jump-game-iii.ts
@@ -3,9 +3,41 @@ function canReach(arr: number[], start: number): boolean {
     const adjList = buildAdjList(arr);
 
     // starting node: starting 'index', target: value '0' at any index
-    return bfs(start, 0, adjList, arr);
+    // return bfs(start, 0, adjList, arr);
+
+    // starting node: starting 'index', target: value '0' at any index
+    return dfs(start, 0, adjList, arr, new Set());
 
 };
+
+function dfs(src: number, targetVal: number, adjList: Map<number, Set<number>>, arr: number[], visited: Set<number>): boolean {
+
+    // Base Case: src (index) out of bounds
+    if(src < 0 || src >= arr.length) {
+        return false;
+    }
+
+    // Base Case: src (index) has been visited
+    if(visited.has(src)) {
+        return false;
+    }
+
+    // Base Case: targetVal found
+    if(arr[src] === targetVal) {
+        return true;
+    }
+
+    visited.add(src);
+
+    for(const neighbor of adjList.get(src)) {
+        if(dfs(neighbor, targetVal, adjList, arr, visited) === true) {
+            return true; // exit early if target found
+        }
+    }
+
+    // target not found
+    return false;
+}
 
 function bfs(src: number, targetVal: number, adjList: Map<number, Set<number>>, arr: number[]): boolean {
 
