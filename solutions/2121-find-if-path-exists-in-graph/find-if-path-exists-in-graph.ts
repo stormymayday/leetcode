baf94fa0b408e16e840e29d2df4efeb1d90aa06b
Aidar <stormymayday7@gmail.com>
2025-09-26 18:56:11 -0700

    Time: 785 ms (46.92%) | Memory: 182.4 MB (44.55%) - LeetSync

--- solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
+++ solutions/2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.ts
@@ -1,47 +1,40 @@
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
-    const uf = new UnionFind(n);
-    for(const edge of edges) {
-        const [src, dst] = edge;
-        uf.union(src, dst);
-    }
-    return uf.isSameComponent(source, destination);
+    const adjList = buildAdjList(n, edges);
+    return dfs(source, destination, adjList, new Set<number>());
 };
 
-class UnionFind {
-    private roots: Map<number, number>;
-    private sizes: Map<number, number>;
-    constructor(n: number) {
-        this.roots = new Map();
-        this.sizes = new Map();
-        for(let i = 0; i < n; i += 1) {
-            this.roots.set(i, i);
-            this.sizes.set(i, 1);
-        }
+function dfs(src: number, dst: number, adjList: Map<number, Set<number>>, visited: Set<number>): boolean {
+
+    // Base Case 1: destination reached
+    if(src === dst) {
+        return true;
     }
-    find(x: number):number {
-        const root = this.roots.get(x);
-        if(root !== x) {
-            this.roots.set(x, this.find(root));
-        }
-        return this.roots.get(x);
+
+    // Base Case 2: visited
+    if(visited.has(src)) {
+        return false;
     }
-    union(x: number, y: number): boolean {
-        const rootX = this.find(x);
-        const rootY = this.find(y);
-        if(rootX === rootY) {
-            return false;
-        } else {
-            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
-                this.roots.set(rootY, rootX);
-                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY));
-            } else {
-                this.roots.set(rootX, rootY);
-                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
-            }
+
+    visited.add(src);
+
+    for(const neighbor of adjList.get(src)) {
+        if(dfs(neighbor, dst, adjList, visited) === true) {
             return true;
         }
     }
-    isSameComponent(x: number, y: number):boolean {
-        return this.find(x) === this.find(y);
+
+    return false;
+
+}
+
+function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+    const adjList = new Map();
+    for(let i = 0; i < n; i += 1) {
+        adjList.set(i, new Set());
     }
+    for(const [src, dst] of edges) {
+        adjList.get(src).add(dst);
+        adjList.get(dst).add(src);
+    }
+    return adjList;
 }
