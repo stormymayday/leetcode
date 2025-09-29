6ec504f5245311c3321c0d85a349af7ec9a574bb
Aidar <stormymayday7@gmail.com>
2025-09-29 10:43:25 -0700

    Time: 53 ms (64.85%) | Memory: 68.9 MB (39.11%) - LeetSync

--- solutions/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.ts
+++ solutions/1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.ts
@@ -1,40 +1,71 @@
 function shortestPathBinaryMatrix(grid: number[][]): number {
-    // Edge Case: starting cell is 1
-    if(grid[0][0] === 1) {
+
+    // Edge Case: blocked start or end
+    if(grid[0][0] === 1 || grid[grid.length - 1][grid[0].length - 1] === 1) {
         return -1;
     }
+
     return matrixBFS(grid);
+    
 };
 
-function matrixBFS(grid:number[][]):number {
-    const visited = new Set<string>();
-    visited.add(`${0},${0}`);
-    const queue: [number, number, number][] = [[0, 0, 1]]; // row, col, distance
-    while(queue.length > 0) {
-        const [row, col, distance] = queue.shift();
-        if(row === grid.length - 1 && col === grid[0].length - 1) {
-            return distance;
-        }
-        const deltas = [
-            [-1, -1],[-1, 0],[-1,1],
-            [0, -1],         [0, 1],
-            [1, -1], [1, 0], [1, 1]
-        ];
-        for(const delta of deltas) {
-            const [rowDelta, colDelta] = delta;
-            const neighborRow = rowDelta + row;
-            const neighborCol = colDelta + col;
-            const neighborPosition = `${neighborRow},${neighborCol}`;
-            if(
-                0 <= neighborRow && neighborRow < grid.length &&
-                0 <= neighborCol && neighborCol < grid[0].length &&
-                !visited.has(neighborPosition) &&
-                grid[neighborRow][neighborCol] !== 1
-            ) {
-                visited.add(neighborPosition);
-                queue.push([neighborRow, neighborCol, distance + 1]);
-            }
-        }
+function matrixBFS(grid: number[][]): number {
+
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+
+    let queue: [number, number, number][] = [];
+    queue.push([0, 0, 1]); // [row, col, distance]
+
+    const visited: boolean[][] = new Array(ROWS);
+    for(let row = 0; row < ROWS; row += 1) {
+        visited[row] = new Array(COLS).fill(false);
     }
-    return -1; // not found
+    visited[0][0] = true;
+
+    while(queue.length > 0) {
+
+        const nextQueue: [number, number, number][] = [];
+
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const [currRow, currCol, currDist] = queue[i];
+
+            if(currRow === ROWS - 1 && currCol === COLS - 1) {
+                return currDist;
+            }
+
+            const directions: [number, number][] = [
+                [-1, -1],[-1, 0],[-1, 1],
+                [0, -1],          [0, 1],
+                [1, -1], [1, 0], [1, 1]
+            ];
+            for(const [rowDelta, colDelta] of directions) {
+                const neighborRow = currRow + rowDelta;
+                const neighborCol = currCol + colDelta;
+                if(
+                    // out of bounds check
+                    0 <= neighborRow && neighborRow < ROWS &&
+                    0 <= neighborCol && neighborCol < COLS &&
+                    // visited check
+                    visited[neighborRow][neighborCol] === false &&
+                    // clear path check
+                    grid[neighborRow][neighborCol] === 0
+                ) {
+                    visited[neighborRow][neighborCol] = true;
+                    nextQueue.push([neighborRow, neighborCol, currDist + 1]);
+                }
+            }
+
+        }
+
+        if(nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
+        }
+
+    }
+
+    return -1;
 }
