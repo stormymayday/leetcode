ee31f1eb6656acb838a52b59357303389ff1c81b
Aidar <stormymayday7@gmail.com>
2025-09-29 12:48:37 -0700

    Time: 13 ms (36.78%) | Memory: 61.6 MB (27.82%) - LeetSync

--- solutions/1036-rotting-oranges/rotting-oranges.ts
+++ solutions/1036-rotting-oranges/rotting-oranges.ts
@@ -1,56 +1,81 @@
 function orangesRotting(grid: number[][]): number {
-    let freshOranges: number = 0;
-    let time: number = 0;
-    let queue: [number, number][] = [];
-    const visited = new Set<string>();
-    for(let row = 0; row < grid.length; row += 1) {
-        for(let col = 0; col < grid[0].length; col += 1) {
-            const current = grid[row][col];
-            if(current === 1) {
+
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+
+    // 1. Scan the grid to:
+    // - count number of fresh oranges
+    let freshOranges = 0;
+    // - Queue up rotten oranges' position with 0 initial time and mark it 'visited'
+    const visited: boolean[][] = new Array(ROWS);
+    for(let row = 0; row < ROWS; row += 1) {
+        visited[row] = new Array(COLS).fill(false);
+    }
+    let queue: [number, number, number][] = [];
+    for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            if (grid[row][col] === 1) {
                 freshOranges += 1;
             }
-            if(current === 2) {
-                queue.push([row, col]);
-                visited.add(`${row},${col}`);
+            if (grid[row][col] === 2) {
+                queue.push([row, col, 0]);
+                visited[row][col] = true;
             }
         }
     }
+
+    // Edge Case: no fresh oranges?
+
+    // 2. Run Multi-Source BFS
+    let time = 0;
     while(queue.length > 0) {
-        const n = queue.length;
-        const nextLayer: [number, number][] = [];
-        for(let i = 0; i < n ; i += 1) {
-            const [row, col] = queue.shift();
-            const deltas = [
+
+        const nextQueue: [number, number, number][] = [];
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const [currRow, currCol, currTime] = queue[i];
+            time = currTime;
+            // freshOranges -= 1;
+
+            const directions: [number, number][] = [
                 [-1, 0], // up
                 [0, 1], // right
                 [1, 0], // down
                 [0, -1], // left
             ];
-            for(const delta of deltas) {
-                const [rowDelta, colDelta] = delta;
-                const neighborRow = rowDelta + row;
-                const neighborCol = colDelta + col;
-                const neighborPosition = `${neighborRow},${neighborCol}`;
+            for(const [rowDelta, colDelta] of directions) {
+                const neighborRow = currRow + rowDelta;
+                const neighborCol = currCol + colDelta;
                 if(
-                    0 <= neighborRow && neighborRow < grid.length &&
-                    0 <= neighborCol && neighborCol < grid[0].length &&
-                    !visited.has(neighborPosition) &&
+                    // out of bounds check
+                    0 <= neighborRow && neighborRow < ROWS &&
+                    0 <= neighborCol && neighborCol < COLS &&
+                    // visited check
+                    visited[neighborRow][neighborCol] === false &&
+                    // fresh orange check
                     grid[neighborRow][neighborCol] === 1
                 ) {
-                    visited.add(neighborPosition);
+                    visited[neighborRow][neighborCol] = true;
+                    nextQueue.push([neighborRow, neighborCol, currTime + 1]);
                     freshOranges -= 1;
-                    nextLayer.push([neighborRow, neighborCol]);
                 }
             }
+
         }
-        if(nextLayer.length !== 0) {
-            time += 1; // avoid off by one error
+
+        if(nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
         }
-        queue = nextLayer;
+
     }
-    if(freshOranges === 0) {
-        return time;
+
+    // 3. Check if freshOranges is 0
+    if(freshOranges !== 0) {
+        return -1; // some fresh oranges left
     } else {
-        return -1;
+        return time; // time it took for all fresh oranges to rot
     }
+    
 };
