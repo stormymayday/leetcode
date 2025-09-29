cbd2c634ccf3de398d77fcd695c886e9c52e6474
Aidar <stormymayday7@gmail.com>
2025-09-28 20:04:56 -0700

    Time: 19 ms (50.00%) | Memory: 66.7 MB (50.00%) - LeetSync

--- solutions/511-all-paths-from-source-lead-to-destination/all-paths-from-source-lead-to-destination.ts
+++ solutions/511-all-paths-from-source-lead-to-destination/all-paths-from-source-lead-to-destination.ts
@@ -1,46 +1,59 @@
 function leadsToDestination(n: number, edges: number[][], source: number, destination: number): boolean {
+
     const adjList = buildAdjList(n, edges);
-    const visiting = new Set<number>();
-    const visited = new Set<number>();
-    return dfs(adjList, source, destination, visiting, visited);
+
+    return backtrackDFS(source, destination, adjList, new Set<number>(), new Set<number>());
+
 };
 
-function dfs(adjList: Map<number, Set<number>>, src: number, dst: number, visiting: Set<number>, visited: Set<number>): boolean {
-    
-    if(visited.has(src)) {
-        return true; // so far, no cycle
-    }
-    
-    if(visiting.has(src)) {
-        return false; // cycle
+function backtrackDFS(
+    src: number,
+    dst: number,
+    adjList: Map<number, Set<number>>,
+    visiting: Set<number>,
+    visited: Set<number>
+): boolean {
+
+    // Base Case 1: no cycle so 'far'
+    if (visited.has(src)) {
+        return true;
     }
 
-    // If this is a leaf node, it should be equal to the destination.
-    if(adjList.get(src).size === 0) {
+    // Base Case 2: cycle
+    if (visiting.has(src)) {
+        return false;
+    }
+
+    // Base Case 3: reached the terminal node (check if it's the destination)
+    const neighbors = adjList.get(src);
+    if (neighbors.size === 0) {
         return src === dst;
     }
 
+    // this placement is important
     visiting.add(src);
 
-    for(const neighbor of adjList.get(src)) {
-        if(dfs(adjList, neighbor, dst, visiting, visited) === false) {
-            return false;
+    for (const neighbor of neighbors) {
+
+        if (backtrackDFS(neighbor, dst, adjList, visiting, visited) === false) {
+            return false; // exit early if loop was found or terminal nodes is not the 'target'
         }
+
     }
 
+    // Backtrack
     visiting.delete(src);
     visited.add(src);
-    return true;
+    return true; 
 
 }
 
 function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
     const adjList = new Map();
-    for(let i = 0; i < n; i += 1) {
+    for (let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const edge of edges) {
-        const [src, dst] = edge;
+    for (const [src, dst] of edges) {
         adjList.get(src).add(dst);
     }
     return adjList;
