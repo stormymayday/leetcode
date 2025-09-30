e4b7ee1c0260561a5d93566e54202d509e9fd6bd
Aidar <stormymayday7@gmail.com>
2025-09-29 18:43:26 -0700

    Time: 10 ms (28.00%) | Memory: 64.5 MB (89.00%) - LeetSync

--- solutions/813-all-paths-from-source-to-target/all-paths-from-source-to-target.ts
+++ solutions/813-all-paths-from-source-to-target/all-paths-from-source-to-target.ts
@@ -1,28 +1,28 @@
 function allPathsSourceTarget(graph: number[][]): number[][] {
     
     const res: number[][] = [];
-    
     backtrackDFS(0, graph.length - 1, graph, [0], res);
-
     return res;
 
 };
 
-function backtrackDFS(src: number, target: number, graph: number[][], path: number[], res: number[][]): void {
+function backtrackDFS(
+    src: number, 
+    target: number, 
+    graph: number[][],
+    path: number[],
+    res: number[][],
+    ): void {
 
-    // path.push(src);
+        if(src === target) {
+            res.push([...path]);
+            return;
+        }
 
-    if(src === target) {
-        res.push([...path]);
-        return;
-    }
-
-    for(const neighbor of graph[src]) {
-        path.push(neighbor);
-        backtrackDFS(neighbor, target, graph, path, res);
-        path.pop();
-    }
-
-    return;
+        for(const neighbor of graph[src]) {
+            path.push(neighbor);
+            backtrackDFS(neighbor, target, graph, path, res);
+            path.pop();
+        }
 
 }
