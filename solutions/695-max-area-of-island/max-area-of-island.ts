5523c4f6514c637e8598232b08a053a147d2e6b6
Aidar <stormymayday7@gmail.com>
2025-09-30 16:18:06 -0700

    Time: 10 ms (37.35%) | Memory: 63.8 MB (8.82%) - LeetSync

--- solutions/695-max-area-of-island/max-area-of-island.ts
+++ solutions/695-max-area-of-island/max-area-of-island.ts
@@ -1,109 +1,55 @@
 function maxAreaOfIsland(grid: number[][]): number {
-  const visited = new Set<string>();
-  let largest = 0;
-  for(let r = 0; r < grid.length; r += 1) {
-    for(let c = 0; c < grid[0].length; c += 1) {
-        if(grid[r][c] === 1 && !visited.has(`${r},${c}`)) {
-            // largest = Math.max(largest, rDFS(grid, r, c, visited));
-            // largest = Math.max(largest, bfs(grid, r, c, visited));
-            largest = Math.max(largest, dfs(grid, r, c, visited));
+
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+
+    const visited: boolean[][] = new Array(ROWS);
+    for (let row = 0; row < ROWS; row += 1) {
+        visited[row] = new Array(COLS).fill(false);
+    }
+
+    let largest = 0;
+    for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            if (grid[row][col] === 1 && visited[row][col] === false) {
+                largest = Math.max(largest, matrixDFS(row, col, grid, visited));
+            }
         }
     }
-  }
-  return largest;
+    return largest;
+
 };
 
-function dfs(grid: number[][], r:number, c: number, visited: Set<string>):number {
-    const position = `${r},${c}`;
-    if(visited.has(position)) {
+function matrixDFS(row: number, col: number, grid: number[][], visited: boolean[][]): number {
+
+    // Base Case 1: out of bounds
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
         return 0;
     }
 
-    visited.add(position);
-    const stack = [[r, c]];
-    let size = 1;
-    while(stack.length > 0) {
-        const [row, col] = stack.pop();
-        const deltas = [[-1, 0], [1, 0], [0, -1], [0,1]];
-        for(const delta of deltas) {
-            const [rowDelta, colDelta] = delta;
-            const neighborRow = row + rowDelta;
-            const neighborCol = col + colDelta;
-            const neighborPosition = `${neighborRow},${neighborCol}`;
-            if(
-                isInBounds(grid, neighborRow, neighborCol) === true
-                && !visited.has(neighborPosition)
-                && grid[neighborRow][neighborCol] === 1
-            ) {
-                visited.add(neighborPosition);
-                size += 1;
-                stack.push([neighborRow, neighborCol]);
-            }
-        }
-    }
-    return size;
-}
-
-function bfs(grid: number[][], r:number, c: number, visited: Set<string>):number {
-    const position = `${r},${c}`;
-    if(visited.has(position)) {
+    // Base Case 2: visited
+    if (visited[row][col] === true) {
         return 0;
     }
 
-    visited.add(position);
-    const queue = [[r, c]];
-    let size = 1;
-    while(queue.length > 0) {
-        const [row, col] = queue.shift();
-        const deltas = [[-1, 0], [1, 0], [0, -1], [0,1]];
-        for(const delta of deltas) {
-            const [rowDelta, colDelta] = delta;
-            const neighborRow = row + rowDelta;
-            const neighborCol = col + colDelta;
-            const neighborPosition = `${neighborRow},${neighborCol}`;
-            if(
-                isInBounds(grid, neighborRow, neighborCol) === true
-                && !visited.has(neighborPosition)
-                && grid[neighborRow][neighborCol] === 1
-            ) {
-                visited.add(neighborPosition);
-                size += 1;
-                queue.push([neighborRow, neighborCol]);
-            }
-        }
-    }
-    return size;
-}
-
-function rDFS(grid: number[][], r:number, c:number, visited: Set<string>):number {
-    // Base Case: out of bounds
-    if(isInBounds(grid, r, c) === false) {
+    // Base Case 3: water
+    if (grid[row][col] === 0) {
         return 0;
     }
 
-    // Base Case: water
-    if(grid[r][c] === 0) {
-        return 0;
-    }
-
-    // Base Case: visited
-    const position = `${r},${c}`;
-    if(visited.has(position)) {
-        return 0;
-    }
-
-    visited.add(position);
+    visited[row][col] = true;
 
     let size = 1;
-    size += rDFS(grid, r - 1, c, visited);
-    size += rDFS(grid, r + 1, c, visited);
-    size += rDFS(grid, r, c - 1, visited);
-    size += rDFS(grid, r, c + 1, visited);
-    return size;
-}
+    const directions: [number, number][] = [
+        [-1, 0], // up
+        [0, 1], // right
+        [1, 0], // down
+        [0, -1], // left
+    ];
+    for (const [rowDelta, colDelta] of directions) {
+        size += matrixDFS(row + rowDelta, col + colDelta, grid, visited);
+    }
 
-function isInBounds(grid:number[][], r: number, c: number):boolean {
-    const rowInBounds: boolean = 0 <= r && r < grid.length;
-    const colInBounds: boolean = 0 <= c && c < grid[0].length;
-    return rowInBounds === true && colInBounds === true;
+    return size;
+
 }
