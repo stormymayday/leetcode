67192bd48b2fb51d6bbbc13eeac6192b501c9015
Aidar <stormymayday7@gmail.com>
2025-10-01 20:24:03 -0700

    Time: 46 ms (33.33%) | Memory: 67.2 MB (14.29%) - LeetSync

--- solutions/971-shortest-bridge/shortest-bridge.ts
+++ solutions/971-shortest-bridge/shortest-bridge.ts
@@ -1,95 +1,139 @@
 function shortestBridge(grid: number[][]): number {
-    // 1. Looking for the first island
-    const firstIsland = new Set<string>();
-    for(let r = 0; r < grid.length; r += 1) {
-        for(let c = 0; c < grid[0].length; c += 1) {
-            // Found a piece of land
-            if(grid[r][c] === 1) {
-                // 2. Explore the first island to capture it's position/coordinates
-                dfs(grid, r, c, firstIsland);
-                // 3. Run bfs from the first island towards the second island
-                return bfs(grid, firstIsland);
+
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
+
+    // matrix for marking position of the first island (needed for BFS) 
+    // Never changes after DFS, always identifies first island
+    const firstIslandPosition: boolean[][] = new Array(ROWS);
+    for (let row = 0; row < ROWS; row += 1) {
+        firstIslandPosition[row] = new Array(COLS).fill(false);
+    }
+
+    // matrix for marking visited cells in BFS (starts with firstIsland positions)
+    // Tracks BFS exploration, grows during BFS
+    const visited: boolean[][] = new Array(ROWS);
+    for (let row = 0; row < ROWS; row += 1) {
+        visited[row] = new Array(COLS).fill(false);
+    }
+
+    const firstIslandQueue: [number, number, number][] = []; // [row, col, distance]
+
+    // 1. Scan grid a piece of land (fisrt island)
+    outer: for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            // 1.1 When we find the first island, run dfs on it to mark it's coordinates in the 'firstIsland' matrix (queue as well?)
+            if (grid[row][col] === 1) {
+                matrixDFS(row, col, grid, firstIslandPosition, visited, firstIslandQueue);
+                break outer;
             }
         }
     }
-    
+
+    // 2. Run BFS from the first island towards the secon island
+    return matrixBFS(grid, firstIslandPosition, visited, firstIslandQueue);
+
 };
 
-function dfs(grid: number[][], r: number, c:number, visited) {
-    // Base Case: out of bounds
-    if(isInBounds(grid, r, c) === false) {
+function matrixDFS(
+    row: number,
+    col: number,
+    grid: number[][],
+    firstIslandPosition: boolean[][],
+    visited: boolean[][],
+    firstIslandQueue: [number, number, number][]
+): void {
+
+    // Base Case 1: out of bounds
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
         return;
     }
 
-    // Base Case: water
-    if(grid[r][c] === 0) {
+    // Base Case 2: water
+    if (grid[row][col] === 0) {
         return;
     }
 
-    // Base Case: visited
-    const position = `${r},${c}`;
-    if(visited.has(position)) {
+    // Base Case 3: visited
+    if (firstIslandPosition[row][col] === true) {
         return;
     }
 
-    // Save current position
-    visited.add(position);
+    // Capturing coordinates and filling up the queue
+    firstIslandPosition[row][col] = true;
+    firstIslandQueue.push([row, col, 0]);
+    visited[row][col] = true;
 
-    dfs(grid, r - 1, c, visited);
-    dfs(grid, r + 1, c, visited);
-    dfs(grid, r, c - 1, visited);
-    dfs(grid, r, c + 1, visited);
+    const directions: [number, number][] = [
+        [-1, 0], // up
+        [0, 1], // right
+        [1, 0], // down
+        [0, -1], // left
+    ];
+    for (const [rowDelta, colDelta] of directions) {
+        matrixDFS(row + rowDelta, col + colDelta, grid, firstIslandPosition, visited, firstIslandQueue);
+    }
 
-    // must return here!
     return;
+
 }
 
-function bfs(grid:number[][], firstIsland: Set<string>):number {
+function matrixBFS(
+    grid: number[][],
+    firstIslandPosition: boolean[][],
+    visited: boolean[][],
+    firstIslandQueue: [number, number, number][]
+): number {
 
-    // const visited = new Set();
+    const ROWS = grid.length;
+    const COLS = grid[0].length;
 
-    const queue = [];
-    for(const position of firstIsland) {
-        // unpacking the position string into numbers
-        const rowCol = position.split(",");
-        // enqueueing the position as integers and adding distance of 0
-        queue.push([parseInt(rowCol[0]), parseInt(rowCol[1]), 0]);
-    }
+    let queue: [number, number, number][] = firstIslandQueue;
 
-    while(queue.length > 0) {
+    while (queue.length > 0) {
 
-        const [row, col, distance] = queue.shift();
+        const nextQueue: [number, number, number][] = [];
+        for (let i = 0; i < queue.length; i += 1) {
 
-        const deltas = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-        for(const delta of deltas) {
-            const [rowDelta, colDelta] = delta;
-            const neighborRow = rowDelta + row;
-            const neighborCol = colDelta + col;
-            const neighborPosition = `${neighborRow},${neighborCol}`;
-            // Out of bounds or Visited
-            if(isInBounds(grid, neighborRow, neighborCol) === false || firstIsland.has(neighborPosition)) {
-                continue;
+            const [currRow, currCol, currDist] = queue[i];
+
+            if (grid[currRow][currCol] === 1 && firstIslandPosition[currRow][currCol] === false) {
+                return currDist - 1;
             }
 
-            // If it's a land then we found the second island
-            if(grid[neighborRow][neighborCol] === 1) {
-                return distance;
+            const directions: [number, number][] = [
+                [-1, 0], // up
+                [0, 1], // right
+                [1, 0], // down
+                [0, -1], // left
+            ];
+            for (const [rowDelta, colDelta] of directions) {
+
+                const neighborRow = currRow + rowDelta;
+                const neighborCol = currCol + colDelta;
+
+                if (
+                    // Out of bounds check
+                    0 <= neighborRow && neighborRow < ROWS &&
+                    0 <= neighborCol && neighborCol < COLS &&
+                    // visited check
+                    visited[neighborRow][neighborCol] === false
+                ) {
+                    visited[neighborRow][neighborCol] = true;
+                    nextQueue.push([neighborRow, neighborCol, currDist + 1]);
+                }
+
             }
-            
-            // Otherwise, mark as visited and enqueue the neighbor increasing the distance
-            firstIsland.add(neighborPosition);
-            queue.push([neighborRow, neighborCol, distance + 1]);
+
+        }
+        if (nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
         }
 
     }
 
-    // Optinal: there should be a second island
     return -1;
 
-}
-
-function isInBounds(grid: number[][], r: number, c: number):boolean {
-    const rowInBounds = 0 <= r && r < grid.length;
-    const colInBounds = 0 <= c && c < grid[0].length;
-    return rowInBounds === true && colInBounds === true;
 }
