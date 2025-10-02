cd6b39da1aa0b764309489e57f1fc28282c9be58
Aidar <stormymayday7@gmail.com>
2025-10-02 14:54:13 -0700

    Time: 34 ms (35.71%) | Memory: 64.9 MB (85.71%) - LeetSync

--- solutions/922-possible-bipartition/possible-bipartition.ts
+++ solutions/922-possible-bipartition/possible-bipartition.ts
@@ -1,44 +1,77 @@
 function possibleBipartition(n: number, dislikes: number[][]): boolean {
+
     const adjList = buildAdjList(n, dislikes);
-    const state = new Map<number, boolean>();
-    for(const node of adjList.keys()) {
-        if(bfs(adjList, node, state) === false) {
-            return false;
+    const colorMap = new Map<number, boolean>();
+
+    // Check each node (graph may have disconnected components)
+    for (const node of adjList.keys()) {
+        // Skip already colored nodes
+        if (!colorMap.has(node)) { // this check is important to avoid false negatives
+            if (bfs(node, adjList, colorMap, true) === false) {
+                // if any of the calls return false, exit early
+                return false;
+            }
         }
     }
+
+    // Otherwise, graph must be bipartite
     return true;
+
 };
 
-function bfs(graph: Map<number, Set<number>>, src: number, state: Map<number, boolean>): boolean {
-    if(!state.has(src)) {
-        state.set(src, true);
+function bfs(src: number, adjList: Map<number, number[]>, colorMap: Map<number, boolean>, nodeColor: boolean): boolean {
+
+    // Extra check. However, bfs should not be called if a 'node' is in 'colorMap'
+    if (colorMap.has(src) && colorMap.get(src) !== nodeColor) {
+        return false;
     }
-    const queue: [number, boolean][] = [[src, state.get(src)]];
-    while(queue.length > 0) {
-        const [node, stateVal] = queue.shift();
-        for(const neighbor of graph.get(node)) {
-            if(!state.has(neighbor)) {
-                state.set(neighbor, !stateVal);
-                queue.push([neighbor, !stateVal]);
-            } else {
-                if(state.get(neighbor) === stateVal) {
-                    return false;
+
+    colorMap.set(src, nodeColor);
+
+    let queue: [number, boolean][] = [];
+    queue.push([src, nodeColor]);
+
+    while (queue.length > 0) {
+
+        const nextQueue: [number, boolean][] = [];
+        for (let i = 0; i < queue.length; i += 1) {
+
+            const [currNode, currColor] = queue[i];
+
+            for (const neighbor of adjList.get(currNode)) {
+                
+                // try 'coloring' the neighbor
+                if (!colorMap.has(neighbor)) {
+                    colorMap.set(neighbor, !currColor);
+                    nextQueue.push([neighbor, !currColor]);
+                } else {
+                    // Neighbor has been visited, check the color
+                    if(colorMap.get(neighbor) === currColor) {
+                        // exit early if parent has the same color
+                        return false;
+                    }
                 }
+
             }
+
+        }
+        if (nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
         }
     }
     return true;
 }
 
-function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
+function buildAdjList(n: number, edges: number[][]): Map<number, number[]> {
     const adjList = new Map();
     for(let i = 1; i <= n; i += 1) {
-        adjList.set(i, new Set());
+        adjList.set(i, []);
     }
-    for(const edge of edges) {
-        const [src, dst] = edge;
-        adjList.get(src).add(dst);
-        adjList.get(dst).add(src);
+    for(const [src, dst] of edges) {
+        adjList.get(src).push(dst);
+        adjList.get(dst).push(src);
     }
     return adjList;
 }
