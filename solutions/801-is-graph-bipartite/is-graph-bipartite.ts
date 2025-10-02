72e73a182bc9da3508d5c9ff4e43af2e54b57161
Aidar <stormymayday7@gmail.com>
2025-10-02 14:28:31 -0700

    Time: 22 ms (9.26%) | Memory: 64 MB (9.26%) - LeetSync

--- solutions/801-is-graph-bipartite/is-graph-bipartite.ts
+++ solutions/801-is-graph-bipartite/is-graph-bipartite.ts
@@ -6,7 +6,7 @@ function isBipartite(graph: number[][]): boolean {
     for(let i = 0; i < graph.length; i += 1) {
         // Skip already colored nodes
         if(!colorMap.has(i)) { // this check is important to avoid false negatives
-            if(dfs(i, graph, colorMap, true) === false) {
+            if(bfs(i, graph, colorMap, true) === false) {
                 // if any of the calls return false, exit early
                 return false;
             }
@@ -16,6 +16,42 @@ function isBipartite(graph: number[][]): boolean {
     // Otherwise, graph must be bipartite
     return true;   
 };
+
+function bfs(src: number, graph: number[][], colorMap: Map<number, boolean>, nodeColor: boolean): boolean {
+
+    let queue: [number, boolean][] = [];
+    queue.push([src, nodeColor]);
+
+    while(queue.length > 0) {
+
+        const nextQueue: [number, boolean][] = [];
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const [currNode, currColor] = queue[i];
+
+            if(colorMap.has(currNode) && colorMap.get(currNode) !== currColor) {
+                return false;
+            }
+
+            colorMap.set(currNode, currColor);
+
+            for(const neighbor of graph[currNode]) {
+
+                if(!colorMap.has(neighbor)) {
+                    nextQueue.push([neighbor, !currColor]);
+                }
+
+            }
+
+        }
+        if(nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
+        }
+    }
+    return true;
+}
 
 function dfs(src: number, graph: number[][], colorMap: Map<number, boolean>, currColor: boolean): boolean {
     // Base Case: if node has been colored
