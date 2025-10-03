e2175e4826889e66262208fbdaa05725e01d740f
Aidar <stormymayday7@gmail.com>
2025-10-03 16:09:30 -0700

    Time: 396 ms (44.55%) | Memory: 60.6 MB (48.42%) - LeetSync

--- solutions/79-word-search/word-search.ts
+++ solutions/79-word-search/word-search.ts
@@ -34,27 +34,27 @@ function backtrackDFS(
 ): boolean {
 
     // Base Case 1: Out of bounds
-    if(row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
+    if (row < 0 || row >= grid.length || col < 0 || col >= grid[0].length) {
         return false;
     }
 
     // Base Case 2: Visited
-    if(visited[row][col] === true) {
+    if (visited[row][col] === true) {
         return false;
     }
 
     // Base Case 3: Wrong character
-    if(grid[row][col] !== word[idx]) {
+    if (grid[row][col] !== word[idx]) {
         return false;
+    }
+
+    // Base Case 4: Word found (search complete)
+    if (idx === word.length - 1) {
+        return true;
     }
 
     // Otherwise, this is a new positon
     visited[row][col] = true; // mark as 'visited'
-
-    // Base Case 4: Word found (search complete)
-    if(idx === word.length - 1) {
-        return true;
-    }
 
     const directions: [number, number][] = [
         [-1, 0], // up
@@ -62,8 +62,8 @@ function backtrackDFS(
         [1, 0], // down
         [0, -1], // left
     ];
-    for(const [rowDelta, colDelta] of directions) {
-        if(backtrackDFS(row + rowDelta, col + colDelta, grid, word, idx + 1, visited) === true) {
+    for (const [rowDelta, colDelta] of directions) {
+        if (backtrackDFS(row + rowDelta, col + colDelta, grid, word, idx + 1, visited) === true) {
             return true;
         }
     }
