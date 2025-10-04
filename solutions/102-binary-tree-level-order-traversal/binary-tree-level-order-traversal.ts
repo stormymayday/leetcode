dca10902a60e2f57ba7178bb885fa5a64fd2d1b9
Aidar <stormymayday7@gmail.com>
2025-10-04 10:48:45 -0700

    Time: 0 ms (100.00%) | Memory: 60.3 MB (52.05%) - LeetSync

--- solutions/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.ts
+++ solutions/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.ts
@@ -13,20 +13,51 @@
  */
 
 function levelOrder(root: TreeNode | null): number[][] {
-    const result = [];
-    function dfs(root, level) {
-        if(root === null) {
-            return;
-        }
-
-        if(result.length === level) {
-            result[level] = [];
-        }
-        result[level].push(root.val);
-
-        dfs(root.left, level + 1);
-        dfs(root.right, level + 1);
-    }
-    dfs(root, 0);
-    return result;
+    const res: number[][] = [];
+    bfs(root, res);
+    return res;
 };
+
+function bfs(root: TreeNode | null, res: number[][]): void {
+
+    if(root === null) {
+        return;
+    }
+
+    let queue: TreeNode[] = [];
+    queue.push(root);
+
+    while(queue.length > 0) {
+
+        const nextQueue: TreeNode[] = [];
+        const currLevelNodes: number[] = [];
+
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const currNode = queue[i];
+
+            currLevelNodes.push(currNode.val);
+
+            if(currNode.left !== null) {
+                nextQueue.push(currNode.left);
+            }
+
+            if(currNode.right !== null) {
+                nextQueue.push(currNode.right);
+            }
+
+        }
+
+        res.push(currLevelNodes);
+
+        if(nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
+        }
+
+    }
+
+    return;
+ 
+}
