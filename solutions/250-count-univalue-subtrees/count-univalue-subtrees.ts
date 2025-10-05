43e683e0ebdfe054206007c58e8ded491d440c1a
Aidar <stormymayday7@gmail.com>
2025-10-04 23:19:05 -0700

    Time: 1 ms (36.36%) | Memory: 57.7 MB (100.00%) - LeetSync

---
+++ solutions/250-count-univalue-subtrees/count-univalue-subtrees.ts
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function countUnivalSubtrees(root: TreeNode | null): number {
+    let res = 0;
+    function dfs(root: TreeNode | null): boolean {
+        // Base Case: null node
+        if (root === null) {
+            return true;
+        }
+        
+        // Base Case 2: Leaf Node
+        if(root.left === null && root.right === null) {
+            res += 1;
+            return true;
+        }
+
+        const left = dfs(root.left);
+        const right = dfs(root.right);
+
+        if(left === true && right === true) {
+
+            if(root.left !== null && root.val !== root.left.val) {
+                return false;
+            }
+
+            if(root.right !== null && root.val !== root.right.val) {
+                return false;
+            }
+
+            res += 1;
+            return true;
+
+        } else {
+            return false;
+        }
+
+        
+    }
+    
+    dfs(root);
+
+    return res;
+};
