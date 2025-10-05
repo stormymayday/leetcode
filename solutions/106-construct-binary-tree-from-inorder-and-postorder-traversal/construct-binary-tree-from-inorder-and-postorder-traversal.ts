6184f9e992cbe43b17598de07c50cfb7ff0b3bed
Aidar <stormymayday7@gmail.com>
2025-10-05 16:35:56 -0700

    Time: 66 ms (23.35%) | Memory: 132 MB (21.56%) - LeetSync

--- solutions/106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.ts
+++ solutions/106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.ts
@@ -21,25 +21,33 @@ function dfs(inorder: number[], postorder: number[]): TreeNode | null {
 
     // Base Case: no more elements in the 'inorder'
     // - Specifically checking 'inorder' length because it will be sliced into two halves
-    if(inorder.length === 0) {
+    if (inorder.length === 0) {
         return null;
     }
 
     // Create root using last element in the 'postorder'
-    const rootVal = postorder.pop();
+    const rootVal = postorder[postorder.length - 1];
     const root = new TreeNode(rootVal);
     // Find index of the 'rootVal' in the 'inorder'
     const rootIdx = inorder.indexOf(rootVal); // O(n)
 
-    // Recurse Right:
-    // - Creating a right subtree
-    // - Everything to the right of 'rootIdx' is the right subtree
-    root.right = dfs(inorder.slice(rootIdx + 1), postorder);
-
     // Recurse Left:
     // - Creating a left subtree
-    // - Everything to the left of 'rootIdx' is the left subtree
-    root.left = dfs(inorder.slice(0, rootIdx), postorder);
+    root.left = dfs(
+        // Everything to the left of rootIndex in inorder is the left subtree
+        inorder.slice(0, rootIdx),
+        // In postorder, left subtree has same length as in inorder
+        postorder.slice(0, rootIdx)
+    );
+
+    // Recurse Right:
+    // - Creating a right subtree
+    root.right = dfs(
+        // Everything to the right of rootIndex in inorder is the right subtree
+        inorder.slice(rootIdx + 1),
+        // Starting from 'rootIdx' up until but not including the last element
+        postorder.slice(rootIdx, -1)
+    );
 
     // Return root
     return root;
