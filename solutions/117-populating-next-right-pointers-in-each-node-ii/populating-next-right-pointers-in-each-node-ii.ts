62d92b431d8a29a1a8c3147a3bbed5b7398693ca
Aidar <stormymayday7@gmail.com>
2025-10-05 23:34:45 -0700

    Time: 68 ms (22.30%) | Memory: 61.2 MB (32.43%) - LeetSync

---
+++ solutions/117-populating-next-right-pointers-in-each-node-ii/populating-next-right-pointers-in-each-node-ii.ts
@@ -0,0 +1,57 @@
+/**
+ * Definition for _Node.
+ * class _Node {
+ *     val: number
+ *     left: _Node | null
+ *     right: _Node | null
+ *     next: _Node | null
+ *     constructor(val?: number, left?: _Node, right?: _Node, next?: _Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function connect(root: _Node | null): _Node | null {
+    if(root === null) {
+        return null;
+    }
+    bfs(root);
+    return root;
+};
+
+function bfs(root: _Node): void {
+
+    let queue: _Node[] = [];
+    queue.push(root);
+
+    while(queue.length > 0) {
+
+        const nextQueue: _Node[] = [];
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const currNode = queue[i];
+
+            if(queue[i + 1] !== undefined) {
+                const neighborNode = queue[i + 1];
+                currNode.next = neighborNode;
+            }
+
+            if(currNode.left !== null) {
+                nextQueue.push(currNode.left);
+            }
+
+            if(currNode.right !== null) {
+                nextQueue.push(currNode.right);
+            }
+
+        }
+        if(nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
+        }
+    }
+}
