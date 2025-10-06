e159d6bcbd1374bacde29019d6141712ccd7a844
Aidar <stormymayday7@gmail.com>
2025-10-05 20:49:39 -0700

    Time: 2 ms (93.94%) | Memory: 60 MB (83.59%) - LeetSync

--- solutions/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
+++ solutions/105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -13,41 +13,59 @@
  */
 
 function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
-    return dfs(inorder, preorder);
-};
 
-function dfs(inorder: number[], preorder: number[]): TreeNode | null {
-
-    // Base Case
-    if(inorder.length === 0) {
-        return null;
+    const inorderValToIdx = new Map<number, number>();
+    for (let idx = 0; idx < inorder.length; idx += 1) {
+        const val = inorder[idx];
+        inorderValToIdx.set(val, idx);
     }
 
-    // Create root using first element of the 'preorder'
-    const rootVal = preorder[0];
-    const root = new TreeNode(rootVal);
-    // Get index of the 'rootVal' in 'inorder'
-    const rootIdx = inorder.indexOf(rootVal);
-    // Calculate size of the left subtree
-    // const leftSubtreeSize = rootIdx; 
+    function dfs(
+        inorderLeftIdx: number,
+        inorderRightIdx: number,
+        preorderLeftIdx: number,
+        preorderRightIdx: number
+    ): TreeNode | null {
 
-    // Construct Left Subtree
-    root.left = dfs(
-        // everything to the left of 'rootIdx'
-        inorder.slice(0, rootIdx),
-        // skipping index 1 (root) + size of the left subtree
-        preorder.slice(1, rootIdx + 1)
+        // Base Case: left and right 'inorder' pointers cross
+        if (inorderLeftIdx > inorderRightIdx) {
+            return null;
+        }
+
+        // Create a root
+        const rootVal = preorder[preorderLeftIdx];
+        const root = new TreeNode(rootVal);
+
+        // Get the 'rootVal' index
+        const rootIdx = inorderValToIdx.get(rootVal);
+
+        // Calculate left subtree size
+        const leftSubtreeSize = rootIdx - inorderLeftIdx;
+
+        // Construct Left Subtree
+        root.left = dfs(
+            inorderLeftIdx, // 'inorderLeftIdx' stays the same (start of the array)
+            rootIdx - 1, // 'inorderRightIdx' goes 1 spot behind 'rootIdx'
+            preorderLeftIdx + 1, // 'preorderLeftIdx' shifts 1 spot forward (skipping 'root')
+            preorderLeftIdx + leftSubtreeSize // 'preorderRightIdx' goes to the 'rootIdx'
+        );
+
+        // Construct Right Subtree
+        root.right = dfs(
+            rootIdx + 1, // 'inorderLeftIdx' goes 1 spot after 'rootIdx'
+            inorderRightIdx, // 'inorderRightIdx' stays the same (end of the array)
+            preorderLeftIdx + leftSubtreeSize + 1, // 'preorderLeftIdx' goes 1 spot after 'rootIdx'
+            preorderRightIdx // 'preorderRightIdx' stays the same (end if the array)
+        );
+
+        return root;
+    }
+
+    return dfs(
+        0,
+        inorder.length - 1,
+        0,
+        preorder.length - 1
     );
 
-    // Construct Right Subtree
-    root.right = dfs(
-        // everything to the right of 'rootIdx'
-        inorder.slice(rootIdx + 1),
-        //
-        preorder.slice(rootIdx + 1)
-
-    );
-
-    return root;
-
-}
+};
