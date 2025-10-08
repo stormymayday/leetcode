5ff9d1c690ae21556d90b730c85e1e32188ce617
Aidar <stormymayday7@gmail.com>
2025-10-08 09:48:15 -0700

    Time: 0 ms (100.00%) | Memory: 58.5 MB (33.90%) - LeetSync

--- solutions/101-symmetric-tree/symmetric-tree.ts
+++ solutions/101-symmetric-tree/symmetric-tree.ts
@@ -18,49 +18,30 @@ function isSymmetric(root: TreeNode | null): boolean {
         return true;
     }
 
-    let queue: (TreeNode | null)[] = [];
-    queue.push(root.left, root.right);
+    function dfs(left: TreeNode | null, right: TreeNode | null): boolean {
 
-    while (queue.length > 0) {
-
-        // linear scan current level using two pointers
-        let left = 0;
-        let right = queue.length - 1;
-        while (left < right) {
-            // both nodes are 'null' - is fine
-            if (queue[left] === null && queue[right] === null) {
-                left += 1;
-                right -= 1;
-                continue;
-            }
-            // only one nodes is 'null' - not symmetric
-            if (
-                (queue[left] === null && queue[right] !== null) ||
-                (queue[left] !== null && queue[right] === null)
-            ) {
-                return false;
-            }
-            // Both are not null, compare values
-            if (queue[left].val !== queue[right].val) {
-                return false;
-            }
-            left += 1;
-            right -= 1;
+        if (left === null && right === null) {
+            return true;
         }
 
-        const nextQueue: (TreeNode | null)[] = [];
-        for (let i = 0; i < queue.length; i += 1) {
-            const curr = queue[i];
-            if (curr !== null) {
-                nextQueue.push(curr.left);
-                nextQueue.push(curr.right);
-            }
+        if (
+            (left === null && right !== null) ||
+            (left !== null && right === null)
+        ) {
+            return false;
         }
 
-        queue = nextQueue;
+        if(left.val !== right.val) {
+            return false;
+        }
+
+        const outer = dfs(left.left, right.right);
+        const inner = dfs(left.right, right.left);
+
+        return outer && inner;
 
     }
 
-    return true;
+    return dfs(root.left, root.right);
 
 };
