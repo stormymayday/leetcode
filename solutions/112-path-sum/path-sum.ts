624f313eedf4b87aaba794bc5572cbbaccf153fe
Aidar <stormymayday7@gmail.com>
2025-10-07 19:31:35 -0700

    Time: 6 ms (5.06%) | Memory: 61.2 MB (7.95%) - LeetSync

--- solutions/112-path-sum/path-sum.ts
+++ solutions/112-path-sum/path-sum.ts
@@ -18,12 +18,35 @@ function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
         return false;
     }
 
-    targetSum -= root.val;
+    let queue: [TreeNode, number][] = [[root, root.val]];
 
-    if(root.left === null && root.right === null) {
-        return targetSum === 0;
+    while(queue.length > 0) {
+
+        const nextQueue: [TreeNode, number][] = [];
+        for(let i = 0; i < queue.length; i += 1) {
+
+            const [currNode, currSum] = queue[i];
+
+            // If current node is a leaf node
+            if(currNode.left === null && currNode.right === null) {
+                if(currSum === targetSum) {
+                    return true;
+                }
+            }
+
+            if(currNode.left !== null) {
+                nextQueue.push([currNode.left, currSum + currNode.left.val]);
+            }
+
+            if(currNode.right !== null) {
+                nextQueue.push([currNode.right, currSum + currNode.right.val]);
+            }
+
+        }
+        queue = nextQueue;
+
     }
 
-    return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
+    return false;
 
 };
