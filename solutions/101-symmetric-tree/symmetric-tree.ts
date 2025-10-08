9bca5196312f91c695f4c110308806387c6fc316
Aidar <stormymayday7@gmail.com>
2025-10-07 17:13:33 -0700

    Time: 0 ms (100.00%) | Memory: 58.5 MB (33.90%) - LeetSync

--- solutions/101-symmetric-tree/symmetric-tree.ts
+++ solutions/101-symmetric-tree/symmetric-tree.ts
@@ -13,35 +13,58 @@
  */
 
 function isSymmetric(root: TreeNode | null): boolean {
-    
-    if(root === null) {
+
+    if (root === null) {
         return true;
     }
 
-    function dfs(left: TreeNode | null, right: TreeNode | null): boolean {
-        // Both are null
-        if(left === null && right === null) {
-            return true;
+    let queue: (TreeNode | null)[] = [];
+    queue.push(root.left, root.right);
+
+    while (queue.length > 0) {
+
+        // linear scan current level using two pointers
+        let left = 0;
+        let right = queue.length - 1;
+        while (left < right) {
+            // both nodes are 'null' - is fine
+            if (queue[left] === null && queue[right] === null) {
+                left += 1;
+                right -= 1;
+                continue;
+            }
+            // only one nodes is 'null' - not symmetric
+            if (
+                (queue[left] === null && queue[right] !== null) ||
+                (queue[left] !== null && queue[right] === null)
+            ) {
+                return false;
+            }
+            // Both are not null, compare values
+            if (queue[left].val !== queue[right].val) {
+                return false;
+            }
+            left += 1;
+            right -= 1;
         }
 
-        // Only one is null
-        if((left !== null && right === null) || (left === null && right !== null)) {
-            return false;
+        const nextQueue: (TreeNode | null)[] = [];
+        for (let i = 0; i < queue.length; i += 1) {
+            const curr = queue[i];
+            if (curr !== null) {
+                nextQueue.push(curr.left);
+                nextQueue.push(curr.right);
+            }
         }
-        
-        // Means both are not null, check values
-        if(left.val !== right.val) {
-            return false;
+
+        if (nextQueue.length > 0) {
+            queue = nextQueue;
+        } else {
+            break;
         }
-
-
-        const outer = dfs(left.left, right.right);
-        const inner = dfs(left.right, right.left);
-
-        return outer && inner;
 
     }
 
-    return dfs(root.left, root.right);
+    return true;
 
 };
