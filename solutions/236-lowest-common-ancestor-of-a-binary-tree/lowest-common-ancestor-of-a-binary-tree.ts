8a077dc2213ae8372428302283d64521cdf1179e
Aidar <stormymayday7@gmail.com>
2025-10-08 22:25:42 -0700

    Time: 56 ms (68.14%) | Memory: 65.9 MB (37.38%) - LeetSync

--- solutions/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
+++ solutions/236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.ts
@@ -14,38 +14,21 @@
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
 
-    // Base Case 1: null node
     if(root === null) {
         return null;
     }
 
-    // Base Case 2: root is 'p' or 'q'
     if(root === p || root === q) {
         return root;
     }
 
-    // Can combine
-    // if(root === null || root === p || root === q) {
-    //     return root;
-    // }
-
-    // Recurse Left
     const left = lowestCommonAncestor(root.left, p, q);
-
-    // Recurse Right
     const right = lowestCommonAncestor(root.right, p, q);
 
-    // If both left and right are not null, then current node (root) is the LCA
     if(left !== null && right !== null) {
         return root;
     }
 
-    // If both left and right are null, return null, LCA is not on this subtree
-    if(left === null && right === null) {
-        return null;
-    }
-
-    // Otherwise, LCA is either 'left' or 'right'. Whichever one is not null
     return left !== null ? left : right;
-	
+
 };
