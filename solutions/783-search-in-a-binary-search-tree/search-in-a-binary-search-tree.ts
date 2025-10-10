b94b9cbcb13a8d1dbf83145fba3707cd77da79da
Aidar <stormymayday7@gmail.com>
2025-10-09 19:22:47 -0700

    Time: 0 ms (100.00%) | Memory: 63.1 MB (27.08%) - LeetSync

--- solutions/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.ts
+++ solutions/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.ts
@@ -13,15 +13,37 @@
  */
 
 function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    
     if(root === null) {
-        return null;
-    }
-
-    if(root.val < val) {
-        return searchBST(root.right, val);
-    } else if(root.val > val) {
-        return searchBST(root.left, val);
-    } else {
         return root;
     }
+
+    const stack: TreeNode[] = [root];
+
+    while(stack.length > 0) {
+
+        const curr = stack.pop();
+
+        if(curr.val === val) {
+            return curr;
+        }
+
+        // Smaller - go left
+        if(val < curr.val) {
+            if(curr.left !== null) {
+                stack.push(curr.left);
+            }
+        }
+
+        // Larger - go right
+        if(val > curr.val) {
+            if(curr.right !== null) {
+                stack.push(curr.right);
+            }
+        }
+
+    }
+
+    return null;
+
 };
