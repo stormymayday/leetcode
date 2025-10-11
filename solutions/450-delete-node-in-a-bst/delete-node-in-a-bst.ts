4939b875fe3c106b9db2cc34668a0d0ae63da75f
Aidar <stormymayday7@gmail.com>
2025-10-10 18:25:38 -0700

    Time: 0 ms (100.00%) | Memory: 66.8 MB (12.92%) - LeetSync

--- solutions/450-delete-node-in-a-bst/delete-node-in-a-bst.ts
+++ solutions/450-delete-node-in-a-bst/delete-node-in-a-bst.ts
@@ -11,36 +11,72 @@
  *     }
  * }
  */
+
 function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
 
-    if (root === null) {
+    function helper(node: TreeNode | null, val: number): TreeNode | null {
+        // Base Case: node is null
+        if(node === null) {
+            return null;
+        }
+
+        // target could be in the left subtree
+        if(val < node.val) {
+            node.left = helper(node.left, val);
+        } 
+        // target could be in the right subtree
+        else if(val > node.val) {
+            node.right = helper(node.right, val);
+        } 
+        // current node is the target
+        else {
+            // node has no children
+            // Note: technically not a necessary check since the next check will assign 'null' from the right subtree
+            if(node.left === null && node.right === null) {
+                node = null;
+            } 
+            // node has no left subtree
+            else if(node.left === null) {
+                // replace node with it's right subtree
+                node = node.right;
+            } 
+            // node has no right subtree
+            else if(node.right === null) {
+                // replace node with it's left subtree
+                node = node.left;
+            }
+            // otherwise, node must have both children
+            else {
+                // 1. Get 'successor' of the current node
+                const successor = getSuccessor(node);
+
+                // 2. Overwrite node's val with successor's val
+                node.val = successor.val;
+
+                // 3. Recursively delete the successor using it's value
+                // Node: successor is in the right subtree
+                node.right = helper(node.right, successor.val);
+            }
+        }
+
+        return node;
+
+    }
+
+    return helper(root, key);
+    
+};
+
+function getSuccessor(node: TreeNode | null): TreeNode | null {
+
+    if(node === null || node.right === null) {
         return null;
     }
 
-    if (key < root.val) {
-        root.left = deleteNode(root.left, key);
-    } else if (key > root.val) {
-        root.right = deleteNode( root.right, key);
-    } else {
-        if (root.left === null && root.right === null) {
-            return null;
-        } else if (root.left === null) {
-            root = root.right;
-        } else if (root.right === null) {
-            root = root.left;
-        } else {
-            const subTreeMin = minval(root.right);
-            root.val = subTreeMin;
-            root.right = deleteNode(root.right, subTreeMin);
-        }
+    let curr = node.right;
+    while(curr.left !== null) {
+        curr = curr.left;
     }
+    return curr;
 
-    return root;
-};
-
-function minval(currentNode) {
-    while (currentNode.left !== null) {
-        currentNode = currentNode.left;
-    }
-    return currentNode.val;
 }
