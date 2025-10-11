9162e9689b1ee33d0444881bc03cead2e22ba8bb
Aidar <stormymayday7@gmail.com>
2025-10-11 15:39:58 -0700

    Time: 56 ms (86.08%) | Memory: 62.3 MB (26.58%) - LeetSync

--- solutions/775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.ts
+++ solutions/775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.ts
@@ -15,9 +15,10 @@
 function preorder(root: _Node | null): number[] {
 
     const res: number[] = [];
-    if(root === null) {
-        return res;
-    }
+    
+    // if(root === null) {
+    //     return res;
+    // }
 
     function dfs(root: _Node | null): void {
         if(root === null) {
