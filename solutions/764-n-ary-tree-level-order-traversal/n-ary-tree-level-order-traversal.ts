06c81a7b38bf55c843dee9538cbfd9de6fc1adc1
Aidar <stormymayday7@gmail.com>
2025-10-11 19:34:29 -0700

    Time: 59 ms (83.33%) | Memory: 62.6 MB (12.50%) - LeetSync

--- solutions/764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.ts
+++ solutions/764-n-ary-tree-level-order-traversal/n-ary-tree-level-order-traversal.ts
@@ -13,37 +13,39 @@
 
 
 function levelOrder(root: _Node | null): number[][] {
-	// Edge Case: null root
-    if(root === null) {
-        return [];
-    }
-
-    let queue: _Node[] = [];
-    queue.push(root);
 
     const res: number[][] = [];
+
+    if(root === null) {
+        return res;
+    }
+
+    let queue: _Node[] = [root];
+
     while(queue.length > 0) {
 
         const nextQueue: _Node[] = [];
-        const currLevelRes: number[] = [];
+        const currLayer: number[] = [];
+
         for(let i = 0; i < queue.length; i += 1) {
 
             const currNode = queue[i];
-            currLevelRes.push(currNode.val);
+
+            currLayer.push(currNode.val);
 
             for(const child of currNode.children) {
+
                 nextQueue.push(child);
+                
             }
 
         }
-
-        res.push(currLevelRes);
-        if(nextQueue.length > 0) {
-            queue = nextQueue;
-        } else {
-            break;
-        }
+        
+        res.push(currLayer);
+        queue = nextQueue;
 
     }
+
     return res;
+	
 };
