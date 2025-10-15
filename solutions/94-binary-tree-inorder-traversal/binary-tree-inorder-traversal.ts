cf35764f83aa44c6304b3175de439b9eb21f1c08
Aidar <stormymayday7@gmail.com>
2025-10-15 10:20:33 -0700

    Time: 0 ms (100.00%) | Memory: 55.5 MB (52.86%) - LeetSync

--- solutions/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
+++ solutions/94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -24,25 +24,35 @@ function inorderTraversal(root: TreeNode | null): number[] {
     
     while(curr !== null) {
 
+        // check if there is NO left child
         if(curr.left === null) {
+            // visit
             res.push(curr.val);
+            // go right
             curr = curr.right;
-        } else {
-
+        } 
+        // otherwise, there IS a left child
+        else {
+            // find inorder predecessor (the right-most node) in the left subtree
             let predecessor: TreeNode | null = curr.left;
             while(predecessor.right !== null && predecessor.right !== curr) {
                 predecessor = predecessor.right;
             }
 
+            // check if there is no pseudo-cycle
             if(predecessor.right === null) {
-
+                // create a pseudo-link back to curr
                 predecessor.right = curr;
+                // go left
                 curr = curr.left;
-
-            } else {
-
+            } 
+            // otherwise, there is a pseudo-cycle
+            else {
+                // remove the pseudo-link
                 predecessor.right = null;
+                // visit
                 res.push(curr.val);
+                // go right
                 curr = curr.right;
 
             }
