d3fcfe7e2dc243c61e46aa9b933ec4145167a0e3
Aidar <stormymayday7@gmail.com>
2025-10-15 10:57:45 -0700

    Time: 1 ms (4.32%) | Memory: 55.8 MB (27.30%) - LeetSync

--- solutions/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.ts
+++ solutions/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.ts
@@ -16,22 +16,30 @@ function postorderTraversal(root: TreeNode | null): number[] {
 
     const res: number[] = [];
 
-    if(root === null) {
+    if (root === null) {
         return res;
     }
 
-    const stack: TreeNode[] = [root];
+    const stack: [TreeNode, boolean][] = [[root, false]];
 
-    while(stack.length > 0) {
-        const currNode = stack.pop();
-        res.push(currNode.val);
-        if(currNode.left) {
-            stack.push(currNode.left);
-        }
-        if(currNode.right) {
-            stack.push(currNode.right);
+    while (stack.length > 0) {
+
+        const [currNode, visited] = stack.pop();
+
+        if (visited === true) {
+            res.push(currNode.val);
+        } else {
+
+            stack.push([currNode, true]);
+
+            if (currNode.right) {
+                stack.push([currNode.right, false]);
+            }
+            if (currNode.left) {
+                stack.push([currNode.left, false]);
+            }
         }
     }
 
-    return res.reverse();
+    return res;
 };
