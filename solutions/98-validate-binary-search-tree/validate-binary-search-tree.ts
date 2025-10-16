70af87e74bec0a5820a581476a079fd40724b5d7
Aidar <stormymayday7@gmail.com>
2025-10-15 21:40:05 -0700

    Time: 1 ms (62.75%) | Memory: 60.5 MB (26.30%) - LeetSync

--- solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
+++ solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
@@ -18,22 +18,50 @@ function isValidBST(root: TreeNode | null): boolean {
         return true;
     }
 
-    const stack: TreeNode[] = [];
     let curr: TreeNode | null = root;
     let prev: TreeNode | null = null;
-    while(curr !== null || stack.length > 0) {
 
-        while(curr !== null) {
-            stack.push(curr);
-            curr = curr.left;
-        }
+    while(curr !== null) {
+        if(curr.left === null) {
+            
+            // BST validation check
+            if(prev !== null && prev.val >= curr.val) {
+                return false;
+            }
 
-        curr = stack.pop();
-        if(prev !== null && prev.val >= curr.val) {
-            return false;
+            prev = curr;
+            curr = curr.right;
+
+
+        } else {
+
+            let predecessor = curr.left;
+            while(predecessor.right !== null && predecessor.right !== curr) {
+                predecessor = predecessor.right;
+            }
+
+            // There is no pseduo-link to curr (yet)
+            if(predecessor.right === null) {
+                // create a pseudo linl and go left
+                predecessor.right = curr;
+                curr = curr.left;
+            } 
+            // There is a pseudo-link to curr
+            else {
+                // remove the link
+                predecessor.right = null;
+
+                // BST Validation Check
+                if(prev !== null && prev.val >= curr.val) {
+                    return false;
+                }
+
+                // go right
+                prev = curr;
+                curr = curr.right;
+            }
+
         }
-        prev = curr;
-        curr = curr.right;
     }
 
     return true;
