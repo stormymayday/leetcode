98ace8aed0f75052826c856a246512ecd674e69d
Aidar <stormymayday7@gmail.com>
2025-10-16 14:37:36 -0700

    Time: 1 ms (63.04%) | Memory: 60.3 MB (32.66%) - LeetSync

--- solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
+++ solutions/98-validate-binary-search-tree/validate-binary-search-tree.ts
@@ -14,56 +14,29 @@
 
 function isValidBST(root: TreeNode | null): boolean {
 
-    if (root === null || (root.left === null && root.right === null)) {
+    if(root === null) {
         return true;
     }
 
-    let curr: TreeNode | null = root;
-    let prev: TreeNode | null = null;
+    const inorder: number[] = [];
 
-    while(curr !== null) {
-        if(curr.left === null) {
-            
-            // BST validation check
-            if(prev !== null && prev.val >= curr.val) {
-                return false;
-            }
+    function helperDFS(node: TreeNode | null): void {
+        if(node === null) {
+            return;
+        }
+        helperDFS(node.left);
+        inorder.push(node.val);
+        helperDFS(node.right);
+    }
 
-            prev = curr;
-            curr = curr.right;
+    helperDFS(root);
 
-
-        } else {
-
-            let predecessor = curr.left;
-            while(predecessor.right !== null && predecessor.right !== curr) {
-                predecessor = predecessor.right;
-            }
-
-            // There is no pseduo-link to curr (yet)
-            if(predecessor.right === null) {
-                // create a pseudo linl and go left
-                predecessor.right = curr;
-                curr = curr.left;
-            } 
-            // There is a pseudo-link to curr
-            else {
-                // remove the link
-                predecessor.right = null;
-
-                // BST Validation Check
-                if(prev !== null && prev.val >= curr.val) {
-                    return false;
-                }
-
-                // go right
-                prev = curr;
-                curr = curr.right;
-            }
-
+    for(let i = 0; i < inorder.length - 1; i += 1) {
+        if(inorder[i] >= inorder[i + 1]) {
+            return false;
         }
     }
 
     return true;
-
+    
 };
