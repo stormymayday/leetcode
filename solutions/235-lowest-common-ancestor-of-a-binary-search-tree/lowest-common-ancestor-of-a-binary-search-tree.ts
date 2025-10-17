5832f9247560c5194c15130c698d44025de58f0a
Aidar <stormymayday7@gmail.com>
2025-10-17 09:58:18 -0700

    Time: 61 ms (66.03%) | Memory: 66.1 MB (47.04%) - LeetSync

--- solutions/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.ts
+++ solutions/235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.ts
@@ -14,24 +14,12 @@
 
 function lowestCommonAncestor(root: TreeNode | null, p: TreeNode | null, q: TreeNode | null): TreeNode | null {
 
-    if(root === null) {
-        return null;
+    if(p.val < root.val && q.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q);
+    } else if(p.val > root.val && q.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q);
+    } else {
+        return root;
     }
-
-    let curr: TreeNode | null = root;
-
-    while(curr !== null) {
-
-        if(p.val < curr.val && q.val < curr.val) {
-            curr = curr.left;
-        } else if(p.val > curr.val && q.val > curr.val) {
-            curr = curr.right;
-        } else {
-            return curr;
-        }
-
-    }
-
-    return curr;
 	
 };
