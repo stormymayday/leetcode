6a38d99dc76effc8b9627dcd5c1b42b9a19506c5
Aidar <stormymayday7@gmail.com>
2025-10-18 09:33:47 -0700

    Time: 44 ms (97.06%) | Memory: 60.3 MB (54.90%) - LeetSync

--- solutions/774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.ts
+++ solutions/774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.ts
@@ -14,15 +14,26 @@
 
 function maxDepth(root: _Node | null): number {
 
+
     if(root === null) {
         return 0;
     }
 
-    let maxChildDepth = 0;
-    for(const child of root.children) {
-        maxChildDepth = Math.max(maxChildDepth, maxDepth(child));
+    function helperDFS(node: _Node): number {
+
+        if(node.children.length === 0) {
+            return 1; // node with node children is at depth of 1
+        }
+
+        let maxSubtreeDepth = 0;
+        for(const child of node.children) {
+            maxSubtreeDepth = Math.max(maxSubtreeDepth, helperDFS(child));
+        }
+
+        return 1 + maxSubtreeDepth;
+
     }
 
-    return 1 + maxChildDepth;
-    
+    return helperDFS(root);
+
 };
