966dbef14147446dd04a398e5da59b9feb8f59cf
Aidar <stormymayday7@gmail.com>
2025-10-20 10:44:10 -0700

    Time: 9 ms (5.42%) | Memory: 64.5 MB (5.03%) - LeetSync

--- solutions/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.ts
+++ solutions/230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.ts
@@ -18,8 +18,7 @@ function kthSmallest(root: TreeNode | null, k: number): number {
         return -Infinity;
     }
 
-    // const maxHeap = new MaxHeap();
-    const maxHeap: number[] = [];
+    const maxHeap = new CustomMaxHeap();
 
     const stack: TreeNode[] = [root];
 
@@ -30,8 +29,7 @@ function kthSmallest(root: TreeNode | null, k: number): number {
         if(maxHeap.length < k) {
             maxHeap.push(currNode.val);
         } else {
-            maxHeap.sort((a, b) => a - b);
-            if(currNode.val < maxHeap[maxHeap.length - 1]) {
+            if(currNode.val < maxHeap.top()) {
                 maxHeap.pop();
                 maxHeap.push(currNode.val);
             }
@@ -47,7 +45,65 @@ function kthSmallest(root: TreeNode | null, k: number): number {
 
     }
 
-    maxHeap.sort((a, b) => a - b);
-    return maxHeap[maxHeap.length - 1];
+    return maxHeap.top();
     
 };
+
+class CustomMaxHeap {
+    private data: number[];
+    public length: number;
+    constructor() {
+        this.data = [];
+        this.length = 0;
+    }
+    push(val: number): void {
+        this.data.push(val);
+        this.length += 1;
+        let currIdx = this.length - 1;
+        let parentIdx = Math.floor((currIdx - 1) / 2);
+        while(currIdx > 0 && this.data[currIdx] > this.data[parentIdx]) {
+            this.swap(currIdx, parentIdx);
+            currIdx = parentIdx;
+            parentIdx = Math.floor((currIdx - 1) / 2);
+        }
+    }
+    pop(): number | undefined {
+        if(this.length === 0) {
+            return undefined;
+        }
+        if(this.length === 1) {
+            this.length = 0;
+            return this.data.pop();
+        }
+        const root = this.data[0];
+        this.data[0] = this.data.pop();
+        this.length -= 1;
+        this.siftDown(0);
+        return root;
+    }
+    siftDown(idx: number): void {
+        let currIdx = idx;
+        while(currIdx < this.length - 1) {
+            const leftChildIdx = currIdx * 2 + 1;
+            const rightChildIdx = currIdx * 2 + 2;
+            const leftChildVal = this.data[leftChildIdx] === undefined ? -Infinity : this.data[leftChildIdx];
+            const rightChildVal = this.data[rightChildIdx] === undefined ? -Infinity : this.data[rightChildIdx];
+            const largerChildIdx = leftChildVal > rightChildVal ? leftChildIdx : rightChildIdx;
+            const largerChildVal = leftChildVal > rightChildVal ? leftChildVal : rightChildVal;
+            if(this.data[currIdx] < largerChildVal) {
+                this.swap(currIdx, largerChildIdx);
+                currIdx = largerChildIdx;
+            } else {
+                break;
+            }
+        }
+    }
+    top(): number | undefined {
+        return this.length > 0 ? this.data[0] : undefined;
+    }
+    swap(idx1: number, idx2: number): void {
+        const temp = this.data[idx1];
+        this.data[idx1] = this.data[idx2];
+        this.data[idx2] = temp;
+    }
+}
