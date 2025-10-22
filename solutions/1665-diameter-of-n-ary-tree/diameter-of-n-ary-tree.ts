4f929e46626be581756159a4497a562178f2ce82
Aidar <stormymayday7@gmail.com>
2025-10-21 19:37:52 -0700

    Time: 54 ms (62.50%) | Memory: 60.5 MB (62.50%) - LeetSync

--- solutions/1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.ts
+++ solutions/1665-diameter-of-n-ary-tree/diameter-of-n-ary-tree.ts
@@ -16,35 +16,50 @@ function diameter(root: _Node): number {
 
     let longestPath = 0;
 
-    function helper(node: _Node): number {
+    if(root === null || root.children.length === 0) {
+        return longestPath;
+    }
 
+    // This function returns height at a given node
+    // The longest path is calculated in-between via
+    // Formula: The Longest Path (diameter) = sum of two longest paths starting at a given node
+    function helper(node: _Node): number {
+        // Base Case: if node has no children, it's height is 1
         if(node.children.length === 0) {
             return 1;
         }
 
-        let deepest = 0;
-        let secondDeepest = 0;
+        // Get the height of two tallest subtrees
+        let tallestSubtree = 0;
+        let secondTallestSubtree = 0;
         for(const child of node.children) {
-            // const subtreeDepth = helper(child) + 1;
-            const subtreeDepth = helper(child);
-            if(subtreeDepth > deepest) {
-                secondDeepest = deepest;
-                deepest = subtreeDepth;
-            } else if(subtreeDepth > secondDeepest) {
-                secondDeepest = subtreeDepth;
+            // 1. Get height of subtree rooted at current child
+            const height = helper(child);
+            // 2. If the height is greater than the tallest (so far)
+            if(height > tallestSubtree) {
+                // 2.1 Overwrite the second tallest value
+                secondTallestSubtree = tallestSubtree;
+                // 2.2 Update the tallest
+                tallestSubtree = height;
+            } 
+            // 3. If height is only greater than the second tallest
+            else if(height > secondTallestSubtree) {
+                // 3.1 Upadate the second tallest
+                secondTallestSubtree = height;
             }
-
         }
 
-        longestPath = Math.max(longestPath, deepest + secondDeepest);
+        // Calculate longest path at the current node updating the global max
+        // Formula: longest path = tallest subtree height + second tallest subtree height
+        longestPath = Math.max(longestPath, tallestSubtree + secondTallestSubtree);
 
-        // return deepest;
-        return deepest + 1;
+        // Return height at the current node
+        return 1 + tallestSubtree;
 
     }
 
     helper(root);
- 
+
     return longestPath;
     
 };
