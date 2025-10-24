40855e8059ed5af5461e7c3226a7cb2cdb1bf8fb
Aidar <stormymayday7@gmail.com>
2025-10-24 13:44:56 -0700

    Time: 0 ms (100.00%) | Memory: 56.9 MB (29.41%) - LeetSync

---
+++ solutions/799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function minDiffInBST(root: TreeNode | null): number {
+
+    let minDiff = Infinity;
+
+    function inorderDFS(node: TreeNode | null, prev: TreeNode | null): TreeNode | null {
+
+        if(node === null) {
+            return prev;
+        }
+
+        const prevNode = inorderDFS(node.left, prev);
+
+        if(prevNode !== null) {
+            minDiff = Math.min(minDiff, Math.abs(node.val - prevNode.val));
+        }
+
+        return inorderDFS(node.right, node);
+
+    }
+
+    inorderDFS(root, null);
+
+    return minDiff;
+    
+};
