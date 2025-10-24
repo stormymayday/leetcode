a912f5c67c52e1222c93d5a2c788c0348f077b70
Aidar <stormymayday7@gmail.com>
2025-10-24 09:20:27 -0700

    Time: 2 ms (32.73%) | Memory: 64.7 MB (38.18%) - LeetSync

--- solutions/617-merge-two-binary-trees/merge-two-binary-trees.ts
+++ solutions/617-merge-two-binary-trees/merge-two-binary-trees.ts
@@ -14,53 +14,20 @@
 
 function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
 
-    if(root1 === null) {
-        return root2;
+    if(root1 === null && root2 === null) {
+        return null;
     }
 
-    if(root2 === null) {
-        return root1;
+    const node = new TreeNode();
+    if(root1 !== null && root2 !== null) {
+        node.val = root1.val + root2.val;
+    } else {
+        node.val = root1 !== null ? root1.val : root2.val;
     }
 
-    let queue: [TreeNode, TreeNode][] = [[root1, root2]];
+    node.left = mergeTrees(root1 !== null ? root1.left : null, root2 !== null ? root2.left : null);
+    node.right = mergeTrees(root1 !== null ? root1.right : null, root2 !== null ? root2.right : null);
 
-    while(queue.length > 0) {
-
-        const nextQueue: [TreeNode, TreeNode][] = [];
-
-        for(let i = 0; i < queue.length; i += 1) {
-
-            const [node1, node2] = queue[i];
-
-            node1.val = node1.val + node2.val;
-
-            if(node1.left !== null || node2.left !== null) {
-
-                const newNode = new TreeNode(0);
-                if(node1.left === null) {
-                    node1.left = newNode;
-                }
-                nextQueue.push([node1.left, node2.left || newNode]);
-
-            }
-
-            if(node1.right !== null || node2.right !== null) {
-
-                const newNode = new TreeNode(0);
-                if(node1.right === null) {
-                    node1.right = newNode;
-                }
-                nextQueue.push([node1.right, node2.right || newNode]);
-
-            }
-
-
-        }
-
-        queue = nextQueue;
-
-    }
-
-    return root1;
+    return node;
     
 };
