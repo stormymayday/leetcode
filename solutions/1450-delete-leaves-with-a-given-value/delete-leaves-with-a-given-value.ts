68ea787f2332b2a82e3b452ce8731a3d350b4c10
Aidar <stormymayday7@gmail.com>
2025-10-23 18:00:26 -0700

    Time: 1 ms (57.14%) | Memory: 62.5 MB (28.57%) - LeetSync

---
+++ solutions/1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.ts
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function removeLeafNodes(root: TreeNode | null, target: number): TreeNode | null {
+    
+    function helper(node: TreeNode | null): TreeNode | null {
+
+        if(node === null) {
+            return null;
+        }
+
+        node.left = helper(node.left);
+        node.right = helper(node.right);
+
+        if(node.left === null && node.right === null && node.val === target) {
+            return null;
+        }
+
+        return node;
+
+    }
+
+    return helper(root);
+
+};
