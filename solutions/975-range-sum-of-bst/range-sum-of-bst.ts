8099a94718821c99a887f2cc88dd4f04f41b80fe
Aidar <stormymayday7@gmail.com>
2025-10-24 10:30:22 -0700

    Time: 0 ms (100.00%) | Memory: 85.3 MB (85.35%) - LeetSync

--- solutions/975-range-sum-of-bst/range-sum-of-bst.ts
+++ solutions/975-range-sum-of-bst/range-sum-of-bst.ts
@@ -1,19 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
 function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
-    if (!root) {
+
+    if(root === null) {
         return 0;
     }
 
     let sum = 0;
-    if (root.val >= low && root.val <= high) {
+
+    if(root.val >= low && root.val <= high) {
         sum += root.val;
     }
-    
-    if (root.val >= low) {
+
+    if(root.val > low) {
         sum += rangeSumBST(root.left, low, high);
     }
-    if (root.val <= high) {
+
+    if(root.val < high) {
         sum += rangeSumBST(root.right, low, high);
     }
-    
+
     return sum;
-}
+    
+};
