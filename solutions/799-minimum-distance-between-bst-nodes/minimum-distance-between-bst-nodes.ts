b02f02494afd46a3ae5e95ad3a274954184abac6
Aidar <stormymayday7@gmail.com>
2025-10-25 15:37:27 -0700

    Time: 0 ms (100.00%) | Memory: 57.7 MB (11.76%) - LeetSync

--- solutions/799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.ts
+++ solutions/799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.ts
@@ -17,20 +17,45 @@ function minDiffInBST(root: TreeNode | null): number {
     let minDiff = Infinity;
     let prev: TreeNode | null = null;
     let curr: TreeNode | null = root;
-    const stack: TreeNode[] = [];
-    while(curr !== null || stack.length > 0){
-        while(curr !== null) {
-            stack.push(curr);
-            curr = curr.left;
+    // Morris In-Order Traversal
+    while(curr !== null) {
+        // No left subtree
+        if(curr.left === null) {
+            // visit
+            if(prev !== null) {
+                minDiff = Math.min(minDiff, curr.val - prev.val);
+            }
+            // update/set 'prev' and go right
+            prev = curr;
+            curr = curr.right;
         }
-        curr = stack.pop()!;
-
-        if(prev !== null) {
-            minDiff = Math.min(minDiff, curr.val - prev.val);
+        // There is a left subtree
+        else {
+            // Get the inorder predecessor
+            let predecessor = curr.left;
+            while(predecessor.right !== null && predecessor.right !== curr) {
+                predecessor = predecessor.right;
+            }
+            // No cycle to curr
+            if(predecessor.right === null) {
+                // create a cycle with curr
+                predecessor.right = curr;
+                // go left
+                curr = curr.left;
+            }
+            // There is a cycle to curr
+            else {
+                // break the cycle
+                predecessor.right = null;
+                // visit
+                if(prev !== null) {
+                    minDiff = Math.min(minDiff, curr.val - prev.val);
+                }
+                // update/set 'prev' and go right
+                prev = curr;
+                curr = curr.right;
+            }
         }
-
-        prev = curr;
-        curr = curr.right;
     }
 
     return minDiff;
