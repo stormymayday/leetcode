cc59dd4879a0c086a15fd080532289ec40bae6dd
Aidar <stormymayday7@gmail.com>
2025-10-25 11:16:50 -0700

    Time: 6 ms (29.55%) | Memory: 65.8 MB (48.86%) - LeetSync

--- solutions/99-recover-binary-search-tree/recover-binary-search-tree.ts
+++ solutions/99-recover-binary-search-tree/recover-binary-search-tree.ts
@@ -16,83 +16,38 @@
  Do not return anything, modify root in-place instead.
  */
 function recoverTree(root: TreeNode | null): void {
-
-    if (root === null) {
-        return;
-    }
-
+    
     let node1: TreeNode | null = null;
     let node2: TreeNode | null = null;
     let prev: TreeNode | null = null;
-    let curr: TreeNode | null = root;
 
-    // Morris In-Order Traversal
-    while (curr !== null) {
-        // no left
-        if (curr.left === null) {
-            // Visit
-            if (prev !== null) {
-                // Found discrepancy!
-                if (prev.val > curr.val) {
-                    // First time: set both node1 and node2
-                    if (node1 === null) {
-                        node1 = prev;
-                        node2 = curr;
-                    }
-                    // Second time: update node2
-                    else {
-                        node2 = curr;
-                        // break;
-                        // exiting early can create cycles!
-                    }
+    function inorderDFS(node: TreeNode | null): void {
+        if(node === null) {
+            return;
+        }
+        inorderDFS(node.left);
+        if(prev !== null) {
+            // Found discrepancy!
+            if(prev.val > node.val) {
+                // First time: set both node1 and node2
+                if(node1 === null) {
+                    node1 = prev;
+                    node2 = node;
+                } 
+                // Second time: update node2 and exit
+                else {
+                    node2 = node;
+                    return;
                 }
-            }
-            // Set/Update 'prev' and go Right
-            prev = curr;
-            curr = curr.right;
-        } else {
-            // Find inorder predecessor
-            let predecessor = curr.left;
-            while (predecessor.right !== null && predecessor.right !== curr) {
-                predecessor = predecessor.right;
-            }
-            // No cycle
-            if (predecessor.right === null) {
-                // create a cycle with curr
-                predecessor.right = curr;
-                // keep going left
-                curr = curr.left;
-            }
-            // Cycle with curr
-            else {
-                // break the cycle
-                predecessor.right = null;
-                // visit
-                if (prev !== null) {
-                    // Found discrepancy!
-                    if (prev.val > curr.val) {
-                        // First time: set both node1 and node2
-                        if (node1 === null) {
-                            node1 = prev;
-                            node2 = curr;
-                        }
-                        // Second time: update node2
-                        else {
-                            node2 = curr;
-                            // break;
-                            // exiting early can create cycles!
-                        }
-                    }
-                }
-                // set/update 'prev' and go right
-                prev = curr;
-                curr = curr.right;
             }
         }
+        prev = node;
+        inorderDFS(node.right);
     }
+    inorderDFS(root);
 
     // Optional Guard
-    if (node1 !== null && node2 !== null) {
+    if(node1 !== null && node2 !== null) {
         const temp = node1.val;
         node1.val = node2.val;
         node2.val = temp;
