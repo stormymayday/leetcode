54dcc2b5896d253e0458ad599296f4f591b92ba0
Aidar <stormymayday7@gmail.com>
2025-10-25 11:05:21 -0700

    Time: 2 ms (98.86%) | Memory: 66.2 MB (17.05%) - LeetSync

--- solutions/99-recover-binary-search-tree/recover-binary-search-tree.ts
+++ solutions/99-recover-binary-search-tree/recover-binary-search-tree.ts
@@ -17,7 +17,7 @@
  */
 function recoverTree(root: TreeNode | null): void {
 
-    if(root === null) {
+    if (root === null) {
         return;
     }
 
@@ -25,39 +25,77 @@ function recoverTree(root: TreeNode | null): void {
     let node2: TreeNode | null = null;
     let prev: TreeNode | null = null;
     let curr: TreeNode | null = root;
-    const stack: TreeNode[] = [];
-    while(curr !== null || stack.length > 0) {
-        while(curr !== null) {
-            stack.push(curr);
-            curr = curr.left;
-        }
-        curr = stack.pop()!;
 
-        if(prev !== null) {
-            // Discrepancy found!
-            if(prev.val > curr.val) {
-                // First time: set both node1 and node2
-                if(node1 === null) {
-                    node1 = prev;
-                    node2 = curr;
-                } 
-                // Second time: update node2
-                else {
-                    node2 = curr;
-                    break;
+    // Morris In-Order Traversal
+    while (curr !== null) {
+        // no left
+        if (curr.left === null) {
+            // Visit
+            if (prev !== null) {
+                // Found discrepancy!
+                if (prev.val > curr.val) {
+                    // First time: set both node1 and node2
+                    if (node1 === null) {
+                        node1 = prev;
+                        node2 = curr;
+                    }
+                    // Second time: update node2
+                    else {
+                        node2 = curr;
+                        // break;
+                        // exiting early can create cycles!
+                    }
                 }
             }
+            // Set/Update 'prev' and go Right
+            prev = curr;
+            curr = curr.right;
+        } else {
+            // Find inorder predecessor
+            let predecessor = curr.left;
+            while (predecessor.right !== null && predecessor.right !== curr) {
+                predecessor = predecessor.right;
+            }
+            // No cycle
+            if (predecessor.right === null) {
+                // create a cycle with curr
+                predecessor.right = curr;
+                // keep going left
+                curr = curr.left;
+            }
+            // Cycle with curr
+            else {
+                // break the cycle
+                predecessor.right = null;
+                // visit
+                if (prev !== null) {
+                    // Found discrepancy!
+                    if (prev.val > curr.val) {
+                        // First time: set both node1 and node2
+                        if (node1 === null) {
+                            node1 = prev;
+                            node2 = curr;
+                        }
+                        // Second time: update node2
+                        else {
+                            node2 = curr;
+                            // break;
+                            // exiting early can create cycles!
+                        }
+                    }
+                }
+                // set/update 'prev' and go right
+                prev = curr;
+                curr = curr.right;
+            }
         }
-
-        prev = curr;
-        curr = curr.right;
     }
 
     // Optional Guard
-    if(node1 !== null && node2 !== null) {
+    if (node1 !== null && node2 !== null) {
         const temp = node1.val;
         node1.val = node2.val;
         node2.val = temp;
     }
-    
+
 };
