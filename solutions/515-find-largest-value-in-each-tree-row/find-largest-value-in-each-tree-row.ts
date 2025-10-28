5931e0bda8b8a5708244ee8b34e65e5fb4d1dfaf
Aidar <stormymayday7@gmail.com>
2025-10-28 16:54:17 -0700

    Time: 0 ms (100.00%) | Memory: 60.8 MB (80.30%) - LeetSync

--- solutions/515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.ts
+++ solutions/515-find-largest-value-in-each-tree-row/find-largest-value-in-each-tree-row.ts
@@ -13,31 +13,23 @@
  */
 
 function largestValues(root: TreeNode | null): number[] {
-
+    
     const res: number[] = [];
 
-    if(root === null) {
-        return res;
+    function inorderDFS(node: TreeNode | null, level: number):void {
+        if(node === null) {
+            return;
+        }
+        if(res.length === level) {
+            res.push(node.val);
+        }
+        res[level] = Math.max(res[level], node.val);
+        inorderDFS(node.left, level + 1);
+        inorderDFS(node.right, level + 1);
     }
 
-    const stack: [TreeNode, number][] = [[root, 0]];
-    while(stack.length > 0) {
-        const [currNode, currLevel] = stack.pop();
-        
-        if(res.length === currLevel) {
-            res.push(currNode.val);
-        } else {
-            res[currLevel] = Math.max(res[currLevel], currNode.val);
-        }
-
-        if(currNode.left !== null) {
-            stack.push([currNode.left, currLevel + 1]);
-        }
-        if(currNode.right !== null) {
-            stack.push([currNode.right, currLevel + 1]);
-        }
-    }
+    inorderDFS(root, 0);
 
     return res;
-    
+
 };
