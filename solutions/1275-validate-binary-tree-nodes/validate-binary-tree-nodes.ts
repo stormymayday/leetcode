b1d1e5651cdfea4c2dd012140c2958adaf0b5e7f
Aidar <stormymayday7@gmail.com>
2025-10-28 20:11:25 -0700

    Time: 21 ms (42.86%) | Memory: 67.3 MB (28.57%) - LeetSync

--- solutions/1275-validate-binary-tree-nodes/validate-binary-tree-nodes.ts
+++ solutions/1275-validate-binary-tree-nodes/validate-binary-tree-nodes.ts
@@ -3,40 +3,48 @@ function validateBinaryTreeNodes(n: number, leftChild: number[], rightChild: num
     const children = new Set([...leftChild, ...rightChild]);
     children.delete(-1);
 
-    if(children.size === n) {
+    if (children.size === n) {
         return false; // should be n-1
     }
 
     // Find root
     let root: number = 0;
-    for(let i = 0; i < n; i += 1) {
-        if(!children.has(i)) {
+    for (let i = 0; i < n; i += 1) {
+        if (!children.has(i)) {
             root = i;
         }
     }
 
     const visited = new Set<number>();
-    const stack: number[] = [root];
+    let queue: number[] = [root];
 
-    while(stack.length > 0) {
+    while (queue.length > 0) {
 
-        const currNode = stack.pop();
+        const nextQueue: number[] = [];
 
-        if(visited.has(currNode)) {
-            return false;
+        for (let i = 0; i < queue.length; i += 1) {
+
+            const currNode = queue[i];
+
+            if (visited.has(currNode)) {
+                return false;
+            }
+            visited.add(currNode);
+
+            if (leftChild[currNode] !== -1) {
+                nextQueue.push(leftChild[currNode]);
+            }
+
+            if (rightChild[currNode] !== -1) {
+                nextQueue.push(rightChild[currNode]);
+            }
+
         }
-        visited.add(currNode);
 
-        if(leftChild[currNode] !== -1) {
-            stack.push(leftChild[currNode]);
-        }
-
-        if(rightChild[currNode] !== -1) {
-            stack.push(rightChild[currNode]);
-        }
+        queue = nextQueue;
 
     }
 
     return visited.size === n;
-    
+
 };
