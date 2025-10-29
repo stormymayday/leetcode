441a856b3e714f53275e711f6e7500449cd4d75e
Aidar <stormymayday7@gmail.com>
2025-10-28 20:30:36 -0700

    Time: 0 ms (100.00%) | Memory: 58.4 MB (12.36%) - LeetSync

--- solutions/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.ts
+++ solutions/103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.ts
@@ -13,39 +13,44 @@
  */
 
 function zigzagLevelOrder(root: TreeNode | null): number[][] {
-
+    
     const res: number[][] = [];
 
-    if (root === null) {
+    if(root === null) {
         return res;
     }
 
     let queue: TreeNode[] = [root];
     let level: number = 0;
 
-    while (queue.length > 0) {
+    while(queue.length > 0) {
 
         const nextQueue: TreeNode[] = [];
         const levelVals: number[] = [];
-        for (let i = 0; i < queue.length; i += 1) {
+
+        for(let i = 0; i < queue.length; i += 1) {
+
             const currNode = queue[i];
+
             levelVals.push(currNode.val);
-            if (currNode.left !== null) {
+
+            if(currNode.left !== null) {
                 nextQueue.push(currNode.left);
             }
-            if (currNode.right !== null) {
+
+            if(currNode.right !== null) {
                 nextQueue.push(currNode.right);
             }
+            
         }
 
-
-        queue = nextQueue;
-        if (level % 2 === 0) {
+        if(level % 2 === 0) {
             res.push(levelVals);
         } else {
             res.push(levelVals.reverse());
         }
         level += 1;
+        queue = nextQueue;
 
     }
 
