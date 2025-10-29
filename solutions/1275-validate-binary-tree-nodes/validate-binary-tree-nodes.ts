fcdb2c49ae6af9e13320573184f8693f089a37f6
Aidar <stormymayday7@gmail.com>
2025-10-28 20:05:42 -0700

    Time: 22 ms (42.86%) | Memory: 67 MB (28.57%) - LeetSync

--- solutions/1275-validate-binary-tree-nodes/validate-binary-tree-nodes.ts
+++ solutions/1275-validate-binary-tree-nodes/validate-binary-tree-nodes.ts
@@ -1,41 +1,33 @@
 function validateBinaryTreeNodes(n: number, leftChild: number[], rightChild: number[]): boolean {
-    
-    // Children Hash Set, will help finding the root
-    const children = new Set<number>([...leftChild, ...rightChild]);
+
+    const children = new Set([...leftChild, ...rightChild]);
     children.delete(-1);
 
     if(children.size === n) {
-        return false; // there is a cycle
+        return false; // should be n-1
     }
 
-    // Number that is not in the 'children' hash set must be the root
-    let root: number = -1;
+    // Find root
+    let root: number = 0;
     for(let i = 0; i < n; i += 1) {
         if(!children.has(i)) {
             root = i;
         }
     }
 
-    // Optional Guard
-    if(root === -1) {
-        return false;
-    }
-
-    // visited hash set will help detect cycles and connectivity
     const visited = new Set<number>();
     function dfs(node: number): boolean {
 
-        // Base Case: reached 'null', so far it's a valid tree
         if(node === -1) {
             return true;
         }
 
         if(visited.has(node)) {
-            return false; // cycle
+            return false;
         }
 
         visited.add(node);
-        
+
         const leftSubtree = dfs(leftChild[node]);
         if(leftSubtree === false) {
             return false;
