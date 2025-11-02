498be83366b2032264a4e41e517aad88d81e5e2d
Aidar <stormymayday7@gmail.com>
2025-11-02 11:36:11 -0800

    Time: 7 ms (50.00%) | Memory: 60.9 MB (100.00%) - LeetSync

--- solutions/333-largest-bst-subtree/largest-bst-subtree.ts
+++ solutions/333-largest-bst-subtree/largest-bst-subtree.ts
@@ -14,63 +14,32 @@
 
 function largestBSTSubtree(root: TreeNode | null): number {
 
-    // Edge Case: must check for null if 'largestBST' starts from 1
-    if(root === null) {
-        return 0
-    }
-
-    // Can set to zero but then must run update on each leaf
-    let largestBST: number = 1;
+    let largestBST: number = 0;
 
     function helper(root: TreeNode | null): [boolean, number, number, number] {
-        // Base Case: In this implementation null doesn't count as a BST
+        // Base Case: In this implementation null counts as a BST of size zero
         if (root === null) {
-            return [false, 0, -Infinity, Infinity];
-        }
-
-        // Base Case: leaf node counts as a BST
-        if (root.left === null && root.right === null) {
-            // Since we set 'largestBST' to be 0
-            // Either update max at each leaf or set 'largestBST' to 1 initially
-            // largestBST = Math.max(largestBST, 1);
-            return [true, 1, root.val, root.val];
+            return [true, 0, Infinity, -Infinity];
         }
 
         const [isLeftBST, leftSize, leftMin, leftMax] = helper(root.left);
         const [isRightBST, rightSize, rightMin, rightMax] = helper(root.right);
 
-        // Both left and right subtrees are BSTs
-        if (
+        if(
             (isLeftBST === true && isRightBST === true) &&
+            // BST property: root must be smaller thant the LARGEST value on the left
+            // and smaller than the SMALLEST value on the right
             (root.val > leftMax && root.val < rightMin)
-            ) {
-            
-            const currSize = 1 + leftSize + rightSize;
-            largestBST = Math.max(largestBST, currSize);
-            return [true, currSize, leftMin, rightMax];
-        }
-        // Left is a BST and right is null
-        else if (
-            (isLeftBST === true && root.right === null) &&
-            (root.val > leftMax)
         ) {
 
             const currSize = 1 + leftSize + rightSize;
+            // Update the maxSize
             largestBST = Math.max(largestBST, currSize);
-            return [true, 1 + leftSize, leftMin, root.val];
-        }
-        // Right is a BST and left is null
-        else if (
-            (isRightBST === true && root.left === null) &&
-            (root.val < rightMin)
-            ) {
-            
-            const currSize = 1 + leftSize + rightSize;
-            largestBST = Math.max(largestBST, currSize);
-            return [true, 1 + rightSize, root.val, rightMax];
-        }
-        // Otherwise, we can't count current as a BST
-        else {
+
+            return [true, currSize, Math.min(leftMin, root.val), Math.max(root.val, rightMax)];
+
+        } else {
+            // min and max do not matter if we return false
             return [false, 0, -Infinity, Infinity];
         }
     }
