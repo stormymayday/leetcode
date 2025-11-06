f0b4e4554b23790cd0d1e42470ec91928b8cc1e3
Aidar <stormymayday7@gmail.com>
2025-11-05 20:39:26 -0800

    Time: 35 ms (87.50%) | Memory: 83.7 MB (41.67%) - LeetSync

--- solutions/648-replace-words/replace-words.ts
+++ solutions/648-replace-words/replace-words.ts
@@ -2,30 +2,48 @@ function replaceWords(dictionary: string[], sentence: string): string {
 
     const arr = sentence.split(" ");
 
-    const sortedDict = [...dictionary].sort((a, b) => a.length - b.length);
+    const root = new TrieNode();
+    for (let i = 0; i < dictionary.length; i += 1) {
+        // going over each character in the word
+        let curr: TrieNode = root;
+        for (let j = 0; j < dictionary[i].length; j += 1) {
+            if (!curr.children.has(dictionary[i][j])) {
+                curr.children.set(dictionary[i][j], new TrieNode());
+            }
+            curr = curr.children.get(dictionary[i][j]);
+        }
+        curr.isWord = true;
+        curr.word = dictionary[i];
+    }
 
-    for(let i = 0; i < arr.length; i += 1) {
-        for(const prefix of sortedDict) {
-            if(isPrefixOf(prefix, arr[i]) === true) {
-                arr[i] = prefix;
-                break;
+
+    for (let i = 0; i < arr.length; i += 1) {
+        // going over each character in the word
+        let curr: TrieNode = root;
+        for (let j = 0; j < arr[i].length; j += 1) {
+            // let curr: TrieNode = root;
+            if (!curr.children.has(arr[i][j])) {
+                break; // need to break out to the next array element
+            }
+            curr = curr.children.get(arr[i][j]);
+            if(curr.isWord === true) {
+                arr[i] = curr.word;
+                // break; //
             }
         }
     }
-        
+
     return arr.join(" ");
-    
+
 };
 
-function isPrefixOf(prefix: string, word: string): boolean {
-    if(prefix.length > word.length) {
-        return false;
-    } else {
-        for(let i = 0; i < prefix.length; i += 1) {
-            if(prefix[i] !== word[i]) {
-                return false;
-            }
-        }
-        return true;
+class TrieNode {
+    children: Map<string, TrieNode>;
+    isWord: boolean;
+    word: string;
+    constructor() {
+        this.children = new Map();
+        this.isWord = false;
+        this.word = "";
     }
-} 
+}
