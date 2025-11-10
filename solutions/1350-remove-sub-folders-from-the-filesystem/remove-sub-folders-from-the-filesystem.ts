fca750922e6b15ccd9667b53e126c5e88bf09877
Aidar <stormymayday7@gmail.com>
2025-11-10 14:57:21 -0800

    Time: 224 ms (11.11%) | Memory: 90 MB (5.55%) - LeetSync

---
+++ solutions/1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.ts
@@ -0,0 +1,76 @@
+function removeSubfolders(folder: string[]): string[] {
+
+    const res: string[] = [];
+
+    const root = new TrieNode();
+
+    // Phase 1: filling the Trie
+    for(let i = 0; i < folder.length; i += 1) {
+
+        const parts = folder[i].split("/");
+
+        let curr: TrieNode = root;
+
+        for(let j = 0; j < parts.length; j += 1) {
+            if(!curr.children.has(parts[j])) {
+                curr.children.set(parts[j], new TrieNode());
+            }
+            curr = curr.children.get(parts[j]);
+        }
+
+        curr.isEnd = true;
+
+    }
+
+    // Phase 2: filter out the sub-folders
+    for(let i = 0; i < folder.length; i += 1) {
+
+        const parts = folder[i].split("/");
+
+        let curr: TrieNode = root;
+
+        for(let j = 0; j < parts.length; j += 1) {
+
+            if(!curr.children.has(parts[j])) {
+                break; // folder does not exist in the Trie
+            } else {
+
+                // Otherwise, (potentially) the folder exists
+
+                curr = curr.children.get(parts[j]);
+
+                if( 
+                    // If this node is the 'end'
+                    curr.isEnd === true &&
+                    // AND this is the 'last part' of the folder string
+                    j === parts.length -1
+                    ) {
+                        // This is valid folder (not a sub-folder)
+                        res.push(folder[i]);
+                } else if(
+                    // If this node is the 'end'
+                    curr.isEnd === true &&
+                    // AND this is NOT the 'last part' of the folder string
+                    j < parts.length -1
+                ) {
+                    // This is a sub-folder
+                    break;
+                }
+            }
+
+        }
+
+    }
+
+    return res;
+    
+};
+
+class TrieNode {
+    children: Map<string, TrieNode>;
+    isEnd: boolean;
+    constructor() {
+        this.children = new Map();
+        this.isEnd = false;
+    }
+}
