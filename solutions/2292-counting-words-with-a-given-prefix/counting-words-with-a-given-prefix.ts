952a783bcd058fe819f6d8202bbd3842e24f7113
Aidar <stormymayday7@gmail.com>
2025-11-10 12:11:13 -0800

    Time: 8 ms (0.00%) | Memory: 61.9 MB (6.45%) - LeetSync

--- solutions/2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.ts
+++ solutions/2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.ts
@@ -1,13 +1,57 @@
 function prefixCount(words: string[], pref: string): number {
 
-    let count: number = 0;
+    const trie = new Trie();
 
-    for(const word of words) {
-        if(word.startsWith(pref)) {
-            count += 1;
-        }
+    for (const word of words) {
+        trie.insert(word);
     }
 
-    return count;
-    
-};
+    let curr: TrieNode | null = trie.root;
+    for(let i = 0; i < pref.length; i += 1) {
+        if(!curr.children.has(pref[i])) {
+            curr = null;
+            break;
+        }
+        curr = curr.children.get(pref[i]);
+    }
+
+    if(curr !== null) {
+        return curr.wordCount;
+    } else {
+        return 0;
+    }
+
+}
+
+class TrieNode {
+    children: Map<string, TrieNode>;
+    wordCount: number;
+    isWord: boolean;
+    constructor() {
+        this.children = new Map();
+        this.wordCount = 0;
+        this.isWord = false;
+    }
+}
+
+class Trie {
+    root: TrieNode;
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    insert(word: string): void {
+        let curr: TrieNode = this.root;
+        for (let i = 0; i < word.length; i += 1) {
+            if (!curr.children.has(word[i])) {
+                curr.children.set(word[i], new TrieNode());
+            }
+            // curr.wordCount += 1;
+            curr = curr.children.get(word[i]);
+            // I think we need to increment counter AFTER we move to a node
+            curr.wordCount += 1;
+        }
+        curr.isWord = true;
+    }
+
+}
