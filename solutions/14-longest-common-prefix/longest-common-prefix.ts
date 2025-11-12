b3c054ea2165343c087f660e006d9676b9f4820c
Aidar <stormymayday7@gmail.com>
2025-11-12 12:04:30 -0800

    Time: 11 ms (5.81%) | Memory: 58.7 MB (8.36%) - LeetSync

--- solutions/14-longest-common-prefix/longest-common-prefix.ts
+++ solutions/14-longest-common-prefix/longest-common-prefix.ts
@@ -1,32 +1,59 @@
 function longestCommonPrefix(strs: string[]): string {
 
-    // Phase 1: Count all the prefixes
-    const prefixCount = new Map();
-    for (const str of strs) {
-        const prefixes = getAllPrefixes(str);
-        for (const prefix of prefixes) {
-            prefixCount.set(prefix, (prefixCount.get(prefix) || 0) + 1);
-        }
+    // Quick Optimization: if there is only one string
+    if (strs.length === 1) {
+        return strs[0]; // just return the string (it's own prefix)
     }
 
-    // Phase 2: Get the longest prefix
-    const res = [""];
-    for (const [prefix, count] of prefixCount.entries()) {
-        // count must be equal to number of input strings
-        if (count === strs.length) {
-            if (res[0].length < prefix.length) {
-                res[0] = prefix;
+    // Phase 1: Populate the Trie
+    const root = new TrieNode();
+    for (const str of strs) {
+
+        // Edge Case: empty string
+        if(str === "") {
+            return "";
+        }
+
+        // Optimization?
+        // If root already has 1 child and current word has a different first character
+        // Thus, 'common' prefix is no longer possible
+        if (root.children.size === 1 && !root.children.has(str[0])) {
+            return ""; // exit early
+        }
+
+        // Otherwise, keep inserting
+        let curr: TrieNode = root;
+
+        // character by character
+        for (let i = 0; i < str.length; i += 1) {
+            if (!curr.children.has(str[i])) {
+                curr.children.set(str[i], new TrieNode());
             }
+            curr = curr.children.get(str[i]);
+        }
+        curr.isEnd = true;
+    }
+
+    // Phase 2: get the common longest prefix
+    const res: string[] = [];
+    let curr: TrieNode = root;
+    // Intuition: keep going down the Trie until there is more than 1 child
+    // AND current node is not 'end' of some string
+    while (curr.children.size === 1 && curr.isEnd === false) {
+        for (const key of curr.children.keys()) {
+            res.push(key);
+            curr = curr.children.get(key);
         }
     }
-    return res[0];
+    return res.join("");
 
 };
 
-function getAllPrefixes(str: string): string[] {
-    const prefixes: string[] = [];
-    for (let i = 1; i <= str.length; i += 1) {
-        prefixes.push(str.slice(0, i));
+class TrieNode {
+    children: Map<string, TrieNode>;
+    isEnd: boolean;
+    constructor() {
+        this.children = new Map();
+        this.isEnd = false;
     }
-    return prefixes;
 }
