3d65d26d5b6995ab14b5523e443310228fd31e93
Aidar <stormymayday7@gmail.com>
2025-11-12 17:34:45 -0800

    Time: 57 ms (100.00%) | Memory: 83.3 MB (0.00%) - LeetSync

--- solutions/1949-implement-trie-ii-prefix-tree/implement-trie-ii-prefix-tree.ts
+++ solutions/1949-implement-trie-ii-prefix-tree/implement-trie-ii-prefix-tree.ts
@@ -1,18 +1,16 @@
 class TrieNode {
     children: Map<string, TrieNode>;
-    wordsStartingHere: number; // counts all words passing through this node (for prefix matching)
-    wordsEndingHere: number; // counts how many times this exact word was inserted (for exact matching)
+    prefixCount: number;
+    wordCount: number;
     constructor() {
         this.children = new Map();
-        this.wordsStartingHere = 0;
-        this.wordsEndingHere = 0;
+        this.prefixCount = 0;
+        this.wordCount = 0;
     }
 }
 
 class Trie {
-
     root: TrieNode;
-
     constructor() {
         this.root = new TrieNode();
     }
@@ -24,9 +22,9 @@ class Trie {
                 curr.children.set(word[i], new TrieNode());
             }
             curr = curr.children.get(word[i]);
-            curr.wordsStartingHere += 1;
+            curr.prefixCount += 1;
         }
-        curr.wordsEndingHere += 1;
+        curr.wordCount += 1;
     }
 
     countWordsEqualTo(word: string): number {
@@ -34,11 +32,10 @@ class Trie {
         for (let i = 0; i < word.length; i += 1) {
             if (!curr.children.has(word[i])) {
                 return 0;
-            } else {
-                curr = curr.children.get(word[i]);
             }
+            curr = curr.children.get(word[i]);
         }
-        return curr.wordsEndingHere;
+        return curr.wordCount;
     }
 
     countWordsStartingWith(prefix: string): number {
@@ -46,69 +43,58 @@ class Trie {
         for (let i = 0; i < prefix.length; i += 1) {
             if (!curr.children.has(prefix[i])) {
                 return 0;
-            } else {
-                curr = curr.children.get(prefix[i]);
             }
+            curr = curr.children.get(prefix[i]);
         }
-        return curr.wordsStartingHere;
+        return curr.prefixCount;
     }
 
     erase(word: string): void {
 
         function helper(node: TrieNode, idx: number): boolean {
-            // Base Case:reached the last character node
-            if (idx === word.length) {
-                // this is not a complete word
-                if (node.wordsEndingHere === 0) {
-                    return false;
-                }
-                // otherwise, this is a complete word
-                else {
 
-                    // Need to decrement count
-                    node.wordsStartingHere -= 1; // was missing this!
-                    node.wordsEndingHere -= 1;
-
-                    // This will send signal to a parent node whether if this child entry should be removed from it's hash map
-                    return node.children.size === 0 && node.wordsEndingHere === 0;
+            // Base Case: reached the last node
+            if(idx === word.length) {
+                if(node.wordCount === 0) {
+                    return false; // not a target word
+                } else {
+                    node.wordCount -= 1;
+                    node.prefixCount -= 1;
+                    return node.children.size === 0 && node.wordCount === 0;
                 }
             }
 
             // Recursive Step
-            // This word (character) does not exist
-            if (!node.children.has(word[idx])) {
-                return false;
-            }
-            // Otherwise, this character exists
-            else {
 
-                const shouldRemoveChildEntry: boolean = helper(node.children.get(word[idx]), idx + 1);
+            if(!node.children.has(word[idx])) {
+                return false;  // not a target word
+            } else {
 
-                // Child node returns true - meaning we can remove the 'character' entry
-                if (shouldRemoveChildEntry === true) {
+                const shouldDeleteChildEntry = helper(node.children.get(word[idx]), idx + 1);
 
+                if(shouldDeleteChildEntry === true) {
+                    
                     node.children.delete(word[idx]);
-                    node.wordsStartingHere -= 1;
+                    node.prefixCount -= 1;
 
-                    // if 'wordsStartingHere' from this node becomes zero
-                    if(node.wordsStartingHere === 0) {
-                        // check if number of children is zero AND wordsEndingHere is zero
-                        // If they are, we can delete this node
-                        return node.children.size === 0 && node.wordsEndingHere === 0;
+                    if(node.prefixCount === 0) {
+                        return node.children.size === 0 && node.wordCount === 0;
                     } else {
-                        return false; // But don't delete this node
+                        return false;
                     }
 
                 } else {
-
-                    node.wordsStartingHere -= 1; // decrement 'prefixes' on the way up
-                    return false; // But don't delete this node
+                    
+                    // shouldDeleteChildEntry === false
+                    node.prefixCount -= 1;
+                    return false;
 
                 }
+
             }
+
         }
         helper(this.root, 0);
-
     }
 }
 
