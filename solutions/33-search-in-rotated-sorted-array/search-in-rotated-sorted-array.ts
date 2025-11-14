69165197e5df1e6b0ae1d734c9c522653141a394
Aidar <stormymayday7@gmail.com>
2025-11-13 20:03:38 -0800

    Time: 0 ms (100.00%) | Memory: 55.7 MB (51.77%) - LeetSync

--- solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -1,39 +1,62 @@
 function search(nums: number[], target: number): number {
 
-    let left = 0;
-    let right = nums.length - 1;
+    let left: number = 0;
+    let right: number = nums.length - 1;
 
     while(left <= right) {
 
-        const mid = Math.floor((left+right)/2);
+        const mid = left + Math.floor((right - left) / 2);
 
         if(target === nums[mid]) {
             return mid;
         }
 
-        // Left Sorted Portion
-        if(nums[mid] >= nums[left]) {
-            if(target > nums[mid]) {
+        // value at 'mid' is greater than value at 'left'
+        if(nums[left] <= nums[mid]) {
+
+            if(
+                // target is less than valie at 'mid' AND 'left'
+                (target < nums[mid] && target < nums[left]) ||
+                // OR target is greater than value at 'mid'
+                target > nums[mid]
+                ) {
+                // discard left
                 left = mid + 1;
-            } else if(target < nums[mid] && target < nums[left]) {
-                left = mid + 1;
-            } else {
-                // target is less that mid but greater than left
+            } 
+            // Otherwise, target must be less than value at 'mid' AND
+            // greater than or equals to value at 'left'
+            // (typing it out for clarity)
+            // Note: it is strictly less than value at mid because it eqality is check
+            else if(target < nums[mid] && target >= nums[left]) {
+                // discard right
                 right = mid - 1;
             }
-        }
-        // Right Sorted Portion
+
+        } 
+        // value at 'mid' is less than value at 'left'
+        // everything on the 'right' is greater than 'mid'?
         else {
-            if(target < nums[mid]) {
+
+            if(target < nums[mid] || target > nums[mid] && target > nums[right]) {
+                // discard right
                 right = mid - 1;
-            } else if(target > nums[mid] && target > nums[right]) {
-                right = mid - 1;
-            } else {
-                // target is greater than mid but less than right
+            } else if(target > nums[mid]) {
+                // discard left
                 left = mid + 1;
             }
+
+            // if(target > nums[mid] && target <= nums[right]) {
+            //     // discard left
+            //     left = mid + 1;
+            // } else if(target > nums[mid]) {
+            //     // discard right
+            //     right = mid - 1;
+            // }
+
         }
+
     }
-    
+
     return -1;
+    
 };
