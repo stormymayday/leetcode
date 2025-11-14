473d93b658027106d8ab271572336f24b002191d
Aidar <stormymayday7@gmail.com>
2025-11-14 11:25:10 -0800

    Time: 0 ms (100.00%) | Memory: 55.3 MB (60.34%) - LeetSync

--- solutions/162-find-peak-element/find-peak-element.ts
+++ solutions/162-find-peak-element/find-peak-element.ts
@@ -1,27 +1,48 @@
 function findPeakElement(nums: number[]): number {
 
-    // Edge Case: single element
-    if(nums.length === 1) {
-        return 0; // return index 0
-        // according to the problem description 'out of bounds values' are treated as -Infinity
-    }
+    let left: number = 0;
+    let right: number = nums.length - 1;
 
-    // First element is greater than second
-    // Note: Due to first edge case, we are guaranteed more than 1 element
-    if(nums[0] > nums[1]) {
-        return 0;
-    }
+    while (left <= right) {
 
-    // Last element is greater than second to last
-    if(nums[nums.length - 1] > nums[nums.length - 2]) {
-        return nums.length - 1;
-    }
+        const mid: number = left + Math.floor((right - left) / 2);
 
-    // Linear Scan until we find first 'peak'
-    for(let i = 1; i < nums.length - 1; i += 1) {
-        if(nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) {
-            return i;
+        // found peak
+        if (
+            // value at 'mid' is greater than w/e is to it's left (array value or -Infinity if index is out of bounds on the left)
+            (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
+            // AND value at 'mid' is greater than w/e is to it's right (array value or -Infinity if index is out of bounds on the right)
+            (nums[mid] > (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
+        ) {
+            return mid;
         }
+        else if (
+            // value at 'mid' is GREATER than the 'real' value to it's he left
+            // If we go left, and that next value is on the left edge 
+            // it can't be peak because current value will be to it's right and it is greater
+            nums[mid] > nums[mid - 1] ||
+            // OR value at 'mid' is on the left edge AND it's smaller than a real value to it's right
+            // (nums[mid] < nums[mid + 1] && mid - 1 < 0)
+            nums[mid] < nums[mid + 1]
+
+        ) {
+            // Therefore, discard left
+            left = mid + 1;
+        }
+
+        else if (
+            // Similarly, if value at 'mid' is greater than the 'real' value to it's right
+            // If we go right, and that next value is on the right edge
+            // it can't be peak because current value will be to it's left and it is greater
+            nums[mid] > nums[mid + 1] ||
+            // OR value at 'mid' is on the right edge AND it's smaller than a real value to it's left
+            // (nums[mid] < nums[mid - 1] && mid + 1 > nums.length - 1)
+            nums[mid] < nums[mid - 1]
+        ) {
+            // Therefore, discard right
+            right = mid - 1;
+        }
+
     }
-    
+
 };
