498dd33c506bfbf565cfa190fc664c3b3a742430
Aidar <stormymayday7@gmail.com>
2025-11-13 16:22:15 -0800

    Time: 30 ms (8.23%) | Memory: 58 MB (31.75%) - LeetSync

--- solutions/69-sqrtx/sqrtx.ts
+++ solutions/69-sqrtx/sqrtx.ts
@@ -1,6 +1,13 @@
 function mySqrt(x: number): number {
 
-    let left: number = 0;
+    // Square root of 0 is 0
+    // Square root of 1 is 1
+    if(x < 2) {
+        return x;
+    }
+
+    // let left: number = 0;
+    let left: number = 1;
     let candidate: number = left;
 
     while(left <= x) {
