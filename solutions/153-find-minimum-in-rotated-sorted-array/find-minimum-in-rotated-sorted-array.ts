e4a378034e3bbc64519723ce9b5373bce8c09e98
Aidar <stormymayday7@gmail.com>
2025-11-14 16:48:28 -0800

    Time: 0 ms (100.00%) | Memory: 55.5 MB (49.93%) - LeetSync

--- solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,20 +1,49 @@
 function findMin(nums: number[]): number {
 
-    let left = 0;
-    let right = nums.length - 1;
+    let left: number = 0;
+    let right: number = nums.length - 1;
 
-    while(left < right) {
+    let candidateIdx: number = 0;
 
-        const mid = Math.floor((left+right)/2);
+    while(left <= right) {
 
-        if(nums[mid] > nums[right]) {
-            left = mid + 1;
-        } else {
-            right = mid;
+        const mid = left + Math.floor((right - left) / 2);
+
+        // If value at 'mid' is greater than or equals to value at 'left'
+        // (equals incase 'mid' and 'left' are the same index)
+        if(nums[mid] >= nums[left]) {
+
+            // If value at 'mid' is also greater than value at 'right'
+            if(nums[mid] > nums[right]) {
+                // then minimum must be to the right
+                // discard left
+                left = mid + 1;
+            }
+            // Otherwise, search space must be sorted in sorted order
+            // (mid is >= left && mid < right)
+            else {
+
+                // compare current value at 'candidateIdx' against value at 'left' and break
+                if(nums[left] < nums[candidateIdx]) {
+                    candidateIdx = left;
+                }
+                break;
+
+            }
+
+        } 
+        // Otherwise, value at 'mid' is strictly greater than value at 'left'
+        else {
+            // update candidate / min
+            if(nums[candidateIdx] > nums[mid]) {
+                candidateIdx = mid;
+            }
+            // we can always discard right
+            right = mid - 1;
         }
 
     }
 
-    return nums[left];
+    return nums[candidateIdx];
     
 };
