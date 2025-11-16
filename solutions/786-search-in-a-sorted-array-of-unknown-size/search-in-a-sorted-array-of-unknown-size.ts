7d1e755d132eb8af71122c2f2c0998bf0e903f27
Aidar <stormymayday7@gmail.com>
2025-11-16 14:30:44 -0800

    Time: 41 ms (100.00%) | Memory: 58.8 MB (44.44%) - LeetSync

--- solutions/786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.ts
+++ solutions/786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.ts
@@ -10,15 +10,21 @@ function search(reader: ArrayReader, target: number): number {
 
     // Phase 1: find range
     let left: number = 0;
+
+    // Edge Case: target is smaller than the smallest value
+    if(target < reader.get(left)) {
+        return -1;
+    }
+
     let right: number = 1; // if 'right' starts at 0 it won't multiply by 2
 
     // Note: if 'right' goes out of bounds it returns a very large number
     while (target > reader.get(right)) {
-        left = right;
+        left = right; // constricting range from left optimizes phase 2
         right = right * 2;
     }
 
-    // Phase 2: Binary Search (if range exists)
+    // Phase 2: Binary Search
     while (left <= right) {
         const mid = left + Math.floor((right - left) / 2);
         const midVal = reader.get(mid);
