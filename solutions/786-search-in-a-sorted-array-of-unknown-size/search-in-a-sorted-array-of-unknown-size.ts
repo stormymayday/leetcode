638523c15a807a500815f03e1cd101e98a32a8e7
Aidar <stormymayday7@gmail.com>
2025-11-16 14:09:49 -0800

    Time: 37 ms (100.00%) | Memory: 58.7 MB (66.67%) - LeetSync

--- solutions/786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.ts
+++ solutions/786-search-in-a-sorted-array-of-unknown-size/search-in-a-sorted-array-of-unknown-size.ts
@@ -8,22 +8,27 @@
 
 function search(reader: ArrayReader, target: number): number {
 
+    // Phase 1: find range
     let left: number = 0;
-    let right: number = 10000;
+    let right: number = 1; // if 'right' starts at 0 it won't multiply by 2
 
-    while(left <= right) {
+    // Note: if 'right' goes out of bounds it returns a very large number
+    while (target > reader.get(right)) {
+        left = right;
+        right = right * 2;
+    }
 
+    // Phase 2: Binary Search (if range exists)
+    while (left <= right) {
         const mid = left + Math.floor((right - left) / 2);
         const midVal = reader.get(mid);
-
-        if(target > midVal) {
+        if (target > midVal) {
             left = mid + 1;
-        } else if(target < midVal) {
+        } else if (target < midVal) {
             right = mid - 1;
         } else {
             return mid;
         }
-
     }
 
     return -1;
