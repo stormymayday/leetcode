bae807d1fa5be0d50476f7c2aeff049f4d7eaf90
Aidar <stormymayday7@gmail.com>
2025-11-15 19:22:25 -0800

    Time: 0 ms (100.00%) | Memory: 56.2 MB (7.39%) - LeetSync

--- solutions/162-find-peak-element/find-peak-element.ts
+++ solutions/162-find-peak-element/find-peak-element.ts
@@ -1,31 +1,56 @@
 function findPeakElement(nums: number[]): number {
 
+    if (nums.length === 1 || nums[0] > nums[1]) {
+        return 0;
+    }
+
+    if (nums[nums.length - 1] > nums[nums.length - 2]) {
+        return nums.length - 1;
+    }
+
     let left: number = 0;
     let right: number = nums.length - 1;
-
+    let candidate: number = 0;
     while (left <= right) {
 
-        const mid: number = left + Math.floor((right - left) / 2);
+        const mid = left + Math.floor((right - left) / 2);
 
-        // found peak
         if (
-            // value at 'mid' is greater than w/e is to it's left (array value or -Infinity if index is out of bounds on the left)
+            // if value at 'mid' is greater than value to it's left
             (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
-            // AND value at 'mid' is greater than w/e is to it's right (array value or -Infinity if index is out of bounds on the right)
+            // AND greater than value to it's right
             (nums[mid] > (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
         ) {
+            // this is a peak
             return mid;
-        }
-        else if (nums[mid] < nums[mid + 1]) {
-            // search on a side that is 'greater'
+        } else if (
+            // if value at 'mid' is greater than value to it's left
+            (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
+            // AND smaller than value to it's right
+            (nums[mid] < (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
+        ) {
+            // There is guaranteed to be a peak somwehere on the right
+            candidate = mid + 1;
             left = mid + 1;
-        }
-
-        else if (nums[mid] < nums[mid - 1]) {
-            // search on a side that is 'greater'
+        } else if (
+            // if value at 'mid' is less than value to it's left
+            (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
+            // AND greater than value to it's right
+            (nums[mid] > (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
+        ) {
+            // There is guaranteed to be a peak somwehere on the left
+            candidate = mid - 1;
+            right = mid - 1;
+        } 
+        // value at mid is smaller than valus to it's left and right
+        else {
+            // can go either way?
+            candidate = mid - 1;
             right = mid - 1;
         }
 
     }
+
+    return candidate;
 
 };
