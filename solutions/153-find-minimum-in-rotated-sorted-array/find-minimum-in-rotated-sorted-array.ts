4e1e7e269e21fa0dbf6abd75e10da6deaa2cad7f
Aidar <stormymayday7@gmail.com>
2025-11-17 14:07:04 -0800

    Time: 0 ms (100.00%) | Memory: 55.8 MB (28.33%) - LeetSync

--- solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -2,40 +2,31 @@ function findMin(nums: number[]): number {
 
     let left: number = 0;
     let right: number = nums.length - 1;
-    let candidate: number = 0;
+    let minIdx: number = 0;
 
-    while (left <= right) {
+    while(left <= right) {
 
-        const mid: number = left + Math.floor((right - left) / 2);
-
-        // update candidate
-        if (nums[mid] < nums[candidate]) {
-            candidate = mid;
-        }
-
-        // value at 'mid' is greater than OR equals to value at 'left'
-        if (nums[mid] >= nums[right]) {
-            // discard left?
-            left = mid + 1;
-        }
-        // value at 'mid' is less than value at 'left'
-        else if (nums[mid] < nums[left]) {
-            // discard right
-            right = mid - 1;
-        }
-        // value at 'left' is smaller than value at 'right'
-        // - remaining search space is sorted
-        else if (nums[left] < nums[right]) {
-            // update candidate
-            if (nums[left] < nums[candidate]) {
-                candidate = left;
+        if(nums[left] <= nums[right]) {
+            if(nums[minIdx] > nums[left]) {
+                minIdx = left;
             }
-            // and exit
             break;
+        }
+
+        const mid = left + Math.floor((right - left) / 2);
+
+        minIdx = mid;
+
+        if(nums[mid] > nums[right]) {
+            left = mid + 1;
+        } 
+        // nums[mid] <= nums[right]
+        else {
+            right = mid - 1;
         }
 
     }
 
-    return nums[candidate];
-
+    return nums[minIdx];
+    
 };
