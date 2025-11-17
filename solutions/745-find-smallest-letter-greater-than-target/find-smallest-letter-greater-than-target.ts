ec4f275f8986854840ec581486522e7d7ffcfe02
Aidar <stormymayday7@gmail.com>
2025-11-16 16:08:08 -0800

    Time: 0 ms (100.00%) | Memory: 56.1 MB (70.87%) - LeetSync

--- solutions/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.ts
+++ solutions/745-find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.ts
@@ -2,16 +2,30 @@ function nextGreatestLetter(letters: string[], target: string): string {
 
     const targetCharCode: number = target.charCodeAt(0) - "a".charCodeAt(0);
 
-    for(let i = 0; i < letters.length; i += 1) {
+    let left: number = 0;
+    let right: number = letters.length - 1;
+    let candidate: number = 0;
 
-        let currCharCode: number = letters[i].charCodeAt(0) - "a".charCodeAt(0); 
+    while(left <= right) {
+        
+        const mid: number = left + Math.floor((right - left) / 2);
+        const CharCodeAtMid: number = letters[mid].charCodeAt(0) - "a".charCodeAt(0); 
 
-        if(currCharCode > targetCharCode) {
-            return letters[i];
+        // If charCode at mid is greater than charCode at target
+        if(CharCodeAtMid > targetCharCode) {
+            // Update the candidate
+            candidate = mid;
+            // discard the right side (go left)
+            right = mid - 1;
+        } 
+        // Otherwise, if charCode at mid is less than OR equals to charCOde at target
+        else {
+            // go right
+            left = mid + 1;
         }
 
     }
 
-    return letters[0];
+    return letters[candidate];
     
 };
