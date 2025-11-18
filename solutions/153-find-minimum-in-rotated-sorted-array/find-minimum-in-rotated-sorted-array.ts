62e6c9e40110a6a6c04e78f23e5ca87db9e28356
Aidar <stormymayday7@gmail.com>
2025-11-18 14:47:27 -0800

    Time: 0 ms (100.00%) | Memory: 56.1 MB (11.11%) - LeetSync

--- solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
+++ solutions/153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.ts
@@ -1,44 +1,28 @@
 function findMin(nums: number[]): number {
 
-    let left: number = 0;
-    let right: number = nums.length - 1;
-    let minIdx: number = 0;
-
+    let left = 0;
+    let right = nums.length - 1;
+    let candidate = 0;
     while(left <= right) {
-        
-        // The entire search space is sorted
+
+        const mid = left + Math.floor((right - left)/2);
+
+        // search space is sorted
         if(nums[left] <= nums[right]) {
-            // compare current min to value at 'left' and break
-            if(nums[minIdx] > nums[left]) {
-                minIdx = left;
+            if(nums[left] < nums[candidate]) {
+                candidate = left;
             }
             break;
         }
 
-        const mid = left + Math.floor((right - left) / 2);
-
-        // Min update here is not necessary
-        // minIdx = mid;
-
-        // If value at 'mid' is greater than value at 'right'
-        // Means that the min is somewhere on the right
-        // Example: [ 3 4 5 1 2 ]
-        //            L   M   R
-        // 5 > 2
-        // Additional note: mid cannot be the mid
         if(nums[mid] > nums[right]) {
             left = mid + 1;
-        } 
-        // Otherwise, value at 'mid' is less than OR equal to value at 'right'
-        // Therefore, it is a potential min
-        else {
-            // Since value at 'mid' is a potential min value, record it
-            minIdx = mid;
+        } else {
+            candidate = mid;
             right = mid - 1;
         }
 
     }
-
-    return nums[minIdx];
+    return nums[candidate];
     
 };
