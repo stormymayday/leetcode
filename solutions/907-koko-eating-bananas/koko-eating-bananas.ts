945f86c14249c29aac8a6fe7b1abe21b529b9f2d
Aidar <stormymayday7@gmail.com>
2025-11-17 17:27:51 -0800

    Time: 6 ms (93.96%) | Memory: 59.3 MB (63.12%) - LeetSync

--- solutions/907-koko-eating-bananas/koko-eating-bananas.ts
+++ solutions/907-koko-eating-bananas/koko-eating-bananas.ts
@@ -1,37 +1,32 @@
 function minEatingSpeed(piles: number[], h: number): number {
 
-    // Left (min) must be 1
-    let left = 1;
-    // Right (max) is the largest element
-    let right = Math.max(...piles);
-    // Initialize speed to the max for now
-    let bananasPerHour = right;
+    // let min: number = Math.min(...piles);
+    let min: number = 1;
+    let max: number = Math.max(...piles);
+    let candidate: number = max;
 
-    while(left <= right) {
+    while (min <= max) {
 
-        const middleEatingSpeed = Math.floor((left + right)/2);
-        let currentTotalTime = 0;
+        const mid = min + Math.floor((max - min) / 2);
+        // candidate = mid;
 
-        for(let i = 0; i < piles.length; i++) {
-
-            currentTotalTime = currentTotalTime + Math.ceil(piles[i]/middleEatingSpeed);
-
+        let time: number = 0;
+        for (let i = 0; i < piles.length; i += 1) {
+            time += Math.ceil(piles[i] / mid);
         }
 
-        if(currentTotalTime <= h) {
-            // Current total time is less than max allowed time
-            // Therefore, it's a possible answer
-            bananasPerHour = middleEatingSpeed;
-            // Slow down
-            right = middleEatingSpeed - 1;
-        } else {
-            // Current total time is greater than max allowed time
-            // Speed up
-            left = middleEatingSpeed + 1;
+        // If we went over the time limit
+        if (time > h) {
+            // increase
+            min = mid + 1;
+        } 
+        // we were under or on time
+        else {
+            candidate = mid;
+            max = mid - 1;
         }
-
     }
 
-    return bananasPerHour;
-    
+    return candidate;
+
 };
