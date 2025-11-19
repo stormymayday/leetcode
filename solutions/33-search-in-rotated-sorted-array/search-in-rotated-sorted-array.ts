6e7cfa3388c963fdd0c05941f152d9652304f5a6
Aidar <stormymayday7@gmail.com>
2025-11-18 16:25:49 -0800

    Time: 0 ms (100.00%) | Memory: 55.9 MB (29.11%) - LeetSync

--- solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
+++ solutions/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.ts
@@ -1,56 +1,35 @@
 function search(nums: number[], target: number): number {
-    
-    // Phase 1: find min idx / pivot
-    let minIdx = findMin(nums);
-
-    // Phase 2: figure out which portion the target can be in and run Binary Search on it
     let left = 0;
     let right = nums.length - 1;
-    if(target >= nums[minIdx] && target <= nums[right]) {
-        left = minIdx;
-    } else {
-        right = minIdx - 1;
-    }
     while(left <= right) {
+
         const mid = left + Math.floor((right - left)/2);
+
+        // 1. Check if 'mid' is target
         if(nums[mid] === target) {
             return mid;
-        } else if(nums[mid] > target) {
-            right = mid - 1;
-        } else {
-            left = mid + 1;
         }
+
+        // 2. Identify which half is sorted
+        // If left half is sorted
+        if(nums[left] <= nums[mid]) {
+            if(nums[left] <= target && target <= nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        } 
+        // right half is sorted
+        // 
+        else {
+            if(nums[mid] <= target && target <= nums[right]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+
     }
     return -1;
-
-};
-
-function findMin(nums: number[]): number {
-
-    let left = 0;
-    let right = nums.length - 1;
-    let candidate = 0;
-    while(left <= right) {
-
-        const mid = left + Math.floor((right - left)/2);
-
-        // search space is sorted
-        if(nums[left] <= nums[right]) {
-            if(nums[left] < nums[candidate]) {
-                candidate = left;
-            }
-            break;
-        }
-
-        if(nums[mid] > nums[right]) {
-            left = mid + 1;
-        } else {
-            // this is a candidate for 'min'
-            candidate = mid;
-            right = mid - 1;
-        }
-
-    }
-    return candidate;
-    
 };
