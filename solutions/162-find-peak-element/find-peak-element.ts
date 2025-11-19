94451104b786e95fdd4a34f9374ae86bfcbdefc6
Aidar <stormymayday7@gmail.com>
2025-11-19 15:35:49 -0800

    Time: 0 ms (100.00%) | Memory: 56 MB (13.25%) - LeetSync

--- solutions/162-find-peak-element/find-peak-element.ts
+++ solutions/162-find-peak-element/find-peak-element.ts
@@ -1,56 +1,35 @@
 function findPeakElement(nums: number[]): number {
 
-    if (nums.length === 1 || nums[0] > nums[1]) {
-        return 0;
-    }
+    let left = 0;
+    let right = nums.length - 1;
 
-    if (nums[nums.length - 1] > nums[nums.length - 2]) {
-        return nums.length - 1;
-    }
-
-    let left: number = 0;
-    let right: number = nums.length - 1;
-    let candidate: number = 0;
     while (left <= right) {
 
         const mid = left + Math.floor((right - left) / 2);
 
+        // Exit condition
         if (
-            // if value at 'mid' is greater than value to it's left
-            (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
-            // AND greater than value to it's right
-            (nums[mid] > (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
+            // If mid is greater than w/e to it's left AND right
+            // (out of bounds considered -Infinity)
+            (mid - 1 < 0 || nums[mid] > nums[mid - 1]) &&
+            (mid + 1 === nums.length || nums[mid] > nums[mid + 1])
         ) {
-            // this is a peak
             return mid;
-        } else if (
-            // if value at 'mid' is greater than value to it's left
-            (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
-            // AND smaller than value to it's right
-            (nums[mid] < (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
+        }
+
+        // Always Go to the 'greater' side
+        if (    
+
+            // If to the left of mis is out of bounds AND mid is less than its' right neighbor
+            (mid - 1 < 0 && nums[mid] < nums[mid + 1]) ||
+            // OR mid is greater than it's left neighbor
+            (nums[mid] > nums[mid - 1])
         ) {
-            // There is guaranteed to be a peak somwehere on the right
-            candidate = mid + 1;
+            // go Right
             left = mid + 1;
-        } else if (
-            // if value at 'mid' is less than value to it's left
-            (nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : -Infinity)) &&
-            // AND greater than value to it's right
-            (nums[mid] > (mid + 1 <= nums.length - 1 ? nums[mid + 1] : -Infinity))
-        ) {
-            // There is guaranteed to be a peak somwehere on the left
-            candidate = mid - 1;
-            right = mid - 1;
-        } 
-        // value at mid is smaller than valus to it's left and right
-        else {
-            // can go either way?
-            candidate = mid - 1;
+        } else {
             right = mid - 1;
         }
 
     }
-
-    return candidate;
-
 };
