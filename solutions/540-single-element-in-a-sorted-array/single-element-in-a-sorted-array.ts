85abf65df0f9063210255eee943c94abcab3ebbd
Aidar <stormymayday7@gmail.com>
2025-11-19 10:55:42 -0800

    Time: 0 ms (100.00%) | Memory: 58.8 MB (30.68%) - LeetSync

--- solutions/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.ts
+++ solutions/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.ts
@@ -1,9 +1,9 @@
 function singleNonDuplicate(nums: number[]): number {
 
     // Edge Case: single element
-    if (nums.length === 1) {
-        return nums[0];
-    }
+    // if (nums.length === 1) {
+    //     return nums[0];
+    // }
 
     let left = 0;
     let right = nums.length - 1;
