7378e30d8ac20f1668f07e1e8b8503dbb92a2f48
Aidar <stormymayday7@gmail.com>
2025-11-19 20:45:38 -0800

    Time: 22 ms (14.59%) | Memory: 58.3 MB (11.79%) - LeetSync

--- solutions/69-sqrtx/sqrtx.ts
+++ solutions/69-sqrtx/sqrtx.ts
@@ -1,28 +1,16 @@
 function mySqrt(x: number): number {
 
-    let left: number = 0;
-    let right: number = x;
-    let candidate: number = 0;
+    let candidate = 0;
 
-    while(left <= right) {
+    for(let i = 1; i <= x; i += 1) {
+        
+        const sqrt = i * i;
 
-        const mid = left + Math.floor((right - left) / 2);
-
-        if((mid * mid) > x) {
-            // we went to far
-            // everything to the right of 'mid' will be guaranteed to be too high
-            // therefore, discard right
-            right = mid - 1;
-        } else if((mid * mid) < x) {
-            // Square is below 'x', which means it's a potential candidate
-            candidate = mid;
-            // everything to the left of 'mid' will be guaranteed to be too low
-            // therefore, discard left
-            left = mid + 1;
-        } else {
-            // mid square is an exact match, return immediately
-            return mid;
+        if(sqrt > x) {
+            break;
         }
+
+        candidate = i;
 
     }
 
