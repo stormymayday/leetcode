27aef39e156eeb8bc40cb835134d9cda708bc2df
Aidar <stormymayday7@gmail.com>
2025-11-20 21:33:21 -0800

    Time: 24 ms (32.20%) | Memory: 58.5 MB (77.97%) - LeetSync

---
+++ solutions/1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.ts
@@ -0,0 +1,23 @@
+function smallestDivisor(nums: number[], threshold: number): number {
+
+    let left = 1;
+    let right = Math.max(...nums);
+    let candidate = 1;
+
+    while (left <= right) {
+
+        const mid = left + Math.floor((right - left) / 2);
+
+        const sum = nums.reduce((acc, num) => acc + Math.ceil(num / mid), 0);
+
+        if (sum > threshold) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+
+    }
+
+    return left;
+
+};
