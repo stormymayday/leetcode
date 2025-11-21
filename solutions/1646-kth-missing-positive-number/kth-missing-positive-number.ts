e9572b6d413b96c3458c208a138c783c334620b1
Aidar <stormymayday7@gmail.com>
2025-11-21 13:18:34 -0800

    Time: 0 ms (100.00%) | Memory: 55.4 MB (79.52%) - LeetSync

--- solutions/1646-kth-missing-positive-number/kth-missing-positive-number.ts
+++ solutions/1646-kth-missing-positive-number/kth-missing-positive-number.ts
@@ -1,24 +1,12 @@
 function findKthPositive(arr: number[], k: number): number {
 
-    let missingNums: number[] = [];
-
-    // For O(1) lookup
-    const numsSet = new Set<number>(arr);
-    
-    // Phase 1: find missing numbers from 1 up to maximum number in the array  
-    for(let i = 1; i <= Math.max(...arr); i += 1) {
-        if(!numsSet.has(i)) {
-            missingNums.push(i);
+    for(let i = 0;  i < arr.length; i += 1) {
+        if(arr[i] <= k) {
+            k += 1;
+        } else {
+            break;
         }
     }
 
-    // If number of missing nums is less than k
-    if(missingNums.length < k) {
-        let missingExtra = k - missingNums.length;
-        for(let i = 1; i <= missingExtra; i += 1) {
-            missingNums.push(arr[arr.length - 1] + i);
-        }
-    }
-    
-    return missingNums[k - 1];
+    return k;
 };
