340abc220860c734a6815764eb8e36cdabb31eb2
Aidar <stormymayday7@gmail.com>
2025-11-25 14:47:38 -0800

    Time: 1 ms (99.36%) | Memory: 57.1 MB (99.63%) - LeetSync

--- solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
+++ solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
@@ -1,77 +1,73 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 
-    const combinedLength = nums1.length + nums2.length;
-
-    let midIdx1 = Math.floor(combinedLength / 2) - 1;
-    let midIdx2 = Math.floor(combinedLength / 2);
-
-    let midIdx1Val = -1;
-    let midIdx2Val = -1;
-
-    let p1 = 0;
-    let p2 = 0;
-
-    let count = 0;
-    while (p1 < nums1.length && p2 < nums2.length) {
-
-        // element in nums1 is smaller
-        if (nums1[p1] < nums2[p2]) {
-            if (count === midIdx1) {
-                midIdx1Val = nums1[p1];
-            }
-            if (count === midIdx2) {
-                midIdx2Val = nums1[p1];
-                // can break if midIdx2Val has been assigned?
-                // break;
-            }
-            count += 1;
-            p1 += 1;
-        }
-        // element in nums2 is smaller or they are equal
-        else {
-            if (count === midIdx1) {
-                midIdx1Val = nums2[p2];
-            }
-            if (count === midIdx2) {
-                midIdx2Val = nums2[p2];
-                // can break if midIdx2Val has been assigned?
-                // break;
-            }
-            count += 1;
-            p2 += 1;
-        }
+    // Will want to run Binary Search on a smaller array
+    // Therefore, let's make sure 'nums1' is the smaller array
+    if(nums1.length > nums2.length) {
+        // don't forget the 'return'
+        return findMedianSortedArrays(nums2, nums1);
     }
 
-    while (p1 < nums1.length) {
-        if (count === midIdx1) {
-            midIdx1Val = nums1[p1];
-        }
-        if (count === midIdx2) {
-            midIdx2Val = nums1[p1];
-            // can break if midIdx2Val has been assigned?
-            // break;
-        }
-        count += 1;
-        p1 += 1;
-    }
+    // Now 'nums1' should be smaller
 
-    while (p2 < nums2.length) {
-        if (count === midIdx1) {
-            midIdx1Val = nums2[p2];
-        }
-        if (count === midIdx2) {
-            midIdx2Val = nums2[p2];
-            // can break if midIdx2Val has been assigned?
-            // break;
-        }
-        count += 1;
-        p2 += 1;
-    }
+    const combinedLength: number = nums1.length + nums2.length;
 
-    if (combinedLength % 2 === 0) {
-        return (midIdx1Val + midIdx2Val) / 2;
-    } else {
-        return midIdx2Val;
+    // adding + 1 to keep the formula same for both odd and even lengths
+    // for odd lengths, this will have 1 more element
+    const leftHalfSize = Math.floor((combinedLength + 1) / 2);
+
+    let left: number = 0;
+    let right: number = nums1.length; // not - 1?
+
+    // perhaps this condition does not make sense / not necessary?
+    // What happens when pointers cross?
+    while (left <= right) {
+
+        // middle of the smaller array (nums1)
+        const mid1 = left + Math.floor((right - left) / 2);
+
+        // middle of the larget array (nums2)
+        const mid2 = leftHalfSize - mid1; // not sure about this, seems correct
+
+        // 'l1' is to the left of 'mid1' can be out of bounds
+        const l1 = mid1 - 1 >= 0 ? nums1[mid1 - 1] : -Infinity;
+        const r1 = mid1 < nums1.length ? nums1[mid1] : Infinity;
+        
+        // Apparantly, these can get out of bounds as well
+        const l2 = mid2 - 1 >= 0 ? nums2[mid2 - 1] : -Infinity;
+        const r2 = mid2 < nums2.length ? nums2[mid2] : Infinity;
+
+        // This is the correct partition
+        // Everything on the left is smaller than everything on the right
+        if(
+            l1 <= r2 &&
+            l2 <= r1
+        ) {
+            // If the length is even
+            if(combinedLength % 2 === 0) {
+                // (max between l1 & l2 + min between r1 & r2) / 2
+                return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
+            } 
+            // the length is odd
+            else {
+                // return the max between l1 & l2
+                return Math.max(l1, l2);
+            }
+        } 
+        // Incorrect Parition
+        // l1 > r2
+        else if(l1 > r2) {
+            // we need to 'shift' to the left
+            right = mid1 - 1;
+        }
+        // Incorrect Partition
+        // l2 > r1
+        else if(l2 > r1) {
+            // we need to 'shift' to the right
+            left = mid1 + 1;
+        }
+
     }
 
 };
+
+
