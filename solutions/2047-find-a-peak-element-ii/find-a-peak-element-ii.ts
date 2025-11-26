e579e15c7a3e067368439129e5436cd7dacc3a93
Aidar <stormymayday7@gmail.com>
2025-11-26 15:38:08 -0800

    Time: 0 ms (100.00%) | Memory: 65.3 MB (100.00%) - LeetSync

--- solutions/2047-find-a-peak-element-ii/find-a-peak-element-ii.ts
+++ solutions/2047-find-a-peak-element-ii/find-a-peak-element-ii.ts
@@ -46,38 +46,3 @@ function findPeakGrid(mat: number[][]): number[] {
     return [-1, -1];
 
 };
-
-function isPeak(matrix: number[][], row: number, col: number): [number, number] {
-    // just need left and right
-    const directions: [number, number][] = [
-        [-1, 0], // up
-        [0, 1], // right
-        [1, 0], // down
-        [0, -1], // left
-    ];
-    for (const [rowDelta, colDelta] of directions) {
-
-        const neighborRow = row + rowDelta;
-        const neighborCol = col + colDelta;
-
-        // if neighbor coordiantes are out of bounds, we default to -1
-        let neighborVal = -1;
-        // Otherwise, we use neighbor's value
-        if (
-            // out of bounds check
-            neighborRow >= 0 && neighborRow < matrix.length &&
-            neighborCol >= 0 && neighborCol < matrix[0].length
-        ) {
-            neighborVal = matrix[neighborRow][neighborCol]
-        }
-
-        // value at given coords is smaller than atleast one of it's neighbors
-        if (matrix[row][col] < neighborVal) {
-            // return false; // it's not a peak
-            return [rowDelta, colDelta]
-        }
-    }
-    // If loop finishes, value at given coords is greater than values of all it's neighbors
-    // return true; // it's a peak
-    return [0, 0]; // it's a peak
-}
