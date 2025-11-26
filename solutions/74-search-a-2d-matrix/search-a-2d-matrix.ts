bad0e3d9f45858ad3070261cf450c14d8248f9cf
Aidar <stormymayday7@gmail.com>
2025-11-25 21:40:35 -0800

    Time: 0 ms (100.00%) | Memory: 56.1 MB (15.81%) - LeetSync

--- solutions/74-search-a-2d-matrix/search-a-2d-matrix.ts
+++ solutions/74-search-a-2d-matrix/search-a-2d-matrix.ts
@@ -1,44 +1,25 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
-    
-    // Second binary search: search within the row
+
+    const ROWS = matrix.length;
+    const COLS = matrix[0].length;
+
     let left = 0;
-    let right = matrix.length - 1;
+    let right = ROWS * COLS - 1;
 
     while(left <= right) {
 
         const mid = left + Math.floor((right - left) / 2);
 
-        // Check if target is in this row's range
-        if(matrix[mid][0] <= target && target <= matrix[mid][matrix[0].length - 1]) {
-            
-            // Second binary search: search within the row
-            let l = 0;
-            let r = matrix[0].length - 1;
-
-            while(l <= r) {
-
-                const m = l + Math.floor((r - l) / 2);
-
-                if(matrix[mid][m] === target) {
-                    return true;
-                } else if(matrix[mid][m] > target) {
-                    r = m - 1;
-                } else {
-                    l = m + 1;
-                }
-
-            }
-
-            return false;
-
-        } else if(matrix[mid][0] > target) {
+        if(matrix[Math.floor(mid / COLS)][mid % COLS] === target) {
+            return true;
+        } else if(matrix[Math.floor(mid / COLS)][mid % COLS] > target) {
             right = mid - 1;
-        } else if(target > matrix[mid][matrix[0].length - 1]) {
+        } else {
             left = mid + 1;
         }
 
     }
 
     return false;
-
+    
 };
