5ec91d69cf04276ba9ebe982c7fd34aa0b6e5d49
Aidar <stormymayday7@gmail.com>
2025-11-26 13:29:35 -0800

    Time: 3 ms (0.00%) | Memory: 66.4 MB (66.67%) - LeetSync

--- solutions/2047-find-a-peak-element-ii/find-a-peak-element-ii.ts
+++ solutions/2047-find-a-peak-element-ii/find-a-peak-element-ii.ts
@@ -1,43 +1,18 @@
 function findPeakGrid(mat: number[][]): number[] {
 
+    let max = -1;
+    const res = [-1, -1];
+
     for(let row = 0; row < mat.length; row += 1) {
         for(let col = 0; col < mat[0].length; col += 1) {
-
-            const currVal = mat[row][col];
-
-            const directions = [
-                [-1, 0], // up
-                [0, 1], // right
-                [1, 0], // down
-                [0, -1], // left
-            ];
-
-            let count = 0;
-            for(const [rowDelta, colDelta] of directions) {
-
-                const neighborRow = row + rowDelta;
-                const neighborCol = col + colDelta;
-
-                let neighborVal = -1;
-                if(
-                    neighborRow >= 0 && neighborRow < mat.length &&
-                    neighborCol >= 0 && neighborCol < mat[0].length
-                ) {
-                    neighborVal = mat[neighborRow][neighborCol];
-                }
-
-                if(currVal > neighborVal) {
-                    count += 1;
-                }
-
+            if(mat[row][col] > max) {
+                max = mat[row][col];
+                res[0] = row;
+                res[1] = col;
             }
-            if(count === 4) {
-                return [row, col];
-            }
-
         }
     }
 
-    return [-1, 1];
+    return res;
 
 };
