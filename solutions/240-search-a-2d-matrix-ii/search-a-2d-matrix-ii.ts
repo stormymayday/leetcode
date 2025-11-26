02533016c41e7d085028f5f530ab85e1a9e98153
Aidar <stormymayday7@gmail.com>
2025-11-26 12:03:57 -0800

    Time: 52 ms (30.00%) | Memory: 58.2 MB (85.00%) - LeetSync

--- solutions/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.ts
+++ solutions/240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.ts
@@ -1,19 +1,21 @@
 function searchMatrix(matrix: number[][], target: number): boolean {
 
     // Mid starts in the top right croner
-    let row = 0;
-    let col = matrix[0].length - 1;
+    let row = matrix.length - 1;
+    let col = 0;
 
-    while(row < matrix.length && col >= 0) {
+    while(row >= 0 && col <= matrix[0].length - 1) {
 
         if(matrix[row][col] === target) {
             return true;
         } else if(matrix[row][col] > target) {
-            // need to reduce the col
-            col -= 1;
-        } else {
-            // need to increase the row
-            row += 1;
+            // need to REDUCE the row
+            row -= 1;
+        } 
+        // target is larget than 'mid'
+        else {
+            // need to INCREASE the COL
+            col += 1;
         }
 
     }
