62b562d5a91fb81410ed5ebf00ed95aef39094a0
Aidar <stormymayday7@gmail.com>
2025-11-26 13:57:14 -0800

    Time: 0 ms (100.00%) | Memory: 55.6 MB (47.16%) - LeetSync

--- solutions/162-find-peak-element/find-peak-element.ts
+++ solutions/162-find-peak-element/find-peak-element.ts
@@ -1,35 +1,28 @@
 function findPeakElement(nums: number[]): number {
 
-    // Edge Cases: 1. There is only 1 element, 2. leftmost element is a peak
-    if (nums.length === 1 || nums[0] > nums[1]) {
-        return 0;
-    }
-
-    // 3. rightmost element is a peak
-    if (nums[nums.length - 1] > nums[nums.length - 2]) {
-        return nums.length - 1;
-    }
-
-    // Starting left and right moving inwards by 1
-    let left = 1;
+    let left = 0;
     let right = nums.length - 1;
 
     while(left <= right) {
 
         const mid = left + Math.floor((right - left)/2);
 
-        // Exit Condition: mid is peak
-        if(nums[mid - 1] < nums[mid] && nums[mid] > nums[mid + 1]) {
+        // 'mid' is greater than w/e is to it's 'left' AND 'right'
+        if(
+            ((mid - 1 >= 0 ? nums[mid - 1] : -Infinity) < nums[mid]) &&
+            ((mid + 1 < nums.length ? nums[mid + 1] : -Infinity) < nums[mid])
+        ) {
             return mid;
-        }
-
-        // Otherwise, always go to the 'higher' side
-        if(nums[mid] > nums[mid - 1]) {
-            left = mid + 1;
-        } else {
+        } 
+        // 'left' is greater than mid
+        else if(
+            (mid - 1 >= 0 ? nums[mid - 1] : -Infinity) > nums[mid]
+        ) {
             right = mid - 1;
+        } else {
+            left = mid + 1;
         }
 
     }
-
+    
 };
