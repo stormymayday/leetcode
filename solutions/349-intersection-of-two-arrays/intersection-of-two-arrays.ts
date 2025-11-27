00321351be4e4f93cd1b3d6db40d724f45a4aa79
Aidar <stormymayday7@gmail.com>
2025-11-27 14:36:07 -0800

    Time: 0 ms (100.00%) | Memory: 56.2 MB (69.87%) - LeetSync

--- solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ solutions/349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,13 +1,14 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
 
     const set1 = new Set<number>(nums1);
-    const res = new Set<number>();
+    const res: number[] = [];
 
     for(let i = 0; i < nums2.length; i += 1) {
         if(set1.has(nums2[i])) {
-            res.add(nums2[i]);
+            res.push(nums2[i]);
+            set1.delete(nums2[i]);
         }
     }
     
-    return Array.from(res);
+    return res;
 };
