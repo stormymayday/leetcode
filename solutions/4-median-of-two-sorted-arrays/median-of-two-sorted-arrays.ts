1b89fdbe03fc4a518880e335328b1e929dd4b6df
Aidar <stormymayday7@gmail.com>
2025-11-26 17:31:27 -0800

    Time: 2 ms (88.08%) | Memory: 59.4 MB (68.97%) - LeetSync

--- solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
+++ solutions/4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.ts
@@ -1,53 +1,50 @@
 function findMedianSortedArrays(nums1: number[], nums2: number[]): number {
 
-    // Let's make sure nums1 is the shorter array
     if(nums1.length > nums2.length) {
         return findMedianSortedArrays(nums2, nums1);
     }
 
-    const combinedLength = nums1.length + nums2.length;
-    const leftHalfSize = Math.floor((nums1.length + nums2.length + 1) / 2);
+    const totalLength = nums1.length + nums2.length;
+    const leftHalfSize = Math.floor((totalLength + 1) / 2);
 
     let left = 0;
     let right = nums1.length;
 
+    // guaranteed to find median
     while(left <= right) {
 
-        // middle of nums1
         const mid1 = left + Math.floor((right - left) / 2);
-
-        // middle of nums2
         const mid2 = leftHalfSize - mid1;
 
-        const l1 = mid1 - 1 >= 0 ? nums1[mid1 - 1] : -Infinity;
+        const l1 = mid1 - 1 >= 0 ? nums1[mid1 - 1] : - Infinity;
         const r1 = mid1 < nums1.length ? nums1[mid1] : Infinity;
 
         const l2 = mid2 - 1 >= 0 ? nums2[mid2 - 1] : -Infinity;
-        const r2 = mid2 < nums2.length? nums2[mid2] : Infinity;
+        const r2 = mid2 < nums2.length ? nums2[mid2] : Infinity;
 
-        // Correct partition
+        // correct partition
         if(
             l1 <= r2 &&
             l2 <= r1
         ) {
-             if(combinedLength % 2 === 0) {
+            // if total length is even
+            if(totalLength % 2 === 0) {
                 return (Math.max(l1, l2) + Math.min(r1, r2)) / 2;
-             } else {
+            } else {
                 return Math.max(l1, l2);
-             }
+            }
+        } else if(l1 > r2) {
+            right = mid1 - 1;
         } 
         // 
-        else if(l1 > r2) {
-            // we want to shift mid1 to the left (these are tricky)
-            right = mid1 - 1;
-        } else if(l2 > r1) {
-            // we want to shift mid1 to the right (these are tricky)
+        else if(l2 > r1) {
             left = mid1 + 1;
         }
 
     }
 
-    // not necessary, return should happen in the loop
+    // not necessary
+    // perhaps if arrays are empty?
     return -Infinity;
     
 };
