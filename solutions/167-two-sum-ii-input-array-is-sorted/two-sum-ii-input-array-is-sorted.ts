3676e87213fbeda4ee8ef7aca2bc2ac626bb9f17
Aidar <stormymayday7@gmail.com>
2025-11-27 17:23:55 -0800

    Time: 1 ms (78.57%) | Memory: 56.7 MB (65.46%) - LeetSync

--- solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -3,7 +3,7 @@ function twoSum(numbers: number[], target: number): number[] {
     let left = 0;
     let right = numbers.length - 1;
 
-    while(left <= right) {
+    while(left < right) {
 
         if(numbers[left] + numbers[right] === target) {
             return [left + 1, right + 1];
