6bd8ec215389f52ca2f2140aa1317cb82c118db9
Aidar <stormymayday7@gmail.com>
2025-11-29 12:56:38 -0800

    Time: 16 ms (95.75%) | Memory: 72 MB (70.78%) - LeetSync

--- solutions/219-contains-duplicate-ii/contains-duplicate-ii.ts
+++ solutions/219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -1,25 +1,19 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
-    
+
     const windowSet = new Set<number>();
     let left = 0;
-    for(let right = 0; right < nums.length; right += 1) {
+    for (let right = 0; right < nums.length; right += 1) {
 
-        if(windowSet.size < k + 1) {
-            if(windowSet.has(nums[right])) {
-                return true;
-            } else {
-                windowSet.add(nums[right]);
-            }
-        } else {
+        // window size should not exceed right - left <= k
+        if (right - left > k) {
             windowSet.delete(nums[left]);
             left += 1;
+        }
 
-            if(windowSet.has(nums[right])) {
-                return true;
-            } else {
-                windowSet.add(nums[right]);
-            }
-
+        if (windowSet.has(nums[right])) {
+            return true;
+        } else {
+            windowSet.add(nums[right]);
         }
 
     }
