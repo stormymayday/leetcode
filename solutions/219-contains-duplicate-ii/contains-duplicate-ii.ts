680e50bb33ad4fa061eefad4071530ca9a2da09e
Aidar <stormymayday7@gmail.com>
2025-11-29 13:01:12 -0800

    Time: 22 ms (76.95%) | Memory: 71.4 MB (84.33%) - LeetSync

--- solutions/219-contains-duplicate-ii/contains-duplicate-ii.ts
+++ solutions/219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -5,6 +5,7 @@ function containsNearbyDuplicate(nums: number[], k: number): boolean {
     for (let right = 0; right < nums.length; right += 1) {
 
         // window size should not exceed right - left <= k
+        // in other words, the window size is up to k + 1
         if (right - left > k) {
             windowSet.delete(nums[left]);
             left += 1;
