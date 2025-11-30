e11336b3450f97a40245483e7061d1650c1f86d1
Aidar <stormymayday7@gmail.com>
2025-11-30 12:35:49 -0800

    Time: 4 ms (85.59%) | Memory: 57.8 MB (94.50%) - LeetSync

--- solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,21 +1,36 @@
 function lengthOfLongestSubstring(s: string): number {
 
-    let charSet = new Set<string>();
-    let longest = 0;
+    const charToIdx = new Map<string, number>();
+    let maxLength = 0;
     let left = 0;
     for(let right = 0; right < s.length; right += 1) {
 
-        while(left < s.length && charSet.has(s[right])) {
-            charSet.delete(s[left]);
-            left += 1;
+        const char = s[right];
+        
+        // char is in the map already
+        if(charToIdx.has(char)) {
+            
+            // check if left is smaller than char idx
+            // issue?
+            if(left <= charToIdx.get(char)) {
+                // we need shrink window by moving the 'left' pointer
+                // left = right;
+                left = charToIdx.get(char) + 1;
+            }
+
+            // Update the index of this character
+            charToIdx.set(char, right);
+
+
+        } 
+        // add char to the map
+        else {
+            charToIdx.set(char, right);
         }
 
-        charSet.add(s[right]);
-        // longest = Math.max(longest, charSet.size);
-        longest = Math.max(longest, right - left + 1);
+        maxLength = Math.max(maxLength, right - left + 1);
 
     }
-
-    return longest;
-
+    
+    return maxLength;
 };
