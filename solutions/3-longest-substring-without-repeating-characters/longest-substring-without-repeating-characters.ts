d863296f27637b0876139bf1c145d99e4bee0a43
Aidar <stormymayday7@gmail.com>
2025-11-29 21:29:09 -0800

    Time: 8 ms (48.20%) | Memory: 59.9 MB (59.27%) - LeetSync

--- solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
+++ solutions/3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.ts
@@ -1,25 +1,20 @@
 function lengthOfLongestSubstring(s: string): number {
 
+    let charSet = new Set<string>();
     let longest = 0;
-
-    const currSubstring = new Set<string>();
     let left = 0;
     for(let right = 0; right < s.length; right += 1) {
 
-        const currChar = s[right];
-
-        while(currSubstring.has(currChar)) {
-
-            currSubstring.delete(s[left]);
+        while(left < s.length && charSet.has(s[right])) {
+            charSet.delete(s[left]);
             left += 1;
-
         }
 
-        currSubstring.add(currChar);
-
-        longest = Math.max(longest, currSubstring.size);
+        charSet.add(s[right]);
+        longest = Math.max(longest, charSet.size);
 
     }
-
+    
     return longest;
+
 };
