eec9e45657fb8dabab1e18675fd8cf7283783344
Aidar <stormymayday7@gmail.com>
2025-11-30 22:29:10 -0800

    Time: 32 ms (70.81%) | Memory: 68.8 MB (42.24%) - LeetSync

--- solutions/940-fruit-into-baskets/fruit-into-baskets.ts
+++ solutions/940-fruit-into-baskets/fruit-into-baskets.ts
@@ -1,5 +1,5 @@
 function totalFruit(fruits: number[]): number {
-    
+
     // key is the 'number/fruit'
     // value is the count of that fruit in the current 'window'
     // size of the map should not exceed 2!
@@ -8,34 +8,25 @@ function totalFruit(fruits: number[]): number {
     let longestWindow = 0;
 
     let left = 0;
-    for(let right = 0; right < fruits.length; right += 1) {
+    for (let right = 0; right < fruits.length; right += 1) {
 
-        // 'fruit/key' is not in the map
-        if(!fruitCount.has(fruits[right])) {
+        // always add
+        fruitCount.set(fruits[right], (fruitCount.get(fruits[right]) || 0) + 1);
 
-            // Shrink the window while map size is 2
-            while(fruitCount.size === 2) {
-                // decrement count for the fruit at 'left' pointer
-                fruitCount.set(fruits[left], fruitCount.get(fruits[left]) - 1);
-                if(fruitCount.get(fruits[left]) === 0) {
-                    // if fruit count reaches zero, delete it
-                    fruitCount.delete(fruits[left]);
-                }
-                left += 1; // advance 'left'
+        // Dragging Left
+        if (fruitCount.size > 2) {
+            // decrement count for the fruit at 'left' pointer
+            fruitCount.set(fruits[left], fruitCount.get(fruits[left]) - 1);
+            if (fruitCount.get(fruits[left]) === 0) {
+                // if fruit count reaches zero, delete it
+                fruitCount.delete(fruits[left]);
             }
-
-            // add the fruit with a count of 1
-            fruitCount.set(fruits[right], 1);
-
-        } 
-        // 'fruit/key' is in the map
-        else {
-            // just increment the count
-            fruitCount.set(fruits[right], fruitCount.get(fruits[right]) + 1);
+            left += 1; // advance 'left'
         }
 
+
         // Update max length if the window is valid
-        if(fruitCount.size <= 2) {
+        if (fruitCount.size <= 2) {
             longestWindow = Math.max(longestWindow, right - left + 1);
         }
 
