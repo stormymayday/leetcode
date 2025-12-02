936a02ac84445fdf16ad0dd9a3a7c698ea4d3af9
Aidar <stormymayday7@gmail.com>
2025-12-01 18:54:22 -0800

    Time: 56 ms (18.18%) | Memory: 64.1 MB (6.39%) - LeetSync

--- solutions/424-longest-repeating-character-replacement/longest-repeating-character-replacement.ts
+++ solutions/424-longest-repeating-character-replacement/longest-repeating-character-replacement.ts
@@ -7,25 +7,32 @@ function characterReplacement(s: string, k: number): number {
     let left = 0;
 
     for(let right = 0; right < s.length; right += 1) {
-
+        
+        // add current char into the hash map
         charCount.set(s[right], (charCount.get(s[right]) || 0) + 1);
+        // update max frequency
         maxFrequency = Math.max(maxFrequency, charCount.get(s[right]));
 
+        // shrink while the window is not valid
         if((right - left + 1) - maxFrequency > k) {
-
+            
+            // remove char at 'left'
             charCount.set(s[left], charCount.get(s[left]) - 1);
 
-            // how do we update max frequency here
+            // update max frequency
+            maxFrequency = Math.max(...charCount.values());
 
+            // not necessary but can reduce the number of iterations for
+            // the maxFrequency update above
             if(charCount.get(s[left]) === 0) {
                 charCount.delete(s[left]);
             }
 
-            left += 1;
+            left += 1; // shrink window from 'left'
 
         }
 
-        // this maxFrequency might be invalid
+        // update max if window is valid
         if((right - left + 1) - maxFrequency <= k) {
             longest = Math.max(longest, right - left + 1);
         }
