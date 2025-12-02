cdf5c7a27d2143bfcc6db52bbbb10f149d7706da
Aidar <stormymayday7@gmail.com>
2025-12-02 13:27:06 -0800

    Time: 6 ms (2.49%) | Memory: 61.4 MB (7.37%) - LeetSync

--- solutions/412-fizz-buzz/fizz-buzz.ts
+++ solutions/412-fizz-buzz/fizz-buzz.ts
@@ -6,26 +6,17 @@ function fizzBuzz(n: number): string[] {
     ]);
 
     const res: string[] = [];
-
     for (let i = 1; i <= n; i += 1) {
-
         const ans: string[] = [];
-
         for(const key of fizzBuzzHash.keys()) {
-
             if(i % key === 0) {
                 ans.push(fizzBuzzHash.get(key));
             }
-
         }
-
         if(ans.length === 0) {
             ans.push(`${i}`);
         }
-
         res.push(ans.join(""));
-
     }
-
     return res;
 };
