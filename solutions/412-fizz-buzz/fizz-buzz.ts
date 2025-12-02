536f3d7306544ca0bc384ddd894a35a2c06b2953
Aidar <stormymayday7@gmail.com>
2025-12-02 12:47:16 -0800

    Time: 0 ms (100.00%) | Memory: 58.9 MB (55.64%) - LeetSync

--- solutions/412-fizz-buzz/fizz-buzz.ts
+++ solutions/412-fizz-buzz/fizz-buzz.ts
@@ -1,22 +1,19 @@
 function fizzBuzz(n: number): string[] {
-    const result = [];
+    const res: string[] = [];
 
-    let i = 1;
+    for(let i = 1; i <= n; i += 1) {
 
-    while(i <= n) {
-
-        if(i % 5 === 0 && i % 3 === 0) {
-            result.push("FizzBuzz");
-        } else if(i % 5 === 0) {
-            result.push("Buzz");
+        if(i % 3 === 0 && i % 5 === 0) {
+            res.push("FizzBuzz");
         } else if(i % 3 === 0) {
-            result.push("Fizz");
+            res.push("Fizz");
+        } else if(i % 5 === 0) {
+            res.push("Buzz");
         } else {
-            result.push(`${i}`);
+            res.push(`${i}`);
         }
 
-        i++;
     }
 
-    return result;
+    return res;
 };
