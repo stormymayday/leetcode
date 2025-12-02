95963b848026d55842fd3c725273dc1711af2e84
Aidar <stormymayday7@gmail.com>
2025-12-02 14:13:07 -0800

    Time: 0 ms (100.00%) | Memory: 55.6 MB (42.20%) - LeetSync

---
+++ solutions/1444-number-of-steps-to-reduce-a-number-to-zero/number-of-steps-to-reduce-a-number-to-zero.ts
@@ -0,0 +1,18 @@
+function numberOfSteps(num: number): number {
+    
+    let count = 0;
+
+    while(num > 0) {
+
+        if(num % 2 === 0) {
+            num /= 2;
+        } else {
+            num -= 1;
+        }
+
+        count += 1;
+
+    }
+
+    return count;
+};
