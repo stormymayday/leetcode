083f16d88b33156bea3db0d2df6578768069dbf1
Aidar <stormymayday7@gmail.com>
2025-12-03 15:19:27 -0800

    Time: 3 ms (57.55%) | Memory: 66.9 MB (45.03%) - LeetSync

--- solutions/189-rotate-array/rotate-array.ts
+++ solutions/189-rotate-array/rotate-array.ts
@@ -2,17 +2,22 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-    if (k === nums.length || k === 0) {
+
+    if(k === nums.length || k === 0) {
         return;
     }
 
     k = k % nums.length;
 
-    const slice1 = nums.slice(0, nums.length - k);
-    const slice2 = nums.slice(nums.length - k);
-    const rotated = [...slice2, ...slice1];
+    // k elements from the from the end
+    const slice1 = nums.slice(nums.length - k);
+    // from start up until length - l
+    const slice2 = nums.slice(0, nums.length - k);
 
-    for (let i = 0; i < nums.length; i += 1) {
-        nums[i] = rotated[i];
+    const rightRotated = [...slice1, ...slice2];
+
+    for(let i = 0; i < nums.length; i += 1) {
+        nums[i] = rightRotated[i];
     }
+    
 };
