7de318a255c9ba7cf3d06faa35ff26e739d8d833
Aidar <stormymayday7@gmail.com>
2025-12-03 23:47:43 -0800

    Time: 6 ms (15.32%) | Memory: 55.4 MB (96.10%) - LeetSync

--- solutions/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
+++ solutions/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
@@ -1,40 +1,22 @@
 function intersect(nums1: number[], nums2: number[]): number[] {
-
-    if(nums1.length < nums2.length) {
-        // we want 'nums1' to be bigger
-        // such that 'visited' array would mirror 'nums2'
-        // thus, reducing extra space
-        return intersect(nums2, nums1);
+    
+    const nums1Count = new Map<number, number>();
+    for(let i = 0; i < nums1.length; i += 1) {
+        nums1Count.set(nums1[i], (nums1Count.get(nums1[i]) || 0) + 1);
     }
-
-    nums1.sort((a, b) => a - b);
-    nums2.sort((a, b) => a - b);
-
-    const visited = new Array(nums2.length).fill(0);
 
     const res: number[] = [];
 
-    for(let i = 0; i < nums1.length; i += 1) {
-
-        for(let j = 0; j < nums2.length; j += 1) {
-
-            if(nums1[i] === nums2[j] && visited[j] === 0) {
-                res.push(nums1[i]);
-                visited[j] = 1;
-                break;
+    for(let i = 0; i < nums2.length; i += 1) {
+        if(nums1Count.has(nums2[i])) {
+            res.push(nums2[i]);
+            nums1Count.set(nums2[i], nums1Count.get(nums2[i]) - 1);
+            if(nums1Count.get(nums2[i]) === 0) {
+                nums1Count.delete(nums2[i]);
             }
-
-            // Sorted Arrays Optimization
-            // No point in comparing once value at j becomes greater
-            if(nums2[j] > nums1[i]) {
-                break;
-            }
-
         }
-
     }
 
     return res;
 
-    
 };
