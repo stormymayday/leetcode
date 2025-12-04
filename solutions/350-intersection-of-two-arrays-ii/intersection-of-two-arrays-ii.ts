d0c12c1b31a68c4232366d07f4b63a5376ce6dd2
Aidar <stormymayday7@gmail.com>
2025-12-04 00:31:01 -0800

    Time: 3 ms (45.65%) | Memory: 53.8 MB (99.70%) - LeetSync

--- solutions/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
+++ solutions/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
@@ -1,20 +1,25 @@
 function intersect(nums1: number[], nums2: number[]): number[] {
-    
-    const nums1Count = new Map<number, number>();
-    for(let i = 0; i < nums1.length; i += 1) {
-        nums1Count.set(nums1[i], (nums1Count.get(nums1[i]) || 0) + 1);
-    }
+
+    nums1.sort((a, b) => a - b);
+    nums2.sort((a, b) => a - b);
 
     const res: number[] = [];
 
-    for(let i = 0; i < nums2.length; i += 1) {
-        if(nums1Count.has(nums2[i])) {
-            res.push(nums2[i]);
-            nums1Count.set(nums2[i], nums1Count.get(nums2[i]) - 1);
-            if(nums1Count.get(nums2[i]) === 0) {
-                nums1Count.delete(nums2[i]);
-            }
+    let p1 = 0;
+    let p2 = 0;
+
+    while(p1 < nums1.length && p2 < nums2.length) {
+
+        if(nums1[p1] === nums2[p2]) {
+            res.push(nums1[p1]);
+            p1 += 1;
+            p2 += 1;
+        } else if(nums1[p1] < nums2[p2]) {
+            p1 += 1;
+        } else {
+            p2 += 1;
         }
+
     }
 
     return res;
