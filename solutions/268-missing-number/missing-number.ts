d7c9fed17260412dafa2df87633485dc5f282281
Aidar <stormymayday7@gmail.com>
2025-12-04 09:03:31 -0800

    Time: 7 ms (39.90%) | Memory: 60.2 MB (23.68%) - LeetSync

--- solutions/268-missing-number/missing-number.ts
+++ solutions/268-missing-number/missing-number.ts
@@ -4,13 +4,13 @@ function missingNumber(nums: number[]): number {
 
     let missingNum = -Infinity;
 
-    for(let i = 0; i < nums.length; i += 1) {
+    for(let i = 0; i <= nums.length; i += 1) {
         if(!numSet.has(i)) {
             missingNum = i;
             break;
         }
     }
 
-    return missingNum === -Infinity ? nums.length : missingNum;
+    return missingNum;
 
 };
