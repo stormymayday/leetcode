b2d9db3d900870ac270b94f7786537749228c584
Aidar <stormymayday7@gmail.com>
2025-12-04 09:33:26 -0800

    Time: 1 ms (76.24%) | Memory: 57.9 MB (67.74%) - LeetSync

--- solutions/268-missing-number/missing-number.ts
+++ solutions/268-missing-number/missing-number.ts
@@ -1,9 +1,9 @@
 function missingNumber(nums: number[]): number {
     const n = nums.length;
-    let totalSum: number = n * (n + 1) / 2;
+    let gaussForumla: number = n * (n + 1) / 2;
     let arrSum: number = 0;
     for (let i = 0; i < n; i += 1) {
         arrSum += nums[i];
     }
-    return totalSum - arrSum;
+    return gaussForumla - arrSum;
 };
