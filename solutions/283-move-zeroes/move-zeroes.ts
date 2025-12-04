6affd020b08521c7660c9e68ffc04c212fe9ba57
Aidar <stormymayday7@gmail.com>
2025-12-03 19:25:48 -0800

    Time: 40 ms (9.21%) | Memory: 60.6 MB (84.49%) - LeetSync

--- solutions/283-move-zeroes/move-zeroes.ts
+++ solutions/283-move-zeroes/move-zeroes.ts
@@ -3,15 +3,36 @@
  */
 function moveZeroes(nums: number[]): void {
 
-    const nonZeroes: number[] = [];
-    for(let i = 0; i < nums.length; i += 1) {
-        if(nums[i] !== 0) {
-            nonZeroes.push(nums[i]);
-            nums[i] = 0;
+    if (nums.length === 0 || nums.length === 1) {
+        return;
+    }
+
+    let left = 0;
+    while (left < nums.length) {
+
+        // find 'zero'
+        while (left < nums.length && nums[left] !== 0) {
+            left += 1;
         }
+        // now 'left' should be pointing at 'zero'
+
+        // initialize 'temp' next to 'left' and find a non-zero
+        let temp = left + 1;
+        while (temp < nums.length && nums[temp] === 0) {
+            temp += 1;
+        }
+        // now 'temp' should ge pointing at a 'non-zero'
+
+        // swap values
+        if(left < nums.length && nums[left] === 0 && temp < nums.length && nums[temp] !== 0) {
+            nums[left] = nums[temp];
+            nums[temp] = 0;
+            // left += 1;
+        }
+
+        left += 1;
+
+
     }
-    for(let i = 0; i < nonZeroes.length; i += 1) {
-        nums[i] = nonZeroes[i];
-    }
-    
+
 };
