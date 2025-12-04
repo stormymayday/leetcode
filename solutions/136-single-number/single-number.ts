938bf5b18d1c0bd95bd031a5204523dc2fcd1b0d
Aidar <stormymayday7@gmail.com>
2025-12-04 14:59:14 -0800

    Time: 311 ms (7.07%) | Memory: 55.1 MB (96.58%) - LeetSync

---
+++ solutions/136-single-number/single-number.ts
@@ -0,0 +1,14 @@
+function singleNumber(nums: number[]): number {
+    for (let i = 0; i < nums.length; i += 1) {
+        let count = 0;
+        for (let j = 0; j < nums.length; j += 1) {
+            if (i !== j && nums[i] === nums[j]) {
+                count += 1;
+                break;
+            }
+        }
+        if (count === 0) {
+            return nums[i];
+        }
+    }
+};
