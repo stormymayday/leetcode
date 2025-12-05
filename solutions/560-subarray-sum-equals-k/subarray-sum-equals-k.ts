33a41ecc441127b454552f183430cde04c4ffdf4
Aidar <stormymayday7@gmail.com>
2025-12-04 19:50:22 -0800

    Time: 12 ms (92.45%) | Memory: 64.6 MB (52.68%) - LeetSync

--- solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.ts
+++ solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.ts
@@ -1,23 +1,22 @@
 function subarraySum(nums: number[], k: number): number {
 
     let count = 0;
+    const prefixSumHash = new Map<number, number>(); // key: prefixSum, value: count
+    let prefixSum = 0;
+    for (let i = 0; i < nums.length; i += 1) {
 
-    for(let i = 0; i < nums.length; i += 1) {
+        prefixSum += nums[i];
 
-        let sum = 0;
-
-        for(let j = i; j < nums.length; j += 1) {
-
-            sum += nums[j];
-
-            if(sum === k) {
-                count += 1;
-            }
-
+        if (prefixSum === k) {
+            count += 1;
         }
 
-    }
+        if (prefixSumHash.has(prefixSum - k)) {
+            count += prefixSumHash.get(prefixSum - k);
+        }
 
+        prefixSumHash.set(prefixSum, (prefixSumHash.get(prefixSum) || 0) + 1);
+    }
     return count;
-    
+
 };
