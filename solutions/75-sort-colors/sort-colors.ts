07ad84d59541cc327093c6b340ecf5bcd1dbdfbe
Aidar <stormymayday7@gmail.com>
2025-12-05 12:34:17 -0800

    Time: 0 ms (100.00%) | Memory: 54.2 MB (90.47%) - LeetSync

--- solutions/75-sort-colors/sort-colors.ts
+++ solutions/75-sort-colors/sort-colors.ts
@@ -2,34 +2,43 @@
  Do not return anything, modify nums in-place instead.
  */
 function sortColors(nums: number[]): void {
-    let numZeroes = 0;
-    let numOnes = 0;
-    let numTwos = 0;
-    for(let i = 0; i < nums.length; i += 1) {
-        if(nums[i] === 0) {
-            numZeroes += 1;
+
+    // Phase 1: Move all 2s to the right
+    // - left should be on '2'
+    // - right should be on non '2'
+    let left = 0;
+    for(let right = 0; right < nums.length; right += 1) {
+
+        if(nums[left] === 2 && nums[right] !== 2) {
+            nums[left] = nums[right];
+            nums[right] = 2;
+            left += 1;
         }
-        if(nums[i] === 1) {
-            numOnes += 1;
+
+        if(nums[left] !== 2) {
+            left += 1;
         }
-        if(nums[i] === 2) {
-            numTwos += 1;
+
+    }
+    // Note: 'left' should be on '2' after loop ends
+
+    // Phase 2: Move all 0s to the left (going backwards?)
+    // - left should be on '0' (it's on 2 initilly)
+    // - right should be on non '0'
+    // - Therefore, perhaps we can 'pre' assign 'left' & 'right'?
+    // - 'left' goes 1 step backwards?
+    // - 'right' goes 2 step backwards?
+    left = nums.length - 1;
+    for(let right = nums.length - 1; right >= 0; right -= 1) {
+        if(nums[left] === 0 && nums[right] !== 0) {
+            nums[left] = nums[right];
+            nums[right] = 0;
+            left -= 1;
+        }
+
+        if(nums[left] !== 0) {
+            left -= 1;
         }
     }
-    let idx = 0;
-    while(numZeroes > 0) {
-        nums[idx] = 0;
-        numZeroes -= 1;
-        idx += 1;
-    }
-    while(numOnes > 0) {
-        nums[idx] = 1;
-        numOnes -= 1;
-        idx += 1;
-    }
-    while(numTwos > 0) {
-        nums[idx] = 2;
-        numTwos -= 1;
-        idx += 1;
-    }
+    
 };
