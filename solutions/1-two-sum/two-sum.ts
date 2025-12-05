aa016e56703d615ab74a7fa14faa65f63d748224
Aidar <stormymayday7@gmail.com>
2025-12-04 20:56:52 -0800

    Time: 36 ms (20.30%) | Memory: 56.4 MB (74.36%) - LeetSync

--- solutions/1-two-sum/two-sum.ts
+++ solutions/1-two-sum/two-sum.ts
@@ -1,16 +1,17 @@
 function twoSum(nums: number[], target: number): number[] {
-    
-    const hashMap = new Map();
 
-    for(let i = 0; i < nums.length; i++) {
-        
-        const difference = target - nums[i];
+    const res: number[] = [];
 
-        if(hashMap.has(difference)) {
-            return [i, hashMap.get(difference)];
-        } else {
-            hashMap.set(nums[i], i);
+    for(let i = 0; i < nums.length; i += 1) {
+        for(let j = i + 1; j < nums.length; j += 1) {
+            if(nums[i] + nums[j] === target) {
+                res[0] = i;
+                res[1] = j;
+                break;
+            }
         }
     }
 
+    return res;
+    
 };
