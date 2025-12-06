89f2c8bd8f52a177ec1bf69014cdfb1ed1e7fb54
Aidar <stormymayday7@gmail.com>
2025-12-06 11:44:56 -0800

    Time: 2 ms (71.83%) | Memory: 64.6 MB (54.96%) - LeetSync

--- solutions/53-maximum-subarray/maximum-subarray.ts
+++ solutions/53-maximum-subarray/maximum-subarray.ts
@@ -1,37 +1,15 @@
 function maxSubArray(nums: number[]): number {
-    return findMaxSubArray(nums, 0, nums.length - 1);
-}
 
-function findMaxSubArray(nums: number[], left: number, right: number): number {
-	// Base case: single element
-    if (left === right) return nums[left];
+    let max = -Infinity;
+    let currSum = 0;
 
-    const mid = Math.floor((left + right) / 2);
-    
-    // Recursive cases
-    const leftMax = findMaxSubArray(nums, left, mid);
-    const rightMax = findMaxSubArray(nums, mid + 1, right);
-    const crossMax = maxCrossingSum(nums, left, mid, right);
-
-    return Math.max(leftMax, rightMax, crossMax);
-}
-
-function maxCrossingSum(nums: number[], left: number, mid: number, right: number): number {
-    // Find max sum on left half (including mid)
-    let leftSum = -Infinity, sum = 0;
-    for (let i = mid; i >= left; i--) {
-        sum += nums[i];
-        leftSum = Math.max(leftSum, sum);
+    for (let i = 0; i < nums.length; i += 1) {
+        currSum += nums[i];
+        max = Math.max(max, currSum);
+        if(currSum < 0) {
+            currSum = 0;
+        }
     }
 
-    // Find max sum on right half (excluding mid)
-    let rightSum = -Infinity;
-    sum = 0;
-    for (let i = mid + 1; i <= right; i++) {
-        sum += nums[i];
-        rightSum = Math.max(rightSum, sum);
-    }
-		
-	// Maximum subarray crossing mid
-    return leftSum + rightSum;
-}
+    return max;
+};
