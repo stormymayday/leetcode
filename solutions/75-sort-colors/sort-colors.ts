b41cbbcf56f91588537beeb1d49144556e00ad18
Aidar <stormymayday7@gmail.com>
2025-12-05 17:25:37 -0800

    Time: 0 ms (100.00%) | Memory: 55.7 MB (37.82%) - LeetSync

--- solutions/75-sort-colors/sort-colors.ts
+++ solutions/75-sort-colors/sort-colors.ts
@@ -5,43 +5,31 @@ function sortColors(nums: number[]): void {
 
     const pivot = 1;
 
-    // Forward Pass: Place all items smaller than the pivot
     let left = 0;
-    for (let right = 0; right < nums.length; right += 1) {
-        // Note: value at left must be greater than or EQUAL to pivot!
-        if(nums[left] >= pivot && nums[right] < pivot) {
-            const temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
-        }
-        // We need to store items less than 'pivot' on the left
-        // Therefore, if value at left pointer is smaller than pivot, we can move on (forward)
-        if(nums[left] < pivot) {
+    let mid = 0;
+    let right = nums.length - 1;
+    while (mid <= right) {
+
+        // if (nums[left] >= pivot && nums[mid] < pivot) {
+        if (nums[mid] < pivot) {
+            swap(nums, left, mid);
             left += 1;
-        }
-    }
-
-    // Backwards Pass: Place all items greater than the pivot
-    left = nums.length - 1;
-    for (let right = nums.length - 1; right >= 0; right -= 1) {
-        // Optimization: if the 'right' pointer encounteres value less than pivot
-        // We have entered the 'sorted' portion from Forward Pass
-        // if(nums[right] < pivot) {
-        //     break;
-        // }
-
-        // Note: value at 'left' must be less than or EQUAL to pivot
-        if(nums[left] <= pivot && nums[right] > pivot) {
-            const temp = nums[left];
-            nums[left] = nums[right];
-            nums[right] = temp;
+            mid += 1;
+        // } else if (nums[right] <= pivot && nums[mid] > pivot) {
+        } else if (nums[mid] > pivot) {
+            swap(nums, right, mid);
+            right -= 1;
+            // mid -= 1;
+        } else {
+            mid += 1;
         }
 
-        // If value is greater than pivot
-        // We can move 'left' towards the start
-        if(nums[left] > pivot) {
-            left -= 1;
-        }
     }
 
 };
+
+function swap(arr: number[], i: number, j: number): void {
+    const temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
