2ab4487d3cb475e17e4165491930455822e60c02
Aidar <stormymayday7@gmail.com>
2025-12-05 19:27:47 -0800

    Time: 4 ms (55.73%) | Memory: 58.9 MB (47.71%) - LeetSync

--- solutions/169-majority-element/majority-element.ts
+++ solutions/169-majority-element/majority-element.ts
@@ -1,17 +1,13 @@
 function majorityElement(nums: number[]): number {
 
     const frequencyMap = new Map<number, number>();
-
-    for(let i = 0; i < nums.length; i += 1) {
-        frequencyMap.set(nums[i], (frequencyMap.get(nums[i]) || 0) + 1);
-    }
-
     let maxCount = 0;
     let res = 0;
-    for(const [key, value] of frequencyMap.entries()) {
-        if(value > maxCount) {
-            maxCount = value;
-            res = key;
+    for (let i = 0; i < nums.length; i += 1) {
+        frequencyMap.set(nums[i], (frequencyMap.get(nums[i]) || 0) + 1);
+        if (frequencyMap.get(nums[i]) > maxCount) {
+            maxCount = frequencyMap.get(nums[i]);
+            res = nums[i];
         }
     }
     return res;
