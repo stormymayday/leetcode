57889cf48798a5ce940ba0741fea8bd6a2060068
Aidar <stormymayday7@gmail.com>
2025-12-07 15:38:54 -0800

    Time: 0 ms (100.00%) | Memory: 58.1 MB (32.16%) - LeetSync

--- solutions/31-next-permutation/next-permutation.ts
+++ solutions/31-next-permutation/next-permutation.ts
@@ -5,16 +5,17 @@ function nextPermutation(nums: number[]): void {
 
     const numBackwards: number[] = [];
 
-    // 1. Going backwards, copy values until a greater value is found
+    // 1. Going backwards, copy values into 'numBackwards' until a greater value is found
     let idx = nums.length - 1;
     outer: while (idx >= 0) {
 
         const currVal = nums[idx];
 
+        // 1.1. Linear scan 'numBackwards' if there is value greater than 'currVal'
         for (let i = 0; i < numBackwards.length; i += 1) {
             // Found greater!
             if (currVal < numBackwards[i]) {
-                // swap with smallest number that is greater than currVal and break
+                // swap and break
                 nums[idx] = numBackwards[i];
                 numBackwards[i] = currVal;
                 break outer;
@@ -22,7 +23,7 @@ function nextPermutation(nums: number[]): void {
 
         }
 
-        // If there is no greater value, copy 'currVal'
+        // If there is no greater value, copy 'currVal' over to 'numBackwards'
         numBackwards.push(currVal);
 
         // Move to next iteration
@@ -30,14 +31,18 @@ function nextPermutation(nums: number[]): void {
 
     }
 
-    // 2. Fill with sorted
-    if(idx < 0) {
+    // 2. Overwrite 'nums' from 'idx + 1' with sorted 'numBackwards'
+    numBackwards.sort((a, b) => a - b);
+    // 2.1 If 'idx' went out of bounds
+    if (idx < 0) {
+        // reset it
         idx = 0;
     } else {
+        // otherwise, move it forward one spot from the 'swap' position
         idx += 1;
     }
-    numBackwards.sort((a, b) => a - b);
-    for(let i = 0; i < numBackwards.length; i += 1) {
+    // 2.2. Overwriting rest of the 'nums' using sorted 'numBackwards' values
+    for (let i = 0; i < numBackwards.length; i += 1) {
         nums[idx] = numBackwards[i];
         idx += 1;
     }
