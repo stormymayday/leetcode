4374b19f2f0c5b5aa5f642bbc7f5524a53af238e
Aidar <stormymayday7@gmail.com>
2025-12-06 18:57:05 -0800

    Time: 1 ms (87.09%) | Memory: 65.5 MB (28.98%) - LeetSync

--- solutions/53-maximum-subarray/maximum-subarray.ts
+++ solutions/53-maximum-subarray/maximum-subarray.ts
@@ -2,12 +2,14 @@ function maxSubArray(nums: number[]): number {
 
     let max = -Infinity;
     let currSum = 0;
-    for(let i = 0; i < nums.length; i += 1) {
-        // Reset currSum if it goes below zero
-        currSum = Math.max(currSum, 0);
+
+    for (let i = 0; i < nums.length; i += 1) {
         currSum += nums[i];
         max = Math.max(max, currSum);
+        if(currSum < 0) {
+            currSum = 0;
+        }
     }
+
     return max;
-    
 };
