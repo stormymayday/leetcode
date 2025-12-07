6e1dcd85d4856c8e5a78fa9724fd2bfac10bf229
Aidar <stormymayday7@gmail.com>
2025-12-06 23:21:04 -0800

    Time: 13 ms (78.95%) | Memory: 100.1 MB (31.58%) - LeetSync

--- solutions/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.ts
+++ solutions/2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.ts
@@ -8,18 +8,12 @@ function rearrangeArray(nums: number[]): number[] {
     for (let i = 0; i < nums.length; i += 1) {
         if (nums[i] < 0) {
             res[p2] = nums[i];
-            // p2 = p2 * 2 + 1;
             p2 += 2;
         } else {
             res[p1] = nums[i];
-            // if(p1 === 0) {
-            //     p1 = 2;
-            // } else {
-                // p1 = p1 * 2;
-                p1 += 2;
-            // }   
+            p1 += 2;
         }
     }
-    
+
     return res;
 };
