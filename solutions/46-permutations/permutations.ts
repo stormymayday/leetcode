44743ca67635c45393b877fce7f101f8f3f1580e
Aidar <stormymayday7@gmail.com>
2025-12-07 12:52:16 -0800

    Time: 2 ms (68.09%) | Memory: 60.1 MB (73.86%) - LeetSync

--- solutions/46-permutations/permutations.ts
+++ solutions/46-permutations/permutations.ts
@@ -1,22 +1,32 @@
 function permute(nums: number[]): number[][] {
+    const n: number = nums.length;
+    const picked: boolean[] = new Array(n).fill(false);
     const res: number[][] = [];
-    const perm: number[] = [];
-    const picked: boolean[] = new Array(nums.length).fill(false);
+    const curr: number[] = [];
     function helper() {
-        if(perm.length === nums.length) {
-            res.push([...perm]);
+        if(curr.length === n) {
+            res.push([...curr]); // O(n) for copying curr
             return;
         }
         for(let i = 0; i < nums.length; i += 1) {
-            if(picked[i] == false) {
+            // If current value was not pickeded
+            if(picked[i] === false) {
+                // choose current
                 picked[i] = true;
-                perm.push(nums[i]);
+                curr.push(nums[i]);
+
+                // explore with current
                 helper();
+
+                // backtrack
                 picked[i] = false;
-                perm.pop();
+                curr.pop();
             }
         }
     }
     helper();
     return res;
+    // Time: O(n x n!) - n for copying curr and n! for generating permutations
+    // Space: O(n + n!) - n for recursion stack and n! for storing permutations
+    // where n is the length of nums
 };
