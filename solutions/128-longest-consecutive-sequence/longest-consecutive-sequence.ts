b454404a90e5ff05cd8606afc9a7188762e2e344
Aidar <stormymayday7@gmail.com>
2025-12-07 18:17:03 -0800

    Time: 38 ms (69.01%) | Memory: 69.3 MB (90.59%) - LeetSync

--- solutions/128-longest-consecutive-sequence/longest-consecutive-sequence.ts
+++ solutions/128-longest-consecutive-sequence/longest-consecutive-sequence.ts
@@ -1,59 +1,31 @@
 function longestConsecutive(nums: number[]): number {
-    const uf = new UnionFind(nums);
-    const numSet = new Set(nums);
-    for(let i = 0; i < nums.length; i += 1) {
-        const num = nums[i];
-        if(numSet.has(num + 1)) {
-            uf.union(num, num + 1);
-        }
-    }
-    return uf.getLargestComponentSize();
-};
 
-class UnionFind {
-    private roots: Map<number, number>;
-    private sizes: Map<number, number>;
-    constructor(nums: number[]) {
-        this.roots = new Map();
-        this.sizes = new Map();
-        for(let i = 0; i < nums.length; i += 1) {
-            const num = nums[i];
-            this.roots.set(num, num);
-            this.sizes.set(num, 1);
-        }
+    if(nums.length === 0) {
+        return 0;
     }
-    find(x: number):number {
-        const root = this.roots.get(x);
-        if(root !== x) {
-            this.roots.set(x, this.find(root));
+
+    const sortedNums = [...nums].sort((a, b) => a - b);
+
+    let maxStreak = 1;
+    let currStreak = 1;
+
+    for (let i = 0; i < sortedNums.length - 1; i += 1) {
+
+        // if current + 1 equals to next element
+        if (sortedNums[i] + 1 === sortedNums[i + 1]) {
+            currStreak += 1;
+            maxStreak = Math.max(maxStreak, currStreak);
         }
-        return this.roots.get(x);
-    }
-    union(x: number, y: number):boolean {
-        const rootX = this.find(x);
-        const rootY = this.find(y);
-        if(rootX === rootY) {
-            return false;
-        } else {
-            if(this.sizes.get(rootX) >= this.sizes.get(rootY)) {
-                this.roots.set(rootY, rootX);
-                this.sizes.set(rootX, this.sizes.get(rootX) + this.sizes.get(rootY)); 
-            } else {
-                this.roots.set(rootX, rootY);
-                this.sizes.set(rootY, this.sizes.get(rootY) + this.sizes.get(rootX));
-            }
-            return true;
+        else if (sortedNums[i] === sortedNums[i + 1]) {
+            continue;
         }
-    }
-    getLargestComponentSize():number {
-        let largest = 0;
-        // Only check nodes that are actually roots
-        for (const [node, root] of this.roots.entries()) {
-            if (node === root) {
-                const size = this.sizes.get(node) || 0;
-                largest = Math.max(largest, size);
-            }
+        else {
+            currStreak = 1;
         }
-        return largest;
+
     }
-}
+
+    return maxStreak;
+
+
+};
