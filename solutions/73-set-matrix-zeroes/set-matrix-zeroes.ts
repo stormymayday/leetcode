ab5fed10635bf4f43721010f0709e5587af7848c
Aidar <stormymayday7@gmail.com>
2025-12-07 23:51:08 -0800

    Time: 2 ms (82.62%) | Memory: 60.3 MB (27.66%) - LeetSync

---
+++ solutions/73-set-matrix-zeroes/set-matrix-zeroes.ts
@@ -0,0 +1,33 @@
+/**
+ Do not return anything, modify matrix in-place instead.
+ */
+function setZeroes(matrix: number[][]): void {
+
+    const ROWS = matrix.length;
+    const COLS = matrix[0].length;
+
+    const rowSet = new Set<number>();
+    const colSet = new Set<number>();
+
+    for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            if (matrix[row][col] === 0) {
+                rowSet.add(row);
+                colSet.add(col);
+            }
+        }
+    }
+
+    for (const row of rowSet) {
+        for (let col = 0; col < COLS; col += 1) {
+            matrix[row][col] = 0;
+        }
+    }
+
+    for (const col of colSet) {
+        for (let row = 0; row < ROWS; row += 1) {
+            matrix[row][col] = 0;
+        }
+    }
+
+};
