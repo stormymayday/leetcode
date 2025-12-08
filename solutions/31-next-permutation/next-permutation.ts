f604cdd9d3a8d47b39d2f950712811ffa33a6a57
Aidar <stormymayday7@gmail.com>
2025-12-07 16:47:10 -0800

    Time: 0 ms (100.00%) | Memory: 56.4 MB (90.20%) - LeetSync

--- solutions/31-next-permutation/next-permutation.ts
+++ solutions/31-next-permutation/next-permutation.ts
@@ -2,45 +2,48 @@
  Do not return anything, modify nums in-place instead.
  */
 function nextPermutation(nums: number[]): void {
+    
+    let swapIdx = -1;
 
-    const numBackwards: number[] = [];
-
-    // 1. Going backwards, copy values into 'numBackwards' until a greater value is found
-    let idx = nums.length - 1;
-    outer: while (idx >= 0) {
-
-        const currVal = nums[idx];
-
-        // Check numBackwards has value greater than currVal
-        // We can check top in O(1) Time
-        if (currVal < numBackwards[numBackwards.length - 1]) {
-            // If there is a greater value, linear scan 'numBackwards' to find smallest element greater than currVal
-            for (let i = 0; i < numBackwards.length; i += 1) {
-                // Found greater!
-                if (currVal < numBackwards[i]) {
-                    // swap and break
-                    nums[idx] = numBackwards[i];
-                    numBackwards[i] = currVal;
-                    break outer;
-                }
-
-            }
+    // 1. Going backwards, looking a smaller element the left of curr
+    for(let i = nums.length - 1; i >= 1; i -= 1) {
+        if(nums[i] > nums[i - 1]) {
+            // this will be swap index 
+            swapIdx = i - 1;
+            break;
         }
-
-        // If there is no greater value, copy 'currVal' over to 'numBackwards'
-        numBackwards.push(currVal);
-
-        // Move to next iteration
-        idx -= 1;
 
     }
 
-    // 2. Overwrite 'nums' from 'idx + 1' with 'numBackwards'
-    // Note: no need to sort / reverse 'numBackwards'
-    idx += 1;
-    for (let i = 0; i < numBackwards.length; i += 1) {
-        nums[idx] = numBackwards[i];
-        idx += 1;
+    // Edge Case: if array was strictly in descending order
+    if(swapIdx === - 1) {
+        // reverse the whole array
+        nums.reverse();
+        return;
+    }
+
+    // Otherwise, we found a swap index
+
+    // 2. Need to find the smallest element greater than one at swap index and swap
+    // We can do it going backwards again
+    for(let i = nums.length - 1; i > swapIdx; i -= 1) {
+        if(nums[i] > nums[swapIdx]) {
+            const temp = nums[i];
+            nums[i] = nums[swapIdx];
+            nums[swapIdx] = temp;
+            break;
+        }
+    }
+
+    // 3. Reverse the array after the swap index
+    let left = swapIdx + 1;
+    let right = nums.length - 1;
+    while(left < right) {
+        const temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+        left += 1;
+        right -= 1;
     }
 
 };
