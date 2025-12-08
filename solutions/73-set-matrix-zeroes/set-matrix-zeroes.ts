837259caf2b9e2a36ea51a46daefd6e3a712df62
Aidar <stormymayday7@gmail.com>
2025-12-08 12:19:26 -0800

    Time: 2 ms (82.48%) | Memory: 59.4 MB (85.90%) - LeetSync

--- solutions/73-set-matrix-zeroes/set-matrix-zeroes.ts
+++ solutions/73-set-matrix-zeroes/set-matrix-zeroes.ts
@@ -6,24 +6,36 @@ function setZeroes(matrix: number[][]): void {
     const ROWS = matrix.length;
     const COLS = matrix[0].length;
 
-    const hashRows = new Array(ROWS).fill(1);
-    const hashCols = new Array(COLS).fill(1);
+    const rowSet = new Set<number>();
+    const colSet = new Set<number>();
 
-    for(let row = 0; row < ROWS; row += 1) {
-        for(let col = 0; col < COLS; col += 1) {
-            if(matrix[row][col] === 0) {
-                hashRows[row] = 0;
-                hashCols[col] = 0;
+    for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            if (matrix[row][col] === 0) {
+                rowSet.add(row);
+                colSet.add(col);
             }
         }
     }
 
-    for(let row = 0; row < ROWS; row += 1) {
-        for(let col = 0; col < COLS; col += 1) {
-            if(hashRows[row] === 0 || hashCols[col] === 0) {
-                matrix[row][col] = 0
+    // for (const row of rowSet) {
+    //     for (let col = 0; col < COLS; col += 1) {
+    //         matrix[row][col] = 0;
+    //     }
+    // }
+
+    // for (const col of colSet) {
+    //     for (let row = 0; row < ROWS; row += 1) {
+    //         matrix[row][col] = 0;
+    //     }
+    // }
+
+    for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            if (rowSet.has(row) || colSet.has(col)) {
+                matrix[row][col] = 0;
             }
         }
     }
-    
+
 };
