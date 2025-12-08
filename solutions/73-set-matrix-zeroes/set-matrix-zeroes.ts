0cf0b41f94941b577935059b5461865a8b87bfa0
Aidar <stormymayday7@gmail.com>
2025-12-08 13:15:43 -0800

    Time: 1 ms (97.33%) | Memory: 59.9 MB (51.81%) - LeetSync

--- solutions/73-set-matrix-zeroes/set-matrix-zeroes.ts
+++ solutions/73-set-matrix-zeroes/set-matrix-zeroes.ts
@@ -6,34 +6,57 @@ function setZeroes(matrix: number[][]): void {
     const ROWS = matrix.length;
     const COLS = matrix[0].length;
 
-    const hashRows = new Array(ROWS).fill(1);
-    const hashCols = new Array(COLS).fill(1);
+    // to mark columns (first column)
+    let firstRow = 1;
+    let firstCol = 1;
 
-    for(let row = 0; row < ROWS; row += 1) {
+    for (let row = 0; row < ROWS; row += 1) {
+        for (let col = 0; col < COLS; col += 1) {
+            if (matrix[row][col] === 0) {
+
+                // Top Left Edge Case
+                if (col === 0) {
+                    // Entire first column will have to be overwritten
+                    firstCol = 0;
+                } else {
+                    // Using First Row to mark the columns
+                    matrix[0][col] = 0;
+                }
+
+                if (row === 0) {
+                    firstRow = 0;
+                } else {
+                    // Using First Column to mark the rows
+                    matrix[row][0] = 0;
+                }
+
+            }
+        }
+    }
+
+    // Now First Row & Col are "technically" marked / overwritten
+    // Note: moving top down can overwrite "markings"
+
+    // Skipping first row & col
+    for (let row = 1; row < ROWS; row += 1) {
+        for (let col = 1; col < COLS; col += 1) {
+            if (matrix[0][col] === 0 || matrix[row][0] === 0) {
+                matrix[row][col] = 0;
+            }
+        }
+    }
+
+
+    if(firstRow === 0) {
         for(let col = 0; col < COLS; col += 1) {
-            if(matrix[row][col] === 0) {
-                hashRows[row] = 0;
-                hashCols[col] = 0;
-            }
+            matrix[0][col] = 0;
         }
     }
 
-    // Iterating over 'hashRows'
-    for(let row = 0; row < ROWS; row += 1) {
-        if(hashRows[row] === 0) {
-            for(let col = 0; col < COLS; col += 1) {
-                matrix[row][col] = 0;
-            }
+    if(firstCol === 0) {
+        for(let row = 0; row < ROWS; row += 1) {
+            matrix[row][0] = 0;
         }
     }
 
-    // Iterating over 'hashCols'
-    for(let col = 0; col < COLS; col += 1) {
-        if(hashCols[col] === 0) {
-            for(let row = 0; row < ROWS; row += 1) {
-                matrix[row][col] = 0;
-            }
-        }
-    }
-    
 };
