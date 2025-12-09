50f0a9f4786781f3b80418baaa1f5d50115e42a5
Aidar <stormymayday7@gmail.com>
2025-12-09 12:55:12 -0800

    Time: 18 ms (60.77%) | Memory: 63.8 MB (73.74%) - LeetSync

--- solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.ts
+++ solutions/560-subarray-sum-equals-k/subarray-sum-equals-k.ts
@@ -1,15 +1,25 @@
 function subarraySum(nums: number[], k: number): number {
-    
-    let count = 0;
 
-    for(let i = 0; i < nums.length; i += 1) {
-        let sum = 0;
-        for(let j = i; j < nums.length; j += 1) {
-            sum += nums[j];
-            if(sum === k) {
-                count += 1;
-            }
+    let count = 0;
+    const prefixSumCount = new Map<number, number>(
+        // [
+        //     [0, 1]
+        // ]
+    );
+    let prefixSum = 0;
+    for (let i = 0; i < nums.length; i += 1) {
+
+        prefixSum += nums[i];
+        
+        if(prefixSum === k) {
+            count += 1;
         }
+
+        if(prefixSumCount.has(prefixSum - k)) {
+            count += prefixSumCount.get(prefixSum - k);
+        }
+
+        prefixSumCount.set(prefixSum, (prefixSumCount.get(prefixSum) || 0) + 1)
     }
 
     return count;
