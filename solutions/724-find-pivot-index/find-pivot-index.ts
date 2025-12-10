4be046e4888ce0c158f0402c345e50c0917bc5f8
Aidar <stormymayday7@gmail.com>
2025-12-10 12:44:46 -0800

    Time: 1 ms (89.16%) | Memory: 55.6 MB (100.00%) - LeetSync

--- solutions/724-find-pivot-index/find-pivot-index.ts
+++ solutions/724-find-pivot-index/find-pivot-index.ts
@@ -2,11 +2,12 @@ function pivotIndex(nums: number[]): number {
     
     let totalSum = nums.reduce((acc, curr) => acc + curr, 0);
 
+    // leftSum is essentially a prefix sum starting from 0
     let leftSum = 0;
     for(let i = 0; i < nums.length; i += 1) {
 
         const curr = nums[i];
-
+        
         const rightSum = totalSum - leftSum - curr;
 
         if(rightSum === leftSum) {
