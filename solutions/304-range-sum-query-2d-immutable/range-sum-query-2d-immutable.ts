e9638a552afebcf8d2a72b612ebb982e5fcf01d7
Aidar <stormymayday7@gmail.com>
2025-12-09 22:22:38 -0800

    Time: 59 ms (50.00%) | Memory: 83.6 MB (18.63%) - LeetSync

--- solutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.ts
+++ solutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.ts
@@ -1,42 +1,63 @@
 class NumMatrix {
-    prefixSums1D: number[][];
+
+    prefixSums2D: number[][];
+
     constructor(matrix: number[][]) {
-        this.prefixSums1D = new Array(matrix.length);
-        let prefixSum = 0;
-        for(let row = 0; row < matrix.length; row += 1) {
-            this.prefixSums1D[row] = new Array(matrix[0].length);
-            for(let col = 0; col < matrix[0].length; col += 1) {
-                prefixSum += matrix[row][col];
-                this.prefixSums1D[row][col] = prefixSum;
+
+        this.prefixSums2D = new Array(matrix.length);
+
+        for (let row = 0; row < matrix.length; row += 1) {
+
+            this.prefixSums2D[row] = new Array(matrix[0].length);
+
+            for (let col = 0; col < matrix[0].length; col += 1) {
+
+                // 1. Use the original value
+                this.prefixSums2D[row][col] = matrix[row][col];
+
+                // 2. If there is a row above, add value of the cell above
+                if (row > 0) {
+                    this.prefixSums2D[row][col] += this.prefixSums2D[row - 1][col];
+                }
+
+                // 3. If there is a col to the left, add value of the cell to the left
+                if (col > 0) {
+                    this.prefixSums2D[row][col] += this.prefixSums2D[row][col - 1];
+                }
+
+                // 4. If there is a row and col above
+                if (col > 0 && row > 0) {
+                    // Subtract the overlapping top-left region that was counted twice
+                    this.prefixSums2D[row][col] -= this.prefixSums2D[row - 1][col - 1];
+                }
+
             }
+
         }
+
     }
 
     sumRegion(row1: number, col1: number, row2: number, col2: number): number {
-        
-        let sum = 0;
 
-        for(let row = row1; row <= row2; row += 1) {
-            
-            const rightSum = this.prefixSums1D[row][col2];
-            let leftSum = 0;
-            // Edge Case 1: The sub-matrix starts at first row and first col
-            if(row === 0 && col1 === 0) {
-                leftSum = 0;
-            } 
-            // Edge Case2 : col1 is 0
-            // Then the 'leftSum' will 'wrap around' to the last column one row above
-            else if(col1 === 0) {
-                leftSum = this.prefixSums1D[row - 1][this.prefixSums1D[0].length - 1];
-            } else {
-                leftSum = this.prefixSums1D[row][col1 - 1];
-            }
-
-            sum += rightSum - leftSum;
-            
+        // Edge Case 1: There is no row above and no column to the left
+        if (row1 === 0 && col1 === 0) {
+            return this.prefixSums2D[row2][col2];
         }
-
-        return sum;
+        // Edge Case 2: There is no row above
+        else if (row1 === 0) {
+            return this.prefixSums2D[row2][col2] - this.prefixSums2D[row2][col1 - 1];
+        }
+        // Edge Case 3: There is no col to the left
+        else if (col1 === 0) {
+            return this.prefixSums2D[row2][col2] - this.prefixSums2D[row1 - 1][col2];
+        }
+        // There is a row above and col to the left
+        else {
+            return this.prefixSums2D[row2][col2] -
+                this.prefixSums2D[row1 - 1][col2] - 
+                this.prefixSums2D[row2][col1 - 1] +
+                this.prefixSums2D[row1 - 1][col1 - 1];
+        }
 
     }
 }
