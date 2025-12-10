c693cada16c38cea1304010e72520bde40942a74
Aidar <stormymayday7@gmail.com>
2025-12-10 14:05:43 -0800

    Time: 12 ms (34.17%) | Memory: 73.2 MB (18.71%) - LeetSync

--- solutions/238-product-of-array-except-self/product-of-array-except-self.ts
+++ solutions/238-product-of-array-except-self/product-of-array-except-self.ts
@@ -1,18 +1,27 @@
 function productExceptSelf(nums: number[]): number[] {
+
     const n = nums.length;
-    const result = new Array(n).fill(1);
 
-    let prefix = 1;
+    const prefixProducts: number[] = new Array(n);
+    let prefixProduct = 1;
     for(let i = 0; i < n; i += 1) {
-        result[i] = prefix;
-        prefix *= nums[i];
+        prefixProduct *= nums[i];
+        prefixProducts[i] = prefixProduct;
     }
 
-    let postfix = 1;
+    const postfixProducts: number[] = new Array(n);
+    let postfixProduct = 1;
     for(let i = n - 1; i >= 0; i -= 1) {
-        result[i] *= postfix;
-        postfix *= nums[i];
+        postfixProduct *= nums[i];
+        postfixProducts[i] = postfixProduct;
     }
 
-    return result;
+    const res: number[] = new Array(n);
+    for(let i = 0; i < res.length; i += 1) {
+        const prefix = i - 1 < 0 ? 1 : prefixProducts[i - 1];
+        const postfix = i + 1 < n ? postfixProducts[i + 1] : 1; 
+        res[i] = prefix * postfix;
+    }
+    return res;
+    
 };
