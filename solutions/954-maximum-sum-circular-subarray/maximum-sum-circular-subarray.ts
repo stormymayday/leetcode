375ece303ff6b9db6628ee54966eb5b9ee0c5610
Aidar <stormymayday7@gmail.com>
2025-12-11 15:15:04 -0800

    Time: 1 ms (96.67%) | Memory: 59.5 MB (96.67%) - LeetSync

--- solutions/954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.ts
+++ solutions/954-maximum-sum-circular-subarray/maximum-sum-circular-subarray.ts
@@ -1,36 +1,33 @@
 function maxSubarraySumCircular(nums: number[]): number {
 
     const n = nums.length;
+    
+    let maxSum = -Infinity;
+    let currSum = 0;
 
-    // Phase 1: fill out the rightMax
-    const rightMax = new Array(n).fill(0);
-    rightMax[n - 1] = nums[n - 1];
-    let postfixSum = nums[n - 1];
-    for(let i = n - 2; i >= 0; i -= 1) {
-        postfixSum += nums[i];
-        rightMax[i] = Math.max(rightMax[i + 1], postfixSum);
-    }
+    let total = 0;
+    let minSum = Infinity;
+    let currMin = 0;
 
-    // Phase 2: Prefix Sum + rightMax[i + 1] vs Kadane's Maximum Subarray
-    let maxSum = -Infinity;  // for Kadane's
-    let currSum = 0; // for Kadane's
-    let prefixSum = 0;
-    for(let i = 0; i < nums.length; i += 1) {
+    for(let i = 0; i < n; i += 1) {
 
-        // Kadane's
         currSum += nums[i];
         maxSum = Math.max(maxSum, currSum);
         if(currSum < 0) {
             currSum = 0;
         }
 
-        // Non-overlapping prefixSum and rightMax
-        prefixSum += nums[i];
-        if(i + 1 < n) {
-            maxSum = Math.max(maxSum, prefixSum + rightMax[i + 1]);
+        currMin += nums[i];
+        minSum = Math.min(minSum, currMin);
+        if(currMin > 0) {
+            currMin = 0;
         }
+
+        total += nums[i];
 
     }
 
-    return maxSum;
+    // Edge Case: all negative numbers
+    return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+
 };
