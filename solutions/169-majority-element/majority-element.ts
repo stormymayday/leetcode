d5173113161e3788a49387b5d5851d8b7514565c
Aidar <stormymayday7@gmail.com>
2025-12-11 18:49:54 -0800

    Time: 1 ms (88.65%) | Memory: 57.3 MB (88.13%) - LeetSync

--- solutions/169-majority-element/majority-element.ts
+++ solutions/169-majority-element/majority-element.ts
@@ -1,28 +1,18 @@
 function majorityElement(nums: number[]): number {
     
-    const n = nums.length;
+    let majorityEl = nums[0];
     let count = 1;
-    let majEl = nums[0];
-
-    for(let i = 1; i < n; i += 1) {
-        if(nums[i] === majEl) {
-            count += 1;
-        } else {
+    for(let i = 1; i < nums.length; i += 1) {
+        if(nums[i] !== majorityEl) {
             count -= 1;
             if(count < 0) {
-                majEl = nums[i];
+                majorityEl = nums[i];
                 count = 1;
             }
-        }
-    }
-
-    // Optional: Verification Step incase there might not be a majority element
-    count = 0;
-    for(let i = 0; i < n; i += 1) {
-        if(nums[i] === majEl) {
+        } else {
             count += 1;
         }
     }
-    return count > Math.floor(n / 2) ? majEl : -1;
+    return majorityEl;
 
 };
