ef51ade2c81d27001fdeef57bf95b1cc75bec4d6
Aidar <stormymayday7@gmail.com>
2025-12-11 19:18:55 -0800

    Time: 8 ms (44.62%) | Memory: 62 MB (42.31%) - LeetSync

--- solutions/229-majority-element-ii/majority-element-ii.ts
+++ solutions/229-majority-element-ii/majority-element-ii.ts
@@ -2,6 +2,8 @@ function majorityElement(nums: number[]): number[] {
 
     const n = nums.length;
 
+    const majority = Math.floor(n / 3);
+
     const res = new Set<number>();
 
     const freqMap = new Map<number, number>();
@@ -10,7 +12,7 @@ function majorityElement(nums: number[]): number[] {
 
         freqMap.set(nums[i], (freqMap.get(nums[i]) || 0) + 1);
 
-        if (freqMap.get(nums[i]) > Math.floor(n / 3)) {
+        if (freqMap.get(nums[i]) > majority) {
             res.add(nums[i]);
             if(res.size === 2) {
                 // There can be no more than 2 majority elements
