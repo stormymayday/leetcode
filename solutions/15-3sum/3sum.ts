b4038864c56a474616a5b9d4d87089dc67a517fd
Aidar <stormymayday7@gmail.com>
2025-12-12 00:52:29 -0800

    Time: 37 ms (55.36%) | Memory: 72.1 MB (88.94%) - LeetSync

--- solutions/15-3sum/3sum.ts
+++ solutions/15-3sum/3sum.ts
@@ -2,52 +2,54 @@ function threeSum(nums: number[]): number[][] {
 
     const n = nums.length;
 
-    const sorted = [...nums].sort((a, b) => a - b);
+        const sorted = [...nums].sort((a, b) => a - b);
 
-    const res = [];
+        const res = [];
 
-    for (let left = 0; left < n - 2; left += 1) {
+        for(let left = 0; left < n - 2; left += 1) {
 
-        if (left > 0 && sorted[left] === sorted[left - 1]) {
-            continue;
-        }
-
-        let mid = left + 1;
-        let right = n - 1;
-        while (mid < right) {
-
-            if (sorted[left] + sorted[mid] + sorted[right] === 0) {
-
-                res.push([sorted[left], sorted[mid], sorted[right]]);
-
-                mid += 1;
-                while (mid < right && sorted[mid] === sorted[mid - 1]) {
-                    mid += 1;
-                }
-
-                right -= 1;
-                while (mid < right && sorted[right] === sorted[right + 1]) {
-                    right -= 1;
-                }
-
-            } else if (sorted[left] + sorted[mid] + sorted[right] > 0) {
-                right -= 1;
-                while (mid < right && sorted[right] === sorted[right + 1]) {
-                    right -= 1;
-                }
-            }
-            // sorted[left] + sorted[mid] + sorted[right] < 0
-            else {
-                mid += 1;
-                while (mid < right && sorted[mid] === sorted[mid - 1]) {
-                    mid += 1;
-                }
+            if(left > 0 && sorted[left] === sorted[left - 1]) {
+                continue;
             }
 
+            let mid = left + 1;
+            let right = n - 1;
+            while(mid < right) {
+
+                const sum = sorted[left] + sorted[mid] + sorted[right];
+
+                if(sum === 0) {
+
+                    res.push([sorted[left], sorted[mid], sorted[right]]);
+
+                    mid += 1;
+                    while(mid < right && sorted[mid] === sorted[mid - 1]) {
+                        mid += 1;
+                    }
+
+                    right -= 1;
+                    while(mid < right && sorted[right] === sorted[right + 1]) {
+                        right -= 1;
+                    }
+
+                } else if(sum > 0) {
+                    right -= 1;
+                    // while(mid < right && sorted[right] === sorted[right + 1]) {
+                    //     right -= 1;
+                    // }
+                } 
+                // sum < 0
+                else {
+                    mid += 1;
+                    // while(mid < right && sorted[mid] === sorted[mid - 1]) {
+                    //     mid += 1;
+                    // }
+                }
+
+            }
+
         }
 
-    }
-
-    return res;
+        return res;
 
 };
