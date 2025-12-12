058d322494642725e847160db480516f71a0e0a7
Aidar <stormymayday7@gmail.com>
2025-12-12 13:14:04 -0800

    Time: 18 ms (59.52%) | Memory: 59.6 MB (50.00%) - LeetSync

--- solutions/18-4sum/4sum.ts
+++ solutions/18-4sum/4sum.ts
@@ -1,33 +1,50 @@
 function fourSum(nums: number[], target: number): number[][] {
-
+    
     const n = nums.length;
 
-    nums.sort((a, b) => a - b);
+    const sorted = [...nums].sort((a, b) => a - b);
 
-    const res = new Set<string>();
+    const res = [];
 
-    for (let i = 0; i < n; i += 1) {
+    for (let i = 0; i < n - 3; i += 1) {
 
-        for (let j = i + 1; j < n; j += 1) {
+        if (i > 0 && sorted[i] === sorted[i - 1]) {
+            continue;
+        }
 
-            const kSet = new Set<number>();
+        for (let j = i + 1; j < n - 2; j += 1) {
 
-            for (let k = j + 1; k < n; k += 1) {
+            if (j > i + 1 && sorted[j] === sorted[j - 1]) {
+                continue;
+            }
 
-                const diff = target - (nums[i] + nums[j] + nums[k]);
+            let k = j + 1;
+            let l = n - 1;
+            while (k < l) {
 
-                if (kSet.has(diff)) {
-                    res.add(JSON.stringify([nums[i], nums[j], nums[k], diff]));
+                const sum = sorted[i] + sorted[j] + sorted[k] + sorted[l];
+
+                if (sum > target) {
+                    l -= 1;
+                } else if (sum < target) {
+                    k += 1;
+                } else {
+                    res.push([sorted[i], sorted[j], sorted[k], sorted[l]]);
+                    k += 1;
+                    while (k < l && sorted[k] === sorted[k - 1]) {
+                        k += 1;
+                    }
+                    l -= 1;
+                    while (k < l && sorted[l] === sorted[l + 1]) {
+                        l -= 1;
+                    }
+
                 }
-
-                kSet.add(nums[k]);
 
             }
 
         }
-
     }
 
-    return Array.from(res).map((quadruplet) => JSON.parse(quadruplet));
-
+    return res;
 };
