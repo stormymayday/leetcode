73a409ac02c2817bebd4073059a375645f78a879
Aidar <stormymayday7@gmail.com>
2025-12-11 20:10:26 -0800

    Time: 0 ms (100.00%) | Memory: 57.3 MB (89.64%) - LeetSync

--- solutions/169-majority-element/majority-element.ts
+++ solutions/169-majority-element/majority-element.ts
@@ -1,18 +1,19 @@
 function majorityElement(nums: number[]): number {
     
-    let majorityEl = nums[0];
-    let count = 1;
-    for(let i = 1; i < nums.length; i += 1) {
-        if(nums[i] !== majorityEl) {
-            count -= 1;
-            if(count < 0) {
-                majorityEl = nums[i];
-                count = 1;
-            }
-        } else {
+    let majorityEl;
+    let count = 0;
+
+    for(let i = 0; i < nums.length; i += 1) {
+        if(count === 0) {
+            majorityEl = nums[i];
+            count = 1;
+        } else if(nums[i] === majorityEl) {
             count += 1;
+        } else {
+            count -= 1;
         }
     }
+
     return majorityEl;
 
 };
