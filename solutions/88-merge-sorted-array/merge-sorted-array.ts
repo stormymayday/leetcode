a50a6d80357b5a3ea87b78c046247daefba1c979
Aidar <stormymayday7@gmail.com>
2025-12-13 17:07:33 -0800

    Time: 0 ms (100.00%) | Memory: 56.3 MB (45.33%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.ts
+++ solutions/88-merge-sorted-array/merge-sorted-array.ts
@@ -2,31 +2,46 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    const temp = [];
 
-    let p1 = 0;
-    let p2 = 0;
-    while (p1 < m && p2 < n) {
-        if (nums1[p1] < nums2[p2]) {
-            temp.push(nums1[p1]);
-            p1 += 1;
+    let p1 = m - 1;
+    let p2 = n - 1;
+    let p3 = m + n - 1;
+
+    while (p1 >= 0 && p2 >= 0) {
+
+        if (nums1[p1] > nums2[p2]) {
+
+            nums1[p3] = nums1[p1];
+            // nums1[p1] = 0;
+            p1 -= 1;
+            p3 -= 1;
+
         } else {
-            temp.push(nums2[p2]);
-            p2 += 1;
+
+            nums1[p3] = nums2[p2];
+            // nums2[p2] = 0;
+            p2 -= 1;
+            p3 -= 1;
+
         }
     }
 
-    while (p1 < m) {
-        temp.push(nums1[p1]);
-        p1 += 1;
+    while (p1 >= 0) {
+
+        nums1[p3] = nums1[p1];
+        // nums1[p1] = 0;
+        p1 -= 1;
+        p3 -= 1;
+
     }
 
-    while (p2 < n) {
-        temp.push(nums2[p2]);
-        p2 += 1;
+    while (p2 >= 0) {
+
+        nums1[p3] = nums2[p2];
+        // nums2[p2] = 0;
+        p2 -= 1;
+        p3 -= 1;
+
     }
 
-    for(let i = 0; i < temp.length; i += 1) {
-        nums1[i] = temp[i];
-    }
 };
