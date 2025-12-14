8d2093ca0a14f3524f30e5e0297ca78f27f5c69e
Aidar <stormymayday7@gmail.com>
2025-12-13 16:48:04 -0800

    Time: 0 ms (100.00%) | Memory: 56.9 MB (8.86%) - LeetSync

--- solutions/88-merge-sorted-array/merge-sorted-array.ts
+++ solutions/88-merge-sorted-array/merge-sorted-array.ts
@@ -2,11 +2,31 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let idx2 = 0;
-    for (let i = m; i < nums1.length; i += 1) {
-        nums1[i] = nums2[idx2];
-        idx2 += 1;
+    const temp = [];
+
+    let p1 = 0;
+    let p2 = 0;
+    while (p1 < m && p2 < n) {
+        if (nums1[p1] < nums2[p2]) {
+            temp.push(nums1[p1]);
+            p1 += 1;
+        } else {
+            temp.push(nums2[p2]);
+            p2 += 1;
+        }
     }
 
-    nums1.sort((a, b) => a - b);
+    while (p1 < m) {
+        temp.push(nums1[p1]);
+        p1 += 1;
+    }
+
+    while (p2 < n) {
+        temp.push(nums2[p2]);
+        p2 += 1;
+    }
+
+    for(let i = 0; i < temp.length; i += 1) {
+        nums1[i] = temp[i];
+    }
 };
