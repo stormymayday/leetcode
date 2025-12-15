25a79f3d594a6ffb77b447a8fa0b7913904ae559
Aidar <stormymayday7@gmail.com>
2025-12-15 00:04:02 -0800

    Time: 0 ms (100.00%) | Memory: 57.1 MB (48.05%) - LeetSync

--- solutions/152-maximum-product-subarray/maximum-product-subarray.ts
+++ solutions/152-maximum-product-subarray/maximum-product-subarray.ts
@@ -1,22 +1,37 @@
 function maxProduct(nums: number[]): number {
-    
+
     const n = nums.length;
-
+    
     let max = -Infinity;
+    let prod = 1;
 
-    for (let i = 0; i < n; i += 1) {
+    for(let i = 0; i < n; i += 1) {
 
-        let prod = 1;
-
-        for (let j = i; j < n; j += 1) {
-
-            prod *= nums[j];
-
-            max = Math.max(max, prod);
-
+        if(nums[i] === 0) {
+            max = Math.max(max, 0);
+            prod = 1;
+            continue;
         }
+
+        prod *= nums[i];
+        max = Math.max(max, prod);
+
+    }
+
+    prod = 1;
+    for(let i = n - 1; i >= 0; i -= 1) {
+
+        if(nums[i] === 0) {
+            max = Math.max(max, 0);
+            prod = 1;
+            continue;
+        }
+
+        prod *= nums[i];
+        max = Math.max(max, prod);
 
     }
 
     return max;
+
 };
