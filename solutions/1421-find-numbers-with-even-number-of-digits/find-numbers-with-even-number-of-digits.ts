24fd389ed474e8ef5526bd5dab307fbb9065cd56
Aidar <stormymayday7@gmail.com>
2025-12-16 16:11:44 -0800

    Time: 0 ms (100.00%) | Memory: 53.8 MB (100.00%) - LeetSync

--- solutions/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.ts
+++ solutions/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.ts
@@ -8,18 +8,22 @@ function findNumbers(nums: number[]): number {
 
         const curr = nums[i];
 
-        if (1 <= curr && curr <= 9) {
-            continue;
-        } else if(10 <= curr && curr <= 99) {
-            count += 1;
-        } else if(100 <= curr && curr <= 999) {
-            continue;
-        } else if(1000 <= curr && curr <= 9999) {
-            count += 1;
-        } else if(10000 <= curr && curr <= 99999) {
-            continue;
-        } else {
-            // max is 10^5 = 100000
+        // if (1 <= curr && curr <= 9) {
+        //     continue;
+        // } else if(10 <= curr && curr <= 99) {
+        //     count += 1;
+        // } else if(100 <= curr && curr <= 999) {
+        //     continue;
+        // } else if(1000 <= curr && curr <= 9999) {
+        //     count += 1;
+        // } else if(10000 <= curr && curr <= 99999) {
+        //     continue;
+        // } else {
+        //     // max is 10^5 = 100000
+        //     count += 1;
+        // }
+
+        if((10 <= curr && curr <= 99) || (1000 <= curr && curr <= 9999) || curr === 100000) {
             count += 1;
         }
         
