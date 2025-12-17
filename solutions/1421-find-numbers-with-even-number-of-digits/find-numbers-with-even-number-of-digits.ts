89f1ced532c248662f51390df6891b94e8604bc7
Aidar <stormymayday7@gmail.com>
2025-12-16 16:10:17 -0800

    Time: 1 ms (85.47%) | Memory: 55.3 MB (95.35%) - LeetSync

--- solutions/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.ts
+++ solutions/1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.ts
@@ -4,13 +4,27 @@ function findNumbers(nums: number[]): number {
 
     let count = 0;
 
-    for(let i = 0; i < n; i += 1) {
-        const curr = String(nums[i]);
-        if(curr.length % 2 === 0) {
+    for (let i = 0; i < n; i += 1) {
+
+        const curr = nums[i];
+
+        if (1 <= curr && curr <= 9) {
+            continue;
+        } else if(10 <= curr && curr <= 99) {
+            count += 1;
+        } else if(100 <= curr && curr <= 999) {
+            continue;
+        } else if(1000 <= curr && curr <= 9999) {
+            count += 1;
+        } else if(10000 <= curr && curr <= 99999) {
+            continue;
+        } else {
+            // max is 10^5 = 100000
             count += 1;
         }
+        
     }
 
     return count;
-    
+
 };
