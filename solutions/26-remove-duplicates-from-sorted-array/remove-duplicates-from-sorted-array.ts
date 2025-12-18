f94e3859b69be5d8acfd542b75cb368d0507ae7f
Aidar <stormymayday7@gmail.com>
2025-12-18 14:38:03 -0800

    Time: 4 ms (20.70%) | Memory: 59.2 MB (15.69%) - LeetSync

--- solutions/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ solutions/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -1,27 +1,13 @@
 function removeDuplicates(nums: number[]): number {
 
-    let k = nums.length;
+    const uniques = new Set(nums);
 
-    let i = 1;
-    while(i < k) { // Outer: O(n)
-
-        // "Looking back" while prev is a duplicate of curr
-        // AND i is less than k!
-        while(i < k && nums[i - 1] === nums[i]) { // Middle: O(n) worst case
-
-            // Shift All values to the left replacing 'curr'
-            for(let j = i; j < k - 1; j += 1) { // Inner: O(n) per shift
-                nums[j] = nums[j + 1];
-            }
-
-            k -= 1;
-
-        }
-
-        i += 1;
-
+    let idx = 0;
+    for(const unique of uniques) {
+        nums[idx] = unique;
+        idx += 1;
     }
 
-    return k;
+    return uniques.size;
     
 };
