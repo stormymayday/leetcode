9b97bbebbebcd0b8c6a62aaf3a46ab45ab8990bd
Aidar <stormymayday7@gmail.com>
2025-12-17 18:32:13 -0800

    Time: 0 ms (100.00%) | Memory: 58.9 MB (22.67%) - LeetSync

--- solutions/1168-duplicate-zeros/duplicate-zeros.ts
+++ solutions/1168-duplicate-zeros/duplicate-zeros.ts
@@ -4,24 +4,69 @@
 function duplicateZeros(arr: number[]): void {
 
     const n = arr.length;
-    
-    let i = 0;
-    while(i < n) {
 
+    // First Pass: count the number of zeroes
+    let numZeroes = 0;
+    for(let i = 0; i < n; i += 1){
         if(arr[i] === 0) {
-            // Starting 'j' at second to last index
-            let j = n - 2;
-            // Going towards 'i'
-            while(j >= i) {
-                // Shifting all elements to the right
-                arr[j + 1] = arr[j]
+            numZeroes += 1;
+        }
+            
+    }
+
+    if(numZeroes === 0) {
+        return;
+    }
+
+    // Second Pass:
+    let i = n - 1;
+    let j = n - 1 + numZeroes;
+    while(i >= 0) {
+
+        // if 'i' is at zero
+        if(arr[i] === 0) {
+            
+            // FIRST ZERO
+            // 'j' is in bounds
+            if(j < n) {
+                arr[j] = 0;
+                j -= 1;
+            } 
+            // 'j' is out of bounds
+            else {
                 j -= 1;
             }
-            // Need to skip duplicated zero
-            i += 2;
-        } else {
-            i += 1;
+
+            // SECOND ZERO
+            // 'j' is in bounds
+            if(j < n) {
+                arr[j] = 0;
+                j -= 1;
+            } 
+            // 'j' is out of bounds
+            else {
+                j -= 1;
+            }
+
+        } 
+        // 'i' is at non-zero
+        else {
+            
+            // 'j' is in bounds
+            if(j < n) {
+                arr[j] = arr[i];
+                j -= 1;
+            } 
+            // 'j' is out of bounds
+            else {
+                j -= 1;
+            }
+
         }
+
+        i -= 1;
+
     }
- 
+
+    
 };
