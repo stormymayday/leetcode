74f6644e106aae8501e4140b7142409c783f9a92
Aidar <stormymayday7@gmail.com>
2025-12-19 00:11:47 -0800

    Time: 0 ms (100.00%) | Memory: 58.6 MB (71.35%) - LeetSync

---
+++ solutions/941-sort-array-by-parity/sort-array-by-parity.ts
@@ -0,0 +1,17 @@
+function sortArrayByParity(nums: number[]): number[] {
+    const res = [];
+
+    for (let i = 0; i < nums.length; i += 1) {
+        if (nums[i] % 2 === 0) {
+            res.push(nums[i]);
+        }
+    }
+
+    for (let i = 0; i < nums.length; i += 1) {
+        if (nums[i] % 2 !== 0) {
+            res.push(nums[i]);
+        }
+    }
+
+    return res;
+};
