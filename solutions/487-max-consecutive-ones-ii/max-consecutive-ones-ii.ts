a3dce7142dff923dd1ee4a1c4394b1995f677414
Aidar <stormymayday7@gmail.com>
2025-12-19 15:48:35 -0800

    Time: 602 ms (15.15%) | Memory: 59.1 MB (15.15%) - LeetSync

---
+++ solutions/487-max-consecutive-ones-ii/max-consecutive-ones-ii.ts
@@ -0,0 +1,35 @@
+function findMaxConsecutiveOnes(nums: number[]): number {
+
+    const n = nums.length;
+
+    let max = 0;
+
+    for (let i = 0; i < n; i += 1) {
+
+        let currMax = 0;
+        let flipsAllowed = 1;
+
+        for (let j = i; j < n; j += 1) {
+
+            if (nums[j] === 1) {
+                currMax += 1;
+                max = Math.max(max, currMax);
+            } else {
+
+                if (flipsAllowed !== 0) {
+                    currMax += 1;
+                    max = Math.max(max, currMax);
+                    flipsAllowed -= 1;
+                } else {
+                    break;
+                }
+
+            }
+
+        }
+
+    }
+
+    return max;
+
+};
