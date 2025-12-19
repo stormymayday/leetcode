6c608334df5212f7a090f65e7616ea0b45104bf4
Aidar <stormymayday7@gmail.com>
2025-12-19 11:35:01 -0800

    Time: 0 ms (100.00%) | Memory: 55.6 MB (39.18%) - LeetSync

--- solutions/27-remove-element/remove-element.ts
+++ solutions/27-remove-element/remove-element.ts
@@ -1,22 +1,22 @@
 function removeElement(nums: number[], val: number): number {
-    
+
     let length = nums.length;
 
     for(let i = nums.length - 1; i >= 0; i -= 1) {
 
         if(nums[i] === val) {
 
-            for(let j = i + 1; j < length; j += 1) {
-
-                nums[j - 1] = nums[j];
-
+            for(let j = i; j < length - 1; j += 1) {
+                nums[j] = nums[j + 1];
             }
 
             length -= 1;
+
         }
 
     }
 
-    return length;
 
+    return length;
+    
 };
