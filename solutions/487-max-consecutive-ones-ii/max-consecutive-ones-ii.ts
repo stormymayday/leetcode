af0cdda1a147692b0ce62422e031946c6fe1c1b9
Aidar <stormymayday7@gmail.com>
2025-12-19 16:20:50 -0800

    Time: 1 ms (96.97%) | Memory: 58.3 MB (72.73%) - LeetSync

--- solutions/487-max-consecutive-ones-ii/max-consecutive-ones-ii.ts
+++ solutions/487-max-consecutive-ones-ii/max-consecutive-ones-ii.ts
@@ -1,31 +1,30 @@
 function findMaxConsecutiveOnes(nums: number[]): number {
 
     const n = nums.length;
-
     let max = 0;
+    let flipsAllowed = 1;
+    let left = 0;
+    for (let right = 0; right < n; right += 1) {
 
-    for (let i = 0; i < n; i += 1) {
+        // Stepping on a non 1 with 'right'
+        if (nums[right] !== 1) {
+            flipsAllowed -= 1; // can go below 0
+        }
 
-        let currMax = 0;
-        let flipsAllowed = 1;
+        // Make window valid
+        while (flipsAllowed < 0) {
 
-        for (let j = i; j < n; j += 1) {
-
-            if (nums[j] === 1) {
-                currMax += 1;
-                max = Math.max(max, currMax);
-            } else {
-
-                if (flipsAllowed !== 0) {
-                    currMax += 1;
-                    max = Math.max(max, currMax);
-                    flipsAllowed -= 1;
-                } else {
-                    break;
-                }
-
+            if (nums[left] !== 1) {
+                flipsAllowed += 1;
             }
 
+            left += 1;
+
+        }
+
+        // Calc max (if window is valid)
+        if (flipsAllowed >= 0) {
+            max = Math.max(max, right - left + 1);
         }
 
     }
