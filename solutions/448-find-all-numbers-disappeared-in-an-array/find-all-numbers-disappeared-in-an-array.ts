1fd5d680bb9cb181b401ccde695d97f9a41c69a6
Aidar <stormymayday7@gmail.com>
2025-12-20 12:14:02 -0800

    Time: 5 ms (94.47%) | Memory: 70.5 MB (96.21%) - LeetSync

--- solutions/448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.ts
+++ solutions/448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.ts
@@ -1,12 +1,29 @@
 function findDisappearedNumbers(nums: number[]): number[] {
 
-    const uniques = new Set<number>(nums);
+    // Phase 1: Negative Marking
+    for(let i = 0; i < nums.length; i += 1) {
 
-    const missing: number[] = [];
-    for(let i = 1; i <= nums.length; i += 1) {
-        if(!uniques.has(i)) {
-            missing.push(i);
-        }
+        // We know that the number are in range [1, n]
+        // Therefore, we can 'map' every number to index = number - 1
+
+        const curr = Math.abs(nums[i]); // Important: Take absolute value first!
+        // As it could have been made negative in prior iteration!
+
+        // Mark value at the target index as negative
+        nums[curr - 1] = Math.abs(nums[curr - 1]) * -1;
+
     }
-    return missing;    
+
+    // Phase 2: Scan the array and check for positive numbers
+    const res: number[] = [];
+    for(let i = 0; i < nums.length; i += 1) {
+
+        // If number is positive, value at this index is 'missing'
+        if(nums[i] > 0) {
+            res.push(i + 1);
+        }
+
+    }
+    return res;
+
 };
