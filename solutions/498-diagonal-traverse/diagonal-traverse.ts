168d64a41e72d95e943ecb41feafe0838cdcaf24
Aidar <stormymayday7@gmail.com>
2025-12-21 13:40:57 -0800

    Time: 2 ms (84.26%) | Memory: 63.3 MB (68.52%) - LeetSync

---
+++ solutions/498-diagonal-traverse/diagonal-traverse.ts
@@ -0,0 +1,49 @@
+function findDiagonalOrder(mat: number[][]): number[] {
+
+    const ROWS = mat.length;
+    const COLS = mat[0].length;
+
+    const res: number[] = [];
+    let idx = 0;
+
+    for(let diag = 0; diag < ROWS + COLS - 1; diag += 1) {
+
+        const temp: number[] = [];
+
+        // Starting Points
+        // While current diagonal is smaller than number of COLS, row stays at index 0. Otherwise (equals or greater), row starts moving down.
+        let row = diag < COLS ? 0 : diag - COLS + 1; // Carefull off by one!
+        // While current diagonal is smaller than number of COLS, col moves with diagonal. Otherwise (quals or greater), it stays at last index
+        let col = diag < COLS ? diag : COLS - 1;  // Carefull off by one!
+
+        // Reading values from the current diagonal (top -> down, right -> left):
+        // - row is increasing
+        // - col is decreasing
+        while(
+            row < ROWS &&
+            col >= 0
+        ) {
+
+            temp.push(mat[row][col]);
+
+            row += 1;
+            col -= 1;
+
+        }
+
+        // Reversing even indexed diagonals
+        if(diag % 2 === 0) {
+            temp.reverse();
+        }
+
+        // Writing into the result (uing the 'idx')
+        for(let i = 0; i < temp.length; i+= 1) {
+            res[idx] = temp[i];
+            idx += 1;
+        }
+
+    }
+
+    return res;
+    
+};
