4bfc23719a765ca8c8e9fbf2ce6baf6a8fb12c1f
Aidar <stormymayday7@gmail.com>
2025-12-20 20:50:56 -0800

    Time: 2 ms (92.24%) | Memory: 61.6 MB (86.34%) - LeetSync

--- solutions/2-add-two-numbers/add-two-numbers.ts
+++ solutions/2-add-two-numbers/add-two-numbers.ts
@@ -11,42 +11,48 @@
  */
 
 function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
-    const dummyNode = new ListNode();
-    let tail = dummyNode;
+
+    const dummy = new ListNode();
+    let curr = dummy;
 
     let p1 = l1;
     let p2 = l2;
+
     let carry = 0;
 
     while(p1 !== null || p2 !== null || carry !== 0) {
 
-        const val1 = p1 !== null ? p1.val : 0;
-        const val2 = p2 !== null ? p2.val : 0;
+        let currSum = 0;
+        if(p1 !== null) {
+            currSum += p1.val;
+        }
+        if(p2 !== null) {
+            currSum += p2.val;
+        }
+        currSum += carry;
 
-        const currentSum = val1 + val2 + carry;
-        let digit = 0;
-
-        if(currentSum > 9) {
-            digit = currentSum % 10;
+        if(currSum >= 10) {
+            currSum = currSum % 10;
             carry = 1;
         } else {
-            digit = currentSum;
             carry = 0;
         }
 
-        const newNode = new ListNode(digit);
-        tail.next = newNode;
-        tail = newNode;
+        const newNode = new ListNode(currSum);
+
+        curr.next = newNode;
+
+        curr = curr.next;
 
         if(p1 !== null) {
             p1 = p1.next;
         }
-
         if(p2 !== null) {
             p2 = p2.next;
         }
 
     }
 
-    return dummyNode.next;
+    return dummy.next;
+    
 };
