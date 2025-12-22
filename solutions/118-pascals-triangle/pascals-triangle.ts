a453210dc1bee770d241ff66939c0fd386be1d28
Aidar <stormymayday7@gmail.com>
2025-12-21 21:16:03 -0800

    Time: 0 ms (100.00%) | Memory: 56.7 MB (44.56%) - LeetSync

--- solutions/118-pascals-triangle/pascals-triangle.ts
+++ solutions/118-pascals-triangle/pascals-triangle.ts
@@ -5,18 +5,18 @@ function generate(numRows: number): number[][] {
     for(let i = 1; i < numRows; i += 1) {
         
         // pre-pending 1
-        const temp: number[] = [1];
+        const currRow: number[] = [1];
 
         for(let j = 0; j < res[i - 1].length - 1; j += 1) {
 
-            temp.push(res[i - 1][j] + res[i - 1][j + 1]);
+            currRow.push(res[i - 1][j] + res[i - 1][j + 1]);
 
         }
 
         // post-pending 1
-        temp.push(1);
+        currRow.push(1);
 
-        res.push(temp);
+        res.push(currRow);
 
     }
 
