3d27ca864e10af638ada19aa426f1e2fdedc2e93
Aidar <stormymayday7@gmail.com>
2025-12-22 14:54:27 -0800

    Time: 8 ms (14.92%) | Memory: 57.1 MB (23.54%) - LeetSync

--- solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,17 +1,28 @@
 function twoSum(numbers: number[], target: number): number[] {
 
     const n = numbers.length;
-    
-    const hashMap = new Map<number, number>(); // key: number, val: index
 
-    for(let i = 0; i < n; i += 1) {
+    // Outer loop: linear scan every number (can skip the last one)
+    for(let i = 0; i < n - 1; i += 1) {
+        
+         // Binary Search for other half (diff)
+        const diff = target - numbers[i];
+        let left = i + 1;
+        let right = n - 1;
+        while(left <= right) {
 
-        if(hashMap.has(target - numbers[i])) {
-            return [hashMap.get(target - numbers[i]), i + 1];
+            const mid = left + Math.floor((right - left) / 2);
+
+            // target is greater than mid, go right
+            if(diff > numbers[mid]) {
+                left = mid + 1;
+            } else if(diff < numbers[mid]) {
+                right = mid - 1;
+            } else {
+                return [i + 1, mid + 1];
+            }
+
         }
-
-        hashMap.set(numbers[i], i + 1);
-
 
     }
 
