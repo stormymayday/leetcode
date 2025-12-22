abffce1dc32e474d2f804e2f7615f7795a4c42f2
Aidar <stormymayday7@gmail.com>
2025-12-22 14:57:23 -0800

    Time: 1 ms (80.64%) | Memory: 57.1 MB (31.13%) - LeetSync

--- solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
+++ solutions/167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.ts
@@ -1,27 +1,15 @@
 function twoSum(numbers: number[], target: number): number[] {
 
-    const n = numbers.length;
+    let left = 0;
+    let right = numbers.length - 1;
+    while(left < right) {
 
-    // Outer loop: linear scan every number (can skip the last one)
-    for(let i = 0; i < n - 1; i += 1) {
-        
-         // Binary Search for other half (diff)
-        const diff = target - numbers[i];
-        let left = i + 1;
-        let right = n - 1;
-        while(left <= right) {
-
-            const mid = left + Math.floor((right - left) / 2);
-
-            // target is greater than mid, go right
-            if(diff > numbers[mid]) {
-                left = mid + 1;
-            } else if(diff < numbers[mid]) {
-                right = mid - 1;
-            } else {
-                return [i + 1, mid + 1];
-            }
-
+        if(numbers[left] + numbers[right] > target) {
+            right -= 1;
+        } else if(numbers[left] + numbers[right] < target) {
+            left += 1;
+        } else {
+            return [left + 1, right + 1];
         }
 
     }
