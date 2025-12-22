a4ed33122ff9f09db64d81d05d12cf0fbd739a9c
Aidar <stormymayday7@gmail.com>
2025-12-21 21:00:02 -0800

    Time: 1 ms (31.93%) | Memory: 58.1 MB (7.54%) - LeetSync

--- solutions/118-pascals-triangle/pascals-triangle.ts
+++ solutions/118-pascals-triangle/pascals-triangle.ts
@@ -1,22 +1,20 @@
 function generate(numRows: number): number[][] {
 
-    // Starting with a default top row for of '1'
-    const res = [[1]];
+    const res: number[][] = [[1]];
 
-    // Going until numRows - 1 (first row is already done)
-    for (let i = 0; i < numRows - 1; i += 1) {
+    for (let i = 1; i < numRows; i += 1) {
 
-        // Get the previous row and prepend & append it with zeroes
-        const temp = [0, ...res[res.length - 1], 0];
+        const curr: number[] = [];
+        const prev: number[] = [0, ...res[i - 1], 0];
 
-        // Note: Current row should be longer by 1 element
-        const currRow = [];
-        for (let j = 0; j < temp.length - 1; j += 1) {
-            const sumNeighbors = temp[j] + temp[j + 1];
-            currRow.push(sumNeighbors);
+        for(let j = 0; j < prev.length - 1; j += 1) {
+
+            const sum = prev[j] + prev[j + 1];
+            curr.push(sum);
+
         }
 
-        res.push(currRow);
+        res.push(curr);
 
     }
 
