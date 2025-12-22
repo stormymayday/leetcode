44f087e6fb0a1b281781e0e0f0ec17476400fef1
Aidar <stormymayday7@gmail.com>
2025-12-21 16:23:18 -0800

    Time: 1 ms (93.52%) | Memory: 61.8 MB (97.22%) - LeetSync

--- solutions/498-diagonal-traverse/diagonal-traverse.ts
+++ solutions/498-diagonal-traverse/diagonal-traverse.ts
@@ -10,9 +10,15 @@ function findDiagonalOrder(mat: number[][]): number[] {
         // even diagonal index - going up -> right
         if (diag % 2 === 0) {
 
-            // Starting row & col (start from bottom-left of diagonal)
+            // Starting row & col (start from bottom-left of the diagonal)
             // Even diagonals: Compare diag with ROWS (we're moving down the left edge first)
+
+            // While diagonal index is smaller than ROWS, row moves with the diagonal
+            // Otherwise, rows stays at the last index
             let row = diag < ROWS ? diag : ROWS - 1;
+
+            // While diagonal index is smaller than ROWS, col stays at index 0
+            // Otherwise, col increases
             let col = diag < ROWS ? 0 : diag - ROWS + 1;
 
             // row decreases and col increases
@@ -24,12 +30,18 @@ function findDiagonalOrder(mat: number[][]): number[] {
             }
 
         } 
-        // odd numbered diagonal  (going down-left)
+        // odd numbered diagonal (going down -> left)
         else {
             
-            // Starting row & col (start from top-right of diagonal)
+            // Starting row & col (start from top-right of the diagonal)
             // Odd diagonals: Compare diag with COLS (we're moving across the top edge first)
+
+            // While diagonal index is smaller than COLS, row stays at index 0
+            // Otherwise, row increases 
             let row = diag < COLS ? 0 : diag - COLS + 1;
+
+            // While diagonal index is smaller than COLS, col moves with the diagonal
+            // Otherwise, col stays at the last index
             let col = diag < COLS ? diag : COLS - 1;
 
             // row increases and col decreases
