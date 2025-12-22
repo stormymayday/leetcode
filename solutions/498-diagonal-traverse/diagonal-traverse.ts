60e9ad03c64832e299bcbaab8a0297f6b04bd027
Aidar <stormymayday7@gmail.com>
2025-12-21 16:16:10 -0800

    Time: 1 ms (93.52%) | Memory: 62.3 MB (88.89%) - LeetSync

--- solutions/498-diagonal-traverse/diagonal-traverse.ts
+++ solutions/498-diagonal-traverse/diagonal-traverse.ts
@@ -7,33 +7,38 @@ function findDiagonalOrder(mat: number[][]): number[] {
     let idx: number = 0;
 
     for (let diag = 0; diag < ROWS + COLS - 1; diag += 1) {
-        // even diagonal index - row decreases and col increases (going up-right)
+        // even diagonal index - going up -> right
         if (diag % 2 === 0) {
 
             // Starting row & col (start from bottom-left of diagonal)
+            // Even diagonals: Compare diag with ROWS (we're moving down the left edge first)
             let row = diag < ROWS ? diag : ROWS - 1;
             let col = diag < ROWS ? 0 : diag - ROWS + 1;
 
-            // Read & Write
+            // row decreases and col increases
             while (row >= 0 && col < COLS) {
-                res[idx++] = mat[row][col];
+                res[idx] = mat[row][col];
                 row -= 1;
                 col += 1;
+                idx += 1;
             }
 
         } 
-        // odd numbered diagonal - row increases and col decreases (going down-left)
+        // odd numbered diagonal  (going down-left)
         else {
             
             // Starting row & col (start from top-right of diagonal)
+            // Odd diagonals: Compare diag with COLS (we're moving across the top edge first)
             let row = diag < COLS ? 0 : diag - COLS + 1;
             let col = diag < COLS ? diag : COLS - 1;
 
-            // Read & Write
+            // row increases and col decreases
             while (row < ROWS && col >= 0) {
-                res[idx++] = mat[row][col];
+                // Reading diagonal & Writing result
+                res[idx] = mat[row][col];
                 row += 1;
                 col -= 1;
+                idx += 1;
             }
 
         }
