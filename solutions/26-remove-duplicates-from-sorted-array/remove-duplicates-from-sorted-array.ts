2d8106e775c1e2c184ae22720f19858de25f03ba
Aidar <stormymayday7@gmail.com>
2025-12-22 21:53:30 -0800

    Time: 2 ms (32.97%) | Memory: 58.8 MB (54.55%) - LeetSync

--- solutions/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
+++ solutions/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.ts
@@ -2,22 +2,19 @@ function removeDuplicates(nums: number[]): number {
 
     let k = nums.length;
 
-    let i = 0;
-    while(i < k) {
+    for(let i = nums.length - 2; i >= 0; i -= 1) {
 
-        while(i < k - 1 && nums[i] === nums[i + 1]) {
+        if(nums[i] === nums[i + 1]) {
 
             for(let j = i; j < k -1; j += 1) {
 
-                nums[j] = nums[j  + 1];
+                nums[j] = nums[j + 1];
 
             }
 
             k -= 1;
 
         }
-
-        i += 1;
 
     }
 
