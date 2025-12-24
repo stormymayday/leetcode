94cc04839b0f6a44023fd38e10f02528dcbca601
Aidar <stormymayday7@gmail.com>
2025-12-24 15:24:34 -0800

    Time: 7 ms (90.79%) | Memory: 63.2 MB (46.05%) - LeetSync

--- solutions/599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.ts
+++ solutions/599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.ts
@@ -3,11 +3,13 @@ function findRestaurant(list1: string[], list2: string[]): string[] {
     let minIdxSum = Infinity;
     const res: string[] = [];
 
+    // Phase 1: populate the hashmap using 'list1'
     const strToIdx1 = new Map<string, number>();
     for(let i = 0; i < list1.length; i += 1) {
         strToIdx1.set(list1[i], i);
     }
 
+    // Phase 2: iterate over 'list2'
     for(let i = 0; i < list2.length; i += 1) {
 
         // common string
@@ -15,26 +17,30 @@ function findRestaurant(list1: string[], list2: string[]): string[] {
 
             const currIdxSum = i + strToIdx1.get(list2[i]);
 
+            // current index sum is smaller than or equals to minSum
             if(currIdxSum <= minIdxSum) {
                 
-                // If list is empty
+                // If list is empty (minSum is Infinity)
                 if(minIdxSum === Infinity) {
-                    minIdxSum = currIdxSum;
+                    // push string and update min
                     res.push(list2[i]);
+                    minIdxSum = currIdxSum;
                 } 
-                // List is not empty, minSum is not Infinity
+                // List is not empty, (minSum is not Infinity)
                 else {
-                    // currSum is strictly smaller than min so far
+                    // currSum is strictly smaller than minSum
                     if(currIdxSum < minIdxSum) {
                         // remove all prev strings
                         while(res.length > 0) {
                             res.pop();
                         }
+                        // push string and update min
                         res.push(list2[i]);
                         minIdxSum = currIdxSum;
                     } 
                     // they are equal
                     else {
+                        // add string
                         res.push(list2[i]);
                     }
 
