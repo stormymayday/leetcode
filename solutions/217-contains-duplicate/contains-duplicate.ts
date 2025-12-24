29e7547b30238855f19fa3c8e798d87f536893ff
Aidar <stormymayday7@gmail.com>
2025-12-23 20:46:47 -0800

    Time: 18 ms (60.12%) | Memory: 74.3 MB (68.66%) - LeetSync

--- solutions/217-contains-duplicate/contains-duplicate.ts
+++ solutions/217-contains-duplicate/contains-duplicate.ts
@@ -1,7 +1,14 @@
 function containsDuplicate(nums: number[]): boolean {
 
-    const set = new Set(nums);
-    
-    return set.size !== nums.length;
+    const seen = new Set<number>();
 
+    for (const num of nums) {
+        if (seen.has(num)) {
+            return true;
+        } else {
+            seen.add(num);
+        }
+    }
+
+    return false;
 };
