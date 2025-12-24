b2caf3345a960240dada8de4e9b69c476dfecd94
Aidar <stormymayday7@gmail.com>
2025-12-23 23:23:14 -0800

    Time: 2 ms (54.26%) | Memory: 56.7 MB (71.32%) - LeetSync

--- solutions/202-happy-number/happy-number.ts
+++ solutions/202-happy-number/happy-number.ts
@@ -1,11 +1,11 @@
 function isHappy(n: number): boolean {
-    
+
     const seen = new Set<number>();
 
-    while(!seen.has(n)) {
+    while (!seen.has(n)) {
         seen.add(n);
         n = sumSquaresOfDigits(n);
-        if(n === 1) {
+        if (n === 1) {
             return true;
         }
     }
@@ -16,10 +16,10 @@ function isHappy(n: number): boolean {
 
 function sumSquaresOfDigits(num: number): number {
     let sum = 0;
-    while(num > 0) {
-        const digit = num % 10;
+    while (num > 0) {
+        const digit = num % 10; // Get the rightmost digit
         sum += digit * digit;
-        num = Math.floor(num / 10);
+        num = Math.floor(num / 10); // Remove the rightmost digit
     }
     return sum;
 }
