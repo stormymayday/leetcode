8bf526105a25d943724a3325fb1f776ee11ea230
Aidar <stormymayday7@gmail.com>
2025-12-25 14:58:22 -0800

    Time: 0 ms (100.00%) | Memory: 54.9 MB (74.50%) - LeetSync

--- solutions/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.ts
+++ solutions/144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.ts
@@ -13,54 +13,21 @@
  */
 
 function preorderTraversal(root: TreeNode | null): number[] {
-    
+
     const res: number[] = [];
 
-    if(root === null) {
-        return res;
-    }
-
-    // Morris
-    let curr: TreeNode | null = root;
-
-    while(curr !== null) {
-
-        // Check if there is NO left child
-        if(curr.left === null) {
-            // visit
-            res.push(curr.val);
-            // move right
-            curr = curr.right; // Can be a loop via pseudo-link
-        }
-        // There IS a left child 
-        else {
-            // Find inorder predecessor (the rightmost node) of the left subtree
-            let predecessor = curr.left;
-            while(predecessor.right !== null && predecessor.right !== curr) {
-                predecessor = predecessor.right;
-            }
-
-            if(predecessor.right === null) {
-                // create a pseudo-link to curr
-                predecessor.right = curr;
-                // visit curr
-                res.push(curr.val);
-                // move left
-                curr = curr.left;
-            } 
-            // predecessor.right points back at curr
-            else {
-                // remove the pseudo-link
-                predecessor.right = null;
-                // move right
-                curr = curr.right;
-                
-            }
-
+    function helper(node: TreeNode | null): void {
+        if(node === null) {
+            return;
         }
 
+        res.push(node.val);
+        helper(node.left);
+        helper(node.right);
     }
+
+    helper(root);
     
     return res;
-
+    
 };
