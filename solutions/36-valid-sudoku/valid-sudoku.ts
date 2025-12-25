d67e7a5526216b760b2322783e7ad353718fc0da
Aidar <stormymayday7@gmail.com>
2025-12-25 01:21:35 -0800

    Time: 2 ms (96.15%) | Memory: 58.4 MB (81.48%) - LeetSync

--- solutions/36-valid-sudoku/valid-sudoku.ts
+++ solutions/36-valid-sudoku/valid-sudoku.ts
@@ -1,33 +1,52 @@
 function isValidSudoku(board: string[][]): boolean {
-    const rowsMap = new Map<number, Set<string>>();
-    const colsMap = new Map<number, Set<string>>();
-    const subGridsMap = new Map<string, Set<string>>(); // the key is `${Math.floor(row/3)},${Math.floor(col/3)}`
-    for(let i = 0; i < 9; i += 1) {
-        rowsMap.set(i, new Set<string>());
-        colsMap.set(i, new Set<string>());
-    }
-    for(let r = 0; r < 3; r += 1) {
-        for(let c = 0; c < 3; c += 1) {
-            subGridsMap.set(`${r},${c}`, new Set<string>());
-        }
-    }
-    for(let row = 0; row < 9; row += 1) {
-        for(let col = 0; col < 9; col += 1) {
-            if(board[row][col] !== '.') {
-                const currentVal: string = board[row][col];
-                if(
-                    !rowsMap.get(row).has(currentVal) &&
-                    !colsMap.get(col).has(currentVal) &&
-                    !subGridsMap.get(`${Math.floor(row/3)},${Math.floor(col/3)}`).has(currentVal)
-                    ) {
-                        rowsMap.get(row).add(currentVal);
-                        colsMap.get(col).add(currentVal);
-                        subGridsMap.get(`${Math.floor(row/3)},${Math.floor(col/3)}`).add(currentVal);
-                } else {
+
+    // Phase 1: Rows & Cols
+    for (let i = 0; i < 9; i += 1) {
+
+        // rows
+        const rowSet = new Set<string>();
+        for (let col = 0; col < 9; col += 1) {
+
+            if (board[i][col] !== '.') {
+                if (rowSet.has(board[i][col])) {
                     return false;
                 }
+                rowSet.add(board[i][col]);
+            }
+
+        }
+
+        // cols
+        const colSet = new Set<string>();
+        for (let row = 0; row < 9; row += 1) {
+            if (board[row][i] !== '.') {
+                if (colSet.has(board[row][i])) {
+                    return false;
+                }
+                colSet.add(board[row][i]);
+            }
+
+        }
+
+        // Phase 2: Sub-Grids
+        const gridSet = new Set<string>();
+        for (let row = 0; row < 3; row += 1) {
+            for (let col = 0; col < 3; col += 1) {
+                let r = Math.floor(i / 3) * 3 + row;
+                let c = (i % 3) * 3 + col;
+                if (board[r][c] !== '.') {
+                    if (gridSet.has(board[r][c])) {
+                        return false;
+                    }
+                    gridSet.add(board[r][c]);
+                }
+
             }
         }
+
     }
+
+
     return true;
+
 };
