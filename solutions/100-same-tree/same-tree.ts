f239b9dcf3a9bb751c6de88ec984a814675a86cf
Aidar <stormymayday7@gmail.com>
2025-12-25 15:13:42 -0800

    Time: 0 ms (100.00%) | Memory: 55.8 MB (31.28%) - LeetSync

--- solutions/100-same-tree/same-tree.ts
+++ solutions/100-same-tree/same-tree.ts
@@ -13,25 +13,21 @@
  */
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-    
-    function helper(pNode: TreeNode | null, qNode: TreeNode | null): boolean {
 
-        if(pNode === null && qNode === null) {
-            return true;
-        }
 
-        if(pNode === null || qNode === null) {
-            return false;
-        }
-
-        if(pNode.val !== qNode.val) {
-            return false;
-        }
-
-        return helper(pNode.left, qNode.left) && helper(pNode.right, qNode.right);
-
+    if (p === null && q === null) {
+        return true;
     }
 
-    return helper(p, q);
+    if (p === null || q === null) {
+        return false;
+    }
+
+    if (p.val !== q.val) {
+        return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
 
 };
