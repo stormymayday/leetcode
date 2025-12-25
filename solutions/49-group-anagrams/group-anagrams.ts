f9684b0888070f194624d2542255ebbc9c7bd1e9
Aidar <stormymayday7@gmail.com>
2025-12-24 21:41:19 -0800

    Time: 23 ms (85.90%) | Memory: 70 MB (21.96%) - LeetSync

--- solutions/49-group-anagrams/group-anagrams.ts
+++ solutions/49-group-anagrams/group-anagrams.ts
@@ -4,19 +4,28 @@ function groupAnagrams(strs: string[]): string[][] {
 
     for(let i = 0; i < strs.length; i += 1) {
 
-        const sorted = strs[i].split("").sort().join("");
-
-        if(!anagrams.has(sorted)) {
-            anagrams.set(sorted, []);
+        const currStr = strs[i];
+        const charCount = new Array(26).fill(0);
+        for(let j = 0; j < currStr.length; j += 1) {
+            const charIdx = currStr[j].charCodeAt(0) - 'a'.charCodeAt(0);
+            charCount[charIdx] += 1;
         }
-        anagrams.get(sorted).push(strs[i]);
+
+        const key = charCount.join('#');
+
+        if(!anagrams.has(key)) {
+            anagrams.set(key, []);
+        }
+
+        anagrams.get(key).push(currStr);
 
     }
 
     const res: string[][] = [];
-    for(const [key, val] of anagrams.entries()) {
-        res.push(val);
+    for(const group of anagrams.values()) {
+        res.push(group);
     }
     return res;
+
     
 };
