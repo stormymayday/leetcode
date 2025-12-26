aafa60916c0c3f4eaa79c4adf8827e1f07a8b74c
Aidar <stormymayday7@gmail.com>
2025-12-25 20:27:27 -0800

    Time: 0 ms (100.00%) | Memory: 55.3 MB (82.98%) - LeetSync

--- solutions/782-jewels-and-stones/jewels-and-stones.ts
+++ solutions/782-jewels-and-stones/jewels-and-stones.ts
@@ -1,19 +1,21 @@
 function numJewelsInStones(jewels: string, stones: string): number {
-    // Create a Set from the 'jewels' string for quick lookup (O(1) average time complexity)
-    const set = new Set(jewels);
-
-    // Initialize a counter to track the number of jewel stones found
+    
     let count = 0;
+    
+    // for each stone
+    for(let stone = 0; stone < stones.length; stone += 1) {
 
-    // Iterate through each stone in the 'stones' string
-    for (const stone of stones) {
-        // Check if the current stone exists in the 'jewels' set
-        if (set.has(stone)) {
-            // If it is a jewel, increment the count
-            count++;
+        // check if it is a jewel
+        for(let jewel = 0; jewel < jewels.length; jewel += 1) {
+
+            if(stones[stone] === jewels[jewel]) {
+                count += 1;
+            }
+
         }
+
     }
 
-    // Return the total count of stones that are jewels
     return count;
-}
+
+};
