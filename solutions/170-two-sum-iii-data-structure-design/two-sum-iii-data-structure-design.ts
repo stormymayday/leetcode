25bdf852d851badce57b94c2142418e5e85c5d05
Aidar <stormymayday7@gmail.com>
2025-12-25 22:24:00 -0800

    Time: 62 ms (80.25%) | Memory: 70.7 MB (16.05%) - LeetSync

--- solutions/170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.ts
+++ solutions/170-two-sum-iii-data-structure-design/two-sum-iii-data-structure-design.ts
@@ -1,40 +1,52 @@
 class TwoSum {
 
     nums: number[];
-    numToIdx: Map<number, number[]>;
+    numToIdx: Map<number, number[]>; // key: number, value: array of indices to differentiate between the same numbers
 
     constructor() {
         this.nums = [];
-        this.numToIdx = new Map<number, []>;
+        this.numToIdx = new Map<number, number[]>();
     }
 
     add(number: number): void {
         
+        // 1. Adding number to the hash map
         if(!this.numToIdx.has(number)) {
             this.numToIdx.set(number, []);
         }
+        // Using current length (before adding the number) as the index
         this.numToIdx.get(number).push(this.nums.length);
+        // 2. Pushing to the array (length increases)
         this.nums.push(number);
 
     }
 
     find(value: number): boolean {
 
+        // Linear scan each number: can this be optimized?
         for(let i = 0; i < this.nums.length; i += 1) {
 
             const diff = value - this.nums[i];
 
             // Difference exists
             if(this.numToIdx.has(diff)) {
-
-                const indices = this.numToIdx.get(diff);
                 
-                for(let j = 0; j < indices.length; j += 1) {
+                // O(n) - if need the actual indices
+                // const indices = this.numToIdx.get(diff);
+                // for(let j = 0; j < indices.length; j += 1) {
+                //     if(i !== indices[j]) {
+                //         return [i, j];
+                //     }
+                // }
 
-                    if(i !== indices[j]) {
-                        return true;
-                    }
-
+                // O(1) - simple true / false
+                // Numbers are different
+                if(diff !== this.nums[i]) {
+                    return true;
+                } 
+                // Numbers are the same (length of 'indices' array is greater than 1)
+                if(this.numToIdx.get(diff).length > 1) {
+                    return true;
                 }
 
             }
