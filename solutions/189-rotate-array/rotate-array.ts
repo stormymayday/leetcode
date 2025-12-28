dc6d8ada1f50139c4080266c7f2c629df625c20c
Aidar <stormymayday7@gmail.com>
2025-12-27 20:00:19 -0800

    Time: 2 ms (65.11%) | Memory: 67.8 MB (30.30%) - LeetSync

--- solutions/189-rotate-array/rotate-array.ts
+++ solutions/189-rotate-array/rotate-array.ts
@@ -2,28 +2,27 @@
  Do not return anything, modify nums in-place instead.
  */
 function rotate(nums: number[], k: number): void {
-
+    
     const n = nums.length;
-
     k = k % n;
 
-    if (
-        // n cannot be equal k after normalization (k = k % n)
-        // n === k ||
-        n === 1 ||
-        k === 0
-    ) {
+    if(n === 1 || k === 0) {
         return;
     }
 
-    const rotated: number[] = new Array(n);
-
-    for (let i = 0; i < n; i += 1) {
-        rotated[(i + k) % n] = nums[i];
-    }
-
-    for (let i = 0; i < n; i += 1) {
-        nums[i] = rotated[i];
-    }
+    // Note: indices are inclusive
+    reverse(nums, 0, n - k - 1);
+    reverse(nums, n - k, n - 1);
+    reverse(nums, 0, n - 1);
 
 };
+
+function reverse(arr: number[], left: number, right: number): void {
+    while(left < right) {
+        const temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        left += 1;
+        right -= 1;
+    }
+}
