4d2df9ff6d185bd8f01cf899ad884cc6b6d16420
Aidar <stormymayday7@gmail.com>
2025-12-27 17:52:28 -0800

    Time: 2 ms (65.11%) | Memory: 65.6 MB (76.39%) - LeetSync

--- solutions/189-rotate-array/rotate-array.ts
+++ solutions/189-rotate-array/rotate-array.ts
@@ -5,14 +5,25 @@ function rotate(nums: number[], k: number): void {
 
     const n = nums.length;
 
+    k = k % n;
+
+    if (
+        // n cannot be equal k after normalization (k = k % n)
+        // n === k ||
+        n === 1 ||
+        k === 0
+    ) {
+        return;
+    }
+
     const rotated: number[] = new Array(n);
 
-    for(let i = 0; i < n; i += 1) {
+    for (let i = 0; i < n; i += 1) {
         rotated[(i + k) % n] = nums[i];
     }
 
-    for(let i = 0; i < n; i += 1) {
+    for (let i = 0; i < n; i += 1) {
         nums[i] = rotated[i];
     }
-    
+
 };
