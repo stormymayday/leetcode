4242ae03ece7b9e5a4b11f0c9e6230457ccfd026
Aidar <stormymayday7@gmail.com>
2025-12-27 17:40:22 -0800

    Time: 0 ms (100.00%) | Memory: 65.7 MB (73.76%) - LeetSync

--- solutions/189-rotate-array/rotate-array.ts
+++ solutions/189-rotate-array/rotate-array.ts
@@ -3,27 +3,16 @@
  */
 function rotate(nums: number[], k: number): void {
 
-    if(k === 0 || k === nums.length || nums.length === 1) {
-        return;
+    const n = nums.length;
+
+    const rotated: number[] = new Array(n);
+
+    for(let i = 0; i < n; i += 1) {
+        rotated[(i + k) % n] = nums[i];
     }
 
-    k = k % nums.length;
-
-    const temp: number[] = [];
-
-    // First copy last k elements
-    for(let i = nums.length - k; i < nums.length; i += 1) {
-        temp.push(nums[i]);
-    }
-
-    // Next copy all elements up to last k
-    for(let i = 0; i < nums.length - k; i += 1) {
-        temp.push(nums[i]);
-    }
-
-    // Finaly, overwrite the original with temp
-    for(let i = 0; i < nums.length; i += 1) {
-        nums[i] = temp[i];
+    for(let i = 0; i < n; i += 1) {
+        nums[i] = rotated[i];
     }
     
 };
