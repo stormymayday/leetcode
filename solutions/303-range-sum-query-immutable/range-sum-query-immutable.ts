b6f76b34eaac5c7c9aa95a696a02db937316a592
Aidar <stormymayday7@gmail.com>
2025-12-28 19:57:22 -0800

    Time: 3 ms (100.00%) | Memory: 63.9 MB (43.74%) - LeetSync

--- solutions/303-range-sum-query-immutable/range-sum-query-immutable.ts
+++ solutions/303-range-sum-query-immutable/range-sum-query-immutable.ts
@@ -1,16 +1,22 @@
 class NumArray {
+
     prefixSums: number[];
+
     constructor(nums: number[]) {
+        
         this.prefixSums = [];
-        let prefixSum = 0;
-        for(let i = 0; i < nums.length; i += 1) {
-            prefixSum += nums[i];
-            this.prefixSums.push(prefixSum);
+        let sum = 0;
+        for(let i = 0; i < nums.length; i+= 1) {
+            sum += nums[i];
+            this.prefixSums.push(sum);
         }
+
     }
 
     sumRange(left: number, right: number): number {
-        return left === 0 ? this.prefixSums[right] : this.prefixSums[right] - this.prefixSums[left - 1];
+        
+        return this.prefixSums[right] - (left > 0 ? this.prefixSums[left - 1] : 0);
+
     }
 }
 
