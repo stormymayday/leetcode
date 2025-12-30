59b45d1ad6a91f04887162133b748e472e91aec9
Aidar <stormymayday7@gmail.com>
2025-12-29 18:32:00 -0800

    Time: 45 ms (76.47%) | Memory: 82.9 MB (30.39%) - LeetSync

--- solutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.ts
+++ solutions/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.ts
@@ -1,48 +1,34 @@
 class NumMatrix {
 
-    prefixSums2D: number[][];
+    prefixSums: number[][];
 
     constructor(matrix: number[][]) {
-        // Plus "Padding" Row of zeroes
-        this.prefixSums2D = new Array(matrix.length + 1);
-        for (let row = 0; row < matrix.length + 1; row += 1) {
-            // Pluse "Padding" Col of zeroes
-            this.prefixSums2D[row] = new Array(matrix[0].length + 1).fill(0);
+
+        const ROWS = matrix.length;
+        const COLS = matrix[0].length;
+
+        this.prefixSums = new Array(ROWS + 1);
+        for (let row = 0; row < ROWS + 1; row += 1) {
+            this.prefixSums[row] = new Array(COLS + 1).fill(0);
         }
 
-        for (let row = 0; row < matrix.length; row += 1) {
-
-            for (let col = 0; col < matrix[0].length; col += 1) {
-
-                // 1. Use the original value
-                this.prefixSums2D[row + 1][col + 1] = matrix[row][col];
-
-                // 2. Add the cell on top
-                this.prefixSums2D[row + 1][col + 1] += this.prefixSums2D[row][col + 1];
-
-                // 3. Add the cell to the left
-                this.prefixSums2D[row + 1][col + 1] += this.prefixSums2D[row + 1][col];
-
-                // Subtract the overlapping top-left region that was counted twice
-                this.prefixSums2D[row + 1][col + 1] -= this.prefixSums2D[row][col];
-
+        for (let row = 0; row < ROWS; row += 1) {
+            for (let col = 0; col < COLS; col += 1) {
+                this.prefixSums[row + 1][col + 1] =
+                    matrix[row][col] +
+                    this.prefixSums[row][col + 1] +
+                    this.prefixSums[row + 1][col] -
+                    this.prefixSums[row][col];
             }
-
         }
 
     }
 
     sumRegion(row1: number, col1: number, row2: number, col2: number): number {
-
-        // Note: using "Padding" Row & Col
-
-        return this.prefixSums2D[row2 + 1][col2 + 1] -
-            // Subtract from the row above 
-            this.prefixSums2D[row1][col2 + 1] -
-            // Subtract from col to th left
-            this.prefixSums2D[row2 + 1][col1] +
-            // Add the top-left diagonal
-            this.prefixSums2D[row1][col1];
+        return this.prefixSums[row2 + 1][col2 + 1] -
+            this.prefixSums[row2 + 1][col1] -
+            this.prefixSums[row1][col2 + 1] +
+            this.prefixSums[row1][col1];
 
     }
 }
