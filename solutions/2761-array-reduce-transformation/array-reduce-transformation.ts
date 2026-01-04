e09c3088caacb6ca245a7927fb56073f70f85cce
Aidar <stormymayday7@gmail.com>
2026-01-03 16:05:46 -0800

    Time: 52 ms (20.27%) | Memory: 55.4 MB (82.12%) - LeetSync

---
+++ solutions/2761-array-reduce-transformation/array-reduce-transformation.ts
@@ -0,0 +1,13 @@
+type Fn = (accum: number, curr: number) => number
+
+function reduce(nums: number[], fn: Fn, init: number): number {
+
+    let res = init;
+
+    for(let i = 0; i < nums.length; i += 1) {
+        res = fn(res, nums[i]);
+    }
+
+    return res;
+    
+};
