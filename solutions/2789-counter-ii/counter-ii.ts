d85c973790ce14c0ee157fab2fd7a2447410e85c
Aidar <stormymayday7@gmail.com>
2026-01-04 18:31:42 -0800

    Time: 53 ms (41.60%) | Memory: 58.2 MB (49.69%) - LeetSync

---
+++ solutions/2789-counter-ii/counter-ii.ts
@@ -0,0 +1,33 @@
+type Counter = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): Counter {
+
+    let count = init;
+
+    return {
+        increment() {
+            count += 1;
+            return count;
+        },
+        decrement() {
+            count -= 1;
+            return count;
+        },
+        reset() {
+            count = init;
+            return count;
+        }
+    }
+    
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
