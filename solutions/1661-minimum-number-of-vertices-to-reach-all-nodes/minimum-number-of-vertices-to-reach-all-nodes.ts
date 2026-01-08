43f85b798c2a6988f5e296af8590c0e8f1c0a326
Aidar <stormymayday7@gmail.com>
2026-01-07 21:10:04 -0800

    Time: 159 ms (15.15%) | Memory: 102.4 MB (9.09%) - LeetSync

--- solutions/1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.ts
+++ solutions/1661-minimum-number-of-vertices-to-reach-all-nodes/minimum-number-of-vertices-to-reach-all-nodes.ts
@@ -1,33 +1,48 @@
 function findSmallestSetOfVertices(n: number, edges: number[][]): number[] {
-    
+
     const adjList = buildAdjList(n, edges);
 
-    const inDegreeMap = new Map<number, number>();
-    for(const node of adjList.keys()) {
-        inDegreeMap.set(node, 0);
-    }
-    for(const node of adjList.keys()) {
-        for(const neighbor of adjList.get(node)) {
-            inDegreeMap.set(neighbor, inDegreeMap.get(neighbor) + 1);
+    const inDegreeMap = buildInDegreeMap(n, adjList);
+
+    const readyQueue: number[] = [];
+
+    for (const [node, inDegreeCount] of inDegreeMap.entries()) {
+        if (inDegreeCount === 0) {
+            readyQueue.push(node);
         }
     }
 
-    const ready: number[] = [];
-    for(const [node, inDegreeCount] of inDegreeMap.entries()) {
-        if(inDegreeCount === 0) {
-            ready.push(node);
-        }
-    }
-    return ready;
+    return readyQueue;
 };
 
 function buildAdjList(n: number, edges: number[][]): Map<number, Set<number>> {
     const adjList = new Map();
-    for(let i = 0; i < n; i += 1) {
+
+    for (let i = 0; i < n; i += 1) {
         adjList.set(i, new Set());
     }
-    for(const [src, dst] of edges) {
+
+    for (const [src, dst] of edges) {
         adjList.get(src).add(dst);
     }
+
     return adjList;
+}
+
+function buildInDegreeMap(n: number, adjList: Map<number, Set<number>>): Map<number, number> {
+
+    const inDegreeMap = new Map<number, number>();
+
+    for (let i = 0; i < n; i += 1) {
+        inDegreeMap.set(i, 0);
+    }
+
+    for (let i = 0; i < n; i += 1) {
+        for (const neighbor of adjList.get(i)) {
+            inDegreeMap.set(neighbor, inDegreeMap.get(neighbor) + 1);
+        }
+    }
+
+    return inDegreeMap;
+
 }
