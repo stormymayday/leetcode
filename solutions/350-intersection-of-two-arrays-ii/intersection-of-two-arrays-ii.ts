a846c24c3ebba1c9b2c79674c53547e2ccbc20bb
Aidar <stormymayday7@gmail.com>
2026-02-01 10:48:06 -0800

    Time: 3 ms (45.63%) | Memory: 56.4 MB (76.31%) - LeetSync

--- solutions/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
+++ solutions/350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.ts
@@ -1,20 +1,22 @@
 function intersect(nums1: number[], nums2: number[]): number[] {
 
-    const freqMap = new Map<number, number>();
-    for (let i = 0; i < nums1.length; i += 1) {
-        freqMap.set(nums1[i], (freqMap.get(nums1[i]) || 0) + 1);
+    const freqMap = {};
+    for(let i = 0; i < nums1.length; i += 1) {
+        if(!freqMap.hasOwnProperty(nums1[i])) {
+            freqMap[nums1[i]] = 0;
+        }
+        freqMap[nums1[i]] += 1;
     }
 
-    const intersection: number[] = [];
-    for (let i = 0; i < nums2.length; i += 1) {
-        if (freqMap.has(nums2[i])) {
-            intersection.push(nums2[i]);
-            freqMap.set(nums2[i], (freqMap.get(nums2[i])) - 1);
-            if(freqMap.get(nums2[i]) === 0) {
-                freqMap.delete(nums2[i]);
+    const res = [];
+    for(let i = 0; i < nums2.length; i += 1) {
+        if(freqMap.hasOwnProperty(nums2[i])) {
+            res.push(nums2[i]);
+            freqMap[nums2[i]] -= 1;
+            if(freqMap[nums2[i]] === 0) {
+                delete freqMap[nums2[i]];
             }
         }
     }
-    return intersection;
-
+    return res;
 };
