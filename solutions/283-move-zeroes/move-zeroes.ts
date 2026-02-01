6461359dd758b96738285728fa6efac3abb1c76f
Aidar <stormymayday7@gmail.com>
2026-02-01 10:26:24 -0800

    Time: 2 ms (61.54%) | Memory: 61.7 MB (19.90%) - LeetSync

--- solutions/283-move-zeroes/move-zeroes.ts
+++ solutions/283-move-zeroes/move-zeroes.ts
@@ -2,20 +2,17 @@
  Do not return anything, modify nums in-place instead.
  */
 function moveZeroes(nums: number[]): void {
-    
     let left = 0;
-    for(let right = 1; right < nums.length; right += 1) {
+    for (let right = 1; right < nums.length; right += 1) {
 
-        while(left < right && nums[left] !== 0) {
+        while (left < right && nums[left] !== 0) {
             left += 1;
         }
 
-        if(nums[left] === 0 && nums[right] !== 0) {
+        if (nums[right] !== 0 && nums[left] === 0) {
             nums[left] = nums[right];
             nums[right] = 0;
         }
 
     }
-
-
 };
