489bfb67fe6ca3b2208db7005268ec269a88e26d
Aidar <stormymayday7@gmail.com>
2026-02-08 12:02:38 -0800

    Time: 286 ms (82.30%) | Memory: 0B (100.00%) - LeetSync

--- solutions/2024-calculate-special-bonus/calculate-special-bonus.sql
+++ solutions/2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,12 +1,9 @@
 -- Write your PostgreSQL query statement below
-SELECT
-    employee_id,
-   CASE
-        WHEN employee_id % 2 <> 0
-            AND name !~ '^M'
+SELECT employee_id,
+    CASE
+        WHEN employee_id % 2 != 0 AND name NOT LIKE 'M%'
         THEN salary
         ELSE 0
-    END AS bonus
-FROM
-    employees
-ORDER BY employee_id;
+    END as bonus
+FROM employees
+ORDER BY employee_id ASC;
