eaa8e745eb7031a3816e9ac0c66e46e8e43478a8
Aidar <stormymayday7@gmail.com>
2026-02-15 15:20:49 -0800

    Time: 289 ms (71.95%) | Memory: 0B (100.00%) - LeetSync

--- solutions/1163-project-employees-iii/project-employees-iii.sql
+++ solutions/1163-project-employees-iii/project-employees-iii.sql
@@ -3,9 +3,9 @@ SELECT project_id, employee_id
 FROM (
     SELECT p.project_id, 
            p.employee_id, 
-           RANK()OVER(PARTITION BY project_id ORDER BY experience_years DESC) AS rnk
+           RANK()OVER(PARTITION BY project_id ORDER BY experience_years DESC) AS rank
     FROM Project p
     JOIN Employee e
     ON p.employee_id = e.employee_id
-)a
-WHERE rnk = 1;
+)
+WHERE rank = 1;
