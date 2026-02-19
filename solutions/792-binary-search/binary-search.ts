a34aa0f3a87b79b9e56bab72f9f3274201c3c1f3
Aidar <stormymayday7@gmail.com>
2026-02-18 22:01:47 -0800

    Time: 0 ms (100.00%) | Memory: 59.1 MB (25.03%) - LeetSync

--- solutions/792-binary-search/binary-search.ts
+++ solutions/792-binary-search/binary-search.ts
@@ -1,19 +1,15 @@
 function search(nums: number[], target: number): number {
-    let left: number = 0;
-    let right: number = nums.length -1;
+    let left = 0;
+    let right = nums.length - 1;
     while(left <= right) {
-
-        // Note: Might not matter for JavaScript (64-bit floating-point numbers)
-        const mid: number = Math.floor(left + (right - left) / 2);
-
+        const mid = Math.floor((left + right) / 2);
         if(target > nums[mid]) {
             left = mid + 1;
         } else if(target < nums[mid]) {
             right = mid - 1;
         } else {
-            return mid; // need to return index
+            return mid;
         }
-
     }
     return -1;
 };
