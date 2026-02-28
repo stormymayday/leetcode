0183944739a9bc4fcd8d6621151be2030aa6cb3d
Aidar <stormymayday7@gmail.com>
2026-02-28 11:36:58 -0800

    Time: 15 ms (42.90%) | Memory: 19.3 MB (84.19%) - LeetSync

---
+++ solutions/242-valid-anagram/valid-anagram.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        freqMap = {}
+
+        # Filling in the hash map using 's'
+        for char in s:
+            if char not in freqMap:
+                freqMap[char] = 0
+            freqMap[char] += 1
+
+        # 'Emptying' the hash map using 't'
+        for char in t:
+            if char not in freqMap:
+                return False
+            freqMap[char] -= 1
+            if freqMap[char] == 0:
+                del freqMap[char]
+
+        return len(freqMap) == 0
