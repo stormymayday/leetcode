f99f96a4e1a5aa41db1995bcf0c80d3495702dbb
Aidar <stormymayday7@gmail.com>
2026-09-23 15:41:22 -0700

    Time: 11 ms (34.97%) | Memory: 23.5 MB (48.88%) - LeetSync

--- solutions/229-majority-element-ii/majority-element-ii.py
+++ solutions/229-majority-element-ii/majority-element-ii.py
@@ -1,18 +1,17 @@
 class Solution:
     def majorityElement(self, nums: list[int]) -> list[int]:
-        
-        n = len(nums)
+
+        n = len(nums)        
         res = []
         freq_count = {}
 
         for num in nums:
             if num not in freq_count:
                 freq_count[num] = 0
-            
             freq_count[num] += 1
         
         for element, count in freq_count.items():
             if count > n / 3:
                 res.append(element)
-
+        
         return res
